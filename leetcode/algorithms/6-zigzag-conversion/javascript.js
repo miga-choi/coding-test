12ec84b0253a76a8168d7826908c32b2d9d2ae49
miga-choi <miga.choi.1004@gmail.com>
2023-03-10 00:17:40 +0900

    leetcode: finish "zigzag-conversion"

---
+++ leetcode/algorithms/6-zigzag-conversion/javascript.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function (s, numRows) {
+  // 1. Make an array with the zigzag sequence
+  const zigzag = [...new Array(numRows).keys()];
+
+  // 2. Make an array with as many strings as we need rows
+  const rows = new Array(numRows).fill('');
+
+  // 3. Append the characters to the row strings in zigzag sequence
+  [...s].forEach((c, i) => (rows[zigzag[i % zigzag.length]] += c));
+
+  // 4. Join the row strings in the array together
+  return rows.join('');
+};
