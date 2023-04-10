3e467d7e0ca920e0de28da6897162485036b4561
miga-choi <miga.choi.1004@gmail.com>
2023-04-11 00:43:23 +0900

    leetcode: "9-palindrome-number" add python answer

---
+++ leetcode/algorithms/9-palindrome-number/python.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        result = True
+        if x < 0:
+            result = False
+        xStr = str(x)
+        for i in range(0, len(xStr) - 1):
+            if (i == len(xStr) - 1 - i):
+                break
+            if (xStr[i] != xStr[len(xStr) - 1 - i]):
+                result = False
+        return result
