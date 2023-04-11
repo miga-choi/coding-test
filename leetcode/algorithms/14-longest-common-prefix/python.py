6f673554b9f47a0504645567690c4f4eaf59e045
miga-choi <miga.choi.1004@gmail.com>
2023-04-12 00:56:47 +0900

    leetcode: "14-longest-common-prefix" add python best solution

--- leetcode/algorithms/14-longest-common-prefix/python.py
+++ leetcode/algorithms/14-longest-common-prefix/python.py
@@ -27,3 +27,20 @@ class Solution(object):
             else:
                 break
         return result
+
+
+# Best Solution:
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        l = list(zip(*strs))
+        prefix = ""
+        for i in l:
+            if (len(set(i)) == 1):
+                prefix += i[0]
+            else:
+                break
+        return prefix
