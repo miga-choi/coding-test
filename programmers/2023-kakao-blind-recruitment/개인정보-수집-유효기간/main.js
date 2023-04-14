c587a43a9bb69fa42e1e3f0a8368b27bad57cd34
miga-choi <miga.choi.1004@gmail.com>
2023-04-14 23:44:20 +0900

    programmers: "2023-kakao-blind-recruitment: 개인정보-수집-유효기간" add best javascript and java solution

--- programmers/2023-kakao-blind-recruitment/개인정보-수집-유효기간/main.js
+++ programmers/2023-kakao-blind-recruitment/개인정보-수집-유효기간/main.js
@@ -20,3 +20,22 @@ function solution(today, terms, privacies) {
   );
   return answer;
 }
+
+// best solution
+function solution(today, terms, privacies) {
+  var answer = [];
+  var [year, month, date] = today.split('.').map(Number);
+  var todates = year * 12 * 28 + month * 28 + date;
+  var t = {};
+  terms.forEach((e) => {
+    let [a, b] = e.split(' ');
+    t[a] = Number(b);
+  });
+  privacies.forEach((e, i) => {
+    var [day, term] = e.split(' ');
+    day = day.split('.').map(Number);
+    var dates = day[0] * 12 * 28 + day[1] * 28 + day[2] + t[term] * 28;
+    if (dates <= todates) answer.push(i + 1);
+  });
+  return answer;
+}
