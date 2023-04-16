8a4546da352c79fb4aa2003b4619fee52de41b51
miga-choi <miga.choi.1004@gmail.com>
2023-04-16 23:40:50 +0900

    leetcode: "20-valid-parentheses" add javascript best solution

--- leetcode/algorithms/20-valid-parentheses/main.py
+++ leetcode/algorithms/20-valid-parentheses/main.py
@@ -1,13 +1,16 @@
-# best solution
-# 1. if it's the left bracket then we append it to the stack
-# 2. else if it's the right bracket and the stack is empty(meaning no matching left bracket), or the left bracket doesn't match
-# 3. finally check if the stack still contains unmatched left bracket
 class Solution(object):
     def isValid(self, s):
         """
         :type s: str
         :rtype: bool
         """
+        return null
+
+    # best solution
+    def bestSolution(self, s):
+        # 1. if it's the left bracket then we append it to the stack
+        # 2. else if it's the right bracket and the stack is empty(meaning no matching left bracket), or the left bracket doesn't match
+        # 3. finally check if the stack still contains unmatched left bracket
         d = {'(': ')', '{': '}', '[': ']'}
         stack = []
         for i in s:
