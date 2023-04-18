f49e4d5ec7a1d703b305c68356971ce6826ef34a
miga-choi <miga.choi.1004@gmail.com>
2023-04-19 00:03:11 +0900

    leetcode: try "27-remove-element"

--- programmers/2023-kakao-blind-recruitment/개인정보-수집-유효기간/main.java
+++ programmers/2023-kakao-blind-recruitment/개인정보-수집-유효기간/main.java
@@ -1,70 +1,67 @@
 import java.text.SimpleDateFormat;
-import java.util.*;
-
-class Solution {
-    public int[] solution(String today, String[] terms, String[] privacies) {
-        List<Integer> answer = new ArrayList<Integer>();
-        try {
-            Map<String, Integer> termsMap = new HashMap<>();
-            for (String term : terms) {
-                String[] termArray = term.split(" ");
-                termsMap.put(termArray[0], Integer.parseInt(termArray[1]));
-            }
-
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd");
-            for (int i = 0; i < privacies.length; i++) {
-                String[] privacyArray = privacies[i].split(" ");
-                Date todayDate = simpleDateFormat.parse(today);
-                Date date = simpleDateFormat.parse(privacyArray[0]);
-                Calendar cal = Calendar.getInstance();
-                cal.setTime(date);
-                cal.add(Calendar.MONTH, termsMap.get(privacyArray[1]));
-                Date newDate = cal.getTime();
-                if (todayDate.compareTo(newDate) >= 0) {
-                    answer.add(i + 1);
-                }
-            }
-        } catch (Exception error_) {
-            System.out.println(error_);
-        }
-        return answer.stream().mapToInt(integer -> integer).toArray();
-    }
-}
-
-
-// best solution
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 class Solution {
-    public int[] solution(String today, String[] terms, String[] privacies) {
-        List<Integer> answer = new ArrayList<>();
-        Map<String, Integer> termMap = new HashMap<>();
-        int date = getDate(today);
 
-        for (String s : terms) {
-            String[] term = s.split(" ");
+  public int[] solution(String today, String[] terms, String[] privacies) {
+    List<Integer> answer = new ArrayList<Integer>();
+    try {
+      Map<String, Integer> termsMap = new HashMap<>();
+      for (String term : terms) {
+        String[] termArray = term.split(" ");
+        termsMap.put(termArray[0], Integer.parseInt(termArray[1]));
+      }
 
-            termMap.put(term[0], Integer.parseInt(term[1]));
+      SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd");
+      for (int i = 0; i < privacies.length; i++) {
+        String[] privacyArray = privacies[i].split(" ");
+        Date todayDate = simpleDateFormat.parse(today);
+        Date date = simpleDateFormat.parse(privacyArray[0]);
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.MONTH, termsMap.get(privacyArray[1]));
+        Date newDate = cal.getTime();
+        if (todayDate.compareTo(newDate) >= 0) {
+          answer.add(i + 1);
         }
-        for (int i = 0; i < privacies.length; i++) {
-            String[] privacy = privacies[i].split(" ");
-
-            if (getDate(privacy[0]) + (termMap.get(privacy[1]) * 28) <= date) {
-                answer.add(i + 1);
-            }
-        }
-        return answer.stream().mapToInt(integer -> integer).toArray();
+      }
+    } catch (Exception error_) {
+      System.out.println(error_);
     }
+    return answer.stream().mapToInt(integer -> integer).toArray();
+  }
 
-    private int getDate(String today) {
-        String[] date = today.split("\\.");
-        int year = Integer.parseInt(date[0]);
-        int month = Integer.parseInt(date[1]);
-        int day = Integer.parseInt(date[2]);
-        return (year * 12 * 28) + (month * 28) + day;
+  // Best Solution
+  public int[] bestSolution(String today, String[] terms, String[] privacies) {
+    List<Integer> answer = new ArrayList<>();
+    Map<String, Integer> termMap = new HashMap<>();
+    int date = getDate(today);
+
+    for (String s : terms) {
+      String[] term = s.split(" ");
+
+      termMap.put(term[0], Integer.parseInt(term[1]));
     }
+    for (int i = 0; i < privacies.length; i++) {
+      String[] privacy = privacies[i].split(" ");
+
+      if (getDate(privacy[0]) + (termMap.get(privacy[1]) * 28) <= date) {
+        answer.add(i + 1);
+      }
+    }
+    return answer.stream().mapToInt(integer -> integer).toArray();
+  }
+
+  private int getDate(String today) {
+    String[] date = today.split("\\.");
+    int year = Integer.parseInt(date[0]);
+    int month = Integer.parseInt(date[1]);
+    int day = Integer.parseInt(date[2]);
+    return (year * 12 * 28) + (month * 28) + day;
+  }
 }
