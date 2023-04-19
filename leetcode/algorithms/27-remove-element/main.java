0cfe301b3fb0ac4b6df46b2636938ef78cde20f6
miga-choi <miga.choi.1004@gmail.com>
2023-04-20 00:21:11 +0900

    leetcode: "27-remove-element" add java solution

---
+++ leetcode/algorithms/27-remove-element/main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+
+  public int removeElement(int[] nums, int val) {
+    List<Integer> newNums = new ArrayList<>();
+    for (Integer num : nums) {
+      if (!num.equals(val)) {
+        newNums.add(num);
+      }
+    }
+    Arrays.sort(nums);
+    for (int i = 0; i < newNums.size(); i++) {
+      nums[i] = newNums.get(i);
+    }
+    return newNums.size();
+  }
+
+  // Best Solution
+  public int bestSolution(int[] nums, int val) {
+    int i = 0;
+    for (int j = 0; j < nums.length; j++) {
+      if (nums[j] != val) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+        i++;
+      }
+    }
+    return i;
+  }
+}
