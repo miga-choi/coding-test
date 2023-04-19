5152a59d7336562dba479fb4d5927b0df7fe3c42
miga-choi <miga.choi.1004@gmail.com>
2023-04-19 23:59:50 +0900

    leetcode: "27-remove-element" add python best solution

--- leetcode/algorithms/27-remove-element/main.py
+++ leetcode/algorithms/27-remove-element/main.py
@@ -2,3 +2,8 @@ class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         nums[:] = sorted(filter(lambda num: num != val, nums))
         return len(nums)
+
+    # Best Solution
+    def bestSolution(self, nums: List[int], val: int) -> int:
+        while val in nums:
+            nums.remove(val)
