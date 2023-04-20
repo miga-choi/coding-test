1d007bae185d2469be46a236c42e6491f8f6d9fc
miga-choi <miga.choi.1004@gmail.com>
2023-04-21 00:06:05 +0900

    leetcode: "28-find-the-index-of-the-first-occurrence-in-a-string" add python solution

---
+++ leetcode/algorithms/28-find-the-index-of-the-first-occurrence-in-a-string/main.py
@@ -0,0 +1,62 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if haystack == "":
+            return -1
+        if needle == "":
+            return 0
+        return haystack.find(needle)
+
+    # Best Solution
+    def strStr(self, haystack, needle):
+        return haystack.find(needle)
+
+    # Rabin Karp, built-in hash, constant time (tested)
+    def bestSolution1(self, haystack, needle):
+        n, h = len(needle), len(haystack)
+        hash_n = hash(needle)
+        for i in range(h - n + 1):
+            if hash(haystack[i : i + n]) == hash_n:
+                return i
+        return -1
+
+    # Rabin Karp, numeral base for both uppercase and lowercase letters, constant time
+    def bestSolution2(self, haystack, needle):
+        def f(c):
+            return ord(c) - ord("A")
+
+        n, h, d, m = len(needle), len(haystack), ord("z") - ord("A") + 1, sys.maxint
+        if n > h:
+            return -1
+        nd, hash_n, hash_h = d ** (n - 1), 0, 0
+        for i in range(n):
+            hash_n = (d * hash_n + f(needle[i])) % m
+            hash_h = (d * hash_h + f(haystack[i])) % m
+        if hash_n == hash_h:
+            return 0
+        for i in range(1, h - n + 1):
+            hash_h = (
+                d * (hash_h - f(haystack[i - 1]) * nd) + f(haystack[i + n - 1])
+            ) % m  # e.g. 10*(1234-1*10**3)+5=2345
+            if hash_n == hash_h:
+                return i
+        return -1
+
+    # KMP
+    def bestSolution3(self, haystack, needle):
+        n, h = len(needle), len(haystack)
+        i, j, nxt = 1, 0, [-1] + [0] * n
+        while i < n:  # calculate next array
+            if j == -1 or needle[i] == needle[j]:
+                i += 1
+                j += 1
+                nxt[i] = j
+            else:
+                j = nxt[j]
+        i = j = 0
+        while i < h and j < n:
+            if j == -1 or haystack[i] == needle[j]:
+                i += 1
+                j += 1
+            else:
+                j = nxt[j]
+        return i - j if j == n else -1
