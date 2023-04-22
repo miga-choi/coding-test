6c77cc09681ff01f6f979a2df281bd62a26e0cd8
miga-choi <miga.choi.1004@gmail.com>
2023-04-22 23:26:41 +0900

    leetcode: "35-search-insert-position" add java solution

---
+++ leetcode/algorithms/35-search-insert-position/main.java
@@ -0,0 +1,32 @@
+class Solution {
+
+  public int searchInsert(int[] nums, int target) {
+    int lo = 0;
+    int hi = nums.length;
+    while (lo < hi) {
+      int mid = (lo + hi) / 2;
+      if (target > nums[mid]) {
+        lo = mid + 1;
+      } else {
+        hi = mid;
+      }
+    }
+    return lo;
+  }
+
+  // Best Solution
+  public int bestSolution(int[] nums, int target) {
+    int low = 0, high = nums.length - 1;
+    while (low <= high) {
+      int mid = (low + high) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] > target) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+    return low;
+  }
+}
