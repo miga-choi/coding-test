991d0edfae79aef875414236067f29f90922c7d4
miga-choi <miga.choi.1004@gmail.com>
2023-04-22 23:19:08 +0900

    leetcode: "35-search-insert-position" add python solution

---
+++ leetcode/algorithms/35-search-insert-position/main.py
@@ -0,0 +1,41 @@
+import math
+import bisect
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        lo, hi = 0, len(nums)
+        while lo < hi:
+            mid = lo + math.floor((hi - lo) / 2)
+            if target > nums[mid]:
+                lo = mid + 1
+            else:
+                hi = mid
+        return lo
+
+    # Best Solution
+    # Linear Search
+    def linearSearch(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return 0
+
+        for i, num in enumerate(nums):
+            if num >= target:
+                return i
+
+        return len(nums)
+
+    # Using bisect module
+    def bisectModule(self, nums: List[int], target: int) -> int:
+        return bisect.bisect_left(nums, target)
+
+    # Binary Search By Hand
+    def binarySearch(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums)
+        while low < high:
+            mid = (low + high) // 2
+            if target > nums[mid]:
+                low = mid + 1
+            else:
+                high = mid
+        return low
