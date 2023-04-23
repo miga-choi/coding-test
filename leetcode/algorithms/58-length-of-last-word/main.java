bb8c4f5b0d26b3f1a68ddc6065e7a015263b493a
miga-choi <miga.choi.1004@gmail.com>
2023-04-23 22:21:22 +0900

    leetcode: "58-length-of-last-word" add java solution

---
+++ leetcode/algorithms/58-length-of-last-word/main.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+
+  public int lengthOfLastWord(String s) {
+    String[] sArray = s.split(" ");
+    List<String> resultArray = new ArrayList<String>();
+    for (int i = 0; i < sArray.length; i++) {
+      if (sArray[i].isEmpty()) {
+        continue;
+      }
+      resultArray.add(sArray[i]);
+    }
+    return resultArray.get(resultArray.size() - 1).length();
+  }
+
+  // Best Solution
+  public int bestSolution(String s) {
+    int length = 0;
+
+    // We are looking for the last word so let's go backward
+    for (int i = s.length() - 1; i >= 0; i--) {
+      if (s.charAt(i) != ' ') { // a letter is found so count
+        length++;
+      } else { // it's a white space instead
+        //  Did we already started to count a word ? Yes so we found the last word
+        if (length > 0) return length;
+      }
+    }
+    return length;
+  }
+}
