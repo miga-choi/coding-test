1d2d5324c2a7e2ab490a2c8ff58312fde06d64cd
miga-choi <miga.choi.1004@gmail.com>
2023-04-26 00:13:28 +0900

    leetcode: "67_add_binary" add java solution

---
+++ leetcode/algorithms/67_add_binary/main.java
@@ -0,0 +1,23 @@
+import java.math.BigInteger;
+
+class Solution {
+
+  public String addBinary(String a, String b) {
+    return (new BigInteger(a, 2).add(new BigInteger(b, 2))).toString(2);
+  }
+
+  // Best Solution
+  public String bestSolution(String a, String b) {
+    StringBuilder sb = new StringBuilder();
+    int i = a.length() - 1, j = b.length() - 1, carry = 0;
+    while (i >= 0 || j >= 0) {
+      int sum = carry;
+      if (i >= 0) sum += a.charAt(i--) - '0';
+      if (j >= 0) sum += b.charAt(j--) - '0';
+      sb.append(sum % 2);
+      carry = sum / 2;
+    }
+    if (carry != 0) sb.append(carry);
+    return sb.reverse().toString();
+  }
+}
