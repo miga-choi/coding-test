08228a90c93335acb65f7c346b1e1f72b0f209ba
miga-choi <miga.choi.1004@gmail.com>
2023-04-28 23:48:59 +0900

    leetcode: "83_remove_duplicates_from_sorted_list" add javascript solution

---
+++ leetcode/algorithms/83_remove_duplicates_from_sorted_list/main.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  let result = head;
+  while (result) {
+    if (result.next && result.val === result.next.val) {
+      result.next = result.next.next;
+      continue;
+    }
+    result = result.next;
+  }
+  return head;
+};
+
+// Best Solution
+var bestSolution = function (head) {
+  var current = head;
+
+  while (current) {
+    if (current.next !== null && current.val == current.next.val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+
+  return head;
+};
