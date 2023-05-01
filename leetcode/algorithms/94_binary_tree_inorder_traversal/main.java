687dcb51c25fe4e7b50bff366976429f12f0fa9e
miga-choi <miga.choi.1004@gmail.com>
2023-05-01 23:56:23 +0900

    leetcode: "94_binary_tree_inorder_traversal" add java solution

---
+++ leetcode/algorithms/94_binary_tree_inorder_traversal/main.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class TreeNode {
+
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Solution {
+
+  public List<Integer> inorderTraversal(TreeNode root) {
+    List<TreeNode> totalRoot = new ArrayList<TreeNode>();
+    List<Integer> result = new ArrayList<Integer>();
+
+    while (root != null || !totalRoot.isEmpty()) {
+      if (root != null) {
+        totalRoot.add(root);
+        root = root.left;
+      } else {
+        root = totalRoot.remove(totalRoot.size() - 1);
+        result.add(root.val);
+        root = root.right;
+      }
+    }
+    return result;
+  }
+
+  // Best Solution
+  public List<Integer> bestSolution(TreeNode root) {
+    List<Integer> list = new ArrayList<Integer>();
+
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    TreeNode cur = root;
+
+    while (cur != null || !stack.empty()) {
+      while (cur != null) {
+        stack.add(cur);
+        cur = cur.left;
+      }
+      cur = stack.pop();
+      list.add(cur.val);
+      cur = cur.right;
+    }
+
+    return list;
+  }
+}
