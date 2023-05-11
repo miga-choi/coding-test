4e29488aeec73c6ba5e55afc62f20c973460e0e5
miga-choi <miga.choi.1004@gmail.com>
2023-05-11 09:09:18 +0900

    leetcode: "111_minimum_depth_of_binary_tree" add java solution

---
+++ leetcode/algorithms/111_minimum_depth_of_binary_tree/main.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+  public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public int minDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+
+    if (root.left == null && root.right == null) {
+      return 1;
+    }
+
+    if (root.left == null) {
+      return minDepth(root.right) + 1;
+    }
+
+    if (root.right == null) {
+      return minDepth(root.left) + 1;
+    }
+
+    return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+  }
+
+  // Best Solution
+  public int bestSolution(TreeNode root) {
+    if (root == null) return 0;
+    int left = minDepth(root.left);
+    int right = minDepth(root.right);
+    return (left == 0 || right == 0)
+      ? left + right + 1
+      : Math.min(left, right) + 1;
+  }
+}
