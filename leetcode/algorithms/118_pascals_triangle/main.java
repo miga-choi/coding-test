818dc60c33cf8fa538707931d61f67003a220887
miga-choi <miga.choi.1004@gmail.com>
2023-05-15 00:12:55 +0900

    leetcode: "118_pascals_triangle" add java solution

---
+++ leetcode/algorithms/118_pascals_triangle/main.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+
+  public List<List<Integer>> generate(int numRows) {
+    List<List<Integer>> result = new ArrayList<List<Integer>>();
+    for (int i = 0; i < numRows; i++) {
+      List<Integer> row = new ArrayList<Integer>();
+      for (int j = 0; j <= i; j++) {
+        if (j == 0 || j == i) {
+          row.add(1);
+        } else {
+          row.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+        }
+      }
+      result.add(row);
+    }
+    return result;
+  }
+
+  // Best Solution
+  public List<List<Integer>> bestSolution(int numRows) {
+    List<List<Integer>> allrows = new ArrayList<List<Integer>>();
+    ArrayList<Integer> row = new ArrayList<Integer>();
+    for (int i = 0; i < numRows; i++) {
+      row.add(0, 1);
+      for (int j = 1; j < row.size() - 1; j++) row.set(
+        j,
+        row.get(j) + row.get(j + 1)
+      );
+      allrows.add(new ArrayList<Integer>(row));
+    }
+    return allrows;
+  }
+}
