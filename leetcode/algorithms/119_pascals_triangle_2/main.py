62573a02416d8c0a5965d62109028043b370e917
miga-choi <miga.choi.1004@gmail.com>
2023-05-16 00:16:24 +0900

    leetcode: "119_pascals_triangle_2" add python solution

---
+++ leetcode/algorithms/119_pascals_triangle_2/main.py
@@ -0,0 +1,22 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        rows = []
+        for i in range(rowIndex + 1):
+            row = []
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    row.append(1)
+                else:
+                    row.append(rows[i - 1][j - 1] + rows[i - 1][j])
+            rows.append(row)
+        return rows[rowIndex]
+
+    # Best Solution
+    def bestSolution(self, rowIndex: int) -> List[int]:
+        row = [1] * (rowIndex + 1)
+        if rowIndex == 0:
+            return row
+        prev_row = self.getRow(rowIndex - 1)
+        for i in range(1, len(row) - 1):
+            row[i] = prev_row[i - 1] + prev_row[i]
+        return row
