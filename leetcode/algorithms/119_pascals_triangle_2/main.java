d62cf5680abdc9515470352edc7e7ceda72a197c
miga-choi <miga.choi.1004@gmail.com>
2023-05-17 00:12:38 +0900

    leetcode: "119_pascals_triangle_2" add java solution

---
+++ leetcode/algorithms/119_pascals_triangle_2/main.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+
+  public List<Integer> getRow(int rowIndex) {
+    List<List<Integer>> rows = new ArrayList<List<Integer>>();
+    for (int i = 0; i <= rowIndex; i++) {
+      List<Integer> row = new ArrayList<Integer>();
+      for (int j = 0; j <= i; j++) {
+        if (j == 0 || j == i) {
+          row.add(1);
+        } else {
+          row.add(rows.get(i - 1).get(j - 1) + rows.get(i - 1).get(j));
+        }
+      }
+      rows.add(row);
+    }
+    return rows.get(rowIndex);
+  }
+
+  // Best Solution
+  public List<Integer> bestSolution(int k) {
+    Integer[] arr = new Integer[k + 1];
+    Arrays.fill(arr, 0);
+    arr[0] = 1;
+
+    for (int i = 1; i <= k; i++) {
+      for (int j = i; j > 0; j--) {
+        arr[j] = arr[j] + arr[j - 1];
+      }
+    }
+
+    return Arrays.asList(arr);
+  }
+}
--- leetcode/algorithms/119_pascals_triangle_2/main.js
+++ leetcode/algorithms/119_pascals_triangle_2/main.js
@@ -22,6 +22,8 @@ var getRow = function (rowIndex) {
 var bestSolution = function (r) {
   var ans = new Array(r + 1);
   ans[0] = ans[r] = 1;
-  for (i = 1, up = r; i < r; i++, up--) ans[i] = (ans[i - 1] * up) / i;
+  for (i = 1, up = r; i < r; i++, up--) {
+    ans[i] = (ans[i - 1] * up) / i;
+  }
   return ans;
 };
