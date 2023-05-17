b4865f785e6a9a6eab369a1eb62cd3b102e76810
miga-choi <miga.choi.1004@gmail.com>
2023-05-18 00:48:56 +0900

    leetcode: "121_best_time_to_buy_and_sell_stock" add javascript solution

---
+++ leetcode/algorithms/121_best_time_to_buy_and_sell_stock/main.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let buy = 0;
+  let profit = 0;
+  for (let sell = 1; sell < prices.length; sell++) {
+    if (prices[sell] > prices[buy]) {
+      if (prices[sell] - prices[buy] > profit) {
+        profit = prices[sell] - prices[buy];
+      }
+    } else {
+      buy = sell;
+    }
+  }
+  return profit;
+};
+
+// Best Solution
+const bestSolution = (prices) => {
+  let left = 0; // Buy
+  let right = 1; // sell
+  let max_profit = 0;
+  while (right < prices.length) {
+    if (prices[left] < prices[right]) {
+      let profit = prices[right] - prices[left]; // our current profit
+
+      max_profit = Math.max(max_profit, profit);
+    } else {
+      left = right;
+    }
+    right++;
+  }
+  return max_profit;
+};
