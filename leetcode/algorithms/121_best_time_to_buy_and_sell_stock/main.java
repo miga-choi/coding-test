32016f58ef9ada6eba165871b2cf6e9ddd15ac86
miga-choi <miga.choi.1004@gmail.com>
2023-05-19 00:06:14 +0900

    leetcode: "121_best_time_to_buy_and_sell_stock" add java solution

---
+++ leetcode/algorithms/121_best_time_to_buy_and_sell_stock/main.java
@@ -0,0 +1,37 @@
+class Solution {
+
+  public int maxProfit(int[] prices) {
+    int buy = 0;
+    int profit = 0;
+
+    for (int sell = 1; sell < prices.length; sell++) {
+      if (prices[sell] > prices[buy]) {
+        if (prices[sell] - prices[buy] > profit) {
+          profit = prices[sell] - prices[buy];
+        }
+      } else {
+        buy = sell;
+      }
+    }
+
+    return profit;
+  }
+
+  // Best Solution
+  public int bestSolution(int[] prices) {
+    int lsf = Integer.MAX_VALUE;
+    int op = 0;
+    int pist = 0;
+
+    for (int i = 0; i < prices.length; i++) {
+      if (prices[i] < lsf) {
+        lsf = prices[i];
+      }
+      pist = prices[i] - lsf;
+      if (op < pist) {
+        op = pist;
+      }
+    }
+    return op;
+  }
+}
