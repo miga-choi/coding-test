5de0549ce3fe9b1d2f11c9d7738d67a958e3d810
miga-choi <miga.choi.1004@gmail.com>
2023-05-20 00:40:46 +0900

    leetcode: "125_valid_palindrome" add python solution

---
+++ leetcode/algorithms/125_valid_palindrome/main.py
@@ -0,0 +1,32 @@
+import re
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = re.sub("[^0-9a-zA-Z]+", "", s).lower()
+        for i in range(len(s)):
+            if i == len(s) - 1:
+                break
+            if s[i] != s[len(s) - 1 - i]:
+                return False
+        return True
+
+    # Best Solution
+    # Solution 1: Two Lines
+    def bestSolution1(self, s: str) -> bool:
+        s = [i for i in s.lower() if i.isalnum()]
+        return s == s[::-1]
+
+    # Solution 2: O(1) space (two-pointer)
+    def bestSolution2(self, s: str) -> bool:
+        i, j = 0, len(s) - 1
+        while i < j:
+            a, b = s[i].lower(), s[j].lower()
+            if a.isalnum() and b.isalnum():
+                if a != b:
+                    return False
+                else:
+                    i, j = i + 1, j - 1
+                    continue
+            i, j = i + (not a.isalnum()), j - (not b.isalnum())
+        return True
