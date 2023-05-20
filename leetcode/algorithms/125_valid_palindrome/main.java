c85725bcdcc7cf0026b87f6a3052003ac527eb6c
miga-choi <miga.choi.1004@gmail.com>
2023-05-21 01:29:31 +0900

    leetcode: "125_valid_palindrome" add java solution

---
+++ leetcode/algorithms/125_valid_palindrome/main.java
@@ -0,0 +1,38 @@
+class Solution {
+
+  public boolean isPalindrome(String s) {
+    s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+    for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+      if (s.charAt(i) != s.charAt(j)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // Best Solution
+  public boolean bestSolution(String s) {
+    if (s.isEmpty()) {
+      return true;
+    }
+    int head = 0, tail = s.length() - 1;
+    char cHead, cTail;
+    while (head <= tail) {
+      cHead = s.charAt(head);
+      cTail = s.charAt(tail);
+      if (!Character.isLetterOrDigit(cHead)) {
+        head++;
+      } else if (!Character.isLetterOrDigit(cTail)) {
+        tail--;
+      } else {
+        if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
+          return false;
+        }
+        head++;
+        tail--;
+      }
+    }
+
+    return true;
+  }
+}
