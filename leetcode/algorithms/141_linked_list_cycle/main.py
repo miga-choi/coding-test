1fd33ea37e03370a0f7d86ba654d41a4fe2d938f
miga-choi <miga.choi.1004@gmail.com>
2023-05-24 00:33:55 +0900

    leetcode: "141_linked_list_cycle" add python solution

---
+++ leetcode/algorithms/141_linked_list_cycle/main.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        stack = []
+        while head != None:
+            if head in stack:
+                return True
+            stack.append(head)
+            head = head.next
+        return False
+
+    # Best Solution
+    # Dictionary/Hash table
+    def bestSolution1(self, head: ListNode) -> bool:
+        dictionary = {}
+        while head:
+            if head in dictionary:
+                return True
+            else:
+                dictionary[head] = True
+            head = head.next
+        return False
+
+    # Two pointers
+    def bestSolution2(self, head: ListNode) -> bool:
+        if not head:
+            return False
+        slow = head
+        fast = head.next
+        while slow != fast:
+            if not fast or not fast.next:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        return True
