66f31aa115ceced81f51d9edf19299edefe5e6b4
miga-choi <miga.choi.1004@gmail.com>
2023-05-24 00:16:22 +0900

    leetcode: "141_linked_list_cycle" add javascript solution

---
+++ leetcode/algorithms/141_linked_list_cycle/main.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  const stack = [];
+  while (head) {
+    if (stack.indexOf(head) > -1) {
+      return true;
+    }
+    stack.push(head);
+    head = head.next;
+  }
+  return false;
+};
+
+// Best Solution
+const bestSolution = (head) => {
+  let fast = head;
+  while (fast && fast.next) {
+    head = head.next;
+    fast = fast.next.next;
+    if (head === fast) return true;
+  }
+  return false;
+};
