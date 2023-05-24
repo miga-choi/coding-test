b9a990324852c42d1279a0cab85bf423178db6e5
miga-choi <miga.choi.1004@gmail.com>
2023-05-25 00:32:24 +0900

    leetcode: "141_linked_list_cycle" add java solution

---
+++ leetcode/algorithms/141_linked_list_cycle/main.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+class Solution {
+
+  public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+      val = x;
+      next = null;
+    }
+  }
+
+  public boolean hasCycle(ListNode head) {
+    List<ListNode> stack = new ArrayList<ListNode>();
+    while (head != null) {
+      if (stack.indexOf(head) > -1) {
+        return true;
+      }
+      stack.add(head);
+      head = head.next;
+    }
+    return false;
+  }
+
+  // Best Solution
+  public boolean bestSolution(ListNode head) {
+    ListNode slow = head, fast = head;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+      if (fast == slow) return true;
+    }
+    return false;
+  }
+}
