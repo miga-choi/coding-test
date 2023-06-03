9c49e3d1468b54deab7a3d31e7004b243d5f5063
miga-choi <miga.choi.1004@gmail.com>
2023-06-04 01:01:19 +0900

    leetcode: "169_majority_element" add java solution

---
+++ leetcode/algorithms/169_majority_element/main.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+
+  public int majorityElement(int[] nums) {
+    Map<Integer, Integer> numsMap = new HashMap<Integer, Integer>();
+
+    for (int i = 0; i < nums.length; i++) {
+      int num = nums[i];
+      int numsNum = numsMap.get(num) == null ? 0 : numsMap.get(num);
+
+      if (numsNum == 0) {
+        numsMap.put(num, 1);
+      } else {
+        numsMap.replace(num, numsNum + 1);
+      }
+
+      if (numsNum >= nums.length / 2) {
+        return num;
+      }
+    }
+
+    return 0;
+  }
+}
