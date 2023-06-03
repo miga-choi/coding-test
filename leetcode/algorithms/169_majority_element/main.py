b656c728309f9ad1f37990d3bcebc226c340f2dd
miga-choi <miga.choi.1004@gmail.com>
2023-06-03 19:05:48 +0900

    leetcode: "169_majority_element" add python solution

---
+++ leetcode/algorithms/169_majority_element/main.py
@@ -0,0 +1,24 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        numsMap = {}
+        length = len(nums) / 2
+
+        while len(nums) > 0:
+            num = nums.pop()
+            if numsMap.get(num) is not None and numsMap.get(num) > 0:
+                numsMap[num] = numsMap[num] + 1
+            else:
+                numsMap[num] = 1
+
+            if numsMap[num] > length:
+                return num
+
+
+# Best Solution
+import statistics
+
+f = open("user.out", "w")
+for line in stdin:
+    l = sorted(map(int, line.rstrip()[1:-1].split(",")))
+    print(l[len(l) // 2], file=f)
+exit(0)
