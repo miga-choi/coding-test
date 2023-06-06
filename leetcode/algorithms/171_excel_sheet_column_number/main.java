fc473612f6c10830aca7c2d5518a270f5d10a5e5
miga-choi <miga.choi.1004@gmail.com>
2023-06-06 13:01:01 +0900

    leetcode: "171_excel_sheet_column_number" add java solution

---
+++ leetcode/algorithms/171_excel_sheet_column_number/main.java
@@ -0,0 +1,24 @@
+class Solution {
+
+  public int titleToNumber(String columnTitle) {
+    int result = 0;
+
+    for (int i = columnTitle.length() - 1; i >= 0; i--) {
+      result += Math.pow(26, columnTitle.length() - 1 - i) * ((int) columnTitle.charAt(i) - 64);
+    }
+
+    return result;
+  }
+
+  // Best Solution
+  public int bestSolution(String s) {
+    if (s == null) return -1;
+    int sum = 0;
+    // for each loop so we don't need to mess with index values.
+    for (char c : s.toUpperCase().toCharArray()) {
+      sum *= 26;
+      sum += c - 'A' + 1;
+    }
+    return sum;
+  }
+}
