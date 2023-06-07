9286a7569d3a772bc4ea53eaad04fcbea88c98ea
miga-choi <miga.choi.1004@gmail.com>
2023-06-07 12:15:08 +0900

    leetcode: "175_combine_two_tables" add mysql solution

---
+++ leetcode/algorithms/175_combine_two_tables/main.mysql.sql
@@ -0,0 +1,15 @@
+-- # Write your MySQL query statement below
+SELECT person.firstName, person.lastName, address.city, address.state
+FROM Person person LEFT OUTER JOIN Address address ON person.personId = address.personId;
+
+
+-- BEST SOLUTION
+-- 1st way (table1 LEFT JOIN table2 ON condition)
+SELECT P.firstName, P.lastName, A.city, A.state
+FROM Person P LEFT JOIN Address A
+on P.personId = A.personId
+
+-- 2nd way (table1 LEFT JOIN table2 USING column_name)
+SELECT P.firstName, P.lastName, A.city, A.state
+FROM Person P
+LEFT JOIN Address A USING (personId)
