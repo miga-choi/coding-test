3381518547c8e7b28420893ff06ff63a64d54dc0
miga-choi <miga.choi.1004@gmail.com>
2023-06-08 17:24:00 +0900

    leetcode: "181_employees_earning_more_than_their_managers" add mysql solution

---
+++ leetcode/algorithms/181_employees_earning_more_than_their_managers/main.mysql.sql
@@ -0,0 +1,11 @@
+-- # Write your MySQL query statement below
+SELECT name AS Employee
+FROM Employee a
+WHERE a.salary > (SELECT salary FROM Employee WHERE id = a.managerId);
+
+
+-- Best Solution
+SELECT e2.name AS Employee
+FROM employee e1
+INNER JOIN employee e2 ON e1.id = e2.managerId
+WHERE e1.salary < e2.salary
