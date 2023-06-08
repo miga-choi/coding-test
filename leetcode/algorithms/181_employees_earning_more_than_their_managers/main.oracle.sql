f766f2a9cc1c6c7ca570a570784d34736987e9e9
miga-choi <miga.choi.1004@gmail.com>
2023-06-08 17:24:51 +0900

    leetcode: "181_employees_earning_more_than_their_managers" add oracle solution

---
+++ leetcode/algorithms/181_employees_earning_more_than_their_managers/main.oracle.sql
@@ -0,0 +1,12 @@
+-- /* Write your PL/SQL query statement below */
+SELECT a.name AS Employee 
+FROM Employee a INNER JOIN Employee b ON a.managerId = b.id 
+WHERE a.salary > b.salary;
+
+
+-- Best Solution
+SELECT e1.name AS 'Employee'
+FROM Employee e1
+LEFT JOIN Employee e2
+ON e1.managerId = e2.id
+WHERE e1.salary > e2.salary
