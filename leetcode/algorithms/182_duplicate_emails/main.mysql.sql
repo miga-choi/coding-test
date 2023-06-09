cef7b7dd3e867e17d8958415beca156c416dc7c7
miga-choi <miga.choi.1004@gmail.com>
2023-06-09 09:13:00 +0900

    leetcode: "182_duplicate_emails" add mysql solution

---
+++ leetcode/algorithms/182_duplicate_emails/main.mysql.sql
@@ -0,0 +1,21 @@
+-- # Write your MySQL query statement below
+SELECT email AS Email 
+FROM Person
+GROUP BY email 
+HAVING count(email) > 1;
+
+
+-- Best Solution
+-- # 1
+SELECT email FROM Person
+GROUP BY email
+HAVING count(email) > 1;
+
+-- # 2.
+SELECT DISTINCT(p1.email) FROM Person p1, Person p2
+WHERE p1.id <> p2.id AND p1.email = p2.email;
+
+-- # 3. 
+SELECT DISTINCT(p1.email) FROM 
+Person p1 JOIN Person p2 ON
+p1.email = p2.email AND p1.id <> p2.id;
