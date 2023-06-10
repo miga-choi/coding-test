0bb400bf1ecc1fc237722889173a91ae66508c26
miga-choi <miga.choi.1004@gmail.com>
2023-06-10 12:01:31 +0900

    leetcode: "183_customers_who_never_order" add oracle solution

---
+++ leetcode/algorithms/183_customers_who_never_order/main.oracle.sql
@@ -0,0 +1,9 @@
+-- /* Write your PL/SQL query statement below */
+SELECT customers.name AS Customers 
+FROM Customers customers, Orders orders 
+WHERE customers.id = orders.customerId(+) AND orders.customerId IS NULL;
+
+
+-- Best Solution
+SELECT name AS Customers FROM Customers
+WHERE Customers.id NOT IN (SELECT customerId FROM Orders);
