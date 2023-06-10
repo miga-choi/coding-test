38467253acfb5dbe4ab0476d52002efb6cdfb612
miga-choi <miga.choi.1004@gmail.com>
2023-06-10 12:00:27 +0900

    leetcode: "183_customers_who_never_order" add mysql solution

---
+++ leetcode/algorithms/183_customers_who_never_order/main.mysql.sql
@@ -0,0 +1,14 @@
+-- # Write your MySQL query statement below
+SELECT customers.name AS Customers 
+FROM Customers customers LEFT OUTER JOIN Orders orders 
+ON customers.id = orders.customerId 
+WHERE orders.customerId IS NULL;
+
+
+-- Best Solution
+SELECT name AS Customers
+FROM Customers
+WHERE id NOT IN (
+    SELECT customerId
+    FROM Orders
+);
