47060981a213f7b92e5ca3a43909bd2598e38fd6
miga-choi <miga.choi.1004@gmail.com>
2023-06-14 00:27:54 +0900

    leetcode: "191_number_of_1_bits" add java solution

---
+++ leetcode/algorithms/191_number_of_1_bits/main.java
@@ -0,0 +1,17 @@
+class Solution {
+
+  // you need to treat n as an unsigned value
+  public int hammingWeight(int n) {
+    return Integer.toBinaryString(n).replaceAll("0", "").length();
+  }
+
+  // Best Solution
+  public static int bestSolution(int n) {
+    int ones = 0;
+    while (n != 0) {
+      ones = ones + (n & 1);
+      n = n >>> 1;
+    }
+    return ones;
+  }
+}
