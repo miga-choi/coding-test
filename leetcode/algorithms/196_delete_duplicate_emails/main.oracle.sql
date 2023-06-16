2e787b690d89996ca0b9ac2aaba1345ba6fb5513
miga-choi <miga.choi.1004@gmail.com>
2023-06-17 01:10:40 +0900

    leetcode: "196_delete_duplicate_emails" add oracle solution

---
+++ leetcode/algorithms/196_delete_duplicate_emails/main.oracle.sql
@@ -0,0 +1,13 @@
+-- /*
+--  Please write a DELETE statement and DO NOT write a SELECT statement.
+--  Write your PL/SQL query statement below
+--  */
+DELETE p1 
+FROM person p1, person p2 
+WHERE p1.email = p2.email 
+AND p1.id > p2.id;
+
+
+-- Best Solution
+DELETE p1 FROM person p1, person p2 
+WHERE p1.email = p2.email AND p1.id > p2.id;
