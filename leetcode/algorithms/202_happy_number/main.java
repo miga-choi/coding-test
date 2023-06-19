d31b59510fb0ac15dd794531f78486dcc4793edc
miga-choi <miga.choi.1004@gmail.com>
2023-06-20 00:12:40 +0900

    leetcode: "202_happy_number" add java solution

---
+++ leetcode/algorithms/202_happy_number/main.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+
+  public boolean isHappy(int n) {
+    if (n <= 1) {
+      return true;
+    }
+    Map<Integer, Boolean> nMap = new HashMap<Integer, Boolean>();
+
+    while (n > 1) {
+      if (nMap.get(n) != null && nMap.get(n)) {
+        return false;
+      }
+      nMap.put(n, true);
+      List<Integer> nList = new ArrayList<Integer>();
+      for (int i = 0; i < String.valueOf(n).length(); i++) {
+        nList.add(String.valueOf(n).charAt(i) - '0');
+      }
+      n =
+        nList
+          .stream()
+          .reduce(
+            0,
+            (total_, current_) -> (int) (total_ + Math.pow(current_, 2))
+          );
+
+      if (n == 1) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  // Best Solution
+  // Floyd's cycle-finding algorithm (Tortoise and the Hare algorithm)
+  public boolean bestSolution(int n) {
+    int s = n, f = n; // slow , fast
+
+    do {
+      s = compute(s); // slow computes only once
+      f = compute(compute(f)); // fast computes 2 times
+
+      if (s == 1) return true; // if we found 1 then happy indeed !!!
+    } while (s != f); // else at some point they will meet in the cycle
+
+    return false; // it's a cycle , not happy at all !!!
+  }
+
+  public int compute(int n) {
+    int s = 0;
+    while (n != 0) {
+      s += (n % 10) * (n % 10);
+      n = n / 10;
+    }
+    return s;
+  }
+}
