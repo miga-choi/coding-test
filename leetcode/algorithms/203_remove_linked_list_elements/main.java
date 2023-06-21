670f163969602c0d07c4bdc573e7428cfa916640
miga-choi <miga.choi.1004@gmail.com>
2023-06-22 00:10:33 +0900

    leetcode: "203_remove_linked_list_elements" add java solution

---
+++ leetcode/algorithms/203_remove_linked_list_elements/main.java
@@ -0,0 +1,77 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class RemoveLinkedListElements {
+
+  public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+      this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+
+  public ListNode removeElements(ListNode head, int val) {
+    if (head == null) {
+      return null;
+    }
+
+    ListNode returnHead = new ListNode();
+    returnHead.next = head;
+
+    ListNode currentHead = head;
+    ListNode prevHead = returnHead;
+
+    while (currentHead != null) {
+      if (currentHead.val == val) {
+        prevHead.next = currentHead.next;
+      } else {
+        prevHead = prevHead.next;
+      }
+      currentHead = currentHead.next;
+    }
+
+    return returnHead.next;
+  }
+
+  // Best Solution
+  // best solution 1
+  public ListNode bestSolution1(ListNode head, int val) {
+    if (head == null) return null;
+    head.next = removeElements(head.next, val);
+    return head.val == val ? head.next : head;
+  }
+
+  // best solution 2
+  public ListNode bestSolution2(ListNode head, int val) {
+    ListNode fakeHead = new ListNode(-1);
+    fakeHead.next = head;
+    ListNode curr = head, prev = fakeHead;
+    while (curr != null) {
+      if (curr.val == val) {
+        prev.next = curr.next;
+      } else {
+        prev = prev.next;
+      }
+      curr = curr.next;
+    }
+    return fakeHead.next;
+  }
+}
