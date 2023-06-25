d80fbe39a4789a6a4c00b7932842883d23424492
miga-choi <miga.choi.1004@gmail.com>
2023-06-25 15:32:12 +0900

    leetcode: "205_isomorphic_strings" add java solution

---
+++ leetcode/algorithms/205_isomorphic_strings/main.java
@@ -0,0 +1,38 @@
+class Solution {
+
+  public boolean isIsomorphic(String s, String t) {
+    if (s.length() != t.length()) {
+      return false;
+    }
+
+    int[] sMap = new int[50000];
+    int[] tMap = new int[50000];
+
+    for (int i = 0; i < s.length(); i++) {
+      if (sMap[s.charAt(i)] != tMap[t.charAt(i)]) {
+        return false;
+      }
+      sMap[s.charAt(i)] = i + 1;
+      tMap[t.charAt(i)] = i + 1;
+    }
+
+    return true;
+  }
+
+
+  // Best Solution
+  public boolean bestSolution(String s, String t) {
+    int map1[] = new int[200];
+    int map2[] = new int[200];
+
+    if (s.length() != t.length()) return false;
+
+    for (int i = 0; i < s.length(); i++) {
+      if (map1[s.charAt(i)] != map2[t.charAt(i)]) return false;
+
+      map1[s.charAt(i)] = i + 1;
+      map2[t.charAt(i)] = i + 1;
+    }
+    return true;
+  }
+}
