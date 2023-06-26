ad136f29d7e24b44eef898ed550e22e9ccbd71c7
miga-choi <miga.choi.1004@gmail.com>
2023-06-27 00:03:47 +0900

    leetcode: "206_reverse_linked_list" add java solution

---
+++ leetcode/algorithms/206_reverse_linked_list/main.java
@@ -0,0 +1,71 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class ListNode {
+
+  int val;
+  ListNode next;
+
+  ListNode() {}
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+class ReverseLinkedList {
+
+  public ListNode reverseList(ListNode head) {
+    ListNode prev = null;
+    ListNode current = head;
+
+    while (current != null) {
+      ListNode next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+
+    return prev;
+  }
+
+  
+  // Best Solution
+  // iterative solution
+  public ListNode bestSolution1(ListNode head) {
+    ListNode newHead = null;
+    while (head != null) {
+      ListNode next = head.next;
+      head.next = newHead;
+      newHead = head;
+      head = next;
+    }
+    return newHead;
+  }
+
+  // recursive solution
+  public ListNode bestSolution2(ListNode head) {
+    /* recursive solution */
+    return reverseListInt(head, null);
+  }
+
+  private ListNode reverseListInt(ListNode head, ListNode newHead) {
+    if (head == null) return newHead;
+    ListNode next = head.next;
+    head.next = newHead;
+    return reverseListInt(next, head);
+  }
+}
