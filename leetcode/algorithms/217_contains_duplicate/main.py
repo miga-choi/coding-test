45514082e08647ad55ffc072d828daeed00f9943
miga-choi <miga.choi.1004@gmail.com>
2023-06-29 09:03:16 +0900

    leetcode: "217_contains_duplicate" add python solution

---
+++ leetcode/algorithms/217_contains_duplicate/main.py
@@ -0,0 +1,18 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        numsMap: dict = {}
+        for i in range(len(nums)):
+            if numsMap.get(nums[i]) != None:
+                return True
+            numsMap.update({nums[i]: True})
+        return False
+
+    
+    # Best Solution
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hset = set()
+        for idx in nums:
+            if idx in hset:
+                return True
+            else:
+                hset.add(idx)
