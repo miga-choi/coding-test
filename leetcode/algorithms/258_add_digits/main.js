483bdf238bdce96f1d51a93b1c382ab9620853a4
miga-choi <miga.choi.1004@gmail.com>
2023-08-04 00:46:04 +0900

    leetcode: "258_add_digits" add javascript solution

---
+++ leetcode/algorithms/258_add_digits/main.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function (num) {
+  let numString = num.toString();
+  while (numString.length > 1) {
+    let num = 0;
+    for (const num_ of numString) {
+      num += parseInt(num_);
+    }
+    numString = num.toString();
+  }
+  return numString;
+};
+
+
+// Best Solution
+var bestSolution = function (num) {
+  return 1 + ((num - 1) % 9);
+};
