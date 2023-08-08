6b26a51574f0ae39ade73358849b8665f0051b84
miga-choi <miga.choi.1004@gmail.com>
2023-08-09 00:07:35 +0900

    leetcode: "268_missing_number" add python solution

---
+++ leetcode/algorithms/268_missing_number/main.py
@@ -0,0 +1,15 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] != i:
+                return i
+        return len(nums)
+
+
+    # Best Solution
+    def bestSolution1(self, nums: List[int]) -> int:
+        return (len(nums) * (len(nums) + 1)) // 2 - sum(nums)
+
+    def bestSolution2(self, nums: List[int]) -> int:
+        return reduce(lambda x, y: x ^ y, list(range(len(nums) + 1)) + nums)
