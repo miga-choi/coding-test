f2d8a9be0f6a476450dfb24db861bc6a14e59690
miga-choi <miga.choi.1004@gmail.com>
2023-08-12 01:14:43 +0900

    leetcode: "222_count_complete_tree_nodes" add javascript solution

---
+++ leetcode/algorithms/222_count_complete_tree_nodes/main.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function (root) {
+  if (!root) {
+    return 0;
+  }
+  return countNodes(root.left) + countNodes(root.right) + 1;
+};
+
+
+// Best Solution
+var bestSolution = function (root) {
+  function leftDepth(node) {
+    if (!node) return 0;
+    return leftDepth(node.left) + 1;
+  }
+
+  function rightDepth(node) {
+    if (!node) return 0;
+    return rightDepth(node.right) + 1;
+  }
+
+  function traverse(node) {
+    const leftLen = leftDepth(node);
+    const rightLen = rightDepth(node);
+
+    if (leftLen === rightLen) return Math.pow(2, leftLen) - 1;
+    return traverse(node.left) + traverse(node.right) + 1;
+  }
+  return traverse(root);
+};
