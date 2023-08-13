0b705e4c5f76f50df61715994c7c7d0d31954c0a
miga-choi <miga.choi.1004@gmail.com>
2023-08-13 19:08:55 +0900

    leetcode: "222_count_complete_tree_nodes" add java solution

---
+++ leetcode/algorithms/222_count_complete_tree_nodes/main.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
+
+
+    // Best Solution
+    int height(TreeNode root) {
+        return root == null ? -1 : 1 + height(root.left);
+    }
+
+    public int bestSolution(TreeNode root) {
+        int h = height(root);
+        return h < 0 ? 0
+                : height(root.right) == h - 1 ? (1 << h) + countNodes(root.right)
+                        : (1 << h - 1) + countNodes(root.left);
+    }
+}
