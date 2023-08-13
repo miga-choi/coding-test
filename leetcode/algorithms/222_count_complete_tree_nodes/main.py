f04f7d5d1877904c5b293581fb92a5360f0f526d
miga-choi <miga.choi.1004@gmail.com>
2023-08-13 19:07:54 +0900

    leetcode: "222_count_complete_tree_nodes" add python solution

---
+++ leetcode/algorithms/222_count_complete_tree_nodes/main.py
@@ -0,0 +1,57 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    class TreeNode:
+        def __init__(self, val=0, left=None, right=None):
+            self.val = val
+            self.left = left
+            self.right = right
+
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        if root == None:
+            return 0
+        return self.countNodes(root.left) + self.countNodes(root.right) + 1
+
+
+    # Best Solution
+    def bestSolution(self, root: TreeNode) -> int:
+        ## RC ##
+        ## APPROACH : RECURSION ##
+        ## TIME COMPLEXICITY : LOG N * LOG N ##
+
+        ## LOGIC ##
+        # If left sub tree height equals right sub tree height then,
+        #       a. left sub tree is perfect binary tree
+        #       b. right sub tree is complete binary tree
+        # If left sub tree height greater than right sub tree height then,
+        #       a. left sub tree is complete binary tree
+        #       b. right sub tree is perfect binary tree
+
+        if not root:
+            return 0
+
+        def depthLeft(node):
+            d = 0
+            while node:
+                d += 1
+                node = node.left
+            return d
+
+        def depthRight(node):
+            d = 0
+            while node:
+                d += 1
+                node = node.right
+            return d
+
+        ld = depthLeft(root.left)
+        rd = depthRight(root.right)
+
+        if ld == rd:
+            return 2 ** (ld + 1) - 1
+        else:
+            return 1 + self.countNodes(root.left) + self.countNodes(root.right)
