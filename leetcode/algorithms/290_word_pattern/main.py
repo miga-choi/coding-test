0ff436bea88a7a0c1541f62a0fb5c32902fc7917
miga-choi <miga.choi.1004@gmail.com>
2023-08-17 00:35:46 +0900

    leetcode: "290_word_pattern" add python solution

---
+++ leetcode/algorithms/290_word_pattern/main.py
@@ -0,0 +1,45 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        sArray: list = s.split(" ")
+
+        if len(sArray) != len(pattern):
+            return False
+
+        sMap: dict = {}
+
+        for i in range(len(pattern)):
+            if sMap.get(pattern[i]) == None:
+                for v in list(sMap.values()):
+                    if sArray[i] == v:
+                        return False
+                sMap.update({pattern[i]: sArray[i]})
+            else:
+                if sMap[pattern[i]] != sArray[i]:
+                    return False
+
+        return True
+
+
+    # Best Solution
+    def bestSolution(self, pattern: str, str: str) -> bool:
+        # dictionary approach
+        # Time complexity: O(n)
+        # Space complexity: O(n)
+
+        words = str.split(" ")
+        if not len(words) == len(pattern):
+            return False
+
+        mapping = dict()  # key is the pattern, value is the word
+
+        for i in range(len(words)):
+            if pattern[i] not in mapping:
+                # values() is a set - fast membership testing - O(1) amortised search
+                if words[i] not in mapping.values():
+                    mapping[pattern[i]] = words[i]
+                else:
+                    return False
+            else:
+                if not mapping[pattern[i]] == words[i]:
+                    return False
+        return True
