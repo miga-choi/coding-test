b0498c58e8d2406357c5b649377e46713dfadfff
miga-choi <miga.choi.1004@gmail.com>
2023-08-19 01:12:39 +0900

    leetcode: "292_nim_game" add java solution

---
+++ leetcode/algorithms/292_nim_game/main.java
@@ -0,0 +1,14 @@
+class NimGame {
+    public boolean canWinNim(int n) {
+        if (n % 4 == 0) {
+            return false;
+        }
+        return true;
+    }
+
+
+    // Best Solution
+    public boolean bestSolution(int n) {
+        return (n % 4 != 0);
+    }
+}
