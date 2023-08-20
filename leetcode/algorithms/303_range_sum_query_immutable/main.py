3027343fe438b6080c2492dfd4a9812e135d18ca
miga-choi <miga.choi.1004@gmail.com>
2023-08-21 00:10:41 +0900

    leetcode: "303_range_sum_query_immutable" add python solution

---
+++ leetcode/algorithms/303_range_sum_query_immutable/main.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class NumArray:
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    def sumRange(self, left: int, right: int) -> int:
+        sum = 0
+        for i in range(left, right + 1):
+            sum += self.nums[i]
+        return sum
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
+
+
+# Best Solution
+class BestSolution:
+    def __init__(self, nums: List[int]):
+        self.sum = []
+        sum_till = 0
+        for i in nums:
+            sum_till += i
+            self.sum.append(sum_till)
+
+    def sumRange(self, i: int, j: int) -> int:
+        if i > 0 and j > 0:
+            return self.sum[j] - self.sum[i - 1]
+        else:
+            return self.sum[i or j]
