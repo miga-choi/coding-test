2cd3d8cd5d33285d84969974c5cdb99bbfbb12ca
miga-choi <miga.choi.1004@gmail.com>
2023-08-23 00:07:27 +0900

    leetcode: "326_power_of_three" add python solution

---
+++ leetcode/algorithms/326_power_of_three/main.py
@@ -0,0 +1,18 @@
+import math
+
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n > 0 and math.log10(n) / math.log10(3) % 1 == 0:
+            return True
+        return False
+
+
+    # Best Solution
+    def bestSolution(self, n: int) -> bool:
+        # Since error exist, can't use float.is_integer() to check
+        # So I choose to check it back
+        if n <= 0:
+            return False
+        res = round(math.log(n, 3))
+        return 3**res == n
