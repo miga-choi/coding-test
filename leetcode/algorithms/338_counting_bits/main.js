f921323e4df10eb28d331b124afffb384f471d40
miga-choi <miga.choi.1004@gmail.com>
2023-08-24 00:25:30 +0900

    leetcode: "338_counting_bits" add javascript solution

---
+++ leetcode/algorithms/338_counting_bits/main.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function (n) {
+  let result = [];
+  for (let i = 0; i <= n; i++) {
+    let sum = 0;
+    const bits = i.toString(2);
+    for (j = 0; j < bits.length; j++) {
+      sum += parseInt(bits[j]);
+    }
+    result.push(sum);
+  }
+  return result;
+};
+
+
+// Best Solution
+const bestSolution = (n) => {
+  let result = Array(n + 1).fill(0);
+  let offset = 1;
+  for (let i = 1; i < n + 1; i++) {
+    if (offset * 2 === i) {
+      offset = i;
+    }
+    result[i] = 1 + result[i - offset];
+  }
+
+  return result;
+};
