35712f56697b44d9f10406b531052ece43077545
miga-choi <miga.choi.1004@gmail.com>
2023-08-25 00:47:28 +0900

    leetcode: "338_counting_bits" add java solution

---
+++ leetcode/algorithms/338_counting_bits/main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class CountingBits {
+    public int[] countBits(int n) {
+        List<Integer> result = new ArrayList<Integer>();
+
+        for (int i = 0; i <= n; i++) {
+            int sum = 0;
+            String bits = Integer.toBinaryString(i);
+            for (int j = 0; j < bits.length(); j++) {
+                sum += Integer.valueOf(String.valueOf(bits.charAt(j)));
+            }
+            result.add(sum);
+        }
+
+        return result.stream().mapToInt(value -> value).toArray();
+    }
+
+
+    // Best Solution
+    public int[] bestSolution(int num) {
+        int[] f = new int[num + 1];
+        for (int i = 1; i <= num; i++)
+            f[i] = f[i >> 1] + (i & 1);
+        return f;
+    }
+}
