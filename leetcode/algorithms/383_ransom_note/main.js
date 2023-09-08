9f20d5e22b7741bf2366f9fed862b9968abecfe0
miga-choi <miga.choi.1004@gmail.com>
2023-09-09 00:06:32 +0900

    leetcode: "383_ransom_note" add javascript solution

---
+++ leetcode/algorithms/383_ransom_note/main.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+  const magazineMap = new Map();
+
+  for (const v of magazine) {
+    if (magazineMap.get(v)) {
+      magazineMap.set(v, magazineMap.get(v) + 1);
+    } else {
+      magazineMap.set(v, 1);
+    }
+  }
+
+  for (const v of ransomNote) {
+    if (magazineMap.get(v) && magazineMap.get(v) > 0) {
+      magazineMap.set(v, magazineMap.get(v) - 1);
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+// Best Solution
+var bestSolution = function (ransomNote, magazine) {
+  for (const char of magazine) {
+    ransomNote = ransomNote.replace(char, "");
+  }
+
+  if (!ransomNote) return true;
+  else return false;
+};
