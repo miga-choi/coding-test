46ad76f726e9fdb354ccef4e680e60c706e361e5
miga-choi <miga.choi.1004@gmail.com>
2023-09-10 00:16:41 +0900

    leetcode: "383_ransom_note" add python solution

---
+++ leetcode/algorithms/383_ransom_note/main.py
@@ -0,0 +1,28 @@
+from typing import Counter
+
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        magazineMap: dict = {}
+
+        for c in magazine:
+            if magazineMap.get(c) != None:
+                magazineMap.update({c: magazineMap.get(c) + 1})
+            else:
+                magazineMap.update({c: 1})
+
+        for c in ransomNote:
+            if magazineMap.get(c) != None and magazineMap.get(c) > 0:
+                magazineMap.update({c: magazineMap.get(c) - 1})
+            else:
+                return False
+
+        return True
+
+
+    # Best Solution
+    def bestSolution(self, ransomNote, magazine):
+        st1, st2 = Counter(ransomNote), Counter(magazine)
+        if st1 & st2 == st1:
+            return True
+        return False
