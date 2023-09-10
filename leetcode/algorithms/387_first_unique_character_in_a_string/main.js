4d265a6d9d9156252abfb46d3f96f05cecf5d188
miga-choi <miga.choi.1004@gmail.com>
2023-09-11 00:05:11 +0900

    leetcode: "387_first_unique_character_in_a_string" add javascript solution

---
+++ leetcode/algorithms/387_first_unique_character_in_a_string/main.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function (s) {
+  const sMap = new Map();
+
+  for (let i = 0; i < s.length; i++) {
+    if (sMap.get(s[i]) != null) {
+      sMap.set(s[i], -1);
+    } else {
+      sMap.set(s[i], i);
+    }
+  }
+
+  let result = -1;
+
+  for (const value of sMap.values()) {
+    if (result === -1 || (value >= 0 && value < result)) {
+      result = value;
+    }
+  }
+
+  return result;
+};
+
+
+// Best Solution
+var bestSolution = function (s) {
+  for (let idx = 0; idx < s.length; idx++) {
+    // If same...
+    if (s.indexOf(s[idx]) === s.lastIndexOf(s[idx])) {
+      // return the index of that unique character
+      return idx;
+    }
+  }
+
+  // If no character appeared exactly once...
+  return -1;
+};
