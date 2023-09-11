fd556617373c8071b35c005ad614ef44526f5277
miga-choi <miga.choi.1004@gmail.com>
2023-09-12 00:14:08 +0900

    leetcode: "387_first_unique_character_in_a_string" add python solution

---
+++ leetcode/algorithms/387_first_unique_character_in_a_string/main.py
@@ -0,0 +1,23 @@
+import collections
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        for c in s:
+            if s.index(c) == s.rindex(c):
+                return s.index(c)
+        return -1
+
+
+    # Best Solution
+    def bestSolution(self, s: str) -> int:
+        hset = collections.Counter(s)
+
+        # Traverse the string from the beginning...
+        for idx in range(len(s)):
+            # If the count is equal to 1, it is the first distinct character in the list.
+            if hset[s[idx]] == 1:
+                return idx
+
+        # If no character appeared exactly once...
+        return -1
