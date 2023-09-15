c6e5e2a101255ca3b55e41b7eeaa8076b5bb4f64
miga-choi <miga.choi.1004@gmail.com>
2023-09-16 01:10:51 +0900

    leetcode: "392_is_subsequence" add java solution

---
+++ leetcode/algorithms/392_is_subsequence/main.java
@@ -0,0 +1,38 @@
+class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        while (s.length() > 0) {
+            int index = t.indexOf(s.charAt(0));
+            if (index == -1) {
+                return false;
+            }
+            t = t.substring(index + 1);
+            s = s.substring(1, s.length());
+        }
+        return true;
+    }
+
+
+    // Best Solution
+    public boolean bestSolution(String s, String t) {
+        int i = 0, j = 0;
+        int n = t.length();
+        int m = s.length();
+        char ss[] = s.toCharArray();
+        char tt[] = t.toCharArray();
+
+        if (m < 1)
+            return true;
+
+        while (i < n) {
+            if (tt[i] == ss[j]) {
+                j++;
+            }
+            i++;
+
+            if (j == m)
+                return true;
+        }
+
+        return false;
+    }
+}
