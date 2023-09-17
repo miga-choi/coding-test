a82252081b24f9ba148974a943be12154aaf436f
miga-choi <miga.choi.1004@gmail.com>
2023-09-17 23:19:20 +0900

    leetcode: "401_binary_watch" add javascript solution

---
+++ leetcode/algorithms/401_binary_watch/main.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number} turnedOn
+ * @return {string[]}
+ */
+var readBinaryWatch = function (turnedOn) {
+  const result = [];
+
+  // hours
+  for (let h = 0; h < 12; h++) {
+    // minutes
+    for (let m = 0; m < 60; m++) {
+      const hCount = h.toString(2).replaceAll("0", "").length;
+      const mCount = m.toString(2).replaceAll("0", "").length;
+      if (hCount + mCount === turnedOn) {
+        result.push(`${h}:${m > 9 ? m : `0${m}`}`);
+      }
+    }
+  }
+
+  return result;
+};
+
+
+// Best Solution
+var bestSolution = function (turnedOn) {
+  const times = [];
+  for (let h = 0; h < 12; h++) {
+    for (let m = 0; m < 60; m++) {
+      const hOnes = h ? h.toString(2).match(/1/g).length : 0;
+      const mOnes = m ? m.toString(2).match(/1/g).length : 0;
+      if (hOnes + mOnes === turnedOn) {
+        times.push(`${h}:${m < 10 ? `0${m}` : m}`);
+      }
+    }
+  }
+  return times;
+};
