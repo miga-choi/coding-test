3b223a7c40a2e412db60d2e105eeccd085d03fb6
miga-choi <miga.choi.1004@gmail.com>
2023-09-20 00:03:39 +0900

    leetcode: "404_sum_of_left_leaves" add java solution

---
+++ leetcode/algorithms/404_sum_of_left_leaves/main.java
@@ -0,0 +1,90 @@
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class SumOfLeftLeaves {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int result = 0;
+
+        if (root.left != null) {
+            if (root.left.left == null && root.left.right == null) {
+                result += root.left.val;
+            } else {
+                result += sumOfLeftLeaves(root.left);
+            }
+        }
+
+        result += sumOfLeftLeaves(root.right);
+
+        return result;
+    }
+
+
+    // Best Solution
+    // Recursive
+    public int bestSolution1(TreeNode root) {
+        if (root == null)
+            return 0;
+        int ans = 0;
+        if (root.left != null) {
+            if (root.left.left == null && root.left.right == null)
+                ans += root.left.val;
+            else
+                ans += bestSolution1(root.left);
+        }
+        ans += bestSolution1(root.right);
+
+        return ans;
+    }
+
+    // Iterative
+    public int bestSolution2(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int ans = 0;
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+
+        while (!stack.empty()) {
+            TreeNode node = stack.pop();
+            if (node.left != null) {
+                if (node.left.left == null && node.left.right == null)
+                    ans += node.left.val;
+                else
+                    stack.push(node.left);
+            }
+            if (node.right != null) {
+                if (node.right.left != null || node.right.right != null)
+                    stack.push(node.right);
+            }
+        }
+
+        return ans;
+    }
+}
