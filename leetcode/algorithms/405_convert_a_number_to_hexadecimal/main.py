0b1adc54c2c2a4f022adc272cc97bb4e27e7f25b
miga-choi <miga.choi.1004@gmail.com>
2023-09-23 00:14:03 +0900

    leetcode: "405_convert_a_number_to_hexadecimal" add python solution

---
+++ leetcode/algorithms/405_convert_a_number_to_hexadecimal/main.py
@@ -0,0 +1,54 @@
+import math
+
+
+class Solution:
+    def toHex(self, num: int) -> str:
+        hex: slice = [
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+        ]
+
+        if num == 0:
+            return "0"
+
+        if num < 0:
+            num += math.pow(2, 32)
+
+        result = ""
+
+        while num > 0:
+            remainder = num % 16
+            result = hex[int(remainder)] + result
+            num = math.floor(num / 16)
+
+        return result
+
+
+    # Best Solution
+    def bestSolution(self, num: int) -> str:
+        if num == 0:
+            return "0"
+        map = "0123456789abcdef"
+        result = ""
+        # if negative (two's compliment)
+        if num < 0:
+            num += 2**32
+        while num > 0:
+            digit = num % 16
+            num = (num - digit) // 16
+            result += str(map[digit])
+        return result[::-1]
