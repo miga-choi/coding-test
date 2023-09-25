497f87f1a1e2a17e41542703ca72097989d46204
miga-choi <miga.choi.1004@gmail.com>
2023-09-26 00:09:22 +0900

    leetcode: "412_fizz_buzz" add javascript solution

---
+++ leetcode/algorithms/412_fizz_buzz/main.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function (n) {
+  const result = new Array();
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(`${i}`);
+    }
+  }
+
+  return result;
+};
+
+
+// Best Solution
+var bestSolution = function (n) {
+  return new Array(n)
+    .fill(0)
+    .map((a, i) => (++i % 3 ? "" : "Fizz") + (i % 5 ? "" : "Buzz") || "" + i);
+};
