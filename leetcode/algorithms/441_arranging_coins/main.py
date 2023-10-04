bd934ed640fed7ef7b99f1df1bb636232c9c34a5
miga-choi <miga.choi.1004@gmail.com>
2023-10-05 00:33:04 +0900

    leetcode: "441_arranging_coins" add python solution

---
+++ leetcode/algorithms/441_arranging_coins/main.py
@@ -0,0 +1,19 @@
+import math
+
+
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        stairs = 1
+        while True:
+            n -= stairs
+            if n == 0:
+                return stairs
+            elif n < 0:
+                return stairs - 1
+            stairs += 1
+
+
+    # Best Solution
+    def bestSolution(self, n: int) -> int:
+        # int is simply to floor the floating point so we get the largest integer smaller than the expression
+        return int((math.sqrt(8 * n + 1) - 1) / 2)
