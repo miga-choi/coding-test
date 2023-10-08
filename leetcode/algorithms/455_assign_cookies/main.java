fda67da6638da382e0d755b3fdfafdb023575f48
miga-choi <miga.choi.1004@gmail.com>
2023-10-09 00:06:39 +0900

    leetcode: "455_assign_cookies" add java solution

---
+++ leetcode/algorithms/455_assign_cookies/main.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+class AssignCookies {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int gi = 0;
+        for (int sv : s) {
+            if (gi >= g.length) {
+                break;
+            }
+            if (sv >= g[gi]) {
+                gi++;
+            }
+        }
+        return gi;
+    }
+
+
+    // Best Solution
+    public int bestSolution(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        for (int j = 0; i < g.length && j < s.length; j++) {
+            if (g[i] <= s[j]) {
+                i++;
+            }
+        }
+        return i;
+    }
+}
