73894dd46166eae9d419db89c09c62d659bc062b
miga-choi <miga.choi.1004@gmail.com>
2023-10-11 00:04:14 +0900

    leetcode: "456_132_pattern" add java solution

---
+++ leetcode/algorithms/456_132_pattern/main.java
@@ -0,0 +1,85 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+class _132Pattern {
+    public boolean find132pattern(int[] nums) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int third = -Double.valueOf(Math.pow(10, 9)).intValue();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] < third) {
+                return true;
+            }
+            while (!stack.empty() && stack.lastElement() < nums[i]) {
+                third = stack.pop();
+            }
+            stack.add(nums[i]);
+        }
+        return false;
+    }
+
+
+    // Best Solution
+    // I. Naive O(n^3) solution
+    public boolean bestSolution1(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] < nums[k] && nums[k] < nums[j])
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // II. Improved O(n^2) solution
+    public boolean bestSolution2(int[] nums) {
+        for (int j = 0, min = Integer.MAX_VALUE; j < nums.length; j++) {
+            min = Math.min(nums[j], min);
+            if (min == nums[j])
+                continue;
+
+            for (int k = nums.length - 1; k > j; k--) {
+                if (min < nums[k] && nums[k] < nums[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    // III. Optimized O(n) solution
+    public boolean bestSolution3(int[] nums) {
+        int[] arr = Arrays.copyOf(nums, nums.length);
+
+        for (int i = 1; i < nums.length; i++) {
+            arr[i] = Math.min(nums[i - 1], arr[i - 1]);
+        }
+
+        for (int j = nums.length - 1, top = nums.length; j >= 0; j--) {
+            if (nums[j] <= arr[j])
+                continue;
+            while (top < nums.length && arr[top] <= arr[j])
+                top++;
+            if (top < nums.length && nums[j] > arr[top])
+                return true;
+            arr[--top] = nums[j];
+        }
+
+        return false;
+    }
+
+    // IV -- One-pass O(n) solution
+    public boolean bestSolution4(int[] nums) {
+        int n = nums.length, top = n, third = Integer.MIN_VALUE;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] < third)
+                return true;
+            while (top < n && nums[i] > nums[top])
+                third = nums[top++];
+            nums[--top] = nums[i];
+        }
+
+        return false;
+    }
+}
