a4c5f043af62a67648d4000208928a3966ab15fc
miga-choi <miga.choi.1004@gmail.com>
2023-10-12 19:07:33 +0900

    leetcode: "459_repeated_substring_pattern" add javascript solution

---
+++ leetcode/algorithms/459_repeated_substring_pattern/main.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var repeatedSubstringPattern = function (s) {
+  return s.slice(1, s.length).concat(s.slice(0, s.length - 1)).indexOf(s) >= 0;
+};
+
+
+// Best Solution
+var bestSolution = function (s) {
+  let size = s.length;
+  let sFold = s.substr(1, size) + s.substr(0, size - 1);
+  return sFold.indexOf(s) != -1;
+};
