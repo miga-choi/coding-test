4bd25b6b828582edcb724ba99fd611a593451c41
miga-choi <miga.choi.1004@gmail.com>
2023-10-16 20:18:47 +0900

    leetcode: "463_island_perimeter" add javascript solution

---
+++ leetcode/algorithms/463_island_perimeter/main.js
@@ -0,0 +1,51 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var islandPerimeter = function (grid) {
+  let result = 0;
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (!grid[i][j]) {
+        continue;
+      }
+
+      result += 4;
+
+      if (j > 0 && grid[i][j - 1]) {
+        result -= 2;
+      }
+      if (i > 0 && grid[i - 1][j]) {
+        result -= 2;
+      }
+    }
+  }
+
+  return result;
+};
+
+
+// Best Solution
+var bestSolution = function (grid) {
+  const rows = grid.length;
+  const cols = grid[0].length;
+
+  var perimeter = 0;
+
+  for (var row = 0; row < rows; row++) {
+    for (var col = 0; col < cols; col++) {
+      if (!grid[row][col]) continue;
+
+      perimeter += 4;
+
+      // abstract the number of adjacent island
+      if (row > 0 && grid[row - 1][col]) perimeter--;
+      if (col > 0 && grid[row][col - 1]) perimeter--;
+      if (row < rows - 1 && grid[row + 1][col]) perimeter--;
+      if (col < cols - 1 && grid[row][col + 1]) perimeter--;
+    }
+  }
+
+  return perimeter;
+};
