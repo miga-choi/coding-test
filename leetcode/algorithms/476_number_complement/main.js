1e7af7e4b05581a828059d6ffb7c55465441617e
miga-choi <miga.choi.1004@gmail.com>
2023-10-18 01:11:57 +0900

    leetcode: "476_number_complement" add javascript solution

---
+++ leetcode/algorithms/476_number_complement/main.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var findComplement = function (num) {
+  const binary = num.toString(2).split("");
+  let result = "";
+
+  for (const i of binary) {
+    if (i === "1") {
+      result += "0";
+    } else {
+      result += "1";
+    }
+  }
+
+  return parseInt(result, 2);
+};
+
+
+// Best Solution
+var bestSolution = function (num) {
+  let d = 2;
+  while (d <= num) {
+    d *= 2;
+  }
+  return d - num - 1;
+};
