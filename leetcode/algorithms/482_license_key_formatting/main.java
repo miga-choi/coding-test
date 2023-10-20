914ac3bea77bc338bf377eaacb05ed219e52672c
miga-choi <miga.choi.1004@gmail.com>
2023-10-21 00:09:52 +0900

    leetcode: "482_license_key_formatting" add java solution

---
+++ leetcode/algorithms/482_license_key_formatting/main.java
@@ -0,0 +1,28 @@
+class LicenseKeyFormatting {
+    public String licenseKeyFormatting(String s, int k) {
+        s = s.replaceAll("-", "").toUpperCase();
+        String result = "";
+
+        for (int i = s.length(); i >= 0; i -= k) {
+            if (i > k) {
+                result = "-" + s.substring(i - k, i) + result;
+            } else {
+                result = s.substring(0, i) + result;
+            }
+        }
+
+        return result;
+    }
+
+
+    // Best Solution
+    public String bestSolution(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != '-') {
+                sb.append(sb.length() % (k + 1) == k ? '-' : "").append(s.charAt(i));
+            }
+        }
+        return sb.reverse().toString().toUpperCase();
+    }
+}
