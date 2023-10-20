3b425e72f61fe8dcbb35118139b725f9806585dc
miga-choi <miga.choi.1004@gmail.com>
2023-10-21 00:08:39 +0900

    leetcode: "482_license_key_formatting" add python solution

---
+++ leetcode/algorithms/482_license_key_formatting/main.py
@@ -0,0 +1,19 @@
+class Solution:
+    def licenseKeyFormatting(self, s: str, k: int) -> str:
+        s = s.replace("-", "").upper()
+        result = ""
+
+        for i in range(len(s) - 1, -1, -k):
+            if i >= k:
+                result = "-" + s[-k:] + result
+            else:
+                result = s[-k:] + result
+            s = s[: i + 1 - k]
+
+        return result
+
+
+    # Best Solution
+    def bestSolution(self, s: str, k: int) -> str:
+        s = s.replace("-", "").upper()[::-1]
+        return "-".join(s[i : i + k] for i in range(0, len(s), k))[::-1]
