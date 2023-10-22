f46353cdcd1fc8405df38f1c15d6128dcb752401
miga-choi <miga.choi.1004@gmail.com>
2023-10-23 00:10:35 +0900

    leetcode: "485_max_consecutive_ones" add python solution

---
+++ leetcode/algorithms/485_max_consecutive_ones/main.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        result = 0
+        curr = 0
+
+        for num in nums:
+            if num == 1:
+                curr += 1
+            else:
+                if curr > result:
+                    result = curr
+                curr = 0
+
+        return max(result, curr)
+
+
+    # Best Solution
+    def bestSolution(self, nums: List[int]) -> int:
+        cnt = 0
+        ans = 0
+        for num in nums:
+            if num == 1:
+                cnt += 1
+                ans = max(ans, cnt)
+            else:
+                cnt = 0
+        return ans
