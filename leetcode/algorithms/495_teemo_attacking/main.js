12e237f414b0a82991087420d9653e3f11f2503a
miga-choi <miga.choi.1004@gmail.com>
2023-10-26 00:20:24 +0900

    leetcode: "495_teemo_attacking" add javascript solution

---
+++ leetcode/algorithms/495_teemo_attacking/main.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} timeSeries
+ * @param {number} duration
+ * @return {number}
+ */
+var findPoisonedDuration = function (timeSeries, duration) {
+  let result = 0;
+  for (let i = 1; i < timeSeries.length; i++) {
+    if (timeSeries[i] - timeSeries[i - 1] >= duration) {
+      result += duration;
+    } else {
+      result += timeSeries[i] - timeSeries[i - 1];
+    }
+  }
+  result += duration;
+  return result;
+};
+
+
+// Best Solution
+var bestSolution = function (timeSeries, duration) {
+  let ans = 0;
+
+  for (let i = 0, len = timeSeries.length; i < len; i++) {
+    if (i === len - 1) {
+      ans += duration;
+      continue;
+    }
+
+    let curItem = timeSeries[i], nextItem = timeSeries[i + 1];
+
+    if (curItem + duration <= nextItem) ans += duration;
+    else ans += nextItem - curItem;
+  }
+
+  return ans;
+};
