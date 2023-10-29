c15caaa7ab5cbfb8f825e4223f722f2a88cc48ac
miga-choi <miga.choi.1004@gmail.com>
2023-10-30 00:09:52 +0900

    leetcode: "500_keyboard_row" add javascript solution

---
+++ leetcode/algorithms/500_keyboard_row/main.js
@@ -0,0 +1,56 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var findWords = function (words) {
+  const rows = ["qwertyuiop", "asdfghjkl", "zxcvbnm"];
+  const result = [];
+  for (let word of words) {
+    let row = -1;
+    for (const c of word) {
+      if (rows[0].indexOf(c.toLowerCase()) >= 0) {
+        if (row === -1) {
+          row = 0;
+        } else if (row !== 0) {
+          row = -1;
+          break;
+        }
+      } else if (rows[1].indexOf(c.toLowerCase()) >= 0) {
+        if (row === -1) {
+          row = 1;
+        } else if (row !== 1) {
+          row = -1;
+          break;
+        }
+      } else if (rows[2].indexOf(c.toLowerCase()) >= 0) {
+        if (row === -1) {
+          row = 2;
+        } else if (row !== 2) {
+          row = -1;
+          break;
+        }
+      }
+    }
+    if (row > -1) {
+      result.push(word);
+    }
+  }
+  return result;
+};
+
+
+// Best Solution
+var bestSolution = function (words) {
+  return words.filter((w) => {
+    // remove word from array if it fails matching all three rows
+    if (
+      !/^[qwertyuiop]*$/i.test(w) &&
+      !/^[asdfghjkl]*$/i.test(w) &&
+      !/^[zxcvbnm]*$/i.test(w)
+    ) {
+      return false;
+    }
+
+    return true;
+  });
+};
