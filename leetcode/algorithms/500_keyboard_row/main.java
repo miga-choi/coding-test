24805a0b2a994be5afb91ba9f4679ff332f08608
miga-choi <miga.choi.1004@gmail.com>
2023-10-31 20:29:04 +0900

    leetcode: "500_keyboard_row" add java solution

---
+++ leetcode/algorithms/500_keyboard_row/main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+class KeyboardRow {
+    public String[] findWords(String[] words) {
+        List<String> result = new ArrayList<String>();
+        for (String word : words) {
+            if (word.toLowerCase().matches("[qwertyuiop]*") || word.toLowerCase().matches("[asdfghjkl]*") || word.toLowerCase().matches("[zxcvbnm]*")) {
+                result.add(word);
+            }
+        }
+        return result.toArray(new String[result.size()]);
+    }
+
+
+    // Best Solution
+    public String[] bestSolution(String[] words) {
+        return Stream.of(words).filter(s -> s.toLowerCase().matches("[qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*")).toArray(String[]::new);
+    }
+}
