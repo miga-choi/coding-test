54121134ae690ba385e26fd69d04470177ed6e4f
miga-choi <miga.choi.1004@gmail.com>
2023-11-03 00:23:46 +0900

    leetcode: "504_base_7" add javascript solution

---
+++ leetcode/algorithms/504_base_7/main.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function (num) {
+  return num.toString(7);
+};
+
+
+// Best Solution
+var bestSolution1 = function (num) {
+  return num.toString(7);
+};
+
+var bestSolution2 = (num) => num.toString(7);
