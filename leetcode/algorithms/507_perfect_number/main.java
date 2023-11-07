c8da156d57d0889bb598bd410665133187ae485e
miga-choi <miga.choi.1004@gmail.com>
2023-11-08 00:03:44 +0900

    leetcode: "507_perfect_number" add java solution

---
+++ leetcode/algorithms/507_perfect_number/main.java
@@ -0,0 +1,32 @@
+class PerfectNumber {
+    public boolean checkPerfectNumber(int num) {
+        if (num == 1) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 1; i < num / 2; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+        return sum * 2 == num;
+    }
+
+
+    // Best Solution
+    public boolean bestSolution(int num) {
+        if (num == 1) {
+            return false;
+        }
+
+        int sum = 0;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                sum += i + num / i;
+            }
+        }
+        sum++;
+
+        return sum == num;
+    }
+}
