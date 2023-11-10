b43e744d19fdccfdafd75db4d9cb05a8fc6f8b96
miga-choi <miga.choi.1004@gmail.com>
2023-11-11 04:18:54 +0900

    leetcode: "511_game_play_analysis_1" add oracle solution

---
+++ leetcode/algorithms/511_game_play_analysis_1/main.oracle.sql
@@ -0,0 +1,12 @@
+-- /* Write your PL/SQL query statement below */
+SELECT player_id AS "player_id",
+    TO_CHAR(MIN(event_date), 'YYYY-MM-DD') AS "first_login"
+FROM Activity
+GROUP BY player_id;
+
+
+-- Best Solution
+SELECT player_id,
+    TO_CHAR(MIN(event_date), 'YYYY-MM-DD') AS first_login
+FROM Activity
+GROUP BY player_id
