1cb67d845dcd0755dd893218c0de709471fc2f2e
miga-choi <miga.choi.1004@gmail.com>
2023-11-12 00:25:09 +0900

    leetcode: "511_game_play_analysis_1" add mssql solution

---
+++ leetcode/algorithms/511_game_play_analysis_1/main.mssql.sql
@@ -0,0 +1,12 @@
+-- /* Write your T-SQL query statement below */
+SELECT player_id,
+    MIN(event_date) AS first_login
+FROM Activity
+GROUP BY player_id;
+
+
+-- Best Solution
+SELECT player_id,
+    TO_CHAR(MIN(event_date), 'YYYY-MM-DD') AS first_login
+FROM Activity
+GROUP BY player_id
