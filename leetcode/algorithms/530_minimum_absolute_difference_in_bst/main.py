cab395d911eb8bd19729b5d69936081833bc412d
miga-choi <miga.choi.1004@gmail.com>
2023-11-18 17:00:54 +0900

    leetcode: "530_minimum_absolute_difference_in_bst" add python solution

---
+++ leetcode/algorithms/530_minimum_absolute_difference_in_bst/main.py
@@ -0,0 +1,54 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class Solution:
+    class TreeNode:
+        def __init__(self, val=0, left=None, right=None):
+            self.val = val
+            self.left = left
+            self.right = right
+
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        array = []
+        min = 10**5
+
+        def bstToArray(node):
+            if node != None:
+                bstToArray(node.left)
+                array.append(node.val)
+                bstToArray(node.right)
+
+        bstToArray(root)
+
+        for i in range(1, len(array)):
+            diff = abs(array[i - 1] - array[i])
+            if diff < min:
+                min = diff
+
+        return min
+
+
+    # Best Solution
+    def bestSolution1(self, root: Optional[TreeNode]) -> int:
+        def fn(node, lo, hi):
+            if not node:
+                return hi - lo
+            left = fn(node.left, lo, node.val)
+            right = fn(node.right, node.val, hi)
+            return min(left, right)
+
+        return fn(root, float("-inf"), float("inf"))
+
+    def bestSolution2(self, root: Optional[TreeNode]) -> int:
+        L = []
+
+        def dfs(node):
+            if node.left:
+                dfs(node.left)
+            L.append(node.val)
+            if node.right:
+                dfs(node.right)
+
+        dfs(root)
+        return min(b - a for a, b in zip(L, L[1:]))
