6b20541fb523d6a4a541c2b56c7c1b9ba90e455e
miga-choi <miga.choi.1004@gmail.com>
2023-11-21 23:22:39 +0900

    leetcode: "543_diameter_of_binary_tree" add javascript solution

---
+++ leetcode/algorithms/543_diameter_of_binary_tree/main.js
@@ -0,0 +1,53 @@
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function (root) {
+  let result = 0;
+
+  getMaxNode(root);
+
+  function getMaxNode(root) {
+    if (!root) {
+      return 0;
+    }
+
+    const left = getMaxNode(root.left);
+    const right = getMaxNode(root.right);
+
+    result = Math.max(result, left + right);
+
+    return Math.max(left, right) + 1;
+  }
+
+  return result;
+};
+
+
+// Best Solution
+var bestSolution = function (root) {
+  let diameter = 0;
+
+  dfs(root);
+
+  return diameter;
+
+  function dfs(node) {
+    if (!node) return 0;
+
+    const left = dfs(node.left);
+    const right = dfs(node.right);
+
+    // update diameter at every node
+    diameter = Math.max(diameter, left + right);
+
+    // update the largest number of edge so far
+    return 1 + Math.max(left, right);
+  }
+};
