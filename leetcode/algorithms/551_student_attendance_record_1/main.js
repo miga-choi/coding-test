1dd902bce7bc2aea2ff9f4f4a7e89a0baa6751b1
miga-choi <miga.choi.1004@gmail.com>
2023-11-23 00:12:00 +0900

    leetcode: "551_student_attendance_record_1" add javascript solution

---
+++ leetcode/algorithms/551_student_attendance_record_1/main.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkRecord = function (s) {
+  if (s.split('').filter((v) => v === 'A').length > 1) {
+    return false;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (i > 1 && s[i] === 'L' && s[i - 1] === 'L' && s[i - 2] === 'L') {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+// Best Solution
+var bestSolution = function (s) {
+  let map = new Map();
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === 'A') {
+      map.set(s[i], map.get(s[i]) === undefined ? 1 : map.get(s[i]) + 1);
+      if (map.get(s[i]) >= 2) return false;
+    } else if (s[i] === 'L' && s[i + 1] === 'L' && s[i + 2] === 'L') {
+      return false;
+    }
+  }
+
+  return true;
+};
