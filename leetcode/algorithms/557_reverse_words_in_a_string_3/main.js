bee52d51b17a73ddf87b992b26806b49c22ac63c
miga-choi <miga.choi.1004@gmail.com>
2023-11-25 00:31:22 +0900

    leetcode: "557_reverse_words_in_a_string_3" add javascript solution

---
+++ leetcode/algorithms/557_reverse_words_in_a_string_3/main.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+  const sArray = s.split(' ');
+  for (let i = 0; i < sArray.length; i++) {
+    sArray[i] = sArray[i].split('').reverse().join('');
+  }
+  return sArray.join(' ');
+};
+
+
+// Best Solution
+var bestSolution = function (s) {
+  const words = s.split(' ');
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].split('').reverse().join('');
+  }
+  return words.join(' ');
+};
