63c487b50202b67fecacb456e9a0447a01be039c
miga-choi <miga.choi.1004@gmail.com>
2023-11-26 00:18:01 +0900

    leetcode: "557_reverse_words_in_a_string_3" add java solution

---
+++ leetcode/algorithms/557_reverse_words_in_a_string_3/main.java
@@ -0,0 +1,35 @@
+class ReverseWordsInAStringIII {
+
+  public String reverseWords(String s) {
+    String[] sArray = s.split(" ");
+    for (int i = 0; i < sArray.length; i++) {
+      sArray[i] = new StringBuffer(sArray[i]).reverse().toString();
+    }
+    return String.join(" ", sArray);
+  }
+
+
+  // Best Solution
+  public String bestSolution(String s) {
+    char[] ca = s.toCharArray();
+    for (int i = 0; i < ca.length; i++) {
+      if (ca[i] != ' ') { // when i is a non-space
+        int j = i;
+        while (j + 1 < ca.length && ca[j + 1] != ' ') {
+          j++;
+        } // move j to the end of the word
+        reverse(ca, i, j);
+        i = j;
+      }
+    }
+    return new String(ca);
+  }
+
+  private void reverse(char[] ca, int i, int j) {
+    for (; i < j; i++, j--) {
+      char tmp = ca[i];
+      ca[i] = ca[j];
+      ca[j] = tmp;
+    }
+  }
+}
