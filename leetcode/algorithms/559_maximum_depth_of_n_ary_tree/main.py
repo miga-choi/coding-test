952c44607bc71daec45c50f86592976edb536a7c
miga-choi <miga.choi.1004@gmail.com>
2023-11-28 11:13:20 +0900

    leetcode: "559_maximum_depth_of_n_ary_tree" add python solution

---
+++ leetcode/algorithms/559_maximum_depth_of_n_ary_tree/main.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if root == None:
+            return 0
+        maxNum = 0
+        for child in root.children:
+            maxNum = max(maxNum, self.maxDepth(child))
+        return maxNum + 1
+
+
+    # Best Solution
+    def bestSolution(self, root: 'Node') -> int:
+        # Base case
+        if root == None:
+            return 0
+        # Depth level of the tree
+        depth = 0
+
+        # Loops through children array
+        for child in root.children:
+            # Compares current depth of depth with a new level of depth
+            # Sets the biggest value to variable depth
+            depth = max(depth, self.maxDepth(child))
+
+        # As going deeper into the tree increases depth by 1
+        print('root ' + str(root.val) + ' depth ' + str(depth + 1))
+        return depth + 1
