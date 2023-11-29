9092f2bcb28279827928215dd3055f33e7c7d9db
miga-choi <miga.choi.1004@gmail.com>
2023-11-29 20:47:54 +0900

    leetcode: "561_array_partition" add javascript solution

---
+++ leetcode/algorithms/561_array_partition/main.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function (nums) {
+  let result = 0;
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 2 === 0) {
+      result += nums[i];
+    }
+  }
+  return result;
+};
+
+
+// Best Solution
+var bestSolution = function (nums) {
+  // sort and sum up the minimum of the pairs
+  return nums
+    .sort((a, b) => a - b)
+    .reduce((sum, cur, i) => (i % 2 === 0 ? (sum += cur) : sum), 0);
+};
