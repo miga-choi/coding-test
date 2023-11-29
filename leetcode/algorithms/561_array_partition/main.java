98679427b1d5a9737d76f6e6b74eb59a4cc9a5f5
miga-choi <miga.choi.1004@gmail.com>
2023-11-30 00:06:19 +0900

    leetcode: "561_array_partition" add java solution

---
+++ leetcode/algorithms/561_array_partition/main.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+class ArrayPartition {
+    public int arrayPairSum(int[] nums) {
+        int result = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                result += nums[i];
+            }
+        }
+        return result;
+    }
+
+
+    // Best Solution
+    public int bestSolution1(int[] nums) {
+        Arrays.sort(nums);
+        int result = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            result += nums[i];
+        }
+        return result;
+    }
+
+    public int bestSolution2(int[] nums) {
+        int[] exist = new int[20001];
+        for (int i = 0; i < nums.length; i++) {
+            exist[nums[i] + 10000]++;
+        }
+        int sum = 0;
+        boolean odd = true;
+        for (int i = 0; i < exist.length; i++) {
+            while (exist[i] > 0) {
+                if (odd) {
+                    sum += i - 10000;
+                }
+                odd = !odd;
+                exist[i]--;
+            }
+        }
+        return sum;
+    }
+}
