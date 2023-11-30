187016ab2d2c947057607a50e8e06758ed7d0f12
miga-choi <miga.choi.1004@gmail.com>
2023-12-01 00:04:32 +0900

    leetcode: "563_binary_tree_tilt" add javascript solution

---
+++ leetcode/algorithms/563_binary_tree_tilt/main.js
@@ -0,0 +1,69 @@
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findTilt = function (root) {
+  let result = 0;
+  /**
+   * @param {TreeNode} node
+   * @return {number}
+   */
+  function getVal(node) {
+    if (node) {
+      const left = getVal(node.left);
+      const right = getVal(node.right);
+      result += Math.abs(left - right);
+      return left + right + node.val;
+    } else {
+      return 0;
+    }
+  }
+  getVal(root);
+  return result;
+};
+
+
+// Best Solution
+// Best Solution 1: DFS
+var bestSolution1 = function (root) {
+  const tilt = { val: 0 };
+  dfs(root, tilt);
+  return tilt.val;
+};
+
+function dfs(root, tilt) {
+  if (!root) return 0;
+  let left = dfs(root.left, tilt);
+  let right = dfs(root.right, tilt);
+  tilt.val += Math.abs(left - right);
+  return root.val + left + right;
+}
+
+// Best Solution 2
+var bestSolution2 = function (root) {
+  let tot = 0;
+
+  findSum(root);
+
+  return tot;
+
+  function findSum(node) {
+    // base case
+    if (node == null) return 0;
+
+    const left = findSum(node.left);
+    const right = findSum(node.right);
+
+    const sum = left + right;
+
+    tot += Math.abs(left - right);
+
+    return sum + node.val;
+  }
+};
