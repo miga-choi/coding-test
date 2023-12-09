28c54e7411916b9b0214aa4617e383d38057a621
miga-choi <miga.choi.1004@gmail.com>
2023-12-10 01:35:53 +0900

    leetcode: "577_employee_bonus" add mssql solution

---
+++ leetcode/algorithms/577_employee_bonus/main.mssql.sql
@@ -0,0 +1,22 @@
+-- /* Write your T-SQL query statement below */
+SELECT Employee.name,
+    Bonus.bonus
+FROM Employee
+    LEFT OUTER JOIN Bonus ON Employee.empId = Bonus.empId
+WHERE ISNULL(Bonus.bonus, 0) < 1000;
+
+
+-- /* Best Solution 1: Basic */
+SELECT e.name,
+    b.bonus
+FROM Employee e
+    LEFT JOIN Bonus b USING(empId)
+WHERE bonus < 1000
+    OR bonus IS NULL;
+
+-- /* Best Solution 2: ISNULL */
+SELECT e.name,
+    b.bonus
+FROM Employee e
+    LEFT JOIN bonus b USING(empId)
+WHERE ISNULL(b.bonus, 0) < 1000;
