23ce093bb1a813e15b5c38357570476dea9d97fb
miga-choi <miga.choi.1004@gmail.com>
2023-12-10 01:34:28 +0900

    leetcode: "577_employee_bonus" add oracle solution

---
+++ leetcode/algorithms/577_employee_bonus/main.oracle.sql
@@ -0,0 +1,16 @@
+-- /* Write your PL/SQL query statement below */
+SELECT employee.name AS name,
+    bonus.bonus AS bonus
+FROM Employee employee,
+    Bonus bonus
+WHERE employee.empId = bonus.empId(+)
+    AND NVL(bonus.bonus, 0) < 1000;
+
+
+--  /* Best Solution */
+SELECT E.name,
+    B.bonus
+FROM Employee E
+    LEFT OUTER JOIN Bonus B ON E.empId = B.empId
+WHERE B.bonus < 1000
+    OR bonus IS NULL
