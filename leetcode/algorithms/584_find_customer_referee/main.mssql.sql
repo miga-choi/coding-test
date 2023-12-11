b35c8e8d75df312d99a160d222f6948a39a55d3e
miga-choi <miga.choi.1004@gmail.com>
2023-12-12 00:38:27 +0900

    leetcode: "584_find_customer_referee" add mssql solution

---
+++ leetcode/algorithms/584_find_customer_referee/main.mssql.sql
@@ -0,0 +1,11 @@
+-- /* Write your T-SQL query statement below */
+SELECT name
+FROM Customer
+WHERE ISNULL(referee_id, 0) <> 2;
+
+
+-- /* Best Solution */
+SELECT name
+FROM Customer
+WHERE referee_id IS NULL
+    OR referee_id <> 2;
