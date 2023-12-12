62b3511fac361f0bb9520f2e594b43f42a788d14
miga-choi <miga.choi.1004@gmail.com>
2023-12-13 00:18:00 +0900

    leetcode: "586_customer_placing_the_largest_number_of_orders" add mysql solution

---
+++ leetcode/algorithms/586_customer_placing_the_largest_number_of_orders/main.mysql.sql
@@ -0,0 +1,66 @@
+-- # Write your MySQL query statement below
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(customer_number) DESC
+LIMIT 1;
+
+
+-- # Best Solution
+-- # Best Solution 1
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(customer_number) DESC
+LIMIT 1;
+
+-- # Best Solution 2
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+HAVING COUNT(order_number) = (
+        SELECT COUNT(order_number) cnt
+        FROM Orders
+        GROUP BY customer_number
+        ORDER BY cnt DESC
+        LIMIT 1
+    );
+
+-- # Best Solution 3
+SELECT sub.cust AS customer_number
+FROM (
+        SELECT customer_number AS cust,
+            COUNT(*) AS 'customer_number'
+        FROM Orders
+        GROUP BY customer_number
+        ORDER BY customer_number DESC
+        LIMIT 1
+    ) AS sub;
+
+-- # Best Solution 4
+SELECT customer_number
+FROM (
+        SELECT customer_number,
+            DENSE_RANK() OVER (
+                ORDER BY cnt DESC
+            ) AS ranking
+        FROM (
+                SELECT customer_number,
+                    COUNT(customer_number) AS cnt
+                FROM Orders
+                GROUP BY customer_number
+            ) sub1
+    ) sub2
+WHERE sub2.ranking = 1;
+
+-- # Best Solution 5
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+HAVING COUNT(order_number) = (
+        SELECT COUNT(*) AS cnt
+        FROM Orders
+        GROUP BY customer_number
+        ORDER BY cnt DESC
+        LIMIT 1
+    );
