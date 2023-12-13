82069209c8fb73bc8cd26b25d01c308e273ec9b0
miga-choi <miga.choi.1004@gmail.com>
2023-12-14 00:58:12 +0900

    leetcode: "586_customer_placing_the_largest_number_of_orders" add mssql solution

---
+++ leetcode/algorithms/586_customer_placing_the_largest_number_of_orders/main.mssql.sql
@@ -0,0 +1,18 @@
+-- /* Write your T-SQL query statement below */
+SELECT sub.customer_number AS customer_number
+FROM (
+        SELECT customer_number,
+            DENSE_RANK() OVER(
+                ORDER BY COUNT(customer_number) DESC
+            ) AS D_RNK
+        FROM Orders
+        GROUP BY customer_number
+    ) sub
+WHERE sub.D_RNK = 1;
+
+
+-- /* Best Solution */
+SELECT TOP 1 customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(customer_number) DESC
