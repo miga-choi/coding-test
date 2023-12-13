2b2e52ff2a96e976c366ba6eeb73d3388bbe4120
miga-choi <miga.choi.1004@gmail.com>
2023-12-14 00:56:15 +0900

    leetcode: "586_customer_placing_the_largest_number_of_orders" add oracle solution

---
+++ leetcode/algorithms/586_customer_placing_the_largest_number_of_orders/main.oracle.sql
@@ -0,0 +1,48 @@
+-- /* Write your PL/SQL query statement below */
+SELECT customer_number
+FROM (
+        SELECT customer_number,
+            DENSE_RANK() OVER(
+                ORDER BY COUNT(customer_number) DESC
+            ) AS D_RNK
+        FROM Orders
+        GROUP BY customer_number
+    )
+WHERE D_RNK = 1;
+
+
+-- /* Best Solution */
+-- /* Best Solution 1 */
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+HAVING COUNT(DISTINCT order_number) IN (
+        SELECT MAX(countn)
+        FROM (
+                SELECT COUNT(DISTINCT order_number) AS countn
+                FROM Orders
+                GROUP BY customer_number
+            )
+    );
+
+-- /* Best Solution 2 */
+SELECT customer_number
+FROM (
+        SELECT customer_number,
+            COUNT(customer_number) AS order_count
+        FROM Orders
+        GROUP BY customer_number
+        ORDER BY order_count DESC NULLS LAST
+    )
+WHERE rownum = 1;
+
+-- /* Best Solution 3 */
+SELECT customer_number
+FROM (
+        SELECT customer_number,
+            COUNT(*)
+        FROM Orders
+        GROUP BY customer_number
+        ORDER BY 2 DESC
+    )
+WHERE ROWNUM = 1;
