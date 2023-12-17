264453d13fdc2665ed956c72119b6ae9824697d7
miga-choi <miga.choi.1004@gmail.com>
2023-12-18 00:04:28 +0900

    leetcode: "590_n_ary_tree_postorder_traversal" add java solution

---
+++ leetcode/algorithms/590_n_ary_tree_postorder_traversal/main.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+class NAryTreePostorderTraversal {
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    };
+
+    public List<Integer> postorder(Node root) {
+        return traversal(root, new ArrayList<Integer>());
+    }
+
+    List<Integer> traversal(Node node, List<Integer> list) {
+        if (node != null) {
+            for (Node child : node.children) {
+                traversal(child, list);
+            }
+            list.add(node.val);
+        }
+        return list;
+    }
+
+
+    // Best Solution
+    // Best Solution 1: Iterative
+    public List<Integer> bestSolution1(Node root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+
+        Stack<Node> stack = new Stack<>();
+        stack.add(root);
+
+        while (!stack.isEmpty()) {
+            root = stack.pop();
+            list.add(root.val);
+            for (Node node : root.children) {
+                stack.add(node);
+            }
+        }
+        Collections.reverse(list);
+        return list;
+    }
+
+    // Best Solution 2: Recursive
+    List<Integer> list = new ArrayList<>();
+
+    public List<Integer> bestSolution2(Node root) {
+        if (root == null) {
+            return list;
+        }
+
+        for (Node node : root.children) {
+            postorder(node);
+        }
+
+        list.add(root.val);
+
+        return list;
+    }
+}
