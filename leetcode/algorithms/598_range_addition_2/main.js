bd7c70293722f335213a615213cffb97fca7550f
miga-choi <miga.choi.1004@gmail.com>
2023-12-25 20:53:33 +0900

    leetcode: "598_range_addition_2" add javascript solution

---
+++ leetcode/algorithms/598_range_addition_2/main.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number[][]} ops
+ * @return {number}
+ */
+var maxCount = function (m, n, ops) {
+  let minM = m;
+  let minN = n;
+  for (let i = 0; i < ops.length; i++) {
+    minM = Math.min(minM, ops[i][0]);
+    minN = Math.min(minN, ops[i][1]);
+  }
+  return minM * minN;
+};
+
+
+// Best Solution
+var bestSolution = function (m, n, ops) {
+  var min_row = m;
+  var min_col = n;
+  for (let i = 0; i < ops.length; i++) {
+    if (ops[i][0] < min_row) {
+      min_row = ops[i][0];
+    }
+    if (ops[i][1] < min_col) {
+      min_col = ops[i][1];
+    }
+  }
+  return min_row * min_col;
+};
