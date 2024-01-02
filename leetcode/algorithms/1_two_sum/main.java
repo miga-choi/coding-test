e3656a0bf5db9e960d07dde56b3263e162b66725
miga-choi <miga.choi.1004@gmail.com>
2024-01-03 00:06:05 +0900

    leetcode: "1_two_sum" add java solution

---
+++ leetcode/algorithms/1_two_sum/main.java
@@ -0,0 +1,67 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] {};
+    }
+
+
+    // Best Solution
+    // Best Solution 1: Brute Force
+    public int[] bestSolution1(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] {}; // No solution found
+    }
+
+    // Best Solution 2: Two-pass Hash Table
+    public int[] bestSolution2(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        // Build the hash table
+        for (int i = 0; i < n; i++) {
+            numMap.put(nums[i], i);
+        }
+
+        // Find the complement
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement) && numMap.get(complement) != i) {
+                return new int[] { i, numMap.get(complement) };
+            }
+        }
+
+        return new int[] {}; // No solution found
+    }
+
+    // Best Solution 3: One-pass Hash Table
+    public int[] bestSolution3(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[] { numMap.get(complement), i };
+            }
+            numMap.put(nums[i], i);
+        }
+
+        return new int[] {}; // No solution found
+    }
+}
