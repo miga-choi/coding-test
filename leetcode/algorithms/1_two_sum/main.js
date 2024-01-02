fdf8333fe8334de578e4a3a7944093ff7e08ef89
miga-choi <miga.choi.1004@gmail.com>
2024-01-02 20:36:49 +0900

    leetcode: "1_two_sum" add javascript solution

--- leetcode/algorithms/1_two_sum/main.js
+++ leetcode/algorithms/1_two_sum/main.js
@@ -15,3 +15,33 @@ var twoSum = function (nums, target) {
     }
   }
 };
+
+
+// Best Solution
+// Best Solution 1: Map
+var bestSolution1 = function (nums, target) {
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      return [map.get(target - nums[i]), i];
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+  return [];
+};
+
+// Best Solution 2: Object
+var bestSolution2 = function (nums, target) {
+  let hash = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const n = nums[i];
+    if (hash[target - n] !== undefined) {
+      return [hash[target - n], i];
+    }
+    hash[n] = i;
+  }
+  return [];
+};
