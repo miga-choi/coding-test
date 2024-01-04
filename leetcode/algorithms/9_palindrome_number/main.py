f8ef1ab7eb9351b689abacb64fce3a6501d82110
miga-choi <miga.choi.1004@gmail.com>
2024-01-05 01:24:06 +0900

    leetcode: "9_palindrome_number" add python solution

--- leetcode/algorithms/9_palindrome_number/main.py
+++ leetcode/algorithms/9_palindrome_number/main.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
         result = True
         if x < 0:
             result = False
@@ -14,3 +10,33 @@ class Solution(object):
             if xStr[i] != xStr[len(xStr) - 1 - i]:
                 result = False
         return result
+
+
+    # Best Solution
+    # Best Solution 1: Reversing the Entire Number
+    def bestSolution1(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        reversed_num = 0
+        temp = x
+
+        while temp != 0:
+            digit = temp % 10
+            reversed_num = reversed_num * 10 + digit
+            temp //= 10
+
+        return reversed_num == x
+
+    # Best Solution 2: Reversing Half of the Number
+    def bestSolution2(self, x: int) -> bool:
+        if x < 0 or (x != 0 and x % 10 == 0):
+            return False
+
+        reversed_num = 0
+
+        while x > reversed_num:
+            reversed_num = reversed_num * 10 + x % 10
+            x //= 10
+
+        return x == reversed_num or x == reversed_num // 10
