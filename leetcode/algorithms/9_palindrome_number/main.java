6025d5342cc86d348343bdbb2deebe99f05288ed
miga-choi <miga.choi.1004@gmail.com>
2024-01-06 01:23:04 +0900

    leetcode: "9_palindrome_number" add java solution

---
+++ leetcode/algorithms/9_palindrome_number/main.java
@@ -0,0 +1,47 @@
+class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        String[] xArray = String.valueOf(x).split("");
+        for (int i = 0; i < xArray.length / 2; i++) {
+            if (!xArray[i].equals(xArray[xArray.length - i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    // Best Solution
+    // Best Solution 1: Reversing the Entire Number
+    public boolean bestSolution1(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        long reversed = 0;
+        long temp = x;
+
+        while (temp != 0) {
+            int digit = (int) (temp % 10);
+            reversed = reversed * 10 + digit;
+            temp /= 10;
+        }
+
+        return (reversed == x);
+    }
+
+    // Best Solution 2: Reversing Half of the Number
+    public boolean bestSolution2(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
+            return false;
+        }
+
+        int reversed = 0;
+
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+
+        return (x == reversed) || (x == reversed / 10);
+    }
+}
