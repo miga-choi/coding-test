bef3f5109109819c8e4405013717b2ab6a8e42c6
miga-choi <miga.choi.1004@gmail.com>
2024-01-08 00:41:08 +0900

    leetcode: "13_roman_to_integer" add python solution

--- leetcode/algorithms/13_roman_to_integer/main.py
+++ leetcode/algorithms/13_roman_to_integer/main.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class RomanToInteger:
+    def romanToInt(self, s: str) -> int:
         roman = {
             "I": 1,
             "V": 5,
@@ -29,3 +25,46 @@ class Solution(object):
             result += roman[char]
 
         return result
+
+
+    # Best Solution
+    # Best Solution 1:
+    def bestSolution1(self, s: str) -> int:
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += translations[char]
+        return number
+
+    # Best Solution 2:
+    def bestSolution2(self, s: str) -> int:
+        m = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+
+        ans = 0
+
+        for i in range(len(s)):
+            if i < len(s) - 1 and m[s[i]] < m[s[i + 1]]:
+                ans -= m[s[i]]
+            else:
+                ans += m[s[i]]
+
+        return ans
