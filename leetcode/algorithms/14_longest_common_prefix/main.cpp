b5d9041ed629c110aa26daea9ce1ba70ad809caa
miga-choi <miga.choi.1004@gmail.com>
2024-01-13 00:32:21 +0900

    leetcode: "14_longest_common_prefix" add c++ solution

---
+++ leetcode/algorithms/14_longest_common_prefix/main.cpp
@@ -0,0 +1,74 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class LongestCommonPrefix
+{
+public:
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        for (int i = 0;; i++)
+        {
+            if (strs[0][i] == '\0')
+            {
+                return strs[0];
+            }
+            for (int j = 1; j < strs.size(); j++)
+            {
+                if (strs[j][i] != strs[0][i])
+                {
+                    return strs[0].substr(0, i);
+                }
+            }
+        }
+    }
+
+
+    // Best Solution
+    // Best Solution 1:
+    string bestSolution1(vector<string> &strs)
+    {
+        string ans = "";
+        sort(strs.begin(), strs.end());
+        int n = strs.size();
+        string first = strs[0], last = strs[n - 1];
+        for (int i = 0; i < min(first.size(), last.size()); i++)
+        {
+            if (first[i] != last[i])
+            {
+                return ans;
+            }
+            ans += first[i];
+        }
+        return ans;
+    }
+
+    // Best Solution 2:
+    string bestSolution2(vector<string> &strs)
+    {
+        int n = strs.size();
+        if (n == 0)
+        {
+            return "";
+        }
+
+        string ans = "";
+        sort(begin(strs), end(strs));
+        string a = strs[0];
+        string b = strs[n - 1];
+
+        for (int i = 0; i < a.size(); i++)
+        {
+            if (a[i] == b[i])
+            {
+                ans = ans + a[i];
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return ans;
+    }
+};
