5af51ef2afe5f95ba5e043fc368ba2d67d5ca984
miga-choi <miga.choi.1004@gmail.com>
2024-01-14 15:51:58 +0900

    leetcode: "20_valid_parentheses" add python solution

--- leetcode/algorithms/20_valid_parentheses/main.py
+++ leetcode/algorithms/20_valid_parentheses/main.py
@@ -1,14 +1,23 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        return False
+class validParentheses(object):
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        for c in s:
+            if c == "(":
+                stack.append(")")
+            elif c == "[":
+                stack.append("]")
+            elif c == "{":
+                stack.append("}")
+            else:
+                if len(stack) == 0 or stack.pop() != c:
+                    return False
+
+        return len(stack) <= 0
 
 
     # Best Solution
-    def bestSolution(self, s):
+    def bestSolution(self, s: str) -> bool:
         # 1. if it's the left bracket then we append it to the stack
         # 2. else if it's the right bracket and the stack is empty(meaning no matching left bracket), or the left bracket doesn't match
         # 3. finally check if the stack still contains unmatched left bracket
