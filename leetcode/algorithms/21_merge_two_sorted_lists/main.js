dacb9565dffa1ce1571acc6f26b08a73d1155620
miga-choi <miga.choi.1004@gmail.com>
2024-01-17 00:44:10 +0900

    leetcode: "21_merge_two_sorted_lists" add javascript solution

--- leetcode/algorithms/21_merge_two_sorted_lists/main.js
+++ leetcode/algorithms/21_merge_two_sorted_lists/main.js
@@ -1,11 +1,4 @@
 /**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
  * @param {ListNode} list1
  * @param {ListNode} list2
  * @return {ListNode}
@@ -42,9 +35,24 @@ var mergeTwoLists = function (list1, list2) {
 };
 
 // Best Solution
-var bestSolution = function (l1, l2) {
-  var mergedHead = { val: -1, next: null },
-    crt = mergedHead;
+// Best Solution 1:
+var bestSolution1 = function (l1, l2) {
+  if (!l1) {
+    return l2;
+  } else if (!l2) {
+    return l1;
+  } else if (l1.val <= l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+};
+
+// Best Solution 2:
+var bestSolution12 = function (l1, l2) {
+  var mergedHead = { val: -1, next: null }, crt = mergedHead;
   while (l1 && l2) {
     if (l1.val > l2.val) {
       crt.next = l2;
