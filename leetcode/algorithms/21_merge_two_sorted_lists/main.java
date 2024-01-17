dd085f0e225d2ee170fc8acda3ec1f88aa4dfd21
miga-choi <miga.choi.1004@gmail.com>
2024-01-18 00:46:35 +0900

    leetcode: "21_merge_two_sorted_lists" add java solution

--- leetcode/algorithms/21_merge_two_sorted_lists/main.java
+++ leetcode/algorithms/21_merge_two_sorted_lists/main.java
@@ -1,14 +1,3 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-
 class MergeTwoSortedLists {
 
   public class ListNode {
@@ -16,7 +5,8 @@ class MergeTwoSortedLists {
     int val;
     ListNode next;
 
-    ListNode() {}
+    ListNode() {
+    }
 
     ListNode(int val) {
       this.val = val;
@@ -29,13 +19,41 @@ class MergeTwoSortedLists {
   }
 
   public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-    return null;
+    ListNode result = new ListNode();
+    ListNode currentNode = result;
+
+    while (list1 != null || list2 != null) {
+      if (list1 != null && list2 != null) {
+        if (list1.val > list2.val) {
+          currentNode.next = list2;
+          list2 = list2.next;
+        } else {
+          currentNode.next = list1;
+          list1 = list1.next;
+        }
+      } else if (list1 != null) {
+        currentNode.next = list1;
+        list1 = list1.next;
+      } else {
+        currentNode.next = list2;
+        list2 = list2.next;
+      }
+      currentNode = currentNode.next;
+    }
+
+    return result.next;
   }
 
+
   // Best Solution
-  public ListNode bestSolution(ListNode l1, ListNode l2) {
-    if (l1 == null) return l2;
-    if (l2 == null) return l1;
+  // Best Solution 1: recursion
+  public ListNode bestSolution1(ListNode l1, ListNode l2) {
+    if (l1 == null) {
+      return l2;
+    }
+    if (l2 == null) {
+      return l1;
+    }
     if (l1.val < l2.val) {
       l1.next = mergeTwoLists(l1.next, l2);
       return l1;
@@ -43,5 +61,29 @@ class MergeTwoSortedLists {
       l2.next = mergeTwoLists(l1, l2.next);
       return l2;
     }
+  }
+
+  // Best Solution 2: iteration
+  public ListNode bestSolution2(ListNode l1, ListNode l2) {
+    ListNode head = new ListNode(0);
+    ListNode handler = head;
+    while (l1 != null && l2 != null) {
+      if (l1.val <= l2.val) {
+        handler.next = l1;
+        l1 = l1.next;
+      } else {
+        handler.next = l2;
+        l2 = l2.next;
+      }
+      handler = handler.next;
+    }
+
+    if (l1 != null) {
+      handler.next = l1;
+    } else if (l2 != null) {
+      handler.next = l2;
+    }
+
+    return head.next;
   }
 }
