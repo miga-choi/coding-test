b171a44a9972e7460798eacea202d4bee2009870
miga-choi <miga.choi.1004@gmail.com>
2024-01-23 00:07:41 +0900

    leetcode: "27_remove_element" add python solution

--- leetcode/algorithms/27_remove_element/main.py
+++ leetcode/algorithms/27_remove_element/main.py
@@ -1,13 +1,33 @@
 from typing import List
 
 
-class Solution:
+class RemoveElement:
     def removeElement(self, nums: List[int], val: int) -> int:
         nums[:] = sorted(filter(lambda num: num != val, nums))
         return len(nums)
 
 
     # Best Solution
-    def bestSolution(self, nums: List[int], val: int) -> int:
+    # Best Solution 1:
+    def bestSolution1(self, nums: List[int], val: int) -> int:
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[index] = nums[i]
+                index += 1
+        return index
+
+    # Best Solution 2:
+    def bestSolution2(self, nums: List[int], val: int) -> int:
         while val in nums:
             nums.remove(val)
+
+    # Best Solution 3:
+    def bestSolution3(self, nums: List[int], val: int) -> int:
+        start, end = 0, len(nums) - 1
+        while start <= end:
+            if nums[start] == val:
+                nums[start], nums[end], end = nums[end], nums[start], end - 1
+            else:
+                start += 1
+        return start
