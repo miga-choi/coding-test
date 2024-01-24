9c9e445f9f303cd61130950f63368cb3a7c1d281
miga-choi <miga.choi.1004@gmail.com>
2024-01-24 21:09:48 +0900

    leetcode: "27_remove_element" add java solution

--- leetcode/algorithms/27_remove_element/main.java
+++ leetcode/algorithms/27_remove_element/main.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 import java.util.List;
 
 class RemoveElement {
-
   public int removeElement(int[] nums, int val) {
     List<Integer> newNums = new ArrayList<>();
     for (Integer num : nums) {
@@ -18,8 +17,22 @@ class RemoveElement {
     return newNums.size();
   }
 
+
   // Best Solution
-  public int bestSolution(int[] nums, int val) {
+  // Best Solution 1:
+  public int bestSolution1(int[] nums, int val) {
+    int index = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != val) {
+        nums[index] = nums[i];
+        index++;
+      }
+    }
+    return index;
+  }
+
+  // Best Solution 2:
+  public int bestSolution2(int[] nums, int val) {
     int i = 0;
     for (int j = 0; j < nums.length; j++) {
       if (nums[j] != val) {
@@ -30,5 +43,16 @@ class RemoveElement {
       }
     }
     return i;
+  }
+
+  // Best Solution 3:
+  public int bestSolution3(int[] nums, int val) {
+    int l = nums.length;
+    for (int i = 0; i < l; i++) {
+      if (nums[i] == val) {
+        nums[i--] = nums[l-- - 1];
+      }
+    }
+    return l;
   }
 }
