d76bd46b5ed1c623ea3e2cf0953234b37bf775b6
miga-choi <miga.choi.1004@gmail.com>
2024-01-27 01:47:11 +0900

    leetcode: "28_find_the_index_of_the_first_occurrence_in_a_string" add java solution

--- leetcode/algorithms/28_find_the_index_of_the_first_occurrence_in_a_string/main.java
+++ leetcode/algorithms/28_find_the_index_of_the_first_occurrence_in_a_string/main.java
@@ -1,5 +1,4 @@
 class FindTheIndexOfTheFirstOccurrenceInAString {
-
   public int strStr(String haystack, String needle) {
     if (haystack == null || haystack.equals("")) {
       return -1;
@@ -10,14 +9,65 @@ class FindTheIndexOfTheFirstOccurrenceInAString {
     return haystack.indexOf(needle, 0);
   }
 
+
   // Best Solution
-  public int bestSolution(String haystack, String needle) {
+  // Best Solution 1:
+  public int bestSolution1(String haystack, String needle) {
     for (int i = 0;; i++) {
       for (int j = 0;; j++) {
-        if (j == needle.length()) return i;
-        if (i + j == haystack.length()) return -1;
-        if (needle.charAt(j) != haystack.charAt(i + j)) break;
+        if (j == needle.length()) {
+          return i;
+        }
+        if (i + j == haystack.length()) {
+          return -1;
+        }
+        if (needle.charAt(j) != haystack.charAt(i + j)) {
+          break;
+        }
       }
     }
+  }
+
+  // Best Solution 2:
+  public int bestSolution2(String haystack, String needle) {
+    int haylength = haystack.length();
+    int needlelength = needle.length();
+    if (haylength < needlelength) {
+      return -1;
+    }
+    for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+      int j = 0;
+      while (j < needle.length() && haystack.charAt(i + j) == needle.charAt(j)) {
+        j++;
+      }
+      if (j == needle.length()) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  // Best Solution 3:
+  public int bestSolution3(String haystack, String needle) {
+    int hLen = haystack.length();
+    int nLen = needle.length();
+    int nIndex = 0;
+    for (int i = 0; i < hLen; i++) {
+      // as long as the characters are equal, increment needleIndex
+      if (haystack.charAt(i) == needle.charAt(nIndex)) {
+        nIndex++;
+      } else {
+        // start from the next index of previous start index
+        i = i - nIndex;
+        // needle should start from index 0
+        nIndex = 0;
+      }
+      // check if needleIndex reached needle length
+      if (nIndex == nLen) {
+        // return the first index
+        return i - nLen + 1;
+      }
+    }
+    return -1;
   }
 }
