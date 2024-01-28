2c02ab787cfa3599a9484cc2f0fb7dd3e3cd3c39
miga-choi <miga.choi.1004@gmail.com>
2024-01-29 00:07:26 +0900

    leetcode: "35_search_insert_position" add javascript solution

--- leetcode/algorithms/35_search_insert_position/README.md
+++ leetcode/algorithms/35_search_insert_position/README.md
@@ -6,21 +6,21 @@ You must write an algorithm with `O(log n)` runtime complexity.
 
 ## Example 1:
 
-```javascript
+```
 Input: (nums = [1, 3, 5, 6]), (target = 5);
 Output: 2;
 ```
 
 ## Example 2:
 
-```javascript
+```
 Input: (nums = [1, 3, 5, 6]), (target = 2);
 Output: 1;
 ```
 
 ## Example 3:
 
-```javascript
+```
 Input: (nums = [1, 3, 5, 6]), (target = 7);
 Output: 4;
 ```
@@ -29,5 +29,5 @@ Output: 4;
 
 - `1 <= nums.length <= 10⁴`
 - `-10⁴ <= nums[i] <= 10⁴`
-- `nums` contains **_distinct_** values sorted in **_ascending_** order.
+- `nums` contains **distinct** values sorted in **ascending** order.
 - `-10⁴ <= target <= 10⁴`
--- leetcode/algorithms/35_search_insert_position/main.js
+++ leetcode/algorithms/35_search_insert_position/main.js
@@ -11,8 +11,10 @@ var searchInsert = function (nums, target) {
   return foundIndex;
 };
 
+
 // Best Solution
-var bestSolution = function (nums, target) {
+// Best Solution 1:
+var bestSolution1 = function (nums, target) {
   let lo = 0, hi = nums.length; // we might need to inseart at the end
   while (lo < hi) {
     // breaks if lo == hi
@@ -25,3 +27,33 @@ var bestSolution = function (nums, target) {
   }
   return lo;
 };
+
+// Best Solution 2:
+var bestSolution2 = function (nums, target) {
+  return binarySearch(nums, target, 0, nums.length - 1);
+};
+
+function binarySearch(array, target, start, end) {
+  // If the target is less then the very last item then insert it at that item index
+  // because anything index less then that has already been confirmed to be less then the target.
+  // Otherwise insert it at that item index + 1
+  // because any index grater then that has already been confirmed to be greater then the target
+  if (start > end) {
+    return start;
+  }
+
+  const midPoint = Math.floor((start + end) / 2);
+
+  // found target
+  if (array[midPoint] === target) {
+    return midPoint;
+  }
+  // search the left side
+  if (array[midPoint] > target) {
+    return binarySearch(array, target, start, midPoint - 1);
+  }
+  // search the right side
+  if (array[midPoint] < target) {
+    return binarySearch(array, target, midPoint + 1, end);
+  }
+}
