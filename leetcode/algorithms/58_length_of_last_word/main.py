fb9747261cd27f43038a58f72372a85f35ed5584
miga-choi <miga.choi.1004@gmail.com>
2024-02-01 00:10:44 +0900

    leetcode: "58_length_of_last_word" add python solution

--- leetcode/algorithms/58_length_of_last_word/main.py
+++ leetcode/algorithms/58_length_of_last_word/main.py
@@ -1,10 +1,25 @@
-class Solution:
+class LengthOfLastWord:
     def lengthOfLastWord(self, s: str) -> int:
         return len(list(filter(lambda s_: s_.strip() != "", s.split(" "))).pop(-1))
 
 
     # Best Solution
-    def bestSolution(self, s):
+    # Best Solution 1:
+    def bestSolution1(self, s: str) -> int:
+        end = len(s) - 1
+        while end > 0 and s[end] == " ":
+            end -= 1
+        beg = end
+        while beg >= 0 and s[beg] != " ":
+            beg -= 1
+        return end - beg
+
+    # Best Solution 2:
+    def bestSolution2(self, s: str) -> int:
+        return len(s.rstrip(" ").split(" ")[-1])
+
+    # Best Solution 3:
+    def bestSolution3(self, s: str) -> int:
         wordlist = s.split()
         if wordlist:
             return len(wordlist[-1])
