a63d68c7d56aeb62bebcadbc3c5286562850a07f
miga-choi <miga.choi.1004@gmail.com>
2024-02-02 19:52:47 +0900

    leetcode: "58_length_of_last_word" add java solution

--- leetcode/algorithms/58_length_of_last_word/main.java
+++ leetcode/algorithms/58_length_of_last_word/main.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 class LengthOfLastWord {
-
   public int lengthOfLastWord(String s) {
     String[] sArray = s.split(" ");
     List<String> resultArray = new ArrayList<String>();
@@ -15,19 +14,36 @@ class LengthOfLastWord {
     return resultArray.get(resultArray.size() - 1).length();
   }
 
+
   // Best Solution
-  public int bestSolution(String s) {
+  // Best Solution 1:
+  public int bestSolution1(String s) {
     int length = 0;
 
     // We are looking for the last word so let's go backward
     for (int i = s.length() - 1; i >= 0; i--) {
-      if (s.charAt(i) != ' ') { // a letter is found so count
+      if (s.charAt(i) != ' ') {
+        // a letter is found so count
         length++;
-      } else { // it's a white space instead
-        //  Did we already started to count a word ? Yes so we found the last word
-        if (length > 0) return length;
+      } else {
+        // it's a white space instead
+        if (length > 0)
+          // Did we already started to count a word ? Yes so we found the last word
+          return length;
       }
     }
     return length;
+  }
+
+  // Best Solution 2:
+  public int bestSolution2(String s) {
+    return s.trim().length() - s.trim().lastIndexOf(" ") - 1;
+  }
+
+  // Best Solution 3:
+  public int bestSolution3(String s) {
+    s = s.trim();
+    int lastIndex = s.lastIndexOf(' ') + 1;
+    return s.length() - lastIndex;
   }
 }
