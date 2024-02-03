01acc1e453ba7a8e199594a5d0b893d768056b63
miga-choi <miga.choi.1004@gmail.com>
2024-02-03 15:45:54 +0900

    leetcode: "58_length_of_last_word" add c++ solution

---
+++ leetcode/algorithms/58_length_of_last_word/main.cpp
@@ -0,0 +1,79 @@
+#include <string>
+using namespace std;
+
+class LengthOfLastWord
+{
+public:
+    int lengthOfLastWord(string s)
+    {
+        int result = 0;
+        for (int i = s.size(); i >= 0; i--)
+        {
+            if (s[i] != ' ' && s[i] != '\0')
+            {
+                result++;
+            }
+            else if (s[i] = ' ' && result > 0)
+            {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+
+    // Best Solution
+    // Best Solution 1:
+    int bestSolution1(string s)
+    {
+        int len = 0, tail = s.length() - 1;
+        while (tail >= 0 && s[tail] == ' ')
+        {
+            tail--;
+        }
+        while (tail >= 0 && s[tail] != ' ')
+        {
+            len++;
+            tail--;
+        }
+        return len;
+    }
+
+    // Best Solution 2:
+    int bestSolution2(string s)
+    {
+        int siz = s.size(), kount = 0, flag = 0;
+        for (int i = siz - 1; i >= 0; i--)
+        {
+            if (s[i] == ' ' && flag)
+            {
+                break;
+            }
+            if (s[i] != ' ')
+            {
+                flag = 1;
+                kount++;
+            }
+        }
+        return kount;
+    }
+
+    // Best Solution 3:
+    int bestSolution3(string s)
+    {
+        int ans = 0;
+        for (int i = s.size() - 1; i >= 0; --i)
+        {
+            if (s[i] == ' ' && ans > 0)
+            {
+                return ans;
+            }
+            if (s[i] != ' ')
+            {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
