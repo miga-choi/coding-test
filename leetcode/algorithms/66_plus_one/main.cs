e4dfe666f98c2b40a74ef05956100f97e4cecbce
miga-choi <miga.choi.1004@gmail.com>
2024-02-05 20:59:30 +0900

    leetcode: "66_plus_one" add c# solution

---
+++ leetcode/algorithms/66_plus_one/main.cs
@@ -0,0 +1,102 @@
+public class PlusOne
+{
+    public int[] PlusOne(int[] digits)
+    {
+        int sum = 0;
+        List<int> result = new List<int>();
+        int up = 1;
+
+        for (int i = digits.Length - 1; i >= 0; i--)
+        {
+            int num = digits[i] + up;
+
+            if (num >= 10)
+            {
+                num -= 10;
+                up = 1;
+            }
+            else
+            {
+                up = 0;
+            }
+
+            result.Insert(0, num);
+        }
+
+        if (up > 0)
+        {
+            result.Insert(0, 1);
+        }
+
+        return result.ToArray();
+    }
+
+
+    // Best Solution
+    // Best Solution 1:
+    public int[] bestSolution1(int[] digits)
+    {
+        int n = digits.Length;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newNumber = new int[n + 1];
+        newNumber[0] = 1;
+
+        return newNumber;
+    }
+
+    // Best Solution 2:
+    public int[] bestSolution2(int[] digits)
+    {
+        for (int i = digits.Length - 1; i >= 0; i--)
+        {
+            if (digits[i] == 9)
+            {
+                digits[i] = 0;
+            }
+            else
+            {
+                digits[i]++;
+                return digits;
+            }
+        }
+        int[] firstOne = new int[] { 1 };
+        return firstOne.Concat(digits).ToArray();
+    }
+
+    // Best Solution 3:
+    public int[] bestSolution3(int[] digits)
+    {
+        int len = digits.Length;
+        int num;
+        int add = 1;
+
+        for (int i = len - 1; i >= 0; i--)
+        {
+            num = digits[i] + add;
+            if (num == 10)
+            {
+                digits[i] = 0;
+            }
+            else
+            {
+                digits[i] = num;
+                return digits;
+            }
+        }
+
+        int[] newDigits = new int[digits.Length + 1];
+
+        newDigits[0] = 1;
+        Array.Copy(digits, 0, newDigits, 1, digits.Length);
+
+        return newDigits;
+    }
+}
