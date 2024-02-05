c3b57ee242818b2c397d922e9f5322c6d53e6f63
miga-choi <miga.choi.1004@gmail.com>
2024-02-05 20:58:43 +0900

    leetcode: "66_plus_one" add java solution

--- leetcode/algorithms/66_plus_one/main.java
+++ leetcode/algorithms/66_plus_one/main.java
@@ -3,12 +3,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 class PlusOne {
-
   public int[] plusOne(int[] digits) {
     List<Integer> result = Arrays
-      .stream(digits)
-      .boxed()
-      .collect(Collectors.toList());
+        .stream(digits)
+        .boxed()
+        .collect(Collectors.toList());
     for (int i = result.size() - 1; i >= 0; i--) {
       result.set(i, result.get(i) + 1);
       if (result.get(i) > 9) {
@@ -21,18 +20,39 @@ class PlusOne {
     return result.stream().mapToInt(v_ -> v_).toArray();
   }
 
+
   // Best Solution
-  public int[] bestSolution(int[] digits) {
-    for (int i = digits.length - 1; i >= 0; i--) {
+  // Best Solution 1:
+  public int[] bestSolution1(int[] digits) {
+    int n = digits.length;
+    for (int i = n - 1; i >= 0; i--) {
       if (digits[i] < 9) {
         digits[i]++;
         return digits;
       }
+
       digits[i] = 0;
     }
 
-    digits = new int[digits.length + 1];
-    digits[0] = 1;
-    return digits;
+    int[] newNumber = new int[n + 1];
+    newNumber[0] = 1;
+
+    return newNumber;
+  }
+
+  // Best Solution 2:
+  public int[] bestSolution2(int[] digits) {
+    int carry = 1;
+    for (int i = digits.length - 1; i >= 0; i--) {
+      digits[i] += carry;
+      if (digits[i] <= 9) {
+        // early return
+        return digits;
+      }
+      digits[i] = 0;
+    }
+    int[] ret = new int[digits.length + 1];
+    ret[0] = 1;
+    return ret;
   }
 }
