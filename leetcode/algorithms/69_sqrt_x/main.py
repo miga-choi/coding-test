71152819da74f52d011ef313c90e41cd496eafba
miga-choi <miga.choi.1004@gmail.com>
2024-02-10 15:17:40 +0900

    leetcode: "69_sqrt_x" add python solution

--- leetcode/algorithms/69_sqrt_x/main.py
+++ leetcode/algorithms/69_sqrt_x/main.py
@@ -1,4 +1,4 @@
-class Solution:
+class SqrtX:
     def mySqrt(self, x: int) -> int:
         for i in range(x + 1):
             if i * i <= x and (i + 1) * (i + 1) > x:
@@ -6,7 +6,8 @@ class Solution:
 
 
     # Best Solution
-    def bestSolution(self, x: int) -> int:
+    # Best Solution 1:
+    def bestSolution1(self, x: int) -> int:
         if x == 0:
             return 0
         first, last = 1, x
@@ -19,3 +20,15 @@ class Solution:
             else:
                 first = mid + 1
         return last
+
+    # Best Solution 2:
+    def bestSolution2(self, x: int) -> int:
+        l, r = 0, x
+        while l <= r:
+            mid = l + (r - l) // 2
+            if mid * mid <= x < (mid + 1) * (mid + 1):
+                return mid
+            elif x < mid * mid:
+                r = mid - 1
+            else:
+                l = mid + 1
