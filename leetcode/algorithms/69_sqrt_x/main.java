dcff2abff0e953b0560b3a3b7098328fd071c98a
miga-choi <miga.choi.1004@gmail.com>
2024-02-11 15:28:29 +0900

    leetcode: "69_sqrt_x" add java solution

--- leetcode/algorithms/69_sqrt_x/main.java
+++ leetcode/algorithms/69_sqrt_x/main.java
@@ -1,5 +1,4 @@
 class SqrtX {
-
   public int mySqrt(int x) {
     if (x == 0) {
       return 0;
@@ -19,22 +18,73 @@ class SqrtX {
     return high;
   }
 
+
   // Best Solution
-  public int bestSolution(int x) {
+  // Best Solution 1:
+  public int bestSolution1(int x) {
     if (x == 0) {
       return 0;
     }
-    int first = 1, last = x;
-    while (first <= last) {
-      int mid = first + (last - first) / 2;
-      if (mid == x / mid) {
-        return mid;
-      } else if (mid > x / mid) {
-        last = mid - 1;
+    int left = 1, right = Integer.MAX_VALUE;
+    while (true) {
+      int mid = left + (right - left) / 2;
+      if (mid > x / mid) {
+        right = mid - 1;
       } else {
-        first = mid + 1;
+        if (mid + 1 > x / (mid + 1)) {
+          return mid;
+        }
+        left = mid + 1;
       }
     }
-    return last;
+  }
+
+  // Best Solution 2:
+  public int bestSolution2(int x) {
+    long r = x;
+    while (r * r > x) {
+      r = (r + x / r) / 2;
+    }
+    return (int) r;
+  }
+
+  // Best Solution 3:
+  public int bestSolution3(int x) {
+    // For special cases when x is 0 or 1, return x.
+    if (x == 0 || x == 1) {
+      return x;
+    }
+
+    // Initialize the search range for the square root.
+    int start = 1;
+    int end = x;
+    int mid = -1;
+
+    // Perform binary search to find the square root of x.
+    while (start <= end) {
+      // Calculate the middle point using "start + (end - start) / 2" to avoid integer
+      // overflow.
+      mid = start + (end - start) / 2;
+
+      // If the square of the middle value is greater than x, move the "end" to the
+      // left (mid - 1).
+      if ((long) mid * mid > (long) x) {
+        end = mid - 1;
+      } else if (mid * mid == x) {
+        // If the square of the middle value is equal to x, we found the square root.
+        return mid;
+      } else {
+        // If the square of the middle value is less than x, move the "start" to the
+        // right (mid + 1).
+        start = mid + 1;
+      }
+    }
+
+    // The loop ends when "start" becomes greater than "end", and "end" is the
+    // integer value of the square root.
+    // However, since we might have been using integer division in the calculations,
+    // we round down the value of "end" to the nearest integer to get the correct
+    // square root.
+    return Math.round(end);
   }
 }
