25a05ac38d65843b046db5477c5934822c4cc1c5
miga-choi <miga.choi.1004@gmail.com>
2024-02-14 22:44:03 +0900

    leetcode: "70_climbing_stairs" add java solution

--- leetcode/algorithms/70_climbing_stairs/main.java
+++ leetcode/algorithms/70_climbing_stairs/main.java
@@ -2,7 +2,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 class ClimbingStairs {
-
   public int climbStairs(int n) {
     if (n < 2) {
       return 1;
@@ -20,10 +19,12 @@ class ClimbingStairs {
     return dp[n - 1];
   }
 
+
   // Best Solution
-  // Recustion (Top Down Approach)
+  // Best Solution 1: Fibonacci sequence
   public int bestSolution1(int n) {
-    if (n == 0) {
+    // base cases
+    if (n <= 0) {
       return 0;
     }
     if (n == 1) {
@@ -32,59 +33,69 @@ class ClimbingStairs {
     if (n == 2) {
       return 2;
     }
+
+    int one_step_before = 2;
+    int two_steps_before = 1;
+    int all_ways = 0;
+
+    for (int i = 2; i < n; i++) {
+      all_ways = one_step_before + two_steps_before;
+      two_steps_before = one_step_before;
+      one_step_before = all_ways;
+    }
+    return all_ways;
+  }
+
+  // Best Solution 2: Recursion
+  public int bestSolution2(int n) {
+    if (n == 0 || n == 1) {
+      return 1;
+    }
     return climbStairs(n - 1) + climbStairs(n - 2);
   }
 
-  // Recustion + Memorization (Top Down Approach)
-  public int bestSolution2(int n) {
+  // Best Solution 3: Memoization
+  public int bestSolution3(int n) {
     Map<Integer, Integer> memo = new HashMap<>();
-    memo.put(1, 1);
-    memo.put(2, 2);
-    return climbStairs(n, memo);
+    return bestSolution3(n, memo);
   }
 
-  private int climbStairs(int n, Map<Integer, Integer> memo) {
-    if (memo.containsKey(n)) {
-      return memo.get(n);
+  private int bestSolution3(int n, Map<Integer, Integer> memo) {
+    if (n == 0 || n == 1) {
+      return 1;
     }
-    memo.put(n, climbStairs(n - 1, memo) + climbStairs(n - 2, memo));
+    if (!memo.containsKey(n)) {
+      memo.put(n, bestSolution3(n - 1, memo) + bestSolution3(n - 2, memo));
+    }
     return memo.get(n);
   }
 
-  // DP (Bottom Up Approach)
-  public int bestSolution3(int n) {
-    if (n <= 1) {
+  // Best Solution 4: Tabulation
+  public int bestSolution4(int n) {
+    if (n == 0 || n == 1) {
       return 1;
     }
 
     int[] dp = new int[n + 1];
-    dp[1] = 1;
-    dp[2] = 2;
+    dp[0] = dp[1] = 1;
 
-    for (int i = 3; i <= n; i++) {
+    for (int i = 2; i <= n; i++) {
       dp[i] = dp[i - 1] + dp[i - 2];
     }
-
     return dp[n];
   }
 
-  // DP + Optimization (Bottom Up Approach)
-  //   To calculate the new value we only leverage the previous two values.
-  //   So we don't need to use an array to store all the previous values.
-  public int bestSolution4(int n) {
-    if (n <= 1) {
+  // Best Solution 5: Tabulation
+  public int bestSolution5(int n) {
+    if (n == 0 || n == 1) {
       return 1;
     }
-
-    int prev1 = 1;
-    int prev2 = 2;
-
-    for (int i = 3; i <= n; i++) {
-      int newValue = prev1 + prev2;
-      prev1 = prev2;
-      prev2 = newValue;
+    int prev = 1, curr = 1;
+    for (int i = 2; i <= n; i++) {
+      int temp = curr;
+      curr = prev + curr;
+      prev = temp;
     }
-
-    return prev2;
+    return curr;
   }
 }
