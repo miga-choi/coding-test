3dc10876dc74df45efa7ade604e865b4b57cd68e
miga-choi <miga.choi.1004@gmail.com>
2024-02-19 21:37:41 +0900

    leetcode: "88_merge_sorted_array" add python solution

--- leetcode/algorithms/88_merge_sorted_array/main.py
+++ leetcode/algorithms/88_merge_sorted_array/main.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-class Solution:
+class MergeSortedArray:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
@@ -12,15 +12,64 @@ class Solution:
 
 
     # Best Solution
-    def merge_two_sorted_arrays(A: List[int], m: int, B: List[int], n: int) -> None:
-        a, b, write_index = m - 1, n - 1, m + n - 1
+    # Best Solution 1: STL
+    def bestSolution1(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        for j in range(n):
+            nums1[m + j] = nums2[j]
+        nums1.sort()
 
-        while b >= 0:
-            if a >= 0 and A[a] > B[b]:
-                A[write_index] = A[a]
-                a -= 1
+    # Best Solution 2: Two Pointer
+    def bestSolution2(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
             else:
-                A[write_index] = B[b]
-                b -= 1
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
 
-            write_index -= 1
+    # Best Solution 3:
+    def bestSolution3(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        while m > 0 and n > 0:
+            if nums1[m - 1] >= nums2[n - 1]:
+                nums1[m + n - 1] = nums1[m - 1]
+                m -= 1
+            else:
+                nums1[m + n - 1] = nums2[n - 1]
+                n -= 1
+        if n > 0:
+            nums1[:n] = nums2[:n]
+
+    # Best Solution 3:
+    def bestSolution3(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        # Initialize nums1's index
+        i = m - 1
+        # Initialize nums2's index
+        j = n - 1
+        # Initialize a variable k to store the last index of the 1st array...
+        k = m + n - 1
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                k -= 1
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                k -= 1
+                j -= 1
+
+    # Best Solution 4:
+    def bestSolution4(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        idx = 0
+        for i in range(len(nums1)):
+            if idx >= n:
+                break
+            if nums1[i] == 0:
+                nums1[i] = nums2[idx]
+                idx += 1
+        nums1.sort()
