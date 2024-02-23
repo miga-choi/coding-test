f826da34b898db5e4403319236636cdc9f084164
miga-choi <miga.choi.1004@gmail.com>
2024-02-23 17:20:30 +0900

    leetcode: "94_binary_tree_inorder_traversal" add c# solution

---
+++ leetcode/algorithms/94_binary_tree_inorder_traversal/main.cs
@@ -0,0 +1,110 @@
+using System.Collections.Generic;
+
+public class BinaryTreeInorderTraversal
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public IList<int> InorderTraversal(TreeNode root)
+    {
+        List<int> result = new List<int>();
+        getVal(root, result);
+        return result;
+    }
+
+    public void getVal(TreeNode node, List<int> array)
+    {
+        if (node != null)
+        {
+            getVal(node.left, array);
+            array.Add(node.val);
+            getVal(node.right, array);
+        }
+    }
+
+
+    // Best Solution
+    // Best Solution 1:
+    List<int> ans = new List<int>();
+    public IList<int> bestSolution1(TreeNode root)
+    {
+        if (root == null)
+        {
+            return ans;
+        }
+        bestSolution1(root.left);
+        ans.Add(root.val);
+        bestSolution1(root.right);
+        return ans;
+    }
+
+    // Best Solution 2: Iterative with stack
+    public IList<int> bestSolution2(TreeNode root)
+    {
+        List<int> result = new List<int>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+
+        while (root != null || stack.Count > 0)
+        {
+            while (root != null)
+            {
+                stack.Push(root);
+                root = root.left;
+            }
+            root = stack.Pop();
+            result.Add(root.val);
+            root = root.right;
+        }
+
+        return result;
+    }
+
+    // Best Solution 3: Morris traversal
+    public IList<int> bestSolution3(TreeNode root)
+    {
+        List<int> result = new List<int>();
+        TreeNode current = root;
+
+        while (current != null)
+        {
+            if (current.left == null)
+            {
+                result.Add(current.val);
+                current = current.right;
+            }
+            else
+            {
+                TreeNode predecessor = current.left;
+
+                while (predecessor.right != null && predecessor.right != current)
+                {
+                    predecessor = predecessor.right;
+                }
+
+                if (predecessor.right == null)
+                {
+                    predecessor.right = current;
+                    current = current.left;
+                }
+                else
+                {
+                    predecessor.right = null;
+                    result.Add(current.val);
+                    current = current.right;
+                }
+            }
+        }
+
+        return result;
+    }
+}
