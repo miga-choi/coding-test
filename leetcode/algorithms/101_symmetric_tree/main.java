cd233178cc4d665cdc02f76ffc11461b40c0566b
miga-choi <miga.choi.1004@gmail.com>
2024-02-29 00:09:16 +0900

    leetcode: "101_symmetric_tree" add java solution

--- leetcode/algorithms/101_symmetric_tree/main.java
+++ leetcode/algorithms/101_symmetric_tree/main.java
@@ -1,29 +1,13 @@
 import java.util.Stack;
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class SymmetricTree {
-
   public class TreeNode {
-
     int val;
     TreeNode left;
     TreeNode right;
 
-    TreeNode() {}
+    TreeNode() {
+    }
 
     TreeNode(int val) {
       this.val = val;
@@ -47,49 +31,54 @@ class SymmetricTree {
     if (leftNode == null && rightNode == null) {
       return true;
     }
-    if (
-      (leftNode == null && rightNode != null) ||
-      (leftNode != null && rightNode == null)
-    ) {
+    if ((leftNode == null && rightNode != null) ||
+        (leftNode != null && rightNode == null)) {
       return false;
     }
     if (leftNode != null && rightNode != null) {
       if (leftNode.val != rightNode.val) {
         return false;
       } else {
-        return (
-          checkNode(leftNode.left, rightNode.right) &&
-          checkNode(leftNode.right, rightNode.left)
-        );
+        return (checkNode(leftNode.left, rightNode.right) &&
+            checkNode(leftNode.right, rightNode.left));
       }
     }
     return false;
   }
 
+
   // Best Solution
-
-  // Recursive--400ms:
+  // Best Solution 1: Recursive
   public boolean bestSolution1(TreeNode root) {
-    return root == null || isSymmetricHelp(root.left, root.right);
+    return root == null || bestSolution1_isSymmetricHelp(root.left, root.right);
   }
 
-  private boolean isSymmetricHelp(TreeNode left, TreeNode right) {
-    if (left == null || right == null) return left == right;
-    if (left.val != right.val) return false;
-    return (
-      isSymmetricHelp(left.left, right.right) &&
-      isSymmetricHelp(left.right, right.left)
-    );
+  private boolean bestSolution1_isSymmetricHelp(TreeNode left, TreeNode right) {
+    if (left == null || right == null) {
+      return left == right;
+    }
+
+    if (left.val != right.val) {
+      return false;
+    }
+
+    return (bestSolution1_isSymmetricHelp(left.left, right.right) &&
+        bestSolution1_isSymmetricHelp(left.right, right.left));
   }
 
-  // Non-recursive(use Stack)--460ms:
+  // Best Solution 2: Non-recursive (use Stack):
   public boolean bestSolution2(TreeNode root) {
-    if (root == null) return true;
+    if (root == null) {
+      return true;
+    }
 
     Stack<TreeNode> stack = new Stack<TreeNode>();
     TreeNode left, right;
+
     if (root.left != null) {
-      if (root.right == null) return false;
+      if (root.right == null) {
+        return false;
+      }
       stack.push(root.left);
       stack.push(root.right);
     } else if (root.right != null) {
@@ -97,13 +86,21 @@ class SymmetricTree {
     }
 
     while (!stack.empty()) {
-      if (stack.size() % 2 != 0) return false;
+      if (stack.size() % 2 != 0) {
+        return false;
+      }
+
       right = stack.pop();
       left = stack.pop();
-      if (right.val != left.val) return false;
+
+      if (right.val != left.val) {
+        return false;
+      }
 
       if (left.left != null) {
-        if (right.right == null) return false;
+        if (right.right == null) {
+          return false;
+        }
         stack.push(left.left);
         stack.push(right.right);
       } else if (right.right != null) {
@@ -111,7 +108,9 @@ class SymmetricTree {
       }
 
       if (left.right != null) {
-        if (right.left == null) return false;
+        if (right.left == null) {
+          return false;
+        }
         stack.push(left.right);
         stack.push(right.left);
       } else if (right.left != null) {
