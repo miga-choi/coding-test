f37ed2c7bb5a729ada81a2bcb3e5db8ad8c45ff6
miga-choi <miga.choi.1004@gmail.com>
2024-03-02 00:12:21 +0900

    leetcode: "104_maximum_depth_of_binary_tree" add c# solution

---
+++ leetcode/algorithms/104_maximum_depth_of_binary_tree/main.cs
@@ -0,0 +1,28 @@
+public class MaximumDepthOfBinaryTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int MaxDepth(TreeNode root)
+    {
+
+    }
+
+
+    // Best Solution
+    // Best Solution 1:
+    public int bestSolution1(TreeNode root)
+    {
+
+    }
+}
