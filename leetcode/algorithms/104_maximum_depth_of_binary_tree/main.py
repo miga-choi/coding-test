9cc0bc84c5f66fe1e7e9f930630dfcc4327929b7
miga-choi <miga.choi.1004@gmail.com>
2024-03-01 14:27:20 +0900

    leetcode: "104_maximum_depth_of_binary_tree" add python solution

--- leetcode/algorithms/104_maximum_depth_of_binary_tree/main.py
+++ leetcode/algorithms/104_maximum_depth_of_binary_tree/main.py
@@ -1,11 +1,5 @@
+from collections import deque
 from typing import Optional
-
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 
 
 class TreeNode:
@@ -15,7 +9,7 @@ class TreeNode:
         self.right = right
 
 
-class Solution:
+class MaximumDepthOfBinaryTree:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if root is None:
             return 0
@@ -35,10 +29,37 @@ class Solution:
 
 
     # Best Solution
-    def bestSolution(self, root: Optional[TreeNode]) -> int:
+    # Best Solution 1: RECURSIVE DFS
+    def bestSolution1(self, root: Optional[TreeNode]) -> int:
         def dfs(root, depth):
             if not root:
                 return depth
             return max(dfs(root.left, depth + 1), dfs(root.right, depth + 1))
 
         return dfs(root, 0)
+
+    # Best Solution 2: Recursive
+    def bestSolution2(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return max(self.bestSolution2(root.left), self.bestSolution2(root.right)) + 1
+
+    # Best Solution 3: Iterative
+    def bestSolution3(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        worklist = deque([root])
+        num_node_level = 1
+        levels = 0
+        while worklist:
+            node = worklist.popleft()
+            if node.left:
+                worklist.append(node.left)
+            if node.right:
+                worklist.append(node.right)
+            num_node_level -= 1
+            if num_node_level == 0:
+                levels += 1
+                num_node_level = len(worklist)
+
+        return levels
