c942a37353b05b38582fd8bddcb5b6f8e3b9b96b
miga-choi <miga.choi.1004@gmail.com>
2024-03-04 00:01:11 +0900

    leetcode: "108_convert_sorted_array_to_binary_search_tree" add javascript solution

--- leetcode/algorithms/108_convert_sorted_array_to_binary_search_tree/main.js
+++ leetcode/algorithms/108_convert_sorted_array_to_binary_search_tree/main.js
@@ -1,12 +1,3 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-
 function TreeNode(val, left, right) {
   this.val = val === undefined ? 0 : val;
   this.left = left === undefined ? null : left;
@@ -31,24 +22,45 @@ var sortedArrayToBST = function (nums) {
   return root;
 };
 
+
 // Best Solution
-var bestSolution = function (nums) {
+// Best Solution 1:
+var bestSolution1 = function (nums) {
   // Call the function recursively...
-  return ConvToBST(nums, 0, nums.length - 1);
+  return bestSolution1_convToBST(nums, 0, nums.length - 1);
 };
+
 // Create a function which will convert any particular range of given nums array...
 // & return its corresponding BST root node....
-var ConvToBST = function (nums, beg, end) {
+var bestSolution1_convToBST = function (nums, beg, end) {
   // If beg > end, return NULL, as we receive a wrong range...
-  if (beg > end) return null;
+  if (beg > end) {
+    return null;
+  }
   // set the middle node...
   var mid = Math.ceil((beg + end) / 2);
   // Initialise root node with value same as nums[mid]...
   var root = new TreeNode(nums[mid]);
   // Assign left subtrees as the same function called on left subranges...
-  root.left = ConvToBST(nums, beg, mid - 1);
+  root.left = bestSolution1_convToBST(nums, beg, mid - 1);
   // Assign right subtrees as the same function called on right subranges...
-  root.right = ConvToBST(nums, mid + 1, end);
+  root.right = bestSolution1_convToBST(nums, mid + 1, end);
   // Return the root node...
+  return root;
+};
+
+// Best Solution 2: Intuitive
+var bestSolution2 = function (nums) {
+  if (!nums.length) {
+    return null;
+  }
+
+  const mid = Math.floor(nums.length / 2);
+  const root = new TreeNode(nums[mid]);
+
+  // subtrees are BSTs as well
+  root.left = sortedArrayToBST(nums.slice(0, mid));
+  root.right = sortedArrayToBST(nums.slice(mid + 1));
+
   return root;
 };
