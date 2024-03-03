6ddd6d57215a12bada439ebb337110973cb0ac51
miga-choi <miga.choi.1004@gmail.com>
2024-03-04 00:02:02 +0900

    leetcode: "108_convert_sorted_array_to_binary_search_tree" add python solution

--- leetcode/algorithms/108_convert_sorted_array_to_binary_search_tree/main.py
+++ leetcode/algorithms/108_convert_sorted_array_to_binary_search_tree/main.py
@@ -1,13 +1,6 @@
 import math
 from typing import List, Optional
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -16,7 +9,7 @@ class TreeNode:
         self.right = right
 
 
-class Solution:
+class ConvertSortedArrayToBinarySearchTree:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
         if nums is None or len(nums) is 0:
             return None
@@ -31,14 +24,41 @@ class Solution:
 
 
     # Best Solution
-    # recursive
-    def bestSolution(self, nums):
-        def rec(nums, start, end):
-            if start <= end:
-                mid = (start + end) // 2
-                node = TreeNode(nums[mid])
-                node.left = rec(nums, start, mid - 1)
-                node.right = rec(nums, mid + 1, end)
-                return node
+    # Best Solution 1: Recursion
+    def bestSolution1(self, nums: List[int]) -> Optional[TreeNode]:
+        total_nums = len(nums)
+        if not total_nums:
+            return None
 
-        return rec(nums, 0, len(nums) - 1)
+        mid_node = total_nums // 2
+        return TreeNode(
+            nums[mid_node],
+            self.sortedArrayToBST(nums[:mid_node]),
+            self.sortedArrayToBST(nums[mid_node + 1 :]),
+        )
+
+    # Best Solution 2: Recursion
+    def bestSolution2(self, nums: List[int]) -> Optional[TreeNode]:
+        if not nums:
+            return None
+
+        mid = len(nums) // 2
+
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid + 1 :])
+
+        return root
+
+    # Best Solution 3:
+    def bestSolution3(self, nums: List[int]) -> Optional[TreeNode]:
+        def convert(left, right):
+            if left > right:
+                return None
+            mid = (left + right) // 2
+            node = TreeNode(nums[mid])
+            node.left = convert(left, mid - 1)
+            node.right = convert(mid + 1, right)
+            return node
+
+        return convert(0, len(nums) - 1)
