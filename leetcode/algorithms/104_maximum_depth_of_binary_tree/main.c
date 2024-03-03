cda41c2ddbea8abcfc83800eb344b0abbac98075
miga-choi <miga.choi.1004@gmail.com>
2024-03-03 23:44:04 +0900

    leetcode: "104_maximum_depth_of_binary_tree" add c solution

---
+++ leetcode/algorithms/104_maximum_depth_of_binary_tree/main.c
@@ -0,0 +1,33 @@
+struct TreeNode
+{
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+int maxDepth(struct TreeNode *root)
+{
+    if (!root)
+    {
+        return 0;
+    }
+
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+
+    return left >= right ? left + 1 : right + 1;
+}
+
+// Best Solution
+int bestSolution(struct TreeNode *root)
+{
+    if (!root)
+    {
+        return 0;
+    }
+
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+
+    return left >= right ? left + 1 : right + 1;
+}
--- leetcode/algorithms/104_maximum_depth_of_binary_tree/main.cs
+++ leetcode/algorithms/104_maximum_depth_of_binary_tree/main.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 public class MaximumDepthOfBinaryTree
 {
     public class TreeNode
@@ -15,14 +18,68 @@ public class MaximumDepthOfBinaryTree
 
     public int MaxDepth(TreeNode root)
     {
+        if (root == null)
+        {
+            return 0;
+        }
 
+        int left = MaxDepth(root.left);
+        int right = MaxDepth(root.right);
+
+        return Math.Max(left, right) + 1;
     }
 
 
     // Best Solution
-    // Best Solution 1:
+    // Best Solution 1: Depth-First Search
     public int bestSolution1(TreeNode root)
     {
+        if (root == null)
+        {
+            return 0;
+        }
 
+        int leftMaxDepth = bestSolution1(root.left);
+        int rightMaxDepth = bestSolution1(root.right);
+
+        return Math.Max(leftMaxDepth, rightMaxDepth) + 1;
+    }
+
+    // Best Solution 2: Breadth-First Search
+    public int bestSolution2(TreeNode root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        var depth = 0;
+
+        while (queue.Count != 0)
+        {
+            var size = queue.Count;
+
+            for (var i = 0; i < size; i++)
+            {
+                var current = queue.Dequeue();
+
+                if (current.left != null)
+                {
+                    queue.Enqueue(current.left);
+                }
+
+                if (current.right != null)
+                {
+                    queue.Enqueue(current.right);
+                }
+            }
+
+            depth++;
+        }
+
+        return depth;
     }
 }
