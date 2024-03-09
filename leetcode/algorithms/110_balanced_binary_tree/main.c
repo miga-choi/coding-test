11f19b13efafe4c63917c69c91fc3786fa2f99a6
miga-choi <miga.choi.1004@gmail.com>
2024-03-09 21:16:00 +0900

    leetcode: "110_balanced_binary_tree" add c solution

---
+++ leetcode/algorithms/110_balanced_binary_tree/main.c
@@ -0,0 +1,64 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+struct TreeNode
+{
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+int getDepth(struct TreeNode *root)
+{
+    if (root)
+    {
+        int left = getDepth(root->left);
+        int right = getDepth(root->right);
+        return (left > right ? left : right) + 1;
+    }
+    return 0;
+}
+
+bool isBalanced(struct TreeNode *root)
+{
+    if (root)
+    {
+        int depth = getDepth(root->left) - getDepth(root->right);
+        if (depth < 0)
+        {
+            depth *= -1;
+        }
+        if (depth > 1)
+        {
+            return false;
+        }
+        if (!isBalanced(root->left) || !isBalanced(root->right))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+// Best Solution
+int bestSolution_maxdepth(struct TreeNode *root, int depth)
+{
+    if (depth < 0 || root == NULL)
+    {
+        return depth; // return depth if it reaches the end or it's already not balanced
+    }
+    int left = bestSolution_maxdepth(root->left, depth + 1);
+    int right = bestSolution_maxdepth(root->right, depth + 1);
+    depth = (left > right) ? left : right; // choose max depth
+    if (abs(left - right) > 1)
+    {
+        depth *= (-1); // not balanced, convert to negative value
+    }
+    return depth;
+}
+
+bool bestSolution(struct TreeNode *root)
+{
+    return bestSolution_maxdepth(root, 0) >= 0; // negative: not balanced, positive: balanced
+}
