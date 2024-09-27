1973b64bd8e400dd8b871a02f77c6da7314049f0
miga-choi <miga.choi.1004@gmail.com>
2024-09-28 02:18:22 +0900

    leetcode: "14_longest_common_prefix" add dart solution

---
+++ leetcode/algorithms/14_longest_common_prefix/main.dart
@@ -0,0 +1,32 @@
+class LongestCommonPrefix {
+  String longestCommonPrefix(List<String> strs) {
+    String result = "";
+
+    for (int i = 0; i < strs[0].length; i++) {
+      String c = strs[0][i];
+      int count = 0;
+      for (String str in strs) {
+        if (i > str.length - 1 || str[i] != c) {
+          return result;
+        }
+        count++;
+        if (count == strs.length) {
+          result += c;
+        }
+      }
+    }
+
+    return result;
+  }
+
+  // Best Solution
+  String bestSolution(List<String> strs) {
+    String commonPrefix = strs.first;
+    for (final word in strs) {
+      while (!word.startsWith(commonPrefix)) {
+        commonPrefix = commonPrefix.substring(0, commonPrefix.length - 1);
+      }
+    }
+    return commonPrefix;
+  }
+}
