54f6b0018303e1b462d1dd8e1f6a9b4fb75e1fe3
miga-choi <miga.choi.1004@gmail.com>
2024-09-29 02:08:18 +0900

    leetcode: "21_merge_two_sorted_lists" add dart solution

---
+++ leetcode/algorithms/21_merge_two_sorted_lists/main.dart
@@ -0,0 +1,53 @@
+class ListNode {
+  int val;
+  ListNode? next;
+  ListNode([this.val = 0, this.next]);
+}
+
+class MergeTwoSortedLists {
+  ListNode? mergeTwoLists(ListNode? list1, ListNode? list2) {
+    if (list1 == null) {
+      return list2;
+    } else if (list2 == null) {
+      return list1;
+    } else {
+      ListNode result = ListNode();
+      ListNode current = result;
+
+      while (list1 != null || list2 != null) {
+        if (list1 != null && list2 != null) {
+          if (list1.val > list2.val) {
+            current.next = list2;
+            list2 = list2.next;
+          } else {
+            current.next = list1;
+            list1 = list1.next;
+          }
+        } else if (list1 != null) {
+          current.next = list1;
+          list1 = list1.next;
+        } else if (list2 != null) {
+          current.next = list2;
+          list2 = list2.next;
+        }
+        current = current.next!;
+      }
+
+      return result.next;
+    }
+  }
+
+  // Best Solution
+  ListNode? bestSolution(ListNode? list1, ListNode? list2) {
+    if (list1 == null) return list2;
+    if (list2 == null) return list1;
+
+    if (list1.val < list2.val) {
+      list1.next = bestSolution(list1.next, list2);
+      return list1;
+    } else {
+      list2.next = bestSolution(list1, list2.next);
+      return list2;
+    }
+  }
+}
