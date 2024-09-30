897621d09935edb4a0e8f2eb734295c51ae3fb30
miga-choi <miga.choi.1004@gmail.com>
2024-09-30 22:49:20 +0900

    leetcode: "27_remove_element" add dart solution

---
+++ leetcode/algorithms/27_remove_element/main.dart
@@ -0,0 +1,18 @@
+class RemoveElement {
+  int removeElement(List<int> nums, int val) {
+    nums.removeWhere((e) => e == val);
+    return 0;
+  }
+
+  // Best Solution
+  int bestSolution(List<int> nums, int val) {
+    int cnt = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != val) {
+        nums[cnt] = nums[i];
+        cnt++;
+      }
+    }
+    return cnt;
+  }
+}
