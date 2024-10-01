504f3d3769372ca19ff5fd9c4f6566bdffd8415a
miga-choi <miga.choi.1004@gmail.com>
2024-10-02 00:06:41 +0900

    leetcode: "66_plus_one" add dart solution

---
+++ leetcode/algorithms/66_plus_one/main.dart
@@ -0,0 +1,36 @@
+class PlusOne {
+  List<int> plusOne(List<int> digits) {
+    int up = 1;
+
+    for (int i = digits.length - 1; i >= 0; i--) {
+      int num = digits[i] + up;
+      if (num >= 10) {
+        num -= 10;
+        up = 1;
+      } else {
+        up = 0;
+      }
+
+      digits[i] = num;
+    }
+
+    if (up > 0) {
+      digits.insert(0, 1);
+    }
+
+    return digits;
+  }
+
+  // Best Solution
+  List<int> bestSolution(List<int> digits) {
+    for (int i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] < 9) {
+        digits[i]++;
+        return digits;
+      }
+      digits[i] = 0;
+    }
+    digits.insert(0, 1);
+    return digits;
+  }
+}
