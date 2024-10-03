c1344899d742fe2e687c8094dbf15e552f6a8db2
miga-choi <miga.choi.1004@gmail.com>
2024-10-04 00:36:54 +0900

    leetcode: "70_climbing_stairs" add dart solution

---
+++ leetcode/algorithms/70_climbing_stairs/main.dart
@@ -0,0 +1,24 @@
+class ClimbingStairs {
+  int climbstairs(int n) {
+    if (n < 2) {
+      return 1;
+    }
+
+    List<int> dp = List<int>.filled(n, 0);
+    dp[0] = 1;
+    dp[1] = 2;
+
+    for (int i = 2; i < n; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n - 1];
+  }
+
+  // Best Solution
+  int bestSolution(int n) {
+    int a = 1, b = 1;
+    while (n-- > 0) a = (b += a) - a;
+    return a;
+  }
+}
