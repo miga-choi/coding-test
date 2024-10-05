7a068cefb19a2260d9ffa3f3f32819c803e4d82c
miga-choi <miga.choi.1004@gmail.com>
2024-10-05 20:20:43 +0900

    leetcode: "94_binary_tree_inorder_traversal" add dart solution

---
+++ leetcode/algorithms/94_binary_tree_inorder_traversal/main.dart
@@ -0,0 +1,50 @@
+class TreeNode {
+  int val;
+  TreeNode? left;
+  TreeNode? right;
+  TreeNode([this.val = 0, this.left, this.right]);
+}
+
+class BinaryTreeInorderTraversal {
+  List<int> inorderTraversal(TreeNode? root) {
+    if (root == null) {
+      return List<int>.empty();
+    }
+
+    List<int> result = List<int>.empty(growable: true);
+
+    int addVal(TreeNode node) {
+      if (node.left != null) {
+        addVal(node.left!);
+      }
+
+      result.add(node.val);
+
+      if (node.right != null) {
+        addVal(node.right!);
+      }
+
+      return node.val;
+    }
+
+    addVal(root);
+
+    return result;
+  }
+
+  // Best Solution
+  List<int> bestSolution(TreeNode? root) {
+    inOrderHelper(TreeNode? root, List<int> list) {
+      if (root != null) {
+        inOrderHelper(root.left, list);
+        list.add(root.val);
+        inOrderHelper(root.right, list);
+      }
+    }
+
+    List<int> list = [];
+    inOrderHelper(root, list);
+
+    return list;
+  }
+}
