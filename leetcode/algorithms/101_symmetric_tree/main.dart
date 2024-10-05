4fb8d3bfa8e42f4dd151aa4669e7ef802e31f471
miga-choi <miga.choi.1004@gmail.com>
2024-10-06 04:08:55 +0900

    leetcode: "101_symmetric_tree" add dart solution

---
+++ leetcode/algorithms/101_symmetric_tree/main.dart
@@ -0,0 +1,45 @@
+class TreeNode {
+  int val;
+  TreeNode? left;
+  TreeNode? right;
+  TreeNode([this.val = 0, this.left, this.right]);
+}
+
+class SymmetricTree {
+  bool isSymmetric(TreeNode? root) {
+    if (root == null) {
+      return true;
+    }
+
+    bool checkSymmetric(TreeNode? p, TreeNode? q) {
+      if (p == null && q == null) {
+        return true;
+      }
+
+      if (p != null && q != null) {
+        return checkSymmetric(p.left, q.right) &&
+            checkSymmetric(p.right, q.left) &&
+            p.val == q.val;
+      }
+
+      return false;
+    }
+
+    return checkSymmetric(root.left, root.right);
+  }
+
+  // Best Solution
+  bool bestSolution(TreeNode? root) => isMirror(root, root);
+
+  bool isMirror(TreeNode? t1, TreeNode? t2) {
+    if (t1 == null && t2 == null) {
+      return true;
+    }
+    if (t1 == null || t2 == null) {
+      return false;
+    }
+    return t1.val == t2.val &&
+        isMirror(t1.left, t2.right) &&
+        isMirror(t1.right, t2.left);
+  }
+}
