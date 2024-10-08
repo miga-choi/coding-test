a5270e391fd713158ec773adca8665b2c7b89c1f
miga-choi <miga.choi.1004@gmail.com>
2024-10-08 23:43:31 +0900

    leetcode: "111_minimum_depth_of_binary_tree" add dart solution

---
+++ leetcode/algorithms/111_minimum_depth_of_binary_tree/main.dart
@@ -0,0 +1,38 @@
+import 'dart:math';
+
+class TreeNode {
+  int val;
+  TreeNode? left;
+  TreeNode? right;
+  TreeNode([this.val = 0, this.left, this.right]);
+}
+
+class Solution {
+  int minDepth(TreeNode? root) {
+    if (root == null) {
+      return 0;
+    }
+
+    int leftDepth = minDepth(root.left);
+    int rightDepth = minDepth(root.right);
+
+    if (leftDepth == 0 && rightDepth == 0) {
+      return 1;
+    } else if (leftDepth == 0) {
+      return rightDepth + 1;
+    } else if (rightDepth == 0) {
+      return leftDepth + 1;
+    }
+
+    return min(leftDepth, rightDepth) + 1;
+  }
+
+  // Best Solution
+  int bestSolution(TreeNode? root) {
+    if (root == null) return 0;
+    int l = minDepth(root.left);
+    int r = minDepth(root.right);
+    if (l == 0 || r == 0) return l + r + 1;
+    return min(l, r) + 1;
+  }
+}
