c975290488f2267372c51a2bfcdd3e3a115835e6
miga-choi <miga.choi.1004@gmail.com>
2024-10-11 00:16:30 +0900

    leetcode: "119_pascals_triangle_2" add c++ solution

---
+++ leetcode/algorithms/119_pascals_triangle_2/main.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+using namespace std;
+
+class PascalsTriangle2
+{
+public:
+    vector<int> getRow(int rowIndex)
+    {
+        vector<vector<int>> result;
+
+        for (int i = 0; i <= rowIndex; i++)
+        {
+            vector<int> row;
+            for (int j = 0; j <= i; j++)
+            {
+                if (j == 0 || j == i)
+                {
+                    row.push_back(1);
+                }
+                else
+                {
+                    row.push_back(result[i - 1][j - 1] + result[i - 1][j]);
+                }
+            }
+            result.push_back(row);
+        }
+
+        return result[rowIndex];
+    }
+
+    // Best Solution
+    vector<int> bestSolution(int rowIndex)
+    {
+        vector<int> res(1, 1);
+        long long prev = 1;
+        for (int k = 1; k <= rowIndex; k++)
+        {
+            long long next_val = prev * (rowIndex - k + 1) / k;
+            res.push_back(next_val);
+            prev = next_val;
+        }
+        return res;
+    }
+};
