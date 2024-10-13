7aeeac6a74ec5f9c46c6add086142ae6c128a816
miga-choi <miga.choi.1004@gmail.com>
2024-10-13 20:41:28 +0900

    leetcode: "121_best_time_to_buy_and_sell_stock" add c++ solution

---
+++ leetcode/algorithms/121_best_time_to_buy_and_sell_stock/main.cpp
@@ -0,0 +1,47 @@
+#include <limits.h>
+#include <vector>
+using namespace std;
+
+class BestTimeToBuyAndSellStock
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int minPrice = 10000;
+        int price = 0;
+        for (int i = 0; i < prices.size(); i++)
+        {
+            if (prices[i] < minPrice)
+            {
+                minPrice = prices[i];
+            }
+            if (prices[i] - minPrice > price)
+            {
+                price = prices[i] - minPrice;
+            }
+        }
+        return price;
+    }
+
+    // Best Solution
+    int bestSolution(vector<int> &prices)
+    {
+        int lsf = INT_MAX;
+        int op = 0;
+        int pist = 0;
+
+        for (int i = 0; i < prices.size(); i++)
+        {
+            if (prices[i] < lsf)
+            {
+                lsf = prices[i];
+            }
+            pist = prices[i] - lsf;
+            if (op < pist)
+            {
+                op = pist;
+            }
+        }
+        return op;
+    }
+};
