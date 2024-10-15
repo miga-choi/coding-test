c008fd7a2bb08047131c3440b61a2686ca805e5e
miga-choi <miga.choi.1004@gmail.com>
2024-10-15 23:30:19 +0900

    leetcode: "136_single_number" add c solution

---
+++ leetcode/algorithms/136_single_number/main.c
@@ -0,0 +1,26 @@
+int singleNumber(int *nums, int numsSize)
+{
+    int result = 0;
+    for (int i = 0; i < numsSize; i++)
+    {
+        result = result ^ nums[i];
+    }
+    return result;
+}
+
+// Best Solution
+int bestSolution(int *nums, int numsSize)
+{
+    // Initialize the unique number...
+    int uniqNum = 0;
+
+    // TRaverse all elements through the loop...
+    for (int idx = 0; idx < numsSize; idx++)
+    {
+        // Concept of XOR...
+        uniqNum = uniqNum ^ nums[idx];
+    }
+
+    // Return the unique number...
+    return uniqNum;
+}
