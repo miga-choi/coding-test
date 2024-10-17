094866a37aa97eb32933515b2993fa9e48a6502d
miga-choi <miga.choi.1004@gmail.com>
2024-10-18 01:21:01 +0900

    leetcode: "144_binary_tree_preorder_traversal" add c solution

---
+++ leetcode/algorithms/144_binary_tree_preorder_traversal/main.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+
+struct TreeNode
+{
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+void addVal(struct TreeNode *node, int *returnSize, int *result)
+{
+    if (node != 0)
+    {
+        result[*returnSize] = node->val;
+        *returnSize = (*returnSize) + 1;
+        addVal(node->left, returnSize, result);
+        addVal(node->right, returnSize, result);
+    }
+}
+
+int *preorderTraversal(struct TreeNode *root, int *returnSize)
+{
+    int *result = (int *)malloc(sizeof(int) * 100);
+    *returnSize = 0;
+    addVal(root, returnSize, result);
+    return result;
+}
+
+// Best Solution
+void preorder(struct TreeNode *root, int *arr, int *returnSize)
+{
+    if (root)
+    {
+        arr[(*returnSize)++] = root->val;
+        preorder(root->left, arr, returnSize);
+        preorder(root->right, arr, returnSize);
+    }
+    return;
+}
+
+int *bestSolution(struct TreeNode *root, int *returnSize)
+{
+    int *arr = (int *)malloc(sizeof(int) * 100);
+    *returnSize = 0;
+    preorder(root, arr, returnSize);
+    return arr;
+    free(arr);
+}
