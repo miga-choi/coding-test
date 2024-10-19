2fc9b76c26e3334f34bf0b6a9445f66edaa02879
miga-choi <miga.choi.1004@gmail.com>
2024-10-20 01:07:48 +0900

    leetcode: "145_binary_tree_postorder_traversal" add dart solution

---
+++ leetcode/algorithms/145_binary_tree_postorder_traversal/main.dart
@@ -0,0 +1,34 @@
+class TreeNode {
+  int val;
+  TreeNode? left;
+  TreeNode? right;
+  TreeNode([this.val = 0, this.left, this.right]);
+}
+
+class BinaryTreePostorderTraversal {
+  void addVal(TreeNode? node, List<int> result) {
+    if (node != null) {
+      addVal(node.left, result);
+      addVal(node.right, result);
+      result.add(node.val);
+    }
+  }
+
+  List<int> postorderTraversal(TreeNode? root) {
+    List<int> result = List<int>.empty(growable: true);
+    addVal(root, result);
+    return result;
+  }
+
+  // Best Solution
+  List<int> bestSolution(TreeNode? root) {
+    if (root == null) {
+      return [];
+    }
+    List<int> result = [];
+    result.addAll(postorderTraversal(root.left));
+    result.addAll(postorderTraversal(root.right));
+    result.add(root.val);
+    return result;
+  }
+}
