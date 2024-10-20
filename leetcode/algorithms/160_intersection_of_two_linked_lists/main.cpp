6ea641ff5acaa0ab6cc02224c98b0302d9f4d0b3
miga-choi <miga.choi.1004@gmail.com>
2024-10-21 00:12:19 +0900

    leetcode: "160_intersection_of_two_linked_lists" add c++ solution

---
+++ leetcode/algorithms/160_intersection_of_two_linked_lists/main.cpp
@@ -0,0 +1,58 @@
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class IntersectionOfTwoLinkedLists
+{
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+    {
+        while (headA != nullptr)
+        {
+            ListNode *temp = headB;
+            while (temp != nullptr)
+            {
+                if (temp == headA)
+                    return temp;
+                temp = temp->next;
+            }
+            headA = headA->next;
+        }
+        return nullptr;
+    }
+
+    // Best Solution
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+    {
+        struct ListNode *p1 = headA;
+        struct ListNode *p2 = headB;
+
+        if (p1 == nullptr || p2 == nullptr)
+            return nullptr;
+
+        while (p1 != nullptr && p2 != nullptr && p1 != p2)
+        {
+            p1 = p1->next;
+            p2 = p2->next;
+
+            // Any time they collide or reach end together without colliding
+            // then return any one of the pointers.
+            if (p1 == p2)
+                return p1;
+
+            // If one of them reaches the end earlier then reuse it
+            // by moving it to the beginning of other list.
+            // Once both of them go through reassigning,
+            // they will be equidistant from the collision point.
+            if (p1 == nullptr)
+                p1 = headB;
+            if (p2 == nullptr)
+                p2 = headA;
+        }
+
+        return p1;
+    }
+};
