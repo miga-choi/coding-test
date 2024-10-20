00236a8c955559f9c1b9f9d676140d3069e9e3f3
miga-choi <miga.choi.1004@gmail.com>
2024-10-21 00:10:31 +0900

    leetcode: "160_intersection_of_two_linked_lists" add c solution

---
+++ leetcode/algorithms/160_intersection_of_two_linked_lists/main.c
@@ -0,0 +1,33 @@
+struct ListNode
+{
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB)
+{
+    while (headA != 0)
+    {
+        struct ListNode *temp = headB;
+        while (temp != 0)
+        {
+            if (temp == headA)
+                return temp;
+            temp = temp->next;
+        }
+        headA = headA->next;
+    }
+    return 0;
+}
+
+// Best Solution
+struct ListNode *bestSolution(struct ListNode *headA, struct ListNode *headB)
+{
+    struct ListNode *a = headA, *b = headB;
+    while (a != b)
+    {
+        a = !a ? headB : a->next;
+        b = !b ? headA : b->next;
+    }
+    return a;
+}
