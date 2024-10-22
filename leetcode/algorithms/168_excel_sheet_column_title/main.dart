a88570f43e2582eedd4d43ed22fb6c08da87efe7
miga-choi <miga.choi.1004@gmail.com>
2024-10-23 00:36:08 +0900

    leetcode: "168_excel_sheet_column_title" add dart solution

---
+++ leetcode/algorithms/168_excel_sheet_column_title/main.dart
@@ -0,0 +1,22 @@
+class ExcelSheetColumnTitle {
+  String convertToTitle(int columnNumber) {
+    String result = "";
+    while (columnNumber > 0) {
+      columnNumber -= 1;
+      result = String.fromCharCode(columnNumber % 26 + 65) + result;
+      columnNumber = (columnNumber / 26).floor();
+    }
+    return result;
+  }
+
+  // Best Solution
+  String bestSolution(int columnNumber) {
+    StringBuffer stringBuffer = StringBuffer();
+    while (columnNumber > 0) {
+      int reminder = (columnNumber - 1) % 26;
+      stringBuffer.write(String.fromCharCode(reminder + 'A'.codeUnitAt(0)));
+      columnNumber = (columnNumber - 1) ~/ 26;
+    }
+    return stringBuffer.toString().split('').reversed.join();
+  }
+}
