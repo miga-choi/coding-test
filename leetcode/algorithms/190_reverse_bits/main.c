eafe5380b4629df127ee876450bdfc4ec8fddcec
miga-choi <miga.choi.1004@gmail.com>
2024-10-26 23:52:17 +0900

    leetcode: "190_reverse_bits" add c solution

---
+++ leetcode/algorithms/190_reverse_bits/main.c
@@ -0,0 +1,24 @@
+#include <stdint.h>
+
+uint32_t reverseBits(uint32_t n)
+{
+    uint32_t m = 0;
+    for (int i = 0; i < 32; i++, n >>= 1)
+    {
+        m <<= 1;
+        m |= n & 1;
+    }
+    return m;
+}
+
+// Best Solution
+uint32_t bestSolution(uint32_t n)
+{
+    uint32_t m = 0;
+    for (int i = 0; i < 32; i++, n >>= 1)
+    {
+        m <<= 1;
+        m |= n & 1;
+    }
+    return m;
+}
