a406cd080109e1fc2b69c232994d9eef0796192d
miga-choi <miga.choi.1004@gmail.com>
2024-10-30 22:45:41 +0900

    leetcode: "203_remove_linked_list_elements" add c solution

---
+++ leetcode/algorithms/203_remove_linked_list_elements/main.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+
+struct ListNode
+{
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode *removeElements(struct ListNode *head, int val)
+{
+    struct ListNode *result = (struct ListNode *)malloc(sizeof(struct ListNode));
+    result->next = head;
+
+    struct ListNode *current = result;
+
+    while (current->next)
+    {
+        if (current->next->val == val)
+        {
+            current->next = current->next->next;
+            continue;
+        }
+        current = current->next;
+    }
+
+    return result->next;
+}
+
+// Best Solution
+struct ListNode *bestSolution(struct ListNode *head, int val)
+{
+    struct ListNode *temp = (struct ListNode *)malloc(sizeof(struct ListNode));
+    temp->next = head;
+    struct ListNode *curr = temp;
+    while (curr->next != NULL)
+    {
+        if (curr->next->val == val)
+            curr->next = curr->next->next;
+        else
+            curr = curr->next;
+    }
+    return temp->next;
+}
