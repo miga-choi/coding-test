76447e81786b210fc1170bd0b62cbe376f65d179
miga-choi <miga.choi.1004@gmail.com>
2024-10-31 21:12:00 +0900

    leetcode: "203_remove_linked_list_elements" add dart solution

---
+++ leetcode/algorithms/203_remove_linked_list_elements/main.dart
@@ -0,0 +1,31 @@
+class ListNode {
+  int val;
+  ListNode? next;
+  ListNode([this.val = 0, this.next]);
+}
+
+class RemovrlinkedListElements {
+  ListNode? removeElements(ListNode? head, int val) {
+    ListNode? result = ListNode();
+    result.next = head;
+    ListNode? current = result;
+
+    while (current?.next != null) {
+      if (current?.next?.val == val) {
+        current?.next = current.next?.next;
+      } else {
+        current = current?.next;
+      }
+    }
+
+    return result.next;
+  }
+
+  // Best Solution
+  ListNode? bestSolution(ListNode? head, int val) {
+    if (head == null) return null;
+    if (head.next != null) head.next = removeElements(head.next, val);
+    if (head.val == val) head = head.next;
+    return head;
+  }
+}
