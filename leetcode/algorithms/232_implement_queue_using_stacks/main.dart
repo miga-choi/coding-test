20cf9ab687372663f4a2a6826329e63f5a8cbaf7
miga-choi <miga.choi.1004@gmail.com>
2024-11-15 18:28:14 +0900

    leetcode: "232_implement_queue_using_stacks" add dart solution

---
+++ leetcode/algorithms/232_implement_queue_using_stacks/main.dart
@@ -0,0 +1,66 @@
+class MyQueue {
+  late final List<int> input;
+  late final List<int> output;
+
+  MyQueue() {
+    input = List<int>.empty(growable: true);
+    output = List<int>.empty(growable: true);
+  }
+
+  // Pushes element `x` to the back of the queue.
+  void push(int x) {
+    input.add(x);
+  }
+
+  // Removes the element from the front of the queue and returns it.
+  int pop() {
+    peek();
+    return output.removeLast();
+  }
+
+  // Returns the element at the front of the queue.
+  int peek() {
+    if (output.isEmpty) {
+      while (input.isNotEmpty) {
+        output.add(input.removeLast());
+      }
+    }
+
+    return output.last;
+  }
+
+  // Returns `true` if the queue is empty, `false` otherwise.
+  bool empty() {
+    return input.isEmpty && output.isEmpty;
+  }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = MyQueue();
+ * obj.push(x);
+ * int param2 = obj.pop();
+ * int param3 = obj.peek();
+ * bool param4 = obj.empty();
+ */
+
+// Best Solution
+class BestSolution {
+  List input = [];
+
+  void push(int x) {
+    input.add(x);
+  }
+
+  int pop() {
+    return input.removeAt(0);
+  }
+
+  int peek() {
+    return input.first;
+  }
+
+  bool empty() {
+    return input.isEmpty;
+  }
+}
