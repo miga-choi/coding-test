e410f8da3c58b6993923094f523a4cc84b4fbee4
miga-choi <miga.choi.1004@gmail.com>
2024-11-18 21:01:42 +0900

    leetcode: "242_valid_anagram" add dart solution

---
+++ leetcode/algorithms/242_valid_anagram/main.dart
@@ -0,0 +1,43 @@
+class ValidAnagram {
+  bool isAnagram(String s, String t) {
+    if (s.length != t.length) {
+      return false;
+    }
+
+    for (String c in s.split('')) {
+      t = t.replaceFirst(c, '');
+    }
+
+    return t.isEmpty;
+  }
+
+  // Best Solution 1
+  bool bestSolution1(String s, String t) {
+    List<String> s1 = s.split('');
+    List<String> s2 = t.split('');
+    s1.sort();
+    s2.sort();
+    bool result = true;
+    if (s1.length != s2.length) {
+      result = false;
+    } else {
+      for (int i = 0; i < s1.length; i++) {
+        if (s1[i] != s2[i]) {
+          result = false;
+          break;
+        }
+      }
+    }
+    return result;
+  }
+
+  // Best Solution 2
+  bool bestSolution2(String s, String t) {
+    if (s.length != t.length) {
+      return false;
+    }
+    var sortedS = (s.split('')..sort()).join('');
+    var sortedT = (t.split('')..sort()).join('');
+    return sortedT == sortedS;
+  }
+}
