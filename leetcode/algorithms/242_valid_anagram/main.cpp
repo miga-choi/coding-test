f14bec62b5de197254a17d8b4aa8e4ac4c0c8ba2
miga-choi <miga.choi.1004@gmail.com>
2024-11-18 21:01:02 +0900

    leetcode: "242_valid_anagram" add c++ solution

---
+++ leetcode/algorithms/242_valid_anagram/main.cpp
@@ -0,0 +1,99 @@
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class ValidAnagram
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+        if (s.length() != t.length())
+        {
+            return false;
+        }
+
+        int alphabetNumArray[26] = {0};
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            alphabetNumArray[s.at(i) - 'a']++;
+            alphabetNumArray[t.at(i) - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (alphabetNumArray[i] > 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // Best Solution 1: Sorting
+    bool bestSolution1(string s, string t)
+    {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+
+    // Best Solution 2: Hash Table
+    bool bestSolution2(string s, string t)
+    {
+        unordered_map<char, int> count;
+
+        // Count the frequency of characters in string `s`
+        for (auto x : s)
+        {
+            count[x]++;
+        }
+
+        // Decrement the frequency of characters in string `t`
+        for (auto x : t)
+        {
+            count[x]--;
+        }
+
+        // Check if any character has non-zero frequency
+        for (auto x : count)
+        {
+            if (x.second != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // Best Solution 3: Hash Table (Using Array)
+    bool bestSolution3(string s, string t)
+    {
+        int count[26] = {0};
+
+        // Count the frequency of characters in string `s`
+        for (char x : s)
+        {
+            count[x - 'a']++;
+        }
+
+        // Decrement the frequency of characters in string `t`
+        for (char x : t)
+        {
+            count[x - 'a']--;
+        }
+
+        // Check if any character has non-zero frequency
+        for (int val : count)
+        {
+            if (val != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
