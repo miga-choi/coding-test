76b722774517e3580e34624d87ea47c58c9c5906
miga-choi <miga.choi.1004@gmail.com>
2024-11-22 00:07:34 +0900

    leetcode: "263_ugly_number" add c++ solution

---
+++ leetcode/algorithms/263_ugly_number/main.cpp
@@ -0,0 +1,29 @@
+class UglyNumber {
+public:
+    bool isUgly(int n) {
+        while (n) {
+            if (!(n % 5)) {
+                n /= 5;
+            } else if (!(n % 3)) {
+                n /= 3;
+            } else if (!(n % 2)) {
+                n /= 2;
+            } else {
+                if (!n || n == 1 || n == 2 || n == 3 || n == 5) {
+                    return true;
+                } else {
+                    break;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Best Solution
+    bool bestSolution(int n) {
+        for (int i = 2; i < 6 && n; i++)
+            while (n % i == 0)
+                n /= i;
+        return n == 1;
+    }
+};
