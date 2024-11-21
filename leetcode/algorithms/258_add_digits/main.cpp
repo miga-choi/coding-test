2a150992d7c2b40aa5b08e6f05cab98af400f19a
miga-choi <miga.choi.1004@gmail.com>
2024-11-21 20:34:12 +0900

    leetcode: "258_add_digits" add c++ solution

---
+++ leetcode/algorithms/258_add_digits/main.cpp
@@ -0,0 +1,28 @@
+class AddDigits {
+public:
+    int addDigits(int num) {
+        int sum = num;
+        while (sum >= 10) {
+            sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return sum;
+    }
+
+    // Best Solution
+    int bestSolution(int num) {
+        int sum = 0;
+        while (num) {
+            sum += (num % 10);
+            num /= 10;
+        }
+        if (sum < 10)
+            return sum;
+        else
+            return addDigits(sum);
+    }
+};
