a1b1f435d83a9245a5205e1e42842d16058c2ab4
miga-choi <miga.choi.1004@gmail.com>
2024-11-22 00:06:03 +0900

    leetcode: "263_ugly_number" add c solution

---
+++ leetcode/algorithms/263_ugly_number/main.c
@@ -0,0 +1,29 @@
+#include <stdbool.h>
+
+bool isUgly(int n) {
+    while (n > 0) {
+        if (n % 5 == 0) {
+            n /= 5;
+        } else if (n % 3 == 0) {
+            n /= 3;
+        } else if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            if (n == 0 || n == 1 || n == 2 || n == 3 || n == 5) {
+                return true;
+            } else {
+                break;
+            }
+        }
+    }
+    return false;
+}
+
+// Best Solution
+bool bestSolution(int n)
+{
+    for (int i = 2; i < 6 && n; i++)
+        while (n % i == 0)
+            n /= i;
+    return n == 1;
+}
