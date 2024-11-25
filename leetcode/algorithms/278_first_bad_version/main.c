a16151ce19a2e0ba877df13a6b59fb4facd211c1
miga-choi <miga.choi.1004@gmail.com>
2024-11-25 23:34:07 +0900

    leetcode: "278_first_bad_version" add c solution

---
+++ leetcode/algorithms/278_first_bad_version/main.c
@@ -0,0 +1,32 @@
+#include <stdbool.h>
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+int firstBadVersion(int n) {
+    int low = 1;
+    int high = n;
+    int mid;
+    while (low < high) {
+        mid = low + ((high - low) / 2);
+        if (isBadVersion(mid)) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return low;
+}
+
+// Best Solution
+int bestSolution(int n) {
+    int start = 0, end = n;
+    while (end - start > 1) {
+        int mid = start + (end - start) / 2;
+        if (isBadVersion(mid))
+            end = mid;
+        else
+            start = mid;
+    }
+    return end;
+}
