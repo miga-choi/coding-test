12553e1b43a6c0500a36adf8891348444823d4a5
miga-choi <miga.choi.1004@gmail.com>
2024-11-26 18:47:45 +0900

    leetcode: "283_move_zeroes" add c solution

---
+++ leetcode/algorithms/283_move_zeroes/main.c
@@ -0,0 +1,49 @@
+void moveZeroes(int *nums, int numsSize) {
+    int newNums[numsSize];
+    int count = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i]) {
+            newNums[count++] = nums[i];
+        }
+    }
+
+    for (int i = 0; i < count; i++) {
+        nums[i] = newNums[i];
+    }
+
+    for (int i = 1; i <= numsSize - count; i++) {
+        nums[numsSize - i] = 0;
+    }
+}
+
+// Best Solution 1
+void bestSolution1(int *nums, int numsSize) {
+    int j = 0;
+
+    // move all the non-zero elements advance
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != 0) {
+            nums[j++] = nums[i];
+        }
+    }
+
+    for (; j < numsSize; j++) {
+        nums[j] = 0;
+    }
+}
+
+// Best Solution 2
+void bestSolution2(int *nums, int numsSize) {
+    int slow = 0;
+    int fast = 0;
+    while (fast < numsSize) {
+        if (nums[fast] != 0) {
+            nums[slow] = nums[fast];
+            slow++;
+        }
+        fast++;
+    }
+    for (int i = slow; i < numsSize; i++)
+        nums[i] = 0;
+}
