03d9de4e31254ff282bb35075d96aec70b231940
miga-choi <miga.choi.1004@gmail.com>
2024-11-27 20:44:52 +0900

    leetcode: "283_move_zeroes" add dart solution

---
+++ leetcode/algorithms/283_move_zeroes/main.dart
@@ -0,0 +1,21 @@
+class MoveZeroes {
+  void moveZeroes(List<int> nums) {
+    int zeroCount = 0;
+    for (int num in nums) {
+      if (num == 0) {
+        zeroCount++;
+      }
+    }
+    nums.removeWhere((int num) => num == 0);
+    nums.addAll(List<int>.filled(zeroCount, 0));
+  }
+
+  // Best Solution
+  void bestSolution(List<int> nums) {
+    List<int> testValues = [];
+    nums.forEach((value) => testValues.add(value));
+    testValues.retainWhere((value) => value == 0);
+    nums.removeWhere((value) => value == 0);
+    nums.addAll(testValues);
+  }
+}
