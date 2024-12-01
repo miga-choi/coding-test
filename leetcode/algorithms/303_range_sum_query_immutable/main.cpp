7930d80ad60d14640ac3d8d16551aa9ca59485ee
miga-choi <miga.choi.1004@gmail.com>
2024-12-01 23:24:33 +0900

    leetcode: "303_range_sum_query_immutable" add c++ solution

---
+++ leetcode/algorithms/303_range_sum_query_immutable/main.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+using namespace std;
+
+class NumArray {
+public:
+    vector<int>& numArray;
+
+    NumArray(vector<int>& nums) : numArray(nums) {}
+
+    int sumRange(int left, int right) {
+        int sum = 0;
+        for (int i = left; i <= right; i++) {
+            sum += numArray[i];
+        }
+        return sum;
+    }
+
+    // Best Solution
+    vector<int>& preSum; // `preSum` will reference to `nums` array, no copy at all!
+
+    NumArray(vector<int>& nums) : preSum(nums) {
+        for (int i = 1; i < preSum.size(); ++i)
+            preSum[i] += preSum[i - 1];
+    }
+
+    int bestSolution_sumRange(int left, int right) {
+        if (left == 0)
+            return preSum[right];
+        return preSum[right] - preSum[left - 1];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
