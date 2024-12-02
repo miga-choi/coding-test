fb3cbf3442fd033391a808c6044bba18bdbffbf9
miga-choi <miga.choi.1004@gmail.com>
2024-12-02 22:24:24 +0900

    leetcode: "303_range_sum_query_immutable" add dart solution

--- leetcode/algorithms/303_range_sum_query_immutable/main.cpp
+++ leetcode/algorithms/303_range_sum_query_immutable/main.cpp
@@ -14,6 +14,29 @@ public:
         }
         return sum;
     }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
+
+
+// Best Solution
+class BestSolution {
+public:
+    vector<int>& numArray;
+
+    BestSolution(vector<int>& nums) : numArray(nums) {}
+
+    int sumRange(int left, int right) {
+        int sum = 0;
+        for (int i = left; i <= right; i++) {
+            sum += numArray[i];
+        }
+        return sum;
+    }
 
     // Best Solution
     vector<int>& preSum; // `preSum` will reference to `nums` array, no copy at all!
@@ -23,15 +46,9 @@ public:
             preSum[i] += preSum[i - 1];
     }
 
-    int bestSolution_sumRange(int left, int right) {
+    int sumRange(int left, int right) {
         if (left == 0)
             return preSum[right];
         return preSum[right] - preSum[left - 1];
     }
 };
-
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray* obj = new NumArray(nums);
- * int param_1 = obj->sumRange(left,right);
- */
---
+++ leetcode/algorithms/303_range_sum_query_immutable/main.dart
@@ -0,0 +1,43 @@
+class NumArray {
+  late final List<int> _numArray;
+
+  NumArray(List<int> nums) {
+    _numArray = nums;
+  }
+
+  int sumRange(int left, int right) {
+    int sum = 0;
+    for (var i = left; i <= right; i++) {
+      sum += _numArray[i];
+    }
+    return sum;
+  }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = NumArray(nums);
+ * int param1 = obj.sumRange(left,right);
+ */
+
+// Best Solution
+class BestSolution {
+  List<int> arr = [];
+
+  BestSolution(List<int> nums) {
+    List<int> sums = [];
+    var currentSum = 0;
+    for (var num in nums) {
+      currentSum += num;
+      sums.add(currentSum);
+    }
+    arr = sums;
+  }
+
+  int sumRange(int left, int right) {
+    if (left == 0) {
+      return arr[right];
+    }
+    return arr[right] - arr[left - 1];
+  }
+}
