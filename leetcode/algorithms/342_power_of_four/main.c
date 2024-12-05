78ce45aab8ef59c03591a0badd422c514398c623
miga-choi <miga.choi.1004@gmail.com>
2024-12-05 17:57:50 +0900

    leetcode: "342_power_of_four" add c solution

---
+++ leetcode/algorithms/342_power_of_four/main.c
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+
+bool isPowerOfFour(int n) {
+    while (n > 1) {
+        if (n % 4) {
+            return false;
+        }
+        n /= 4;
+    }
+
+    return n == 1;
+}
+
+// Best Solution
+bool bestSolution(int n) {
+    return n > 0 && (n & (n - 1)) == 0 && (n - 1) % 3 == 0;
+}
