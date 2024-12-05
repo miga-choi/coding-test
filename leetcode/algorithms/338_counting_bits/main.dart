5039c374bd87067a0090bdd1c0f1ef201be008f3
miga-choi <miga.choi.1004@gmail.com>
2024-12-05 17:57:13 +0900

    leetcode: "338_counting_bits" add dart solution

---
+++ leetcode/algorithms/338_counting_bits/main.dart
@@ -0,0 +1,30 @@
+class CountingBits {
+  List<int> countBits(int n) {
+    List<int> result = List<int>.filled(n + 1, 0);
+
+    for (int i = 0; i <= n; i++) {
+      int sum = 0;
+      int num = i;
+      while (num > 0) {
+        if (num % 2 == 1) {
+          sum++;
+        }
+        num = (num / 2).floor();
+      }
+      result[i] = sum;
+    }
+
+    return result;
+  }
+
+  // Best Solution
+  List<int> bestSolution(int n) {
+    List<int> result = List.filled(n + 1, 0);
+
+    for (int i = 1; i <= n; i++) {
+      result[i] = result[i >> 1] + (i & 1);
+    }
+
+    return result;
+  }
+}
