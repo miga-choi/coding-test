f8431919a17c3f5a4da084e7d1170e0aeedc095d
miga-choi <miga.choi.1004@gmail.com>
2024-12-06 13:46:40 +0900

    leetcode: "342_power_of_four" add c++ solution

---
+++ leetcode/algorithms/342_power_of_four/main.cpp
@@ -0,0 +1,17 @@
+class PowerOfFour {
+public:
+    bool isPowerOfFour(int n) {
+        while (n > 1) {
+            if (n % 4) {
+                return false;
+            }
+            n /= 4;
+        }
+        return n == 1;
+    }
+
+    // Best Solution
+    bool bestSolution(int n) {
+        return n > 0 && (n & (n - 1)) == 0 && (n - 1) % 3 == 0;
+    }
+};
