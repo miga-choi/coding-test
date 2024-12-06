31b9da84cad286eb64fc350cffb0642d96a36958
miga-choi <miga.choi.1004@gmail.com>
2024-12-07 05:16:05 +0900

    leetcode: "344_reverse_string" add c solution

---
+++ leetcode/algorithms/344_reverse_string/main.c
@@ -0,0 +1,21 @@
+void reverseString(char *s, int sSize) {
+    for (int i = 0; i < sSize / 2; i++) {
+        char temp = s[i];
+        s[i] = s[sSize - i - 1];
+        s[sSize - i - 1] = temp;
+    }
+}
+
+// Best Solution
+void bestSolution(char *s, int sSize) {
+    int i = 0;
+    int j = sSize - 1;
+
+    while (i <= j) {
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        i++;
+        j--;
+    }
+}
