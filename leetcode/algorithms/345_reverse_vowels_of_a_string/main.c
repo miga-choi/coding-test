77ce39890ac246e6b3855e0f744e144ed1679efe
miga-choi <miga.choi.1004@gmail.com>
2024-12-08 19:36:10 +0900

    leetcode: "345_reverse_vowels_of_a_string" add c solution

---
+++ leetcode/algorithms/345_reverse_vowels_of_a_string/main.c
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include <string.h>
+
+char *reverseVowels(char *s) {
+    int sLength = 0;
+    int vowelsLength = 0;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (s[i] == 'A' || s[i] == 'E' || s[i] == 'I' ||
+            s[i] == 'O' || s[i] == 'U' || s[i] == 'a' ||
+            s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
+            s[i] == 'u') {
+            vowelsLength++;
+        }
+        sLength++;
+    }
+
+    char *vowels = (char *)malloc(sizeof(char) * vowelsLength);
+
+    int count = 0;
+    for (int i = 0; i < sLength; i++) {
+        if (s[i] == 'A' || s[i] == 'E' || s[i] == 'I' ||
+            s[i] == 'O' || s[i] == 'U' || s[i] == 'a' ||
+            s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
+            s[i] == 'u') {
+            vowels[count++] = s[i];
+        }
+    }
+
+    count = 0;
+    for (int i = 0; i < sLength; i++) {
+        if (s[i] == 'A' || s[i] == 'E' || s[i] == 'I' ||
+            s[i] == 'O' || s[i] == 'U' || s[i] == 'a' ||
+            s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
+            s[i] == 'u') {
+            s[i] = vowels[vowelsLength - count - 1];
+            count++;
+        }
+    }
+
+    return s;
+}
+
+// Best Solution
+bool isVowel(char c) {
+    char vowels[] = "aeiouAEIOU";
+    for (int i = 0; vowels[i] != '\0'; i++) {
+        if (c == vowels[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+char *bestSolution(char *s) {
+    int start = 0;
+    int end = strlen(s) - 1;
+
+    while (start < end) {
+        while (start < end && !isVowel(s[start])) {
+            start++;
+        }
+        while (start < end && !isVowel(s[end])) {
+            end--;
+        }
+
+        // Swap vowels
+        char temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+
+        start++;
+        end--;
+    }
+    return s;
+}
