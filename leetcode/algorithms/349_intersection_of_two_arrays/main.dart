e155d4a660477b284610230300dda697dfceeea6
miga-choi <miga.choi.1004@gmail.com>
2024-12-11 06:08:48 +0900

    leetcode: "349_intersection_of_two_arrays" add dart solution

---
+++ leetcode/algorithms/349_intersection_of_two_arrays/main.dart
@@ -0,0 +1,22 @@
+class IntersectionOfTwoArrays {
+  List<int> intersection(List<int> nums1, List<int> nums2) {
+    Set<int> numsSet = Set<int>();
+
+    for (int num1 in nums1) {
+      for (int num2 in nums2) {
+        if (num1 == num2) {
+          numsSet.add(num1);
+        }
+      }
+    }
+
+    return numsSet.toList();
+  }
+
+  // Best Solution
+  List<int> bestSolution(List<int> nums1, List<int> nums2) {
+    Set<int> newNums1 = nums1.toSet();
+    Set<int> newNum2 = nums2.toSet();
+    return newNums1.intersection(newNum2).toList();
+  }
+}
