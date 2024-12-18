5d588d32f391a5f96af259a630221f58da72c77b
miga-choi <miga.choi.1004@gmail.com>
2024-12-18 18:44:16 +0900

    leetcode: "387_first_unique_character_in_a_string" add dart solution

---
+++ leetcode/algorithms/387_first_unique_character_in_a_string/main.dart
@@ -0,0 +1,52 @@
+class FirstUniqueCharacterInAString {
+  // List
+  int firstUniqChar1(String s) {
+    List<int> alphabetNumArray = List<int>.filled(26, 0);
+    List<String> sArray = s.split("");
+
+    for (String c in sArray) {
+      alphabetNumArray[c.codeUnitAt(0) - 'a'.codeUnitAt(0)] += 1;
+    }
+
+    for (int i = 0; i < sArray.length; i++) {
+      if (alphabetNumArray[sArray[i].codeUnitAt(0) - 'a'.codeUnitAt(0)] == 1) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+
+  // Map
+  int firstUniqChar2(String s) {
+    Map<String, int> alphabetNumMap = Map<String, int>.new();
+    List<String> sArray = s.split("");
+
+    for (String c in sArray) {
+      if (alphabetNumMap[c] == null) {
+        alphabetNumMap[c] = 0;
+      } else {
+        alphabetNumMap[c] = alphabetNumMap[c]! + 1;
+      }
+    }
+
+    for (int i = 0; i < sArray.length; i++) {
+      if (alphabetNumMap[sArray[i]] == 1) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+
+  // Best Solution
+  int bestSolution(String s) {
+    final inputArr = s.split('');
+    for (final value in inputArr) {
+      if (inputArr.indexOf(value) == inputArr.lastIndexOf(value)) {
+        return inputArr.indexOf(value);
+      }
+    }
+    return -1;
+  }
+}
