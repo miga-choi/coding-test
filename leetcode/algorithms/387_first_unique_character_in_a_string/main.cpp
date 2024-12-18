d635414962b40ec297a4a5ec2c596b1855c8def7
miga-choi <miga.choi.1004@gmail.com>
2024-12-18 18:43:37 +0900

    leetcode: "387_first_unique_character_in_a_string" add c++ solution

---
+++ leetcode/algorithms/387_first_unique_character_in_a_string/main.cpp
@@ -0,0 +1,57 @@
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class FirstUniqueCharacterInAString {
+public:
+    int firstUniqChar(string s) {
+        int alphabetNumArray[26] = {0};
+
+        for (int i = 0; i < s.size(); i++) {
+            alphabetNumArray[s.at(i) - 'a']++;
+        }
+
+        for (int i = 0; i < s.size(); i++) {
+            if (alphabetNumArray[s.at(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    // Best Solution 1
+    int bestSolution1(string s) {
+        unordered_map<char, int> m;
+
+        for (char &c : s) {
+            m[c]++;
+        }
+
+        for (int i = 0; i < s.size(); i++) {
+            if (m[s[i]] == 1)
+                return i;
+        }
+
+        return -1;
+    }
+
+    // Best Solution 2
+    int bestSolution2(string s) {
+        unordered_map<char, pair<int, int>> m;
+        int idx = s.size();
+
+        for (int i = 0; i < s.size(); i++) {
+            m[s[i]].first++;
+            m[s[i]].second = i;
+        }
+
+        for (const auto &[c, p] : m) {
+            if (p.first == 1) {
+                idx = min(idx, p.second);
+            }
+        }
+
+        return idx == s.size() ? -1 : idx;
+    }
+};
