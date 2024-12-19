f4d44f0a5ee403510d5047e63115722c088c3249
miga-choi <miga.choi.1004@gmail.com>
2024-12-19 18:10:55 +0900

    leetcode: "389_find_the_difference" add c solution

---
+++ leetcode/algorithms/389_find_the_difference/main.c
@@ -0,0 +1,30 @@
+#include <string.h>
+
+char findTheDifference(char* s, char* t) {
+    int alphabetNumArray[26] = {0};
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        alphabetNumArray[s[i] - 'a']++;
+    }
+
+    for (int i = 0; t[i] != '\0'; i++) {
+        if (alphabetNumArray[t[i] - 'a'] == 0) {
+            return t[i];
+        } else {
+            alphabetNumArray[t[i] - 'a']--;
+        }
+    }
+
+    return ' ';
+}
+
+// Best Solution
+char bestSolution(char* s, char* t) {
+    // Passing the diff: (t[i]-s[i]) to t[i+1]
+    for (int i = 0; i < strlen(s); i++) {
+        t[i + 1] += t[i] - s[i];
+    }
+
+    // The diff will be carried over to the last element eventually
+    return t[strlen(t) - 1];
+}
