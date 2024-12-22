e506e397a18299ba76a599f283f8e158cacf8fd0
miga-choi <miga.choi.1004@gmail.com>
2024-12-23 00:03:57 +0900

    leetcode: "401_binary_watch" add dart solution

---
+++ leetcode/algorithms/401_binary_watch/main.dart
@@ -0,0 +1,40 @@
+class BinaryWatch {
+  List<String> readBinaryWatch(int turnedOn) {
+    List<String> result = List<String>.empty(growable: true);
+
+    // Hour
+    for (int h = 0; h < 12; h++) {
+      int hBitCount = h.toRadixString(2).replaceAll("0", "").length;
+
+      // Minute
+      for (int m = 0; m < 60; m++) {
+        int mBitCount = m.toRadixString(2).replaceAll("0", "").length;
+
+        // Count
+        if (hBitCount + mBitCount == turnedOn) {
+          result.add(m < 10 ? "$h:0$m" : "$h:$m");
+        }
+      }
+    }
+
+    return result;
+  }
+
+  // Best Solution
+  List<String> bestSolution(int turnedOn) {
+    List<String> res = [];
+    for (int hour = 0; hour < 12; hour++) {
+      for (int min = 0; min < 60; min++) {
+        String temp = hour.toRadixString(2) + min.toRadixString(2);
+        int count = 0;
+        for (int i = 0; i < temp.length; i++) {
+          if (temp[i] == '1') count++;
+        }
+        if (count == turnedOn) {
+          res.add('$hour:${min.toString().padLeft(2, '0')}');
+        }
+      }
+    }
+    return res;
+  }
+}
