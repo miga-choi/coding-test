20eff569ee6560eae2ae5ed535840c1bad35b270
miga-choi <miga.choi.1004@gmail.com>
2024-12-25 22:35:55 +0900

    leetcode: "405_convert_a_number_to_hexadecimal" add c solution

---
+++ leetcode/algorithms/405_convert_a_number_to_hexadecimal/main.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+
+char* toHex(int num) {
+    char* hex = "0123456789abcdef";
+    unsigned int unum = num;
+    int count = 0;
+    char* result = (char*)malloc(sizeof(char) * 9);
+    result[8] = '\0';
+
+    while (1) {
+        result[7 - count] = hex[unum % 16];
+        unum >>= 4;
+        count++;
+        if (!unum) {
+            break;
+        }
+    }
+
+    return &result[8 - count];
+}
+
+// Best Solution
+char* bestSolution(int num) {
+    char *hex = (char *)malloc(sizeof(char) * 10);
+    char nib;
+    int i = 0;
+    unsigned int x = num;
+    hex[9] = '\0';
+    while (1) {
+        nib = (x & 0xF);
+        hex[8 - i] = (nib > 9) ? ('a' + nib - 10) : ('0' + nib);
+        x >>= 4;
+        if (!x) break;
+        i++;
+    }
+    return &hex[8 - i];
+}
