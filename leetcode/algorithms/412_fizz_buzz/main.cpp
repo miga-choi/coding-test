e946b683c39d6ddf9f622f063dfe3bd16fda27b3
miga-choi <miga.choi.1004@gmail.com>
2024-12-28 00:08:55 +0900

    leetcode: "412_fizz_buzz" add c++ solution

---
+++ leetcode/algorithms/412_fizz_buzz/main.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+using namespace std;
+
+class FizzBuzz {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> result;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.push_back("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.push_back("Fizz");
+            } else if (i % 5 == 0) {
+                result.push_back("Buzz");
+            } else {
+                result.push_back(to_string(i));
+            }
+        }
+
+        return result;
+    }
+
+    // Best Solution
+    vector<string> bestSolution(int n) {
+        vector<string> res(n);
+
+        for (int i = 1; i <= n; i++) {
+            res[i - 1] = to_string(i);
+        }
+
+        for (int i = 2; i < n; i += 3) {
+            res[i] = "Fizz";
+        }
+
+        for (int i = 4; i < n; i += 5) {
+            res[i] = "Buzz";
+        }
+
+        for (int i = 14; i < n; i += 15) {
+            res[i] = "FizzBuzz";
+        }
+
+        return res;
+    }
+};
