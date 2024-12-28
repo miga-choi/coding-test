e9413c5b2b7375bcbd950be1b8147721e8ccbc74
miga-choi <miga.choi.1004@gmail.com>
2024-12-29 00:46:42 +0900

    leetcode: "412_fizz_buzz" add dart solution

---
+++ leetcode/algorithms/412_fizz_buzz/main.dart
@@ -0,0 +1,38 @@
+class FizzBuzz {
+  List<String> fizzBuzz(int n) {
+    List<String> result = List<String>.filled(n, "");
+
+    for (var i = 1; i <= n; i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        result[i - 1] = "FizzBuzz";
+      } else if (i % 3 == 0 && i % 5 == 0) {
+        result[i - 1] = "Fizz";
+      } else if (i % 3 == 0 && i % 5 == 0) {
+        result[i - 1] = "Buzz";
+      } else {
+        result[i - 1] = "$i";
+      }
+    }
+
+    return result;
+  }
+
+  // Best Solution
+  List<String> bestSolution(int n) {
+    var v = new List.filled(n, "");
+
+    for (int i = 1; i <= n; i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        v[i - 1] = ("FizzBuzz");
+      } else if (i % 3 == 0) {
+        v[i - 1] = ("Fizz");
+      } else if (i % 5 == 0) {
+        v[i - 1] = ("Buzz");
+      } else {
+        v[i - 1] = (i.toString());
+      }
+    }
+
+    return v;
+  }
+}
