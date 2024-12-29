12f7c8291306ddd568d8b07afde45ba957c35d1b
miga-choi <miga.choi.1004@gmail.com>
2024-12-30 00:37:40 +0900

    leetcode: "414_third_maximum_number" add c++ solution

---
+++ leetcode/algorithms/414_third_maximum_number/main.cpp
@@ -0,0 +1,51 @@
+#include <vector>
+#include <limits>
+#include <set>
+using namespace std;
+
+class ThirdMaximumNumber {
+public:
+  int thirdMax(vector<int> &nums) {
+    int first = nums[0];
+    long second = LONG_MIN;
+    long thrid = LONG_MIN;
+
+    for (int num : nums) {
+      if (num > first) {
+        thrid = second;
+        second = first;
+        first = num;
+      } else if (num < first && num > second) {
+        thrid = second;
+        second = num;
+      } else if (num < second && num > thrid) {
+        thrid = num;
+      }
+    }
+
+    return thrid == LONG_MIN ? first : thrid;
+  }
+
+  // Best Solution 1: Track the largest three values in a set
+  int bestSolution1(vector<int> &nums) {
+    set<int> top3;
+    for (int num : nums) {
+      top3.insert(num);
+      if (top3.size() > 3) {
+        top3.erase(top3.begin());
+      }
+    }
+    return top3.size() == 3 ? *top3.begin() : *top3.rbegin();
+  }
+
+  // Best Solution 2: Alternatively
+  int bestSolution2(vector<int> &nums) {
+    set<int> top3;
+    for (int num : nums) {
+      if (top3.insert(num).second && top3.size() > 3) {
+        top3.erase(top3.begin());
+      }
+    }
+    return top3.size() == 3 ? *top3.begin() : *top3.rbegin();
+  }
+};
