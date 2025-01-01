cc50356bab74fd7e8a04ea25e517bbcd7db1de76
miga-choi <miga.choi.1004@gmail.com>
2025-01-02 00:38:22 +0900

    leetcode: "434_number_of_segments_in_a_string" add c++ solution

---
+++ leetcode/algorithms/434_number_of_segments_in_a_string/main.cpp
@@ -0,0 +1,69 @@
+#include <sstream>
+#include <string>
+using namespace std;
+
+class NumberOfSegmentsInAString {
+public:
+  int countSegments(string s) {
+    int count = 0;
+    int begin = 0;
+
+    for (char c : s) {
+      if (c != ' ') {
+        begin = 1;
+      }
+      if (begin && c == ' ') {
+        count++;
+        begin = 0;
+      }
+    }
+
+    return count + begin;
+  }
+
+  // Best Solution 1
+  int bestSolution1(string s) {
+    int res = 0;
+    s.push_back(' ');
+    for (int i = 1; i < s.size(); ++i) {
+      if (s[i] == ' ' && s[i - 1] != ' ') {
+        ++res;
+      }
+    }
+    return res;
+  }
+
+  // Best Solution 2: Brute force
+  int bestSolution2(string s) {
+    istringstream iss(s);
+    string word;
+    int count = 0;
+
+    while (iss >> word) {
+      count++;
+    }
+
+    return count;
+  }
+
+  // Best Solution 3: Efficient Method
+  int bestSolution3(string s) {
+    int count = 0;
+    bool inSegment = false;
+
+    for (char c : s) {
+      if (c != ' ') {
+        // If the character is not a space, we are in a segment
+        if (!inSegment) {
+          inSegment = true;
+          count++;
+        }
+      } else {
+        // If the character is a space, we are not in a segment
+        inSegment = false;
+      }
+    }
+
+    return count;
+  }
+};
