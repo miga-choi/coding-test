3afe1cfa61adcb01eae753e7176902289bc94703
miga-choi <miga.choi.1004@gmail.com>
2025-01-04 01:06:04 +0900

    leetcode: "448_find_all_numbers_disappeared_in_an_array" add c solution

---
+++ leetcode/algorithms/448_find_all_numbers_disappeared_in_an_array/main.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findDisappearedNumbers(int* nums, int numsSize, int* returnSize) {
+  int* numArray = (int*)calloc(numsSize, sizeof(int));
+
+  for (int i = 0; i < numsSize; i++) {
+    numArray[nums[i] - 1] = 1;
+  }
+
+  int* result = (int*)malloc(sizeof(int) * numsSize);
+  int count = 0;
+
+  for (int i = 1; i <= numsSize; i++) {
+    if (!numArray[i - 1]) {
+      result[count++] = i;
+    }
+  }
+
+  *returnSize = count;
+  return result;
+}
+
+// Best Solution
+int* bestSolution(int* nums, int numsSize, int* returnSize) {
+  int m;
+  for (int c = 0; c < numsSize; c++) {
+    m = abs(nums[c]) - 1;
+    nums[m] = -1 * abs(nums[m]);
+  }
+
+  *returnSize = 0;
+  for (int c = 0; c < numsSize; c++) {
+    if (nums[c] > 0) {
+      (*returnSize)++;
+    }
+  }
+
+  int* ret = (int*)calloc(*returnSize, sizeof(int));
+  int s = 0;
+  for (int c = 0; c < numsSize; c++) {
+    if (nums[c] > 0) {
+      ret[s++] = c + 1;
+    }
+  }
+
+  return ret;
+}
