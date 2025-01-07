06ae5aa5969f6dc27a7d967f876111b4054e5f03
miga-choi <miga.choi.1004@gmail.com>
2025-01-07 21:02:00 +0900

    leetcode: "456_132_pattern" add c solution

---
+++ leetcode/algorithms/456_132_pattern/main.c
@@ -0,0 +1,25 @@
+#include <stdbool.h>
+#include <limits.h>
+
+bool find132pattern(int* nums, int numsSize) {
+
+}
+
+// Best Solution
+bool bestSolution(int* nums, int numsSize) {
+    int peak = numsSize, min = INT_MIN;
+
+    for (int i = numsSize - 1; i > -1; i--) {
+        if (nums[i] < min) {
+            return true;
+        }
+        while (peak < numsSize && nums[i] > nums[peak]) {
+            min = nums[peak];
+            peak++;
+        }
+        peak--;
+        nums[peak] = nums[i];
+    }
+
+    return false;
+}
