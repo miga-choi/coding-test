bec795447e278a5dd7e3c7c1c4a32a0c0a5a169f
miga-choi <miga.choi.1004@gmail.com>
2025-01-07 21:01:05 +0900

    leetcode: "455_assign_cookies" add dart solution

---
+++ leetcode/algorithms/455_assign_cookies/main.dart
@@ -0,0 +1,34 @@
+class AssignCookies {
+  int findContentChildren(List<int> g, List<int> s) {
+    g.sort((int a, int b) => a - b);
+    s.sort((int a, int b) => a - b);
+
+    int i = 0;
+
+    for (int j = 0; i < g.length && j < s.length; j++) {
+      if (g[i] <= s[j]) {
+        i++;
+      }
+    }
+
+    return i;
+  }
+
+  // Best Solution
+  int bestSolution(List<int> g, List<int> s) {
+    g.sort();
+    s.sort();
+
+    int ind = 0;
+    int cookiesind = 0;
+
+    while (ind < g.length && cookiesind < s.length) {
+      if (s[cookiesind] >= g[ind]) {
+        ind++;
+      }
+      cookiesind++;
+    }
+
+    return ind;
+  }
+}
