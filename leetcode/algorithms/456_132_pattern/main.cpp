0e3d71c7d2595f0c661a8b873e791ac64e1f82ac
miga-choi <miga.choi.1004@gmail.com>
2025-01-08 20:31:14 +0900

    leetcode: "456_132_pattern" add c++ solution

---
+++ leetcode/algorithms/456_132_pattern/main.cpp
@@ -0,0 +1,44 @@
+#include <limits>
+#include <stack>
+#include <vector>
+using namespace std;
+
+class OneThreeTwoPattern {
+public:
+    bool find132pattern(vector<int> &nums) {
+        int peak = nums.size();
+        int min = INT_MIN;
+
+        for (int i = nums.size() - 1; i > -1; i--) {
+            if (nums[i] < min) {
+                return true;
+            }
+            while (peak < nums.size() && nums[i] > nums[peak]) {
+                min = nums[peak];
+                peak++;
+            }
+            peak--;
+            nums[peak] = nums[i];
+        }
+
+        return false;
+    }
+
+    // Best Solution
+    bool bestSolution(vector<int> &nums) {
+        int s3 = INT_MIN;
+        stack<int> st;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            if (nums[i] < s3) {
+                return true;
+            } else {
+                while (!st.empty() && nums[i] > st.top()) {
+                    s3 = st.top();
+                    st.pop();
+                }
+            }
+            st.push(nums[i]);
+        }
+        return false;
+    }
+};
