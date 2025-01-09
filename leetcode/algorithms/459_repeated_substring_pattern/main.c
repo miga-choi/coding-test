59eddab2dd53145d2f3705dc562d0ad928452adb
miga-choi <miga.choi.1004@gmail.com>
2025-01-09 18:40:40 +0900

    leetcode: "459_repeated_substring_pattern" add c solution

---
+++ leetcode/algorithms/459_repeated_substring_pattern/main.c
@@ -0,0 +1,28 @@
+#include <stdbool.h>
+#include <string.h>
+
+bool repeatedSubstringPattern(char* s) {}
+
+// Best Solution
+bool bestSolution(char* s) {
+    int len = strlen(s);
+    int j;
+
+    for (int i = 1; i <= len / 2; i++) {
+        if (len % i == 0) {
+            int isRepeated = 1;
+            for (int j = i; j < len; j++) {
+                if (s[j] != s[j - i]) {
+                    isRepeated = 0;
+                    break;
+                }
+            }
+            if (isRepeated) {
+                return true;
+            }
+        }
+    }
+
+    // No repeated substring found
+    return false;
+}
