6722ad1e82fafc73997787c53c1e66a97ba1468d
miga-choi <miga.choi.1004@gmail.com>
2025-01-12 02:55:23 +0900

    leetcode: "463_island_perimeter" add c++ solution

---
+++ leetcode/algorithms/463_island_perimeter/main.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+using namespace std;
+
+class IslandPerimeter {
+public:
+    int islandPerimeter(vector<vector<int>> &grid) {
+        int cnt = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid.at(i).size(); j++) {
+                if (grid.at(i).at(j)) {
+                    cnt += 4;
+                    if (i > 0 && grid.at(i - 1).at(j)) {
+                        cnt -= 2;
+                    }
+                    if (j > 0 && grid.at(i).at(j - 1)) {
+                        cnt -= 2;
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+
+    // Best Solution
+    int bestSolution(vector<vector<int>> &grid) {
+        int count = 0, repeat = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                if (grid[i][j] == 1) {
+                    count++;
+                    if (i != 0 && grid[i - 1][j] == 1) {
+                        repeat++;
+                    }
+                    if (j != 0 && grid[i][j - 1] == 1) {
+                        repeat++;
+                    }
+                }
+            }
+        }
+        return 4 * count - repeat * 2;
+    }
+};
