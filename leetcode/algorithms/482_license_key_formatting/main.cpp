9aa91c57b36a5f648548f8e68224d09410a9c310
miga-choi <miga.choi.1004@gmail.com>
2025-01-15 17:30:17 +0900

    leetcode: "482_license_key_formatting" add c++ solution

---
+++ leetcode/algorithms/482_license_key_formatting/main.cpp
@@ -0,0 +1,43 @@
+#include <string>
+using namespace std;
+
+class LicenseKeyFormatting {
+public:
+    string licenseKeyFormatting(string s, int k) {
+        string result = "";
+        int j = 1;
+
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (s[i] != '-') {
+                if ((j % (k + 1)) == 0) {
+                    result += "-";
+                    i++;
+                } else {
+                    result += toupper(s[i]);
+                }
+                j++;
+            }
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
+
+    // Best Solution
+    string bestSolution(string s, int k) {
+        string res;
+        for (auto i = s.rbegin(); i < s.rend(); i++) {
+            // ignore '-' in original string
+            if (*i != '-') {
+                if (res.size() % (k + 1) == k) {
+                    // every (K+1)th char is '-' from tail
+                    res += '-';
+                }
+                res += toupper(*i);
+            }
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
