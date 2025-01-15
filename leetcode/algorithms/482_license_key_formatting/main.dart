42ed9050ce988e3cfcb1d8c10a211e9bcb681bee
miga-choi <miga.choi.1004@gmail.com>
2025-01-16 05:59:45 +0900

    leetcode: "482_license_key_formatting" add dart solution

---
+++ leetcode/algorithms/482_license_key_formatting/main.dart
@@ -0,0 +1,37 @@
+class LicenseKeyFormatting {
+  String licenseKeyFormatting(String s, int k) {
+    String result = "";
+    List<String> sArray = s.replaceAll("-", "").split("");
+    for (int i = 1; i <= sArray.length; i++) {
+      if (i < sArray.length && (i % k) == 0) {
+        result = "-${sArray[sArray.length - i].toUpperCase()}$result";
+      } else {
+        result = "${sArray[sArray.length - i].toUpperCase()}$result";
+      }
+    }
+    return result;
+  }
+
+  // Best Solution
+  String bestSolution(String s, int k) {
+    final _dash = '-'.codeUnits[0];
+
+    var result = <int>[];
+    var currentGroupLength = 0;
+    for (var char in s.codeUnits.reversed) {
+      if (char == _dash) {
+        continue;
+      }
+      result.add(char);
+      currentGroupLength++;
+      if (currentGroupLength == k) {
+        result.add(_dash);
+        currentGroupLength = 0;
+      }
+    }
+    if (!result.isEmpty && result.last == _dash) {
+      result.removeLast();
+    }
+    return String.fromCharCodes(result.reversed).toUpperCase();
+  }
+}
