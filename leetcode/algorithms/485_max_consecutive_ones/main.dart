f688af80309344bdceda63c82ab130670d3f5087
miga-choi <miga.choi.1004@gmail.com>
2025-01-17 00:43:44 +0900

    leetcode: "485_max_consecutive_ones" add dart solution

---
+++ leetcode/algorithms/485_max_consecutive_ones/main.dart
@@ -0,0 +1,33 @@
+class MaxConsecutiveOnes {
+  int findMaxConsecutiveOnes(List<int> nums) {
+    int max = 0;
+    int current = 0;
+    for (int num in nums) {
+      if (num > 0) {
+        current++;
+        if (current > max) {
+          max = current;
+        }
+      } else {
+        current = 0;
+      }
+    }
+    return max;
+  }
+
+  // Best Solution
+  int bestSolution(List<int> nums) {
+    int count = 0, flag = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] == 1) {
+        count++;
+        if (count > flag) {
+          flag = count;
+        }
+      } else {
+        count = 0;
+      }
+    }
+    return flag;
+  }
+}
