70ed55b29320e9b1a498c21a74ea662ba41bcf5f
miga-choi <miga.choi.1004@gmail.com>
2025-01-17 00:43:07 +0900

    leetcode: "485_max_consecutive_ones" add c++ solution

---
+++ leetcode/algorithms/485_max_consecutive_ones/main.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class MaxConsecutiveOnes {
+public:
+    int findMaxConsecutiveOnes(vector<int> &nums) {
+        int max = 0;
+        int current = 0;
+        for (int num : nums) {
+            if (num > 0) {
+                current++;
+                if (current > max) {
+                    max = current;
+                }
+            } else {
+                current = 0;
+            }
+        }
+        return max;
+    }
+
+    // Best Solution
+    int bestSolution(vector<int> &nums) {
+        int max_cnt = 0, cnt = 0;
+        for (int n : nums) {
+            if (n == 1) {
+                max_cnt = max(++cnt, max_cnt);
+            } else {
+                cnt = 0;
+            }
+        }
+        return max_cnt;
+    }
+};
