d1c521c9f0703050d9fe266ee34d1ddeabff68a8
miga-choi <miga.choi.1004@gmail.com>
2025-01-18 21:21:16 +0900

    leetcode: "492_construct_the_rectangle" add c solution

---
+++ leetcode/algorithms/492_construct_the_rectangle/main.c
@@ -0,0 +1,35 @@
+#include <math.h>
+#include <stdlib.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* constructRectangle(int area, int* returnSize) {
+    int* result = (int*)malloc(sizeof(int) * 2);
+    int diff = area;
+
+    for (int i = 1; i <= sqrt(area); i++) {
+        if ((area % i) == 0) {
+            if (abs((area / i) - i) < diff) {
+                result[0] = area / i;
+                result[1] = i;
+            }
+        }
+    }
+
+    *returnSize = 2;
+    return result;
+}
+
+// Best Solution
+int* bestSolution(int area, int* returnSize) {
+    int* arr = (int*)malloc(sizeof(int) * 2);
+    int width = sqrt(area);
+    while (area % width != 0) {
+        width--;
+    }
+    arr[0] = area / width;
+    arr[1] = width;
+    *returnSize = 2;
+    return arr;
+}
