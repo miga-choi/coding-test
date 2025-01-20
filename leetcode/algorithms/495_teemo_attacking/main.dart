8794315e4e92de244f13d8baf0e8e26ee5ec2ce0
miga-choi <miga.choi.1004@gmail.com>
2025-01-20 20:31:59 +0900

    leetcode: "495_teemo_attacking" add dart solution

---
+++ leetcode/algorithms/495_teemo_attacking/main.dart
@@ -0,0 +1,22 @@
+class TeemoAttacking {
+  int findPoisonedDuration(List<int> timeSeries, int duration) {
+    int total = 0;
+    for (var i = 1; i < timeSeries.length; i++) {
+      if (timeSeries[i] > timeSeries[i - 1] + duration) {
+        total += duration;
+      } else {
+        total += timeSeries[i] - timeSeries[i - 1];
+      }
+    }
+    return total + duration;
+  }
+
+  // Best Solution
+  int bestSolution(List<int> timeSeries, int duration) {
+    int out = duration;
+    for (int i = 1; i < timeSeries.length; i++) {
+      out += timeSeries[i] - timeSeries[i - 1] > duration ? duration : timeSeries[i] - timeSeries[i - 1];
+    }
+    return out;
+  }
+}
