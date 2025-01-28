31fb238edc98c63e51354d6201be7caeb2c9320b
miga-choi <miga.choi.1004@gmail.com>
2025-01-29 04:45:41 +0900

    leetcode: "507_perfect_number" add c++ solution

---
+++ leetcode/algorithms/507_perfect_number/main.cpp
@@ -0,0 +1,26 @@
+class PerfectNumber {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum = 0;
+        for (int i = 1; i < num / 2; i++) {
+            if (!(num % i)) {
+                sum += i;
+            }
+        }
+        return sum * 2 == num;
+    }
+
+    // Solution
+    bool solution(int num) {
+        int sum = 0;
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i * i != num) {
+                    sum += num / i;
+                }
+            }
+        }
+        return sum - num == num;
+    }
+};
