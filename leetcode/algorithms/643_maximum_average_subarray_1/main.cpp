9b72bbc1872264c01b010beb89f98cd71db3a229
miga-choi <miga.choi.1004@gmail.com>
2025-03-23 18:27:21 +0900

    leetcode: "643_maximum_average_subarray_1" add c++ solution

---
+++ leetcode/algorithms/643_maximum_average_subarray_1/main.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class MaximumAverageSubarrayI {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double currSum = 0, maxSum = 0;
+
+        for (int i = 0; i < k; i++) {
+            currSum += nums[i];
+        }
+        maxSum = currSum;
+
+        for (int i = k; i < nums.size(); i++) {
+            currSum += nums[i] - nums[i - k];
+            maxSum = max(currSum, maxSum);
+        }
+
+        return maxSum / k;
+    }
+};
