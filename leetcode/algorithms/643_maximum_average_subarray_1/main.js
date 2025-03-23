c92a87c76b62517e1d025ee3ad6c2355516ef63e
miga-choi <miga.choi.1004@gmail.com>
2025-03-24 02:39:34 +0900

    leetcode: "643_maximum_average_subarray_1" add javascript solution

---
+++ leetcode/algorithms/643_maximum_average_subarray_1/main.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function (nums, k) {
+  let currSum = 0, maxSum = 0;
+
+  for (let i = 0; i < k; i++) {
+    currSum += nums[i];
+  }
+  maxSum = currSum;
+
+  for (let i = k; i < nums.length; i++) {
+    currSum += nums[i] - nums[i - k];
+    maxSum = Math.max(currSum, maxSum);
+  }
+
+  return maxSum / k;
+};
