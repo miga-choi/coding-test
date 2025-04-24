f7ff496af1fa2a2566b21fd0ccfe451b87f4c873
miga-choi <miga.choi.1004@gmail.com>
2025-04-25 08:50:23 +0900

    leetcode: "693_binary_number_with_alternating_bits" add c++ solution

---
+++ leetcode/algorithms/693_binary_number_with_alternating_bits/main.cpp
@@ -0,0 +1,29 @@
+class BinaryNumberWithAlternatingBits {
+public:
+    bool hasAlternatingBits(int n) {
+        int prev = -1;
+
+        while (n > 0) {
+            if (n % 2 == prev) {
+                return false;
+            }
+            prev = n % 2;
+            n >>= 1;
+        }
+
+        return true;
+    }
+
+
+    // Solution
+    bool solution(int n) {
+        int d = n & 1;
+
+        while ((n & 1) == d) {
+            d = 1 - d;
+            n >>= 1;
+        }
+
+        return n == 0;
+    }
+};
