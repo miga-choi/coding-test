117d06cfe17f095f59fba0f9a6a87f3cd21e8a86
miga-choi <miga.choi.1004@gmail.com>
2025-05-21 23:28:50 +0900

    leetcode: "706_design_hash_map" add c solution

---
+++ leetcode/algorithms/706_design_hash_map/main.c
@@ -0,0 +1,36 @@
+/**
+ * Your MyHashMap struct will be instantiated and called as such:
+ * MyHashMap* obj = myHashMapCreate();
+ * myHashMapPut(obj, key, value);
+
+ * int param_2 = myHashMapGet(obj, key);
+
+ * myHashMapRemove(obj, key);
+
+ * myHashMapFree(obj);
+*/
+
+typedef struct
+{
+
+} MyHashMap;
+
+MyHashMap *myHashMapCreate()
+{
+}
+
+void myHashMapPut(MyHashMap *obj, int key, int value)
+{
+}
+
+int myHashMapGet(MyHashMap *obj, int key)
+{
+}
+
+void myHashMapRemove(MyHashMap *obj, int key)
+{
+}
+
+void myHashMapFree(MyHashMap *obj)
+{
+}
