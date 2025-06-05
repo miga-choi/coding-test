3372669d23cf58050b1f66fc622d3563ff670b71
miga-choi <miga.choi.1004@gmail.com>
2025-06-05 21:47:43 +0900

    leetcode: "728_self_dividing_numbers" add c solution

---
+++ leetcode/algorithms/728_self_dividing_numbers/main.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* selfDividingNumbers(int left, int right, int* returnSize) {
+    *returnSize = 0;
+    int* result = (int*)malloc(sizeof(int) * (right - left + 1));
+
+    for (int i = left, n = 0; i <= right; i++) {
+        int v = i;
+
+        while (v > 0) {
+            if (v % 10 == 0 || i % (v % 10) > 0) {
+                break;
+            }
+
+            v /= 10;
+        }
+
+        if (v == 0) {
+            result[(*returnSize)++] = i;
+        }
+    }
+
+    return result;
+}
+
+
+// Solution
+int *solution(int left, int right, int *returnSize) {
+    *returnSize = 0;
+    int *result = (int *)malloc(sizeof(int) * (right - left + 1));
+
+    for (int i = left, n = 0; i <= right; i++) {
+        for (n = i; n > 0; n /= 10) {
+            if (n % 10 == 0 || i % (n % 10) != 0) {
+                break;
+            }
+        }
+
+        if (n == 0) {
+            result[(*returnSize)++] = i;
+        }
+    }
+
+    return result;
+}
