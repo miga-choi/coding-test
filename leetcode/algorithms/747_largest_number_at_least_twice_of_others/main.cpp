2873e652b92da97c1072cca680c4a5946435eab2
miga-choi <miga.choi.1004@gmail.com>
2025-06-19 14:53:25 +0900

    leetcode: "747_largest_number_at_least_twice_of_others" add c++ solution

---
+++ leetcode/algorithms/747_largest_number_at_least_twice_of_others/main.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+using namespace std;
+
+class LargestNumberAtLeastTwiceOfOthers {
+public:
+    int dominantIndex(vector<int>& nums) {
+        int max = -1;
+        int second = -1;
+        int index = -1;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > max) {
+                second = max;
+                max = nums[i];
+                index = i;
+            } else if (nums[i] > second) {
+                second = nums[i];
+            }
+        }
+
+        return max >= second * 2 ? index : -1;
+    }
+
+
+    // Solution
+    int solution(vector<int>& nums) {
+        int max = -1, index = -1, second = -1;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > max) {
+                second = max;
+                max = nums[i];
+                index = i;
+            } else if (nums[i] > second) {
+                second = nums[i];
+            }
+        }
+
+        return second * 2 <= max ? index : -1;
+    }
+};
