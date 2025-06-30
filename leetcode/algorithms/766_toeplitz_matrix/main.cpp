f0579ddcb751295559b69050292d0c4b69f46f4e
miga-choi <miga.choi.1004@gmail.com>
2025-06-30 18:10:23 +0900

    leetcode: "766_toeplitz_matrix" add c++ solution

---
+++ leetcode/algorithms/766_toeplitz_matrix/main.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+
+class ToeplitzMatrix {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        for (int i = 0; i < matrix.size() - 1; i++) {
+            for (int j = 0; j < matrix[i].size() - 1; j++) {
+                if (matrix[i][j] != matrix[i + 1][j + 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
