01f5c5f77a716bb61fee02241f8c19d779bac461
miga-choi <miga.choi.1004@gmail.com>
2025-07-15 15:27:01 +0900

    leetcode: "2_add_two_numbers" add java solution

---
+++ leetcode/algorithms/2_add_two_numbers/main.java
@@ -0,0 +1,79 @@
+class AddTwoNumbers {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+
+        ListNode result = new ListNode();
+        ListNode head = result;
+
+        while (l1 != null || l2 != null || carry > 0) {
+            int sum = carry;
+            carry = 0;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            if (sum > 9) {
+                sum -= 10;
+                carry = 1;
+            }
+
+            head.next = new ListNode(sum);
+            head = head.next;
+        }
+
+        return result.next;
+    }
+
+
+    // Solution
+    public ListNode solution(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode tail = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int digit1 = (l1 != null) ? l1.val : 0;
+            int digit2 = (l2 != null) ? l2.val : 0;
+
+            int sum = digit1 + digit2 + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            ListNode newNode = new ListNode(digit);
+            tail.next = newNode;
+            tail = tail.next;
+
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+        }
+
+        ListNode result = dummyHead.next;
+        dummyHead.next = null;
+
+        return result;
+    }
+}
