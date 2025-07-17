4f2ff182196ac5eb608a51163aaecdb843a7d83b
miga-choi <miga.choi.1004@gmail.com>
2025-07-18 02:25:30 +0900

    leetcode: "3_longest_substring_without_repeating_characters" add javascript solution

--- leetcode/algorithms/3_longest_substring_without_repeating_characters/main.js
+++ leetcode/algorithms/3_longest_substring_without_repeating_characters/main.js
@@ -3,18 +3,81 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-  const map = {};
-  var length = 0;
+  const charMap = Array(128).fill(-1);
+  let left = 0;
+  let maxLength = 0;
 
-  return s.split("").reduce((maxLength, v, i) => {
-    if (map[v] >= length) {
-      length = map[v] + 1;
+  for (let right = 0; right < s.length; right++) {
+    const currentChar = s[right];
+
+    if (charMap[currentChar] >= left) {
+      left = charMap[currentChar] + 1;
     }
-    map[v] = i;
-    if (maxLength >= i - length + 1) {
-      return maxLength;
-    } else {
-      return i - length + 1;
+
+    charMap[currentChar] = right;
+
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+};
+
+
+// Solution
+// Solution 1: Sliding Window & Set
+var solution1 = function (s) {
+  let left = 0;
+  let maxLength = 0;
+  let charSet = new Set();
+
+  for (let right = 0; right < s.length; right++) {
+    while (charSet.has(s[right])) {
+      charSet.delete(s[left]);
+      left++;
     }
-  }, 0);
+
+    charSet.add(s[right]);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+};
+
+// Solution 2: Sliding Window and Hashing
+var solution2 = function (s) {
+  let maxLength = 0;
+  let left = 0;
+  let count = {};
+
+  for (let right = 0; right < s.length; right++) {
+    let c = s[right];
+    count[c] = (count[c] || 0) + 1;
+
+    while (count[c] > 1) {
+      count[s[left]] -= 1;
+      left++;
+    }
+
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+};
+
+// Solution 3: the last position where each character was seen
+var solution3 = function (s) {
+  let maxLength = 0;
+  let left = 0;
+  let lastSeen = {};
+
+  for (let right = 0; right < s.length; right++) {
+    let c = s.charAt(right);
+    if (c in lastSeen && lastSeen[c] >= left) {
+      left = lastSeen[c] + 1;
+    }
+    maxLength = Math.max(maxLength, right - left + 1);
+    lastSeen[c] = right;
+  }
+
+  return maxLength;
 };
