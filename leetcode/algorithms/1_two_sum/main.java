91c7bce60863cfcf5f700ae7640b116e3cb29a0b
miga-choi <miga.choi.1004@gmail.com>
2025-07-24 01:46:21 +0900

    leetcode: "1_two_sum" add java solution

--- leetcode/algorithms/1_two_sum/main.java
+++ leetcode/algorithms/1_two_sum/main.java
@@ -2,7 +2,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
+    /**
+     * Brute-Force
+     * - Time Complexity: O(N²)
+     * - Space Complexity: O(1)
+     */
+    public int[] twoSum1(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
@@ -10,6 +15,26 @@ class TwoSum {
                 }
             }
         }
+
+        return new int[] {};
+    }
+
+    /**
+     * HashMap
+     * - Time Complexity: O(N)
+     * - Space Complexity: O(N)
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { i, map.get(complement) };
+            }
+            map.put(nums[i], i);
+        }
+
         return new int[] {};
     }
 
