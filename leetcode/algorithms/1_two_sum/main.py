3d03a03eeea1cfa1413f738a2d259b9c16d6e5c3
miga-choi <miga.choi.1004@gmail.com>
2025-07-25 12:54:22 +0900

    leetcode: "1_two_sum" add python solution

--- leetcode/algorithms/1_two_sum/main.py
+++ leetcode/algorithms/1_two_sum/main.py
@@ -2,13 +2,31 @@ from typing import List
 
 
 class TwoSum:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(0, len(nums), 1):
-            for j in range(i + 1, len(nums), 1):
-                if i >= len(nums):
-                    continue
+    # Brute-Force
+    # - Time Complexity: O(N²)
+    # - Space Complexity: O(1)
+    def twoSum1(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+        
+        return []
+
+    # Dictionary
+    # - Time Complexity: O(N)
+    # - Space Complexity: O(N)
+    def twoSum2(self, nums: List[int], target: int) -> List[int]:
+        map: dict[int, int] = {}
+
+        for i in range(len(nums)):
+            complement: int = target - nums[i]
+            if complement in map:
+                return [i, map[complement]]
+            map[nums[i]] = i
+
+        return []
+
 
 
     # Solution
