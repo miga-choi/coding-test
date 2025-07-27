ae9c7fbfcac3672a2efa5a802bcd3e4916538ef7
miga-choi <miga.choi.1004@gmail.com>
2025-07-27 20:56:04 +0900

    leetcode: "26_remove_duplicates_from_sorted_array" add javascript solution

--- leetcode/algorithms/26_remove_duplicates_from_sorted_array/main.js
+++ leetcode/algorithms/26_remove_duplicates_from_sorted_array/main.js
@@ -1,23 +1,48 @@
 /**
+ * Two Pointers
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(1)
+ *
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function (nums) {
-  return [...new Set(nums)]
+var removeDuplicates1 = function (nums) {
+  let left = 0;
+
+  for (let right = 0; right < nums.length; right++) {
+    if (nums[left] != nums[right]) {
+      nums[++left] = nums[right];
+    }
+  }
+
+  return left + 1;
+};
+
+/**
+ * Set & Array
+ * - Time Complexity: O(?)
+ * - Space Complexity: O(?)
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates2 = function (nums) {
+  // return [...new Set(nums)].sort((p, n) => p - n).map((v, i) => (nums[i] = v)).length;
+  return Array.from(new Set(nums))
     .sort((p, n) => p - n)
-    .map((v, i) => {
-      nums[i] = v;
-    }).length;
+    .map((v, i) => (nums[i] = v)).length;
 };
 
 
 // Solution
 var solution = function (nums) {
   let i = 0;
+
   for (let j = 0; j < nums.length; j++) {
     if (nums[j] != nums[i]) {
       nums[++i] = nums[j];
     }
   }
+
   return ++i;
 };
