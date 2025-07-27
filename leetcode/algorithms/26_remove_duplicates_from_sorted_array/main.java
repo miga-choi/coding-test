304af444f70a277b50789814b7ced9d42e49c8e9
miga-choi <miga.choi.1004@gmail.com>
2025-07-27 20:56:48 +0900

    leetcode: "26_remove_duplicates_from_sorted_array" add java solution

--- leetcode/algorithms/26_remove_duplicates_from_sorted_array/main.java
+++ leetcode/algorithms/26_remove_duplicates_from_sorted_array/main.java
@@ -1,19 +1,43 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 class RemoveDuplicatesFromSortedArray {
-  public int removeDuplicates(int[] nums) {
-    Set<Integer> numsSet = new HashSet<Integer>();
+  /**
+   * Two Pointers
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(1)
+   */
+  public int removeDuplicates1(int[] nums) {
+    int left = 0;
+
+    for (int right = 0; right < nums.length; right++) {
+      if (nums[left] != nums[right]) {
+        nums[++left] = nums[right];
+      }
+    }
+
+    return left + 1;
+  }
+
+  /**
+   * TreeSet
+   * - Time Complexity: O(?)
+   * - Space Complexity: O(?)
+   */
+  public int removeDuplicates2(int[] nums) {
+    SortedSet<Integer> set = new TreeSet<>();
+
     for (int num : nums) {
-      numsSet.add(num);
+      set.add(num);
     }
-    Object[] numsArray = numsSet.stream().toArray();
-    Arrays.sort(numsArray);
-    for (int i = 0; i < numsArray.length; i++) {
-      nums[i] = (int) numsArray[i];
+
+    int i = 0;
+
+    for (int v : set) {
+      nums[i++] = v;
     }
-    return numsArray.length;
+
+    return i;
   }
 
 
@@ -21,23 +45,27 @@ class RemoveDuplicatesFromSortedArray {
   // Solution 1
   public int solution1(int[] nums) {
     int j = 1;
+
     for (int i = 1; i < nums.length; i++) {
       if (nums[i] != nums[i - 1]) {
         nums[j] = nums[i];
         j++;
       }
     }
+
     return j;
   }
 
   // Solution 2
   public int solution2(int[] nums) {
     int i = nums.length > 0 ? 1 : 0;
+
     for (int n : nums) {
       if (n > nums[i - 1]) {
         nums[i++] = n;
       }
     }
+
     return i;
   }
 }
