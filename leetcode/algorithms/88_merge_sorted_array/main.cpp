d7550b064156d64d10f18580720b43685ddbc7fb
miga-choi <miga.choi.1004@gmail.com>
2025-07-29 02:16:43 +0900

    leetcode: "26_remove_duplicates_from_sorted_array" add c++ solution

--- leetcode/algorithms/88_merge_sorted_array/main.cpp
+++ leetcode/algorithms/88_merge_sorted_array/main.cpp
@@ -3,11 +3,26 @@ using namespace std;
 
 class MergeSortedArray {
 public:
+    /**
+     * Three Pointers: from end to begin
+     * - Time Complexity: O(M+N)
+     * - Time Complexity: O(1)
+     */
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for (int i = 0; i < n; i++) {
-            nums1[m + i] = nums2[i];
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p = m + n - 1;
+
+        while (p2 >= 0) {
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p] = nums2[p2];
+                p2--;
+            }
+            p--;
         }
-        sort(nums1.begin(), nums1.end());
     }
 
 
