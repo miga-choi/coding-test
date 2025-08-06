8c1e1ec87e78e1669421cf22ec98818c2aff5d28
miga-choi <miga.choi.1004@gmail.com>
2025-08-07 01:20:23 +0900

    leetcode: "6_zigzag_conversion" add c solution

---
+++ leetcode/algorithms/6_zigzag_conversion/main.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(N)
+ */
+char* convert(char* s, int numRows) {
+    int sLen = strlen(s);
+
+    if (numRows == 1 || numRows >= sLen) {
+        char* result = (char*)malloc(sizeof(char) * (sLen + 1));
+        strcpy(result, s);
+        return result;
+    }
+
+    char* result = (char*)malloc(sizeof(char) * (sLen + 1));
+
+    int cycleLen = numRows * 2 - 2;
+    int result_idx = 0;
+    for (int row = 0; row < numRows; row++) {
+        for (int cycle = 0; row + cycle < sLen; cycle += cycleLen) {
+            result[result_idx++] = s[row + cycle];
+
+            if (row > 0 && row < numRows - 1) {
+                int diag_idx = cycle + cycleLen - row;
+                if (diag_idx < sLen) {
+                    result[result_idx++] = s[diag_idx];
+                }
+            }
+        }
+    }
+
+    result[result_idx] = '\0';
+
+    return (char*)result;
+}
+
+
+// Solution
+char* convert(char* s, int numRows) {
+    int len = strlen(s);
+
+    if (numRows == 1 || numRows >= len) {
+        char* result = (char*)malloc(sizeof(char) * (len + 1));
+        strcpy(result, s);
+        return result;
+    }
+
+    char* result = (char*)malloc(sizeof(char) * (len + 1));
+
+    int cycleLen = 2 * numRows - 2;
+    int result_idx = 0;
+
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j + i < len; j += cycleLen) {
+            result[result_idx++] = s[j + i];
+
+            if (i != 0 && i != numRows - 1) {
+                int diag_idx = j + cycleLen - i;
+                
+                if (diag_idx < len) {
+                    result[result_idx++] = s[diag_idx];
+                }
+            }
+        }
+    }
+
+    result[result_idx] = '\0';
+    
+    return result;
+}
