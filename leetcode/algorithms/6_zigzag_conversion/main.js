563bee9a0ddf55c71acfeab9c4d0ca870df06086
miga-choi <miga.choi.1004@gmail.com>
2025-08-08 02:02:06 +0900

    leetcode: "6_zigzag_conversion" add javascript solution

--- leetcode/algorithms/6_zigzag_conversion/main.js
+++ leetcode/algorithms/6_zigzag_conversion/main.js
@@ -1,18 +1,102 @@
 /**
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(N)
+ *
  * @param {string} s
  * @param {number} numRows
  * @return {string}
  */
 var convert = function (s, numRows) {
-  // 1. Make an array with the zigzag sequence
-  const zigzag = [...new Array(numRows).keys()];
+  const sLen = s.length;
 
-  // 2. Make an array with as many strings as we need rows
-  const rows = new Array(numRows).fill("");
+  if (numRows == 1 || numRows > sLen) {
+    return s;
+  }
 
-  // 3. Append the characters to the row strings in zigzag sequence
-  [...s].forEach((c, i) => (rows[zigzag[i % zigzag.length]] += c));
+  const cycleLen = numRows * 2 - 2;
+  let result = "";
+  for (let row = 0; row < numRows; row++) {
+    for (let cycle = row; cycle < sLen; cycle += cycleLen) {
+      result += s[cycle];
 
-  // 4. Join the row strings in the array together
+      if (row > 0 && row < numRows - 1) {
+        const diag_idx = cycle + cycleLen - row * 2;
+        if (diag_idx < sLen) {
+          result += s[diag_idx];
+        }
+      }
+    }
+  }
+
+  return result;
+};
+
+
+// Solution
+/**
+ * Solution 1
+ *
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(N)
+ *
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var solution1 = function (s, numRows) {
+  if (numRows == 1 || numRows >= s.length) {
+    return s;
+  }
+
+  const rows = new Array(numRows).fill(new Array(1).fill(""));
+
+  let currentRow = 0;
+  let goingDown = false;
+
+  for (const c of s) {
+    rows[currentRow] += c;
+
+    if (currentRow == 0 || currentRow == numRows - 1) {
+      goingDown = !goingDown;
+    }
+
+    currentRow += goingDown ? 1 : -1;
+  }
+
   return rows.join("");
+};
+
+/**
+ * Solution 2
+ *
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(N)
+ *
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var solution2 = function (s, numRows) {
+  const sLen = s.length;
+
+  if (numRows == 1 || numRows > sLen) {
+    return s;
+  }
+
+  const cycleLen = numRows * 2 - 2;
+  let result = "";
+  for (let row = 0; row < numRows; row++) {
+    for (let cycle = row; cycle < sLen; cycle += cycleLen) {
+      result += s[cycle];
+
+      if (row > 0 && row < numRows - 1) {
+        const diag_idx = cycle + cycleLen - row * 2;
+        if (diag_idx < sLen) {
+          result += s[diag_idx];
+        }
+      }
+    }
+  }
+
+  return result;
 };
