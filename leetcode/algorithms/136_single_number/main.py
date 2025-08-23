4ecbabfe61f14c1e791a721ec46ba5a22a799de3
miga-choi <miga.choi.1004@gmail.com>
2025-08-23 09:37:22 +0900

    leetcode: "136_single_number" add python solution

--- leetcode/algorithms/136_single_number/main.py
+++ leetcode/algorithms/136_single_number/main.py
@@ -1,15 +1,16 @@
-from functools import reduce
 from typing import List
 
 
 class SingleNumber:
+    ##
+    # Bit Manipulation
+    # - Time Complexity: O(N)
+    # - Space Complexity: O(1)
+    #
     def singleNumber(self, nums: List[int]) -> int:
         result = 0
+
         for num in nums:
             result = result ^ num
+
         return result
-
-
-    # Solution
-    def solution(self, nums: List[int]) -> int:
-        return reduce(lambda total, el: total ^ el, nums)
