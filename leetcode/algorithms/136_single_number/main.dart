fc6f975abe562733e0fed92ff1f9af5fb4130ea3
miga-choi <miga.choi.1004@gmail.com>
2025-08-23 09:37:48 +0900

    leetcode: "136_single_number" add dart solution

--- leetcode/algorithms/136_single_number/main.dart
+++ leetcode/algorithms/136_single_number/main.dart
@@ -1,19 +1,16 @@
 class SingleNumber {
+  /**
+   * Bit Manipulation
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(1)
+   */
   int singleNumber(List<int> nums) {
     int result = 0;
-    for (int num in nums) {
+
+    for (final int num in nums) {
       result ^= num;
     }
-    return result;
-  }
 
-
-  // Solution
-  int solution(List<int> nums) {
-    int result = 0;
-    for (int num in nums) {
-      result = result ^ num;
-    }
     return result;
   }
 }
