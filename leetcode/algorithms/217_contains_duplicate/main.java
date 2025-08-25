e3a6ea3a144398d4bed3eda367658ce6a1031188
miga-choi <miga.choi.1004@gmail.com>
2025-08-25 18:19:36 +0900

    leetcode: "217_contains_duplicate" add java solution

--- leetcode/algorithms/217_contains_duplicate/main.java
+++ leetcode/algorithms/217_contains_duplicate/main.java
@@ -1,60 +1,27 @@
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
+import java.util.Set;
 
 class ContainsDuplicate {
+  /**
+   * Set
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(N)
+   */
   public boolean containsDuplicate(int[] nums) {
-    Map<Integer, Boolean> numsMap = new HashMap<Integer, Boolean>();
-    for (int i = 0; i < nums.length; i++) {
-      if (numsMap.containsKey(nums[i])) {
-        return true;
-      }
-      numsMap.put(nums[i], true);
-    }
-    return false;
-  }
-
-
-  // Solution
-  // Solution 1
-  public boolean solution1(int[] nums) {
-    // Create a hashset...
-    HashSet<Integer> hset = new HashSet<Integer>();
-
-    // Traverse all the elements through the loop...
-    for (int idx = 0; idx < nums.length; idx++) {
-      // Searches hashset. if present, it contains duplicate...
-      if (hset.contains(nums[idx])) {
-        return true;
-      }
-
-      // if not present it will update hashset...
-      hset.add(nums[idx]);
-    }
-
-    // Otherwise return false...
-    return false;
-  }
-
-  // Solution 2
-  public boolean solution2(int[] nums) {
-    // Base case...
-    if (nums == null || nums.length == 0) {
+    if (nums.length < 2) {
       return false;
     }
 
-    // Create a hashset...
-    HashSet<Integer> hset = new HashSet<Integer>();
+    Set<Integer> set = new HashSet<>();
 
-    // Traverse all the elements through the loop...
-    for (int idx : nums) {
-      // If it contains duplicate...
-      if (!hset.add(idx)) {
+    for (int num : nums) {
+      if (set.contains(num)) {
         return true;
+      } else {
+        set.add(num);
       }
     }
 
-    // Otherwise return false...
     return false;
   }
 }
