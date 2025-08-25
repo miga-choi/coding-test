4aaca3f52aeb0d544d7f36a363e06a58a7914255
miga-choi <miga.choi.1004@gmail.com>
2025-08-25 18:19:01 +0900

    leetcode: "217_contains_duplicate" add javascript solution

--- leetcode/algorithms/217_contains_duplicate/main.js
+++ leetcode/algorithms/217_contains_duplicate/main.js
@@ -1,20 +1,39 @@
 /**
+ * Set
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(N)
+ *
  * @param {number[]} nums
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
-  const numsMap = {};
-  for (let i = 0; i < nums.length; i++) {
-    if (numsMap[nums[i]]) {
-      return true;
-    }
-    numsMap[nums[i]] = true;
+  if (nums.length < 2) {
+    return false;
   }
+
+  const set = new Set();
+
+  for (const num of nums) {
+    if (set.has(num)) {
+      return true;
+    } else {
+      set.add(num);
+    }
+  }
+
   return false;
 };
 
 
 // Solution
+/**
+ * Set
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(N)
+ *
+ * @param {number[]} nums
+ * @return {boolean}
+ */
 var solution = function (nums) {
   const s = new Set(nums);
   return s.size !== nums.length;
