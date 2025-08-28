b15a0afdbc8f1fc1c211a41ced2a48e985a46f22
miga-choi <miga.choi.1004@gmail.com>
2025-08-29 01:48:42 +0900

    leetcode: "28_find_the_index_of_the_first_occurrence_in_a_string" add dart solution

--- leetcode/algorithms/28_find_the_index_of_the_first_occurrence_in_a_string/main.dart
+++ leetcode/algorithms/28_find_the_index_of_the_first_occurrence_in_a_string/main.dart
@@ -1,21 +1,114 @@
 class FindTheIndexOfTheFirstOccurrenceInAString {
+  /**
+   * Brute-Force
+   * - Time Complexity: O(N * M)
+   * - Space Complexity: O(1)
+   */
   int strStr(String haystack, String needle) {
-    return haystack.indexOf(needle);
+    int haystackLen = haystack.length;
+    int needleLen = needle.length;
+
+    if (needleLen > haystackLen) {
+      return -1;
+    }
+
+    for (
+      int haystackIdx = 0;
+      haystackIdx <= haystackLen - needleLen;
+      haystackIdx++
+    ) {
+      bool isMatch = true;
+
+      for (int needleIdx = 0; needleIdx < needleLen; needleIdx++) {
+        if (haystack[haystackIdx + needleIdx] != needle[needleIdx]) {
+          isMatch = false;
+          break;
+        }
+      }
+
+      if (isMatch) {
+        return haystackIdx;
+      }
+    }
+
+    return -1;
   }
 
 
   // Solution
-  int solution(String haystack, String needle) {
-    if (needle.isEmpty || haystack.isEmpty || needle.length > haystack.length) {
+  /**
+   * Solution 1
+   * 
+   * String indexOf()
+   * - Time Complexity: O(N * M)
+   * - Space Complexity: O(1)
+   */
+  int solution1(String haystack, String needle) {
+    return haystack.indexOf(needle);
+  }
+
+  /**
+   * Solution 2
+   * 
+   * Knuth-Morris-Pratt (KMP)
+   * - Time Complexity: O(N + M)
+   * - Space Complexity: O(M)
+   */
+  List<int> _computeLPSArray(String needle) {
+    int needleLen = needle.length;
+    List<int> lps = List.filled(needleLen, 0);
+    int length = 0;
+    int idx = 1;
+
+    while (idx < needleLen) {
+      if (needle[idx] == needle[length]) {
+        length++;
+        lps[idx] = length;
+        idx++;
+      } else {
+        if (length != 0) {
+          length = lps[length - 1];
+        } else {
+          lps[idx] = 0;
+          idx++;
+        }
+      }
+    }
+
+    return lps;
+  }
+
+  int solution2(String haystack, String needle) {
+    int haystackLen = haystack.length;
+    int needleLen = needle.length;
+
+    if (needleLen > haystackLen) {
       return -1;
     }
 
-    for (int i = 0; i <= haystack.length - needle.length; i++) {
-      final String substring = haystack.substring(i, i + needle.length);
-      if (substring == needle) {
-        return i;
+    List<int> lps = _computeLPSArray(needle);
+
+    int haystackIdx = 0;
+    int needleIdx = 0;
+
+    while (haystackIdx < haystackLen) {
+      if (haystack[haystackIdx] == needle[needleIdx]) {
+        haystackIdx++;
+        needleIdx++;
+      }
+
+      if (needleIdx == needleLen) {
+        return haystackIdx - needleIdx;
+      } else if (haystackIdx < haystackLen &&
+          haystack[haystackIdx] != needle[needleIdx]) {
+        if (needleIdx != 0) {
+          needleIdx = lps[needleIdx - 1];
+        } else {
+          haystackIdx++;
+        }
       }
     }
+
     return -1;
   }
 }
