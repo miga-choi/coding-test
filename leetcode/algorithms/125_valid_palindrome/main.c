38c44c5fd47a50046c06adb44f7f2dc481f39132
miga-choi <miga.choi.1004@gmail.com>
2025-08-30 01:18:14 +0900

    leetcode: "125_valid_palindrome" add c solution

--- leetcode/algorithms/125_valid_palindrome/main.c
+++ leetcode/algorithms/125_valid_palindrome/main.c
@@ -2,28 +2,33 @@
 #include <stdbool.h>
 #include <string.h>
 
+/**
+ * Two-Pointer
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(1)
+ */
 bool isPalindrome(char* s) {
     int prefix = 0;
     int suffix = strlen(s) - 1;
 
     while (prefix < suffix) {
-        char prefixChar = s[prefix];
-        char suffixChar = s[suffix];
+        char prefix_char = s[prefix];
+        char suffix_char = s[suffix];
 
-        if (!((prefixChar >= '0' && prefixChar <= '9') ||
-              (prefixChar >= 'A' && prefixChar <= 'Z') ||
-              (prefixChar >= 'a' && prefixChar <= 'z'))) {
+        if (!((prefix_char >= '0' && prefix_char <= '9') ||
+              (prefix_char >= 'A' && prefix_char <= 'Z') ||
+              (prefix_char >= 'a' && prefix_char <= 'z'))) {
             prefix++;
             continue;
         }
-        if (!((suffixChar >= '0' && suffixChar <= '9') ||
-              (suffixChar >= 'A' && suffixChar <= 'Z') ||
-              (suffixChar >= 'a' && suffixChar <= 'z'))) {
+        if (!((suffix_char >= '0' && suffix_char <= '9') ||
+              (suffix_char >= 'A' && suffix_char <= 'Z') ||
+              (suffix_char >= 'a' && suffix_char <= 'z'))) {
             suffix--;
             continue;
         }
 
-        if (tolower(prefixChar) != tolower(suffixChar)) {
+        if (tolower(prefix_char) != tolower(suffix_char)) {
             return false;
         }
 
@@ -36,36 +41,31 @@ bool isPalindrome(char* s) {
 
 
 // Solution
-int isAlphanum(char c) {
-    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
-}
-
-int convertAlphaLower(char c) {
-    return (c >= 'A' && c <= 'Z') ? c + 32 : c;
-}
-
+/**
+ * Two-Pointer
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(1)
+ */
 bool solution(char* s) {
-    int l, r;
+    int left = 0;
+    int right = strlen(s) - 1;
 
-    r = strlen(s) - 1;
-
-    l = 0;
-    while (l < r) {
-        if (isAlphanum(s[l])) {
-            if (isAlphanum(s[r])) {
-                if (convertAlphaLower(s[l]) != convertAlphaLower(s[r])) {
-                    return 0;
-                }
-
-                l++;
-                r--;
-            } else {
-                r--;
-            }
-        } else {
-            l++;
+    while (left < right) {
+        while (left < right && !isalnum(s[left])) {
+            left++;
         }
+
+        while (left < right && !isalnum(s[right])) {
+            right--;
+        }
+
+        if (tolower(s[left]) != tolower(s[right])) {
+            return false;
+        }
+
+        left++;
+        right--;
     }
 
-    return (1);
+    return true;
 }
