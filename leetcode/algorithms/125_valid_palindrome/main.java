64b70d9a951eca47c6efc5ed45eb0abe977e9cac
miga-choi <miga.choi.1004@gmail.com>
2025-09-01 03:51:43 +0900

    leetcode: "125_valid_palindrome" add java solution

--- leetcode/algorithms/125_valid_palindrome/main.java
+++ leetcode/algorithms/125_valid_palindrome/main.java
@@ -1,36 +1,76 @@
 class ValidPalindrome {
+  /*
+   * Two-Pointer
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(1)
+   */
   public boolean isPalindrome(String s) {
     s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
-    for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
-      if (s.charAt(i) != s.charAt(j)) {
+
+    for (int left = 0, right = s.length() - 1; left < right; left++, right--) {
+      if (s.charAt(left) != s.charAt(right)) {
         return false;
       }
     }
+
     return true;
   }
 
 
   // Solution
-  public boolean solution(String s) {
-    if (s.isEmpty()) {
-      return true;
-    }
-    int head = 0, tail = s.length() - 1;
-    char cHead, cTail;
-    while (head <= tail) {
-      cHead = s.charAt(head);
-      cTail = s.charAt(tail);
-      if (!Character.isLetterOrDigit(cHead)) {
-        head++;
-      } else if (!Character.isLetterOrDigit(cTail)) {
-        tail--;
-      } else {
-        if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
-          return false;
-        }
-        head++;
-        tail--;
+  /*
+   * Solution 1
+   * 
+   * Compare strings
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(N)
+   */
+  public boolean solution1(String s) {
+    StringBuilder filteredChars = new StringBuilder();
+
+    for (char c : s.toCharArray()) {
+      if (Character.isLetterOrDigit(c)) {
+        filteredChars.append(Character.toLowerCase(c));
       }
+    }
+
+    String original = filteredChars.toString();
+    String reversed = filteredChars.reverse().toString();
+
+    return original.equals(reversed);
+  }
+
+  /*
+   * Solution 2
+   * 
+   * Two-Pointer
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(1)
+   */
+  public boolean solution2(String s) {
+    int left = 0;
+    int right = s.length() - 1;
+
+    while (left < right) {
+      char leftChar = s.charAt(left);
+      char rightChar = s.charAt(right);
+
+      if (!Character.isLetterOrDigit(leftChar)) {
+        left++;
+        continue;
+      }
+
+      if (!Character.isLetterOrDigit(rightChar)) {
+        right--;
+        continue;
+      }
+
+      if (Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) {
+        return false;
+      }
+
+      left++;
+      right--;
     }
 
     return true;
