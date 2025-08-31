6112aa213530ae724d5b27ea8ce2cd9c301cf408
miga-choi <miga.choi.1004@gmail.com>
2025-09-01 03:52:29 +0900

    leetcode: "125_valid_palindrome" add python solution

--- leetcode/algorithms/125_valid_palindrome/main.py
+++ leetcode/algorithms/125_valid_palindrome/main.py
@@ -2,32 +2,83 @@ import re
 
 
 class ValidPalindrome:
+    ##
+    # Two-Pointer
+    # - Time Complexity: O(N)
+    # - Space Complexity: O(1)
     def isPalindrome(self, s: str) -> bool:
-        s = re.sub("[^0-9a-zA-Z]+", "", s).lower()
-        for i in range(len(s)):
-            if i == len(s) - 1:
-                break
-            if s[i] != s[len(s) - 1 - i]:
+        left: int = 0
+        right: int = len(s) - 1
+
+        while left < right:
+            if not s[left].isalnum():
+                left += 1
+                continue
+
+            if not s[right].isalnum():
+                right -= 1
+                continue
+
+            if s[left].lower() != s[right].lower():
                 return False
+
+            left += 1
+            right -= 1
+
         return True
 
 
     # Solution
-    # Solution 1: Two Lines
+    ##
+    # Solution 1
+    #
+    # Compare strings
+    # - Time Complexity: O(N)
+    # - Space Complexity: O(N)
+    #
     def solution1(self, s: str) -> bool:
-        s = [i for i in s.lower() if i.isalnum()]
-        return s == s[::-1]
+        filtered_chars = []
 
-    # Solution 2: O(1) space (two-pointer)
+        for char in s:
+            if char.isalnum():
+                filtered_chars.append(char.lower())
+
+        processed_s = "".join(filtered_chars)
+
+        return processed_s == processed_s[::-1]
+
+    ##
+    # Solution 2
+    #
+    # Two-Pointer
+    # - Time Complexity: O(N)
+    # - Space Complexity: O(1)
+    #
     def solution2(self, s: str) -> bool:
-        i, j = 0, len(s) - 1
-        while i < j:
-            a, b = s[i].lower(), s[j].lower()
-            if a.isalnum() and b.isalnum():
-                if a != b:
-                    return False
-                else:
-                    i, j = i + 1, j - 1
-                    continue
-            i, j = i + (not a.isalnum()), j - (not b.isalnum())
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
         return True
+
+    ##
+    # Solution 3
+    #
+    # Regular Expressions
+    # - Time Complexity: O(N)
+    # - Space Complexity: O(N)
+    #
+    def solution3(self, s: str) -> bool:
+        s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+        return s == s[::-1]
