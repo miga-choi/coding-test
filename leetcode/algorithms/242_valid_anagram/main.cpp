052a970da74338ff70e774897f9a7001bf50d271
miga-choi <miga.choi.1004@gmail.com>
2025-09-03 02:10:06 +0900

    leetcode: "242_valid_anagram" add c++ solution

--- leetcode/algorithms/242_valid_anagram/main.cpp
+++ leetcode/algorithms/242_valid_anagram/main.cpp
@@ -1,19 +1,27 @@
+#include <algorithm>
 #include <string>
-#include <unordered_map>
+#include <vector>
 using namespace std;
 
 class ValidAnagram {
 public:
+    /**
+     * Frequency Counter
+     * - Time Complexity: O(N)
+     * - Space Complexity: O(1)
+     */
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) {
+        int len = s.length();
+
+        if (len != t.length()) {
             return false;
         }
 
         int alphabetNumArray[26] = {0};
 
-        for (int i = 0; i < s.length(); i++) {
-            alphabetNumArray[s.at(i) - 'a']++;
-            alphabetNumArray[t.at(i) - 'a']--;
+        for (int i = 0; i < len; i++) {
+            alphabetNumArray[s[i] - 'a']++;
+            alphabetNumArray[t[i] - 'a']--;
         }
 
         for (int i = 0; i < 26; i++) {
@@ -27,58 +35,49 @@ public:
 
 
     // Solution
-    // Solution 1: Sorting
+    /**
+     * Solution 1
+     * 
+     * Frequency Counter
+     * - Time Complexity: O(N)
+     * - Space Complexity: O(1)
+     */
     bool solution1(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        vector<int> counts(26, 0);
+
+        for (int i = 0; i < s.length(); ++i) {
+            counts[s[i] - 'a']++;
+            counts[t[i] - 'a']--;
+        }
+
+        for (int count : counts) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Solution 2
+     *
+     * Sort
+     * - Time Complexity: O(N * logᴺ)
+     * - Space Complexity: O(1) or O(logᴺ)
+     */
+    bool solution2(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
+
         return s == t;
-    }
-
-    // Solution 2: Hash Table
-    bool solution2(string s, string t) {
-        unordered_map<char, int> count;
-
-        // Count the frequency of characters in string `s`
-        for (auto x : s) {
-            count[x]++;
-        }
-
-        // Decrement the frequency of characters in string `t`
-        for (auto x : t) {
-            count[x]--;
-        }
-
-        // Check if any character has non-zero frequency
-        for (auto x : count) {
-            if (x.second != 0) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    // Solution 3: Hash Table (Using Array)
-    bool solution3(string s, string t) {
-        int count[26] = {0};
-
-        // Count the frequency of characters in string `s`
-        for (char x : s) {
-            count[x - 'a']++;
-        }
-
-        // Decrement the frequency of characters in string `t`
-        for (char x : t) {
-            count[x - 'a']--;
-        }
-
-        // Check if any character has non-zero frequency
-        for (int val : count) {
-            if (val != 0) {
-                return false;
-            }
-        }
-
-        return true;
     }
 };
