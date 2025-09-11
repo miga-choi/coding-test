045f7bc40793709eadc8c7e169f831767c70e574
miga-choi <miga.choi.1004@gmail.com>
2025-09-11 14:39:17 +0900

    leetcode: "21_merge_two_sorted_lists" add python solution

--- leetcode/algorithms/21_merge_two_sorted_lists/main.py
+++ leetcode/algorithms/21_merge_two_sorted_lists/main.py
@@ -8,96 +8,73 @@ class ListNode:
 
 
 class MergeTwoSortedLists:
+    ##
+    # Iteration
+    # - Time Complexity: O(N + M)
+    # - Space Complexity: O(1)
+    #
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        result: ListNode = ListNode()
-        currentNode: ListNode = result
+        dummy: Optional[ListNode] = ListNode()
+        curr_head: Optional[ListNode] = dummy
 
-        while list1 or list2:
-            if list1 and list2:
-                if list1.val > list2.val:
-                    currentNode.next = list2
-                    list2 = list2.next
-                else:
-                    currentNode.next = list1
-                    list1 = list1.next
-            elif list1:
-                currentNode.next = list1
-                list1 = list1.next
-            else:
-                currentNode.next = list2
+        while list1 != None and list2 != None:
+            if list1.val > list2.val:
+                curr_head.next = list2
                 list2 = list2.next
+            else:
+                curr_head.next = list1
+                list1 = list1.next
             currentNode = currentNode.next
 
-        return result.next
+        curr_head.next = list2 if list1 == None else list1
+
+        return dummy.next
 
 
     # Solution
-    # Solution 1:
+    ##
+    # Solution 1
+    #
+    # Iteration
+    # - Time Complexity: O(N + M)
+    # - Space Complexity: O(1)
+    #
     def solution1(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        cur = dummy = ListNode()
+        dummy = ListNode()
+        current = dummy
 
         while list1 and list2:
             if list1.val < list2.val:
-                cur.next = list1
-                list1, cur = list1.next, list1
-            else:
-                cur.next = list2
-                list2, cur = list2.next, list2
-
-        if list1 or list2:
-            cur.next = list1 if list1 else list2
-
-        return dummy.next
-
-    # Solution 2: iteratively
-    def solution2(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = cur = ListNode(0)
-
-        while list1 and list2:
-            if list1.val < list2.val:
-                cur.next = list1
+                current.next = list1
                 list1 = list1.next
             else:
-                cur.next = list2
+                current.next = list2
                 list2 = list2.next
-            cur = cur.next
+            current = current.next
 
-        cur.next = list1 or list2
+        if list1:
+            current.next = list1
+        elif list2:
+            current.next = list2
 
         return dummy.next
 
-    # Solution 3: recursively
-    def solution3(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if not list1 or not list2:
-            return list1 or list2
-            
+    ##
+    # Solution 2
+    #
+    # Recursion
+    # - Time Complexity: O(N + M)
+    # - Space Complexity: O(N + M)
+    #
+    def solution2(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+
         if list1.val < list2.val:
-            list1.next = self.solution3(list1.next, list2)
+            list1.next = self.solution2(list1.next, list2)
             return list1
         else:
-            list2.next = self.solution3(list1, list2.next)
+            list2.next = self.solution2(list1, list2.next)
             return list2
-
-    # Solution 4: in-place, iteratively
-    def solution4(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if None in (list1, list2):
-            return list1 or list2
-
-        dummy = cur = ListNode(0)
-        dummy.next = list1
-
-        while list1 and list2:
-            if list1.val < list2.val:
-                list1 = list1.next
-            else:
-                nxt = cur.next
-                cur.next = list2
-                tmp = list2.next
-                list2.next = nxt
-                list2 = tmp
-
-            cur = cur.next
-
-        cur.next = list1 or list2
-
-        return dummy.next
