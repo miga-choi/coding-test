3a5c41589821042ff3fa9710f6fc7cc713cd3d73
miga-choi <miga.choi.1004@gmail.com>
2025-09-13 01:03:47 +0900

    leetcode: "83_remove_duplicates_from_sorted_list" add javascript solution

--- leetcode/algorithms/83_remove_duplicates_from_sorted_list/main.js
+++ leetcode/algorithms/83_remove_duplicates_from_sorted_list/main.js
@@ -4,29 +4,46 @@ function ListNode(val, next) {
 }
 
 /**
+ * Iteration
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(1)
+ *
  * @param {ListNode} head
  * @return {ListNode}
  */
 var deleteDuplicates = function (head) {
-  let result = head;
-  while (result) {
-    if (result.next && result.val === result.next.val) {
-      result.next = result.next.next;
-      continue;
+  let currHead = head;
+
+  while (currHead) {
+    if (currHead.next && currHead.val === currHead.next.val) {
+      currHead.next = currHead.next.next;
+    } else {
+      currHead = currHead.next;
     }
-    result = result.next;
   }
+
   return head;
 };
 
 
 // Solution
-// Solution 1
-var solution1 = function (head) {
-  var current = head;
+/**
+ * Iteration
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(1)
+ *
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var solution = function (head) {
+  if (head === null || head.next === null) {
+    return head;
+  }
 
-  while (current) {
-    if (current.next !== null && current.val == current.next.val) {
+  let current = head;
+
+  while (current !== null && current.next !== null) {
+    if (current.val === current.next.val) {
       current.next = current.next.next;
     } else {
       current = current.next;
@@ -34,31 +51,4 @@ var solution1 = function (head) {
   }
 
   return head;
-};
-
-// Solution 2
-var solution2 = function (head) {
-  // Special case...
-  if (head == null || head.next == null) {
-    return head;
-  }
-
-  // Initialize a pointer curr with the address of head node...
-  let curr = head;
-
-  // Traverse all element through a while loop if curr node and the next node of curr node are present...
-  while (curr != null && curr.next != null) {
-    // If the value of curr is equal to the value of prev...
-    // It means the value is present in the linked list...
-    if (curr.val == curr.next.val) {
-      // Hence we do not need to include curr again in the linked list...
-      // So we increment the value of curr...
-      curr.next = curr.next.next;
-    } else {
-      // Otherwise, we increment the curr pointer...
-      curr = curr.next;
-    }
-  }
-
-  return head; // Return the sorted linked list without any duplicate element...
 };
