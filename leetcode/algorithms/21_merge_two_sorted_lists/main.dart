f1755299b7343a9442d516b2d359e46b461f8a95
miga-choi <miga.choi.1004@gmail.com>
2025-09-12 16:02:08 +0900

    leetcode: "21_merge_two_sorted_lists" add dart solution

--- leetcode/algorithms/21_merge_two_sorted_lists/main.dart
+++ leetcode/algorithms/21_merge_two_sorted_lists/main.dart
@@ -5,41 +5,72 @@ class ListNode {
 }
 
 class MergeTwoSortedLists {
+  /**
+   * Iteration
+   * - Time Complexity: O(N + M)
+   * - Space Complexity: O(1)
+   */
   ListNode? mergeTwoLists(ListNode? list1, ListNode? list2) {
-    if (list1 == null) {
-      return list2;
-    } else if (list2 == null) {
-      return list1;
-    } else {
-      ListNode result = ListNode();
-      ListNode current = result;
+    ListNode dummy = ListNode();
+    ListNode currHead = dummy;
 
-      while (list1 != null || list2 != null) {
-        if (list1 != null && list2 != null) {
-          if (list1.val > list2.val) {
-            current.next = list2;
-            list2 = list2.next;
-          } else {
-            current.next = list1;
-            list1 = list1.next;
-          }
-        } else if (list1 != null) {
-          current.next = list1;
-          list1 = list1.next;
-        } else if (list2 != null) {
-          current.next = list2;
-          list2 = list2.next;
-        }
-        current = current.next!;
+    while (list1 != null && list2 != null) {
+      if (list1.val > list2.val) {
+        currHead.next = list2;
+        list2 = list2.next;
+      } else {
+        currHead.next = list1;
+        list1 = list1.next;
       }
-
-      return result.next;
+      currHead = currHead.next!;
     }
+
+    currHead.next = list1 == null ? list2 : list1;
+
+    return dummy.next;
   }
 
 
   // Solution
-  ListNode? solution(ListNode? list1, ListNode? list2) {
+  /**
+   * Solution 1
+   * 
+   * Iteration
+   * - Time Complexity: O(N + M)
+   * - Space Complexity: O(1)
+   */
+  ListNode? solution1(ListNode? list1, ListNode? list2) {
+    ListNode dummy = ListNode(0);
+    ListNode current = dummy;
+
+    while (list1 != null && list2 != null) {
+      if (list1.val <= list2.val) {
+        current.next = list1;
+        list1 = list1.next;
+      } else {
+        current.next = list2;
+        list2 = list2.next;
+      }
+      current = current.next!;
+    }
+
+    if (list1 != null) {
+      current.next = list1;
+    } else if (list2 != null) {
+      current.next = list2;
+    }
+
+    return dummy.next;
+  }
+
+  /**
+   * Solution 2
+   * 
+   * Recursion
+   * - Time Complexity: O(N + M)
+   * - Space Complexity: O(N + M)
+   */
+  ListNode? solution2(ListNode? list1, ListNode? list2) {
     if (list1 == null) {
       return list2;
     }
@@ -47,11 +78,11 @@ class MergeTwoSortedLists {
       return list1;
     }
 
-    if (list1.val < list2.val) {
-      list1.next = solution(list1.next, list2);
+    if (list1.val <= list2.val) {
+      list1.next = solution2(list1.next, list2);
       return list1;
     } else {
-      list2.next = solution(list1, list2.next);
+      list2.next = solution2(list1, list2.next);
       return list2;
     }
   }
