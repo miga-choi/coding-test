119808359b3ad5b218ac627cd009abefaa576979
miga-choi <miga.choi.1004@gmail.com>
2025-09-15 01:17:45 +0900

    leetcode: "141_linked_list_cycle" add c solution

--- leetcode/algorithms/141_linked_list_cycle/main.c
+++ leetcode/algorithms/141_linked_list_cycle/main.c
@@ -6,14 +6,19 @@ struct ListNode {
     struct ListNode* next;
 };
 
+/**
+ * Floyd's Tortoise and Hare Algorithm
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(1)
+ */
 bool hasCycle(struct ListNode* head) {
-    struct ListNode* node1 = head;
-    struct ListNode* node2 = head;
+    struct ListNode* ahead = head;
+    struct ListNode* behind = head;
 
-    while (node2 != 0 && node2->next != 0) {
-        node1 = node1->next;
-        node2 = node2->next->next;
-        if (node1 == node2) {
+    while (ahead && ahead->next) {
+        ahead = ahead->next->next;
+        behind = behind->next;
+        if (ahead == behind) {
             return true;
         }
     }
@@ -23,18 +28,27 @@ bool hasCycle(struct ListNode* head) {
 
 
 // Solution
+/**
+ * Floyd's Tortoise and Hare Algorithm
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(1)
+ */
 bool solution(struct ListNode* head) {
-    // Initialize two node slow and fast point to the hesd node...
-    struct ListNode* fastptr = head;
-    struct ListNode* slowptr = head;
-    while (fastptr != NULL && fastptr->next != NULL) {
-        // Move slow pointer by 1 node and fast at 2 at each step.
-        slowptr = slowptr->next;
-        fastptr = fastptr->next->next;
-        // If both the pointers meet at any point, then the cycle is present and return true...
-        if (slowptr == fastptr) {
+    if (head == NULL || head->next == NULL) {
+        return false;
+    }
+
+    struct ListNode* slow = head;
+    struct ListNode* fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) {
             return true;
         }
     }
+
     return false;
 }
