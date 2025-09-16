f958439ff614908d6e35c888641752fd05a90fae
miga-choi <miga.choi.1004@gmail.com>
2025-09-17 03:03:18 +0900

    leetcode: "141_linked_list_cycle" add python solution

--- leetcode/algorithms/141_linked_list_cycle/main.py
+++ leetcode/algorithms/141_linked_list_cycle/main.py
@@ -9,36 +9,37 @@ class ListNode:
 
 class LinkedListCycle:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        stack = []
-        while head != None:
-            if head in stack:
+        ahead: Optional[ListNode] = head
+        behind: Optional[ListNode] = head
+
+        while ahead != None and ahead.next != None:
+            ahead = ahead.next.next
+            behind = behind.next
+
+            if ahead == behind:
                 return True
-            stack.append(head)
-            head = head.next
+
         return False
 
 
     # Solution
-    # Solution 1: Dictionary/Hash table
-    def solution1(self, head: ListNode) -> bool:
-        dictionary = {}
-        while head:
-            if head in dictionary:
-                return True
-            else:
-                dictionary[head] = True
-            head = head.next
-        return False
-
-    # Solution 2: Two pointers
-    def solution2(self, head: ListNode) -> bool:
-        if not head:
+    ##
+    # Floyd's Tortoise and Hare Algorithm
+    # - Time Complexity: O(N)
+    # - Space Complexity: O(1)
+    #
+    def solution(self, head: ListNode) -> bool:
+        if not head or not head.next:
             return False
+
         slow = head
         fast = head.next
+
         while slow != fast:
             if not fast or not fast.next:
                 return False
+
             slow = slow.next
             fast = fast.next.next
+
         return True
