faac8a90f7036de575ab375e82ff29d4f721d652
miga-choi <miga.choi.1004@gmail.com>
2025-09-16 17:14:47 +0900

    leetcode: "141_linked_list_cycle" add javascript solution

--- leetcode/algorithms/141_linked_list_cycle/main.js
+++ leetcode/algorithms/141_linked_list_cycle/main.js
@@ -4,31 +4,54 @@ function ListNode(val) {
 }
 
 /**
+ * Floyd's Tortoise and Hare Algorithm
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(1)
+ *
  * @param {ListNode} head
  * @return {boolean}
  */
 var hasCycle = function (head) {
-  const stack = [];
-  while (head) {
-    if (stack.indexOf(head) > -1) {
+  let ahead = head;
+  let behind = head;
+
+  while (ahead && ahead.next) {
+    ahead = ahead.next.next;
+    behind = behind.next;
+    if (ahead == behind) {
       return true;
     }
-    stack.push(head);
-    head = head.next;
   }
+
   return false;
 };
 
 
 // Solution
-const solution = (head) => {
+/**
+ * Floyd's Tortoise and Hare Algorithm
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(1)
+ *
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var solution = function (head) {
+  if (head === null || head.next === null) {
+    return false;
+  }
+
+  let slow = head;
   let fast = head;
-  while (fast && fast.next) {
-    head = head.next;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
     fast = fast.next.next;
-    if (head === fast) {
+
+    if (slow === fast) {
       return true;
     }
   }
+
   return false;
 };
