581360763cdc1a83051aca4ff46cf6283700dbdd
miga-choi <miga.choi.1004@gmail.com>
2025-09-20 18:34:01 +0900

    leetcode: "206_reverse_linked_list" add c solution

--- leetcode/algorithms/206_reverse_linked_list/main.c
+++ leetcode/algorithms/206_reverse_linked_list/main.c
@@ -1,32 +1,86 @@
+#include <stddef.h>
+
 struct ListNode {
     int val;
     struct ListNode* next;
 };
 
-struct ListNode* reverseList(struct ListNode* head) {
-    struct ListNode* temp = 0;
-    struct ListNode* result = 0;
-
-    while (head) {
-        temp = head->next;
-        head->next = result;
-        result = head;
-        head = temp;
+/**
+ * Iteration
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(N)
+ */
+struct ListNode* reverseList_iterative(struct ListNode* head) {
+    if (head == NULL || head->next == NULL) {
+        return head;
     }
 
-    return result;
+    struct ListNode* stack[5000];
+    int top = -1;
+
+    struct ListNode* temp = head;
+    while (temp != NULL) {
+        stack[++top] = temp;
+        temp = temp->next;
+    }
+
+    struct ListNode* new_head = stack[top];
+    temp = new_head;
+    top--;
+
+    while (top >= 0) {
+        struct ListNode* current_node = stack[top--];
+        temp->next = current_node;
+        temp = temp->next;
+    }
+
+    temp->next = NULL;
+
+    return new_head;
 }
 
 
 // Solution
-struct ListNode* solution(struct ListNode* head) {
-    struct ListNode* nextNode = 0;
-    struct ListNode* prevNode = 0;
-    while (head) {
-        nextNode = head->next;
-        head->next = prevNode;
-        prevNode = head;
-        head = nextNode;
+/**
+ * Solution 1
+ * 
+ * Iteration
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(1)
+ */
+struct ListNode* solution1(struct ListNode* head) {
+    struct ListNode* prev = NULL;
+    struct ListNode* current = head;
+    struct ListNode* next_temp = NULL;
+
+    while (current != NULL) {
+        next_temp = current->next;
+        
+        current->next = prev;
+        
+        prev = current;
+        current = next_temp;
     }
-    return prevNode;
+
+    return prev;
+}
+
+/**
+ * Solution 2
+ * 
+ * Recursion
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(N)
+ */
+struct ListNode* solution2(struct ListNode* head) {
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+
+    struct ListNode* newHead = solution2(head->next);
+
+    head->next->next = head;
+    head->next = NULL;
+
+    return newHead;
 }
