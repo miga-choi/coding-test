32160a0bbdce4e213f41498cc7ea0779f8a39ee9
miga-choi <miga.choi.1004@gmail.com>
2025-09-22 01:11:40 +0900

    leetcode: "206_reverse_linked_list" add java solution

--- leetcode/algorithms/206_reverse_linked_list/main.java
+++ leetcode/algorithms/206_reverse_linked_list/main.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 class ReverseLinkedList {
   class ListNode {
     int val;
@@ -16,46 +18,76 @@ class ReverseLinkedList {
     }
   }
 
+  /**
+   * Iteration
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(N)
+   */
   public ListNode reverseList(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+    Stack<ListNode> stack = new Stack<>();
+
+    while (head != null) {
+      stack.push(head);
+      head = head.next;
+    }
+
+    ListNode newHead = stack.pop();
+    ListNode currHead = newHead;
+
+    while (!stack.empty()) {
+      currHead.next = stack.pop();
+      currHead = currHead.next;
+    }
+
+    currHead.next = null;
+
+    return newHead;
+  }
+
+
+  // Solution
+  /**
+   * Solution 1
+   * 
+   * Iteration
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(1)
+   */
+  public ListNode solution1(ListNode head) {
     ListNode prev = null;
     ListNode current = head;
 
     while (current != null) {
-      ListNode next = current.next;
+      ListNode nextTemp = current.next;
       current.next = prev;
       prev = current;
-      current = next;
+      current = nextTemp;
     }
 
     return prev;
   }
 
-
-  // Solution
-  // Solution 1: iterative solution
-  public ListNode solution1(ListNode head) {
-    ListNode newHead = null;
-    while (head != null) {
-      ListNode next = head.next;
-      head.next = newHead;
-      newHead = head;
-      head = next;
-    }
-    return newHead;
-  }
-
-  // Solution 2: recursive solution
-  private ListNode reverseListInt(ListNode head, ListNode newHead) {
-    if (head == null) {
-      return newHead;
-    }
-    ListNode next = head.next;
-    head.next = newHead;
-    return reverseListInt(next, head);
-  }
-
+  /**
+   * Solution 2
+   * 
+   * Recursion
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(N)
+   */
   public ListNode solution2(ListNode head) {
-    /* recursive solution */
-    return reverseListInt(head, null);
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+    ListNode newHead = solution2(head.next);
+
+    head.next.next = head;
+    head.next = null;
+
+    return newHead;
   }
 }
