747fb006a62ed8e97e402e930ce3f0b9c32ef856
miga-choi <miga.choi.1004@gmail.com>
2025-09-23 02:47:40 +0900

    leetcode: "206_reverse_linked_list" add dart solution

--- leetcode/algorithms/206_reverse_linked_list/main.dart
+++ leetcode/algorithms/206_reverse_linked_list/main.dart
@@ -5,28 +5,77 @@ class ListNode {
 }
 
 class ReverseLinkedList {
+  /**
+   * Iteration
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(N)
+   */
   ListNode? reverseList(ListNode? head) {
-    ListNode? temp = null;
-    ListNode? result = null;
-
-    while (head != null) {
-      temp = head.next;
-      head.next = result;
-      result = head;
-      head = temp;
+    if (head == null || head.next == null) {
+      return head;
     }
 
-    return result;
+    List<ListNode?> stack = List<ListNode>.empty(growable: true);
+
+    while (head != null) {
+      stack.add(head);
+      head = head.next;
+    }
+
+    ListNode? newHead = stack.removeLast();
+    ListNode? currHead = newHead;
+
+    while (stack.isNotEmpty) {
+      currHead?.next = stack.removeLast();
+      currHead = currHead?.next;
+    }
+
+    currHead?.next = null;
+
+    return newHead;
   }
 
 
   // Solution
-  ListNode? solution(ListNode? head) {
-    final tm = ListNode();
-    while (head != null) {
-      tm.next = ListNode(head.val, tm.next);
-      head = head.next;
+  /**
+   * Solution 1
+   * 
+   * Iteration
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(1)
+   */
+  ListNode? solution1(ListNode? head) {
+    ListNode? prev = null;
+    ListNode? current = head;
+    ListNode? nextTemp;
+
+    while (current != null) {
+      nextTemp = current.next;
+      current.next = prev;
+      prev = current;
+      current = nextTemp;
     }
-    return tm.next;
+
+    return prev;
+  }
+
+  /**
+   * Solution 2
+   * 
+   * Recursion
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(1)
+   */
+  ListNode? solution2(ListNode? head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+    ListNode? p = solution2(head.next);
+
+    head.next!.next = head;
+    head.next = null;
+
+    return p;
   }
 }
