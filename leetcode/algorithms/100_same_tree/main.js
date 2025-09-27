ad42328df764c7a59a8d78e7f7b5f2370f2d6843
miga-choi <miga.choi.1004@gmail.com>
2025-09-28 01:15:53 +0900

    leetcode: "100_same_tree" add javascript solution

--- leetcode/algorithms/100_same_tree/main.js
+++ leetcode/algorithms/100_same_tree/main.js
@@ -5,130 +5,111 @@ function TreeNode(val, left, right) {
 }
 
 /**
+ * Recursion: DFS
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(H)
+ *
  * @param {TreeNode} p
  * @param {TreeNode} q
  * @return {boolean}
  */
 var isSameTree = function (p, q) {
-  return checkNode(p, q);
-};
-
-/**
- * @param {TreeNode} node1
- * @param {TreeNode} node2
- * @return {boolean}
- */
-var checkNode = function (node1, node2) {
-  if (!node1 && !node2) {
-    return true;
-  }
-
-  if (!node1 || !node2) {
-    return false;
-  }
-
-  if (node1.val !== node2.val) {
-    return false;
-  }
-
-  return (
-    checkNode(node1.left, node2.left) && checkNode(node1.right, node2.right)
-  );
-};
-
-
-// Solution
-// Solution 1
-function solution1(p, q) {
-  return JSON.stringify(p) === JSON.stringify(q);
-}
-
-// Solution 2: Recursive (DFS) Preorder traversal
-function solution2(p, q) {
-  // Base case: If both trees are empty, they are identical.
   if (p === null && q === null) {
     return true;
   }
 
-  // If one of the trees is empty and the other is not, they are not identical.
-  if (p === null || q === null) {
+  if (p === null || q === null || p.val !== q.val) {
     return false;
   }
 
-  // Compare the values of the current nodes.
-  if (p.val !== q.val) {
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+
+
+// Solution
+/**
+ * Solution 1
+ *
+ * Recursion: DFS
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(H)
+ *
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+function solution1(p, q) {
+  if (p === null && q === null) {
+    return true;
+  }
+
+  if (p === null || q === null || p.val !== q.val) {
     return false;
   }
 
-  // Recursively check the left and right subtrees.
-  return solution2(p.left, q.left) && solution2(p.right, q.right);
+  return solution1(p.left, q.left) && solution1(p.right, q.right);
 }
 
-// Solution 3: Level-order traversal using Queues
+/**
+ * Solution 2
+ *
+ * Iteration: DFS (Stack)
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(H)
+ *
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+function solution2(p, q) {
+  const stack = [[p, q]];
+
+  while (stack.length > 0) {
+    const [nodeP, nodeQ] = stack.pop();
+
+    if (nodeP === null && nodeQ === null) {
+      continue;
+    }
+
+    if (nodeP === null || nodeQ === null || nodeP.val !== nodeQ.val) {
+      return false;
+    }
+
+    stack.push([nodeP.right, nodeQ.right]);
+    stack.push([nodeP.left, nodeQ.left]);
+  }
+
+  return true;
+}
+
+/**
+ * Solution 3
+ *
+ * Iteration: BFS (Queue)
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(W)
+ *
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
 function solution3(p, q) {
-  // Create queues for both trees.
-  const queue1 = [p];
-  const queue2 = [q];
+  const queue = [[p, q]];
 
-  while (queue1.length > 0 && queue2.length > 0) {
-    const node1 = queue1.shift();
-    const node2 = queue2.shift();
+  while (queue.length > 0) {
+    const [nodeP, nodeQ] = queue.shift();
 
-    // If the values of the current nodes are not equal, the trees are not identical.
-    if (!node1 && !node2) {
+    if (nodeP === null && nodeQ === null) {
       continue;
     }
-    if (!node1 || !node2 || node1.val !== node2.val) {
+
+    if (nodeP === null || nodeQ === null || nodeP.val !== nodeQ.val) {
       return false;
     }
 
-    // Add the left and right children of both nodes to their respective queues.
-    queue1.push(node1.left);
-    queue1.push(node1.right);
-    queue2.push(node2.left);
-    queue2.push(node2.right);
+    queue.push([nodeP.left, nodeQ.left]);
+    queue.push([nodeP.right, nodeQ.right]);
   }
 
-  // If both queues are empty, the trees are identical.
-  return queue1.length === 0 && queue2.length === 0;
-}
-
-// Solution 4: Level-order traversal using Stack
-function solution4(p, q) {
-  const stack1 = [p];
-  const stack2 = [q];
-
-  while (stack1.length > 0 && stack2.length > 0) {
-    const node1 = stack1.pop();
-    const node2 = stack2.pop();
-
-    if (!node1 && !node2) {
-      continue;
-    } else if (!node1 || !node2 || node1.val !== node2.val) {
-      return false;
-    }
-
-    stack1.push(node1.left);
-    stack2.push(node2.left);
-    stack1.push(node1.right);
-    stack2.push(node2.right);
-  }
-
-  return stack1.length === 0 && stack2.length === 0;
-}
-
-// Solution 5: Tree Hashing
-function solution5(p, q) {
-  const hash1 = solution5_computeTreeHash(p);
-  const hash2 = solution5_computeTreeHash(q);
-  return hash1 === hash2;
-}
-
-function solution5_computeTreeHash(node) {
-  if (node === null) {
-    return "null";
-  }
-  const leftHash = solution5_computeTreeHash(node.left);
-  const rightHash = solution5_computeTreeHash(node.right);
-  return `(${node.val}${leftHash}${rightHash})`;
+  return true;
 }
