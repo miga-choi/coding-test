51c1f8faf36ba5f27403d1e2f05ecc09e03d9634
miga-choi <miga.choi.1004@gmail.com>
2025-09-30 01:36:39 +0900

    leetcode: "104_maximum_depth_of_binary_tree" add c solution

--- leetcode/algorithms/104_maximum_depth_of_binary_tree/main.c
+++ leetcode/algorithms/104_maximum_depth_of_binary_tree/main.c
@@ -1,9 +1,16 @@
+#include <stdlib.h> // NULL
+
 struct TreeNode {
     int val;
     struct TreeNode* left;
     struct TreeNode* right;
 };
 
+/**
+ * Recursion: DFS
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(H)
+ */
 int maxDepth(struct TreeNode* root) {
     if (!root) {
         return 0;
@@ -17,13 +24,22 @@ int maxDepth(struct TreeNode* root) {
 
 
 // Solution
+/**
+ * Recursion: DFS
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(H)
+ */
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
 int solution(struct TreeNode* root) {
-    if (!root) {
+    if (root == NULL) {
         return 0;
     }
 
-    int left = maxDepth(root->left);
-    int right = maxDepth(root->right);
+    int leftDepth = solution(root->left);
+    int rightDepth = solution(root->right);
 
-    return left >= right ? left + 1 : right + 1;
+    return 1 + max(leftDepth, rightDepth);
 }
