ae12ebad230e80b34f1aacc6eb52e5d22fe9437b
miga-choi <miga.choi.1004@gmail.com>
2025-10-02 03:27:22 +0900

    leetcode: "104_maximum_depth_of_binary_tree" add dart solution

--- leetcode/algorithms/104_maximum_depth_of_binary_tree/main.dart
+++ leetcode/algorithms/104_maximum_depth_of_binary_tree/main.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'dart:math';
 
 class TreeNode {
@@ -7,31 +8,111 @@ class TreeNode {
   TreeNode([this.val = 0, this.left, this.right]);
 }
 
+typedef _NodeDepthRecord = (TreeNode node, int depth);
+
 class MaximumDepthOfBinaryTree {
+  /**
+   * Recursion: DFS
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(H)
+   */
   int maxDepth(TreeNode? root) {
-    int returnDepth(TreeNode? node, int depth) {
-      if (node == null) {
-        return depth;
-      }
-      return 1 + max(returnDepth(node.left, depth), returnDepth(node.right, depth));
+    if (root == null) {
+      return 0;
     }
 
-    return returnDepth(root, 0);
+    final int left = maxDepth(root.left);
+    final int right = maxDepth(root.right);
+
+    return max(left, right) + 1;
   }
 
 
   // Solution
-  int solution(TreeNode? root) {
-    return visiter(root);
+  /**
+   * Solution 1
+   * 
+   * Recursion: DFS
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(H)
+   */
+  int solution1(TreeNode? root) {
+    if (root == null) {
+      return 0;
+    }
+
+    int leftDepth = maxDepth(root.left);
+    int rightDepth = maxDepth(root.right);
+
+    return max(leftDepth, rightDepth) + 1;
   }
 
-  int visiter(TreeNode? root) {
-    if (root == null) return 0;
-    int sum = 1;
+  /**
+   * Solution 2
+   * 
+   * Iteration: BFS (Queue)
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(W)
+   */
+  int solution2(TreeNode? root) {
+    if (root == null) {
+      return 0;
+    }
 
-    final leftSum = visiter(root.left);
-    final rightSum = visiter(root.right);
+    Queue<TreeNode> queue = Queue();
+    queue.add(root);
+    int depth = 0;
 
-    return sum + max(leftSum, rightSum);
+    while (queue.isNotEmpty) {
+      depth++;
+      int levelSize = queue.length;
+
+      for (int i = 0; i < levelSize; i++) {
+        TreeNode currentNode = queue.removeFirst();
+
+        if (currentNode.left != null) {
+          queue.add(currentNode.left!);
+        }
+        if (currentNode.right != null) {
+          queue.add(currentNode.right!);
+        }
+      }
+    }
+
+    return depth;
+  }
+
+  /**
+   * Solution 3
+   * 
+   * Iteration: DFS (Stack)
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(H)
+   */
+  int solution3(TreeNode? root) {
+    if (root == null) {
+      return 0;
+    }
+
+    List<_NodeDepthRecord> stack = [];
+    stack.add((root, 1));
+    int maxD = 0;
+
+    while (stack.isNotEmpty) {
+      _NodeDepthRecord current = stack.removeLast();
+      TreeNode node = current.$1;
+      int depth = current.$2;
+
+      maxD = max(maxD, depth);
+
+      if (node.right != null) {
+        stack.add((node.right!, depth + 1));
+      }
+      if (node.left != null) {
+        stack.add((node.left!, depth + 1));
+      }
+    }
+
+    return maxD;
   }
 }
