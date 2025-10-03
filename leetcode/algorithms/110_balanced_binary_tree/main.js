c372fe9a4748044ae43b7532d1b5645ffa27b62f
miga-choi <miga.choi.1004@gmail.com>
2025-10-04 04:21:16 +0900

    leetcode: "110_balanced_binary_tree" add javascript solution

--- leetcode/algorithms/110_balanced_binary_tree/main.js
+++ leetcode/algorithms/110_balanced_binary_tree/main.js
@@ -5,30 +5,34 @@ function TreeNode(val, left, right) {
 }
 
 /**
+ * Recursion: DFS
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(H)
+ */
+/**
  * @param {TreeNode} node
  * @return {number}
  */
-const checkDepth = (node) => {
-  if (!node) {
+const getMaxDepth = (node) => {
+  if (node === null) {
     return 0;
   }
 
-  const leftDepth = checkDepth(node.left);
-  const rightDepth = checkDepth(node.right);
-
-  if (leftDepth === -1 || rightDepth === -1) {
+  const leftDepth = getMaxDepth(node.left);
+  if (leftDepth < 0) {
     return -1;
   }
 
-  if (leftDepth - rightDepth > 1 || rightDepth - leftDepth > 1) {
+  const rightDepth = getMaxDepth(node.right);
+  if (rightDepth < 0) {
     return -1;
   }
 
-  if (leftDepth >= rightDepth) {
-    return leftDepth + 1;
-  } else {
-    return rightDepth + 1;
+  if (Math.abs(leftDepth - rightDepth) > 1) {
+    return -1;
   }
+
+  return Math.max(leftDepth, rightDepth) + 1;
 };
 
 /**
@@ -36,79 +40,45 @@ const checkDepth = (node) => {
  * @return {boolean}
  */
 var isBalanced = function (root) {
-  return checkDepth(root) === -1 ? false : true;
+  return getMaxDepth(root) !== -1;
 };
 
 
 // Solution
-// Solution 1
-var solution1 = function (root) {
-  // If the tree is empty, we can say it’s balanced...
-  if (root == null) {
-    return true;
-  }
-  // Height Function will return -1, when it’s an unbalanced tree...
-  if (solution1_height(root) == -1) {
-    return false;
-  }
-  return true;
-};
-
-// Create a function to return the “height” of a current subtree using recursion...
-var solution1_height = function (root) {
-  // Base case...
-  if (root == null) {
-    return 0;
-  }
-  // Height of left subtree...
-  let leftHeight = solution1_height(root.left);
-  // Height of height subtree...
-  let rightHight = solution1_height(root.right);
-  // In case of left subtree or right subtree unbalanced, return -1...
-  if (leftHeight == -1 || rightHight == -1) {
-    return -1;
-  }
-  // If their heights differ by more than ‘1’, return -1...
-  if (Math.abs(leftHeight - rightHight) > 1) {
-    return -1;
-  }
-  // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
-  return Math.max(leftHeight, rightHight) + 1;
-};
-
-// olution 2: DFS (Postorder)
-var solution2 = function (root) {
-  let dfs = function (node) {
-    if (!node) {
+/**
+ * Recursion: DFS
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(H)
+ *
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var solution = function (root) {
+  /**
+   * @param {TreeNode} node
+   * @returns {number}
+   */
+  function checkHeight(node) {
+    if (node === null) {
       return 0;
     }
-    let left = 1 + dfs(node.left);
-    let right = 1 + dfs(node.right);
-    if (Math.abs(left - right) > 1) {
-      return Infinity;
+
+    const leftHeight = checkHeight(node.left);
+    if (leftHeight === -1) {
+      return -1;
     }
-    return Math.max(left, right);
-  };
 
-  return dfs(root) == Infinity ? false : true;
-};
+    const rightHeight = checkHeight(node.right);
+    if (rightHeight === -1) {
+      return -1;
+    }
 
-// Solution 3: DFS (Top-Down recursion)
-var solution3 = function (root) {
-  if (!root) {
-    return true;
+    if (Math.abs(leftHeight - rightHeight) > 1) {
+      return -1;
+    }
+
+    return 1 + Math.max(leftHeight, rightHeight);
   }
 
-  let height = function (node) {
-    if (!node) {
-      return 0;
-    }
-    return 1 + Math.max(height(node.left), height(node.right));
-  };
-
-  return (
-    Math.abs(height(root.left) - height(root.right)) < 2 &&
-    solution3(root.left) &&
-    solution3(root.right)
-  );
+  return checkHeight(root) !== -1;
 };
