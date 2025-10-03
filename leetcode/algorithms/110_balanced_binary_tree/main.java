8bf6a0f3552596a2784d6aeeb90151e03a91ac08
miga-choi <miga.choi.1004@gmail.com>
2025-10-04 04:21:42 +0900

    leetcode: "110_balanced_binary_tree" add java solution

--- leetcode/algorithms/110_balanced_binary_tree/main.java
+++ leetcode/algorithms/110_balanced_binary_tree/main.java
@@ -1,7 +1,3 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
-
 class BalancedBinaryTree {
   public class TreeNode {
     int val;
@@ -22,92 +18,67 @@ class BalancedBinaryTree {
     }
   }
 
-  public boolean isBalanced(TreeNode root) {
-    return checkDepth(root) > -1;
-  }
-
-  public int checkDepth(TreeNode node_) {
-    if (node_ == null) {
+  /**
+   * Recursion: DFS
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(H)
+   */
+  private int getMaxDepth(TreeNode node) {
+    if (node == null) {
       return 0;
     }
 
-    int leftDepth = checkDepth(node_.left);
-    int rightDepth = checkDepth(node_.right);
+    int leftDepth = getMaxDepth(node.left);
+    if (leftDepth < 0) {
+      return -1;
+    }
 
-    if (leftDepth == -1 ||
-        rightDepth == -1 ||
-        Math.abs(leftDepth - rightDepth) > 1) {
+    int rightDepth = getMaxDepth(node.right);
+    if (rightDepth < 0) {
+      return -1;
+    }
+
+    if (Math.abs(leftDepth - rightDepth) > 1) {
       return -1;
     }
 
     return Math.max(leftDepth, rightDepth) + 1;
   }
 
+  public boolean isBalanced(TreeNode root) {
+    return getMaxDepth(root) != -1;
+  }
+
 
   // Solution
-  // Solution 1
-  public boolean solution1(TreeNode root) {
-    // If the tree is empty, we can say it’s balanced...
-    if (root == null) {
-      return true;
-    }
-    // Height Function will return -1, when it’s an unbalanced tree...
-    if (solution1_height(root) == -1) {
-      return false;
-    }
-    return true;
-  }
-
-  // Create a function to return the “height” of a current subtree using recursion...
-  public int solution1_height(TreeNode root) {
-    // Base case...
-    if (root == null) {
+  /**
+   * Recursion: DFS
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(H)
+   */
+  private int checkHeight(TreeNode node) {
+    if (node == null) {
       return 0;
     }
-    // Height of left subtree...
-    int leftHeight = solution1_height(root.left);
-    // Height of height subtree...
-    int rightHight = solution1_height(root.right);
-    // In case of left subtree or right subtree unbalanced, return -1...
-    if (leftHeight == -1 || rightHight == -1) {
+
+    int leftHeight = checkHeight(node.left);
+    if (leftHeight == -1) {
       return -1;
     }
-    // If their heights differ by more than ‘1’, return -1...
-    if (Math.abs(leftHeight - rightHight) > 1) {
+
+    int rightHeight = checkHeight(node.right);
+    if (rightHeight == -1) {
       return -1;
     }
-    // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
-    return Math.max(leftHeight, rightHight) + 1;
+
+    if (Math.abs(leftHeight - rightHeight) > 1) {
+      return -1;
+    }
+
+    return Math.max(leftHeight, rightHeight) + 1;
   }
 
-  // Solution 2: Iterative PostOrder Traversal
-  public boolean solution2(TreeNode root) {
-    if (root == null) {
-      return true;
-    }
-    Stack<TreeNode> stack = new Stack<TreeNode>();
-    Map<TreeNode, Integer> map = new HashMap<TreeNode, Integer>();
-    stack.push(root);
-    while (!stack.isEmpty()) {
-      TreeNode node = stack.pop();
-      if ((node.left == null || node.left != null && map.containsKey(node.left))
-          && (node.right == null || node.right != null && map.containsKey(node.right))) {
-        int left = node.left == null ? 0 : map.get(node.left);
-        int right = node.right == null ? 0 : map.get(node.right);
-        if (Math.abs(left - right) > 1) {
-          return false;
-        }
-        map.put(node, Math.max(left, right) + 1);
-      } else {
-        if (node.left != null && !map.containsKey(node.left)) {
-          stack.push(node);
-          stack.push(node.left);
-        } else {
-          stack.push(node);
-          stack.push(node.right);
-        }
-      }
-    }
-    return true;
+  public boolean solution(TreeNode root) {
+    return checkHeight(root) != -1;
   }
 }
