6972eaa606c15c35c1d9247a81b1014ec0ac1641
miga-choi <miga.choi.1004@gmail.com>
2025-10-05 20:34:27 +0900

    leetcode: "110_balanced_binary_tree" add python solution

--- leetcode/algorithms/110_balanced_binary_tree/main.py
+++ leetcode/algorithms/110_balanced_binary_tree/main.py
@@ -9,68 +9,82 @@ class TreeNode:
 
 
 class BalancedBinaryTree:
-    def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        return self.checkDepth(root) is not -1
-
-    def checkDepth(self, node_: Optional[TreeNode]) -> int:
-        if node_ is None:
+    ##
+    # Recursion: DFS
+    # - Time Complexity: O(N)
+    # - Space Complexity: O(H)
+    #
+    def checkDepth(self, node: Optional[TreeNode]) -> int:
+        if node is None:
             return 0
 
-        leftDepth = self.checkDepth(node_.left)
-        rightDepth = self.checkDepth(node_.right)
+        leftDepth = self.checkDepth(node.left)
 
-        if leftDepth is -1 or rightDepth is -1:
+        if leftDepth < 0:
             return -1
+
+        rightDepth = self.checkDepth(node.right)
+
+        if rightDepth < 0:
+            return -1
+
         if abs(leftDepth - rightDepth) > 1:
             return -1
 
         return max(leftDepth, rightDepth) + 1
 
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        return self.checkDepth(root) != -1
+
 
     # Solution
-    # Solution 1: Recursion
-    def solution1(self, root: Optional[TreeNode]) -> bool:
-        def check(root: Optional[TreeNode]):
-            if root is None:
-                return 0
-            left = check(root.left)
-            right = check(root.right)
-            if left == -1 or right == -1 or abs(left - right) > 1:
-                return -1
-            return 1 + max(left, right)
-
-        return check(root) != -1
-
-    # Solution 2: Iterative (Postorder traversal)
-    def solution2(self, root: Optional[TreeNode]) -> bool:
-        stack, node, last, depths = [], root, None, {}
-        while stack or node:
-            if node:
-                stack.append(node)
-                node = node.left
-            else:
-                node = stack[-1]
-                if not node.right or last == node.right:
-                    node = stack.pop()
-                    left, right = depths.get(node.left, 0), depths.get(node.right, 0)
-                    if abs(left - right) > 1:
-                        return False
-                    depths[node] = 1 + max(left, right)
-                    last = node
-                    node = None
-                else:
-                    node = node.right
-        return True
-
-    # Solution 3: Iterative (Postorder traversal)
-    def solution3(self, root: Optional[TreeNode]) -> bool:
-        return self.solution3_height(root) >= 0
-
-    def solution3_height(self, root: Optional[TreeNode]):
-        if root is None:
+    ##
+    # Solution 1
+    #
+    # Recursion: DFS (Top-down)
+    # - Time Complexity: O(N * Logᴺ)
+    # - Space Complexity: O(H)
+    #
+    def height(self, node: Optional[TreeNode]) -> int:
+        if not node:
             return 0
-        leftheight = self.solution3_height(root.left)
-        rightheight = self.solution3_height(root.right)
-        if leftheight < 0 or rightheight < 0 or abs(leftheight - rightheight) > 1:
-            return -1
-        return max(leftheight, rightheight) + 1
+        return 1 + max(self.height(node.left), self.height(node.right))
+
+    def solution1(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        left_h = self.height(root.left)
+        right_h = self.height(root.right)
+
+        if abs(left_h - right_h) > 1:
+            return False
+
+        return self.isBalanced(root.left) and self.isBalanced(root.right)
+
+    ##
+    # Solution 2
+    #
+    # Recursion: DFS (Bottom-up)
+    # - Time Complexity: O(N)
+    # - Space Complexity: O(H)
+    #
+    def solution2(self, root: Optional[TreeNode]) -> bool:
+        def check_height(node: Optional[TreeNode]) -> int:
+            if not node:
+                return 0
+
+            left_height = check_height(node.left)
+            if left_height == -1:
+                return -1
+
+            right_height = check_height(node.right)
+            if right_height == -1:
+                return -1
+
+            if abs(left_height - right_height) > 1:
+                return -1
+
+            return 1 + max(left_height, right_height)
+
+        return check_height(root) != -1
