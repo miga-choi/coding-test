4d6d12aaac3f1d63c05e31700731c111eb527d0d
miga-choi <miga.choi.1004@gmail.com>
2025-10-11 03:13:12 +0900

    leetcode: "226_invert_binary_tree" add dart solution

--- leetcode/algorithms/226_invert_binary_tree/main.dart
+++ leetcode/algorithms/226_invert_binary_tree/main.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 class TreeNode {
   int val;
   TreeNode? left;
@@ -6,18 +8,77 @@ class TreeNode {
 }
 
 class InvertBinaryTree {
+  /**
+   * Recursiont: DFS
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(H)
+   */
   TreeNode? invertTree(TreeNode? root) {
-    if (root != null) {
-      TreeNode? tempNode = root.left;
-      root.left = invertTree(root.right);
-      root.right = invertTree(tempNode);
+    if (root == null) {
+      return null;
     }
+
+    TreeNode? temp = root.left;
+    root.left = invertTree(root.right);
+    root.right = invertTree(temp);
+
     return root;
   }
 
 
   // Solution
-  TreeNode? solution(TreeNode? root) {
-    return root == null ? root : TreeNode(root.val, solution(root.right), solution(root.left));
+  /**
+   * Solution 1
+   * 
+   * Recursiont: DFS
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(H)
+   */
+  TreeNode? solution1(TreeNode? root) {
+    if (root == null) {
+      return null;
+    }
+
+    solution1(root.left);
+    solution1(root.right);
+
+    TreeNode? temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+
+    return root;
+  }
+
+  /**
+   * Solution 2
+   * 
+   * Iteration: BFS (Queue)
+   * - Time Complexity: O(N)
+   * - Space Complexity: O(W)
+   */
+  TreeNode? solution2(TreeNode? root) {
+    if (root == null) {
+      return null;
+    }
+
+    Queue<TreeNode> queue = Queue();
+    queue.add(root);
+
+    while (queue.isNotEmpty) {
+      TreeNode currentNode = queue.removeFirst();
+
+      TreeNode? temp = currentNode.left;
+      currentNode.left = currentNode.right;
+      currentNode.right = temp;
+
+      if (currentNode.left != null) {
+        queue.add(currentNode.left!);
+      }
+      if (currentNode.right != null) {
+        queue.add(currentNode.right!);
+      }
+    }
+
+    return root;
   }
 }
