bea6a129fb8f867470bf58b6f6daa153f50e365a
miga-choi <miga.choi.1004@gmail.com>
2025-10-12 02:27:05 +0900

    leetcode: "543_diameter_of_binary_tree" add c solution

--- leetcode/algorithms/543_diameter_of_binary_tree/main.c
+++ leetcode/algorithms/543_diameter_of_binary_tree/main.c
@@ -1,45 +1,67 @@
+#include <stdlib.h>
+
 struct TreeNode {
     int val;
     struct TreeNode* left;
     struct TreeNode* right;
 };
 
-int returnDepth(struct TreeNode* node, int* max) {
+/**
+ * Recursion: DFS
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(H)
+ */
+#define MAX(a, b) (a > b ? a : b)
+
+int maxDiameter(int* max_diameter, struct TreeNode* node) {
     if (!node) {
         return 0;
     }
-    int left = returnDepth(node->left, max);
-    int right = returnDepth(node->right, max);
 
-    if (left + right > *max) {
-        *max = left + right;
+    int left_diameter = maxDiameter(max_diameter, node->left);
+    int right_diameter = maxDiameter(max_diameter, node->right);
+
+    if (left_diameter + right_diameter > *max_diameter) {
+        *max_diameter = left_diameter + right_diameter;
     }
 
-    return (left > right ? left : right) + 1;
+    return MAX(left_diameter, right_diameter) + 1;
 }
 
 int diameterOfBinaryTree(struct TreeNode* root) {
-    int max = 0;
-    returnDepth(root, &max);
-    return max;
+    int max_diameter = 0;
+
+    maxDiameter(&max_diameter, root);
+
+    return max_diameter;
 }
 
+
 // Solution
-int rec(struct TreeNode* root, int* d) {
-    if (!root) {
+/**
+ * Recursion: DFS
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(H)
+ */
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int depth_and_update_diameter(struct TreeNode* node, int* max_diameter) {
+    if (node == NULL) {
         return 0;
     }
 
-    int ld = rec(root->left, d);
-    int rd = rec(root->right, d);
+    int leftDepth = depth_and_update_diameter(node->left, max_diameter);
+    int rightDepth = depth_and_update_diameter(node->right, max_diameter);
 
-    *d = *d > ld + rd ? *d : ld + rd;
+    *max_diameter = max(*max_diameter, leftDepth + rightDepth);
 
-    return (ld > rd ? ld : rd) + 1;
+    return 1 + max(leftDepth, rightDepth);
 }
 
 int solution(struct TreeNode* root) {
-    int d = 0;
-    rec(root, &d);
-    return d;
+    int max_diameter = 0;
+    depth_and_update_diameter(root, &max_diameter);
+    return max_diameter;
 }
