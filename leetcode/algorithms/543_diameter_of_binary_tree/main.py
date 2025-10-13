15fb203383fd09bea84741ed8c8fb9601511761f
miga-choi <miga.choi.1004@gmail.com>
2025-10-14 01:45:11 +0900

    leetcode: "543_diameter_of_binary_tree" add python solution

--- leetcode/algorithms/543_diameter_of_binary_tree/main.py
+++ leetcode/algorithms/543_diameter_of_binary_tree/main.py
@@ -9,71 +9,50 @@ class TreeNode:
 
 
 class DiameterOfBinaryTree:
-    result = 0
+    """
+    Recursion: DFS (Bottom-up)
+    - Time Complexity: O(N)
+    - Space Complexity: O(H)
+    """
+    max_diameter: int = 0
 
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        self.getMaxNode(root)
-
-        return self.result
-
-    def getMaxNode(self, node: TreeNode) -> int:
+    def getMaxDiameter(self, node: TreeNode) -> int:
         if node == None:
             return 0
 
-        left = self.getMaxNode(node.left)
-        right = self.getMaxNode(node.right)
+        left_diameter: int = self.getMaxDiameter(node=node.left)
+        right_diameter: int = self.getMaxDiameter(node=node.right)
 
-        self.result = max(self.result, left + right)
+        if left_diameter + right_diameter > self.max_diameter:
+            self.max_diameter = left_diameter + right_diameter
 
-        return max(left, right) + 1
+        return max(left_diameter, right_diameter) + 1
+
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.getMaxDiameter(root)
+        return self.max_diameter
 
 
     # Solution
-    # Solution 1
-    def solution1(self, root: Optional[TreeNode]) -> int:
-        self.ans = 0
+    """
+    Recursion: DFS (Bottom-up)
+    - Time Complexity: O(N)
+    - Space Complexity: O(H)
+    """
+    def solution(self, root: Optional[TreeNode]) -> int:
+        self.diameter = 0
 
-        def depth(p):
-            if not p:
+        def depth(node: Optional[TreeNode]) -> int:
+            if not node:
                 return 0
-            left, right = depth(p.left), depth(p.right)
-            self.ans = max(self.ans, left + right)
-            return 1 + max(left, right)
+
+            left_depth = depth(node.left)
+            right_depth = depth(node.right)
+
+            self.diameter = max(self.diameter, left_depth + right_depth)
+
+            return 1 + max(left_depth, right_depth)
 
         depth(root)
-        return self.ans
 
-    # Solution 2: Implementation
-    def solution2(self, root: Optional[TreeNode]) -> int:
-        # Implement depth
-        def depth(node: Optional[TreeNode]) -> int:
-            return 1 + max(depth(node.left), depth(node.right)) if node else 0
-
-        return depth(root.left) + depth(root.right)  # calculate diameter
-
-    # Solution 3
-    diameter: int
-
-    def __init__(self):
-        self.diameter = 0  # stores the maximum diameter calculated
-
-    def depth(self, node: Optional[TreeNode]) -> int:
-        """
-        This function needs to do the following:
-            1. Calculate the maximum depth of the left and right sides of the given node
-            2. Determine the diameter at the given node and check if its the maximum
-        """
-        # Calculate maximum depth
-        left = self.depth(node.left) if node.left else 0
-        right = self.depth(node.right) if node.right else 0
-        # Calculate diameter
-        if left + right > self.diameter:
-            self.diameter = left + right
-        # Make sure the parent node(s) get the correct depth from this node
-        return 1 + (left if left > right else right)
-
-    def solution3(self, root: Optional[TreeNode]) -> int:
-        # if not root:
-        #     return 0
-        self.depth(root)  # root is guaranteed to be a TreeNode object
         return self.diameter
