b26b27cfb746d38975e774a90723d97a5036baaa
miga-choi <miga.choi.1004@gmail.com>
2025-10-13 20:44:20 +0900

    leetcode: "543_diameter_of_binary_tree" add java solution

--- leetcode/algorithms/543_diameter_of_binary_tree/main.java
+++ leetcode/algorithms/543_diameter_of_binary_tree/main.java
@@ -1,9 +1,3 @@
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-
 class DiameterOfBinaryTree {
     class TreeNode {
         int val;
@@ -24,157 +18,58 @@ class DiameterOfBinaryTree {
         }
     }
 
-    int result = 0;
+    /**
+     * Recursion: DFS
+     * - Time Complexity: O(N)
+     * - Space Complexity: O(H)
+     */
+    private int diameter = 0;
 
-    public int diameterOfBinaryTree(TreeNode root) {
-        getMaxNode(root);
-        return result;
-    }
-
-    public int getMaxNode(TreeNode node) {
+    private int getMaxDiameter(TreeNode node) {
         if (node == null) {
             return 0;
         }
 
-        int left = getMaxNode(node.left);
-        int right = getMaxNode(node.right);
+        int leftDiameter = getMaxDiameter(node.left);
+        int rightDiameter = getMaxDiameter(node.right);
 
-        result = Math.max(result, left + right);
+        if (leftDiameter + rightDiameter > diameter) {
+            diameter = leftDiameter + rightDiameter;
+        }
 
-        return Math.max(left, right) + 1;
+        return Math.max(leftDiameter, rightDiameter) + 1;
+    }
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        getMaxDiameter(root);
+
+        return diameter;
     }
 
 
     // Solution
-    // Worst Solution
-    public int worstSolution(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        HashMap<BigInteger[], TreeNode> treeMap = new HashMap<BigInteger[], TreeNode>();
-        treeMap.put(new BigInteger[] { BigInteger.ZERO, BigInteger.ZERO }, root);
-        int maxY = 0;
-        int maxDistance = 0;
-        boolean going = true;
-        ArrayList<BigInteger> topSet = new ArrayList<BigInteger>();
-        ArrayList<BigInteger[]> finalSet = new ArrayList<BigInteger[]>();
-        while (going) {
-            going = false;
-            ArrayList<BigInteger> newTopSet = new ArrayList<BigInteger>();
-            Iterator<BigInteger[]> itrKeys = treeMap.keySet().iterator();
-            HashMap<BigInteger[], TreeNode> newTreeMap = new HashMap<BigInteger[], TreeNode>();
-            while (itrKeys.hasNext()) {
-                BigInteger[] coord = itrKeys.next();
-                TreeNode nd = treeMap.get(coord);
-                if (nd.left != null || nd.right != null) {
-                    going = true;
-                    BigInteger base = coord[0].multiply(BigInteger.valueOf(2L));
-                    if (nd.left == null) {
-                        newTreeMap.put(new BigInteger[] { base.add(BigInteger.ONE), coord[1].add(BigInteger.ONE) },
-                                nd.right);
-                        newTopSet.add(base.add(BigInteger.ONE));
-                    } else {
-                        newTreeMap.put(new BigInteger[] { base.subtract(BigInteger.ONE), coord[1].add(BigInteger.ONE) },
-                                nd.left);
-                        newTopSet.add(base.subtract(BigInteger.ONE));
-                        if (nd.right != null) {
-                            newTreeMap.put(new BigInteger[] { base.add(BigInteger.ONE), coord[1].add(BigInteger.ONE) },
-                                    nd.right);
-                        }
-                    }
-                } else {
+    /**
+     * Recursion: DFS
+     * - Time Complexity: O(N)
+     * - Space Complexity: O(H)
+     */
+    private int maxDiameter = 0;
 
-                    finalSet.add(coord);
-                }
-            }
-
-            if (going) {
-                treeMap = newTreeMap;
-                maxY++;
-                topSet = newTopSet;
-
-            }
-        }
-        finalSet.add(new BigInteger[] { BigInteger.ZERO, BigInteger.ZERO });
-        for (BigInteger xTop : topSet) {
-            System.out.println(xTop);
-            for (BigInteger[] loc : finalSet) {
-                BigInteger power = loc[0];
-                for (int j = 0; j < maxY - loc[1].intValue(); j++) {
-                    power = power.multiply(new BigInteger("2"));
-                }
-                BigInteger xLeft = power;
-                BigInteger xRight = power;
-                int twos = 0;
-                int dist = 0;
-                if (xTop.compareTo(xLeft) < 0) {
-                    xLeft = xTop;
-                }
-                if (xTop.compareTo(xRight) > 0) {
-                    xRight = xTop;
-                }
-
-                for (double i = xLeft.doubleValue(); i < xRight.doubleValue() + 1; i += 0) {
-                    BigDecimal testD = new BigDecimal(i);
-                    BigInteger test = testD.toBigInteger();
-                    twos = 0;
-                    BigInteger compare = test.mod(new BigInteger("2"));
-                    while (compare.equals(new BigInteger("0"))) {
-                        if (test.equals(BigInteger.ZERO)) {
-                            twos = maxY;
-                            break;
-
-                        }
-                        twos++;
-                        test = test.divide(new BigInteger("2"));
-                        compare = test.mod(new BigInteger("2"));
-                    }
-                    i += Math.pow(2, twos);
-                    dist = twos * 2 - maxY + loc[1].intValue();
-                    if (dist > maxDistance) {
-                        maxDistance = dist;
-                    }
-                }
-            }
-        }
-        return maxDistance;
+    public int Solution(TreeNode root) {
+        calculateDepth(root);
+        return maxDiameter;
     }
 
-    // Solution 1
-    int max = 0;
-
-    public int solution1(TreeNode root) {
-        maxDepth(root);
-        return max;
-    }
-
-    private int maxDepth(TreeNode root) {
-        if (root == null)
-            return 0;
-
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-
-        max = Math.max(max, left + right);
-
-        return Math.max(left, right) + 1;
-    }
-
-    // Solution 2
-    public int solution2(TreeNode root) {
-        if (root == null) {
+    private int calculateDepth(TreeNode node) {
+        if (node == null) {
             return 0;
         }
-        int dia = depth(root.left) + depth(root.right);
-        int ldia = diameterOfBinaryTree(root.left);
-        int rdia = diameterOfBinaryTree(root.right);
-        return Math.max(dia, Math.max(ldia, rdia));
-    }
 
-    public int depth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        return 1 + Math.max(depth(root.left), depth(root.right));
+        int leftDepth = calculateDepth(node.left);
+        int rightDepth = calculateDepth(node.right);
+
+        maxDiameter = Math.max(maxDiameter, leftDepth + rightDepth);
+
+        return Math.max(leftDepth, rightDepth) + 1;
     }
 }
