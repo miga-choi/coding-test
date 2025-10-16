a6689ecfef2f03828a9109511b8789bfdeda18a3
miga-choi <miga.choi.1004@gmail.com>
2025-10-16 20:46:08 +0900

    leetcode: "20_valid_parentheses" add javascript solution

--- leetcode/algorithms/20_valid_parentheses/main.js
+++ leetcode/algorithms/20_valid_parentheses/main.js
@@ -1,4 +1,8 @@
 /**
+ * LIFO (Last-In, First-Out): Stack
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(N)
+ *
  * @param {string} s
  * @return {boolean}
  */
@@ -6,43 +10,61 @@ var isValid = function (s) {
   const stack = [];
 
   for (const c of s) {
-    if (c === "(") {
-      stack.push(")");
-    } else if (c === "[") {
-      stack.push("]");
-    } else if (c === "{") {
-      stack.push("}");
-    } else {
-      if (stack.pop() !== c) {
+    switch (c) {
+      case "(":
+        stack.push(")");
+        break;
+      case "[":
+        stack.push("]");
+        break;
+      case "{":
+        stack.push("}");
+        break;
+      default:
+        if (stack.length === 0 || stack.pop() !== c) {
+          return false;
+        }
+        break;
+    }
+  }
+
+  return stack.length === 0;
+};
+
+
+// Solution
+/**
+ * LIFO (Last-In, First-Out): Stack
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(N)
+ *
+ * @param {string} s
+ * @return {boolean}
+ */
+var solution = function (s) {
+  const stack = [];
+  const map = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+
+    if (char === "(" || char === "{" || char === "[") {
+      stack.push(char);
+    } else if (char === ")" || char === "}" || char === "]") {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      const lastOpen = stack.pop();
+      if (lastOpen !== map[char]) {
         return false;
       }
     }
   }
 
-  return stack.length <= 0;
-};
-
-
-// Solution
-var solution = function (s) {
-  // Initialize stack to store the closing brackets expected...
-  let stack = [];
-
-  // Traverse each charater in input string...
-  for (let idx = 0; idx < s.length; idx++) {
-    // If open parentheses are present, push it to stack...
-    if (s[idx] === "{") {
-      stack.push("}");
-    } else if (s[idx] === "[") {
-      stack.push("]");
-    } else if (s[idx] === "(") {
-      stack.push(")");
-    }
-    // If a close bracket is found, check that it matches the last stored open bracket
-    else if (stack.pop() !== s[idx]) {
-      return false;
-    }
-  }
-
-  return !stack.length;
+  return stack.length === 0;
 };
