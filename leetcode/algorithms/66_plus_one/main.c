decae48e33363256501cf40bcce352f8c3e5464b
miga-choi <miga.choi.1004@gmail.com>
2025-10-18 01:03:58 +0900

    leetcode: "66_plus_one" add c solution

--- leetcode/algorithms/66_plus_one/main.c
+++ leetcode/algorithms/66_plus_one/main.c
@@ -3,109 +3,64 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* plusOne(int* digits, int digitsSize, int* returnSize) {
-    *returnSize = digitsSize;
-    int* result = (int*)malloc(sizeof(int) * *returnSize);
-    int up = 1;
 
+/**
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(N)
+ */
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    int* result = (int*)malloc(sizeof(int) * (digitsSize + 1));
+
+    int up = 1;
     for (int i = digitsSize - 1; i >= 0; i--) {
-        int num = digits[i] + up;
-        if (num >= 10) {
-            num -= 10;
+        digits[i] += up;
+        up = 0;
+
+        if (digits[i] > 9) {
+            result[i + 1] = digits[i] - 10;
             up = 1;
         } else {
-            up = 0;
+            result[i + 1] = digits[i];
         }
-
-        result[i] = num;
     }
 
+    *returnSize = digitsSize;
     if (up > 0) {
-        (*returnSize)++;
-        result = realloc(result, sizeof(int) * *returnSize);
         result[0] = 1;
-        result[*returnSize - 1] = 0;
+        (*returnSize)++;
+        return result;
     }
 
-    return result;
+    return result + 1;
 }
 
 
 // Solution
-// Solution 1
-int* solution1(int* digits, int digitsSize, int* returnSize) {
-    *returnSize = digitsSize;
-    int* plusOne = (int*)malloc(digitsSize * sizeof(int));
-    if (plusOne == NULL) {
-        return (NULL);
-    }
-    for (int i = 0; i < digitsSize; i++) {
-        plusOne[i] = digits[i];
-    }
-
-    plusOne[digitsSize - 1]++;
-    for (int i = digitsSize - 1; i - 1 >= 0; i--) {
-        if (plusOne[i] == 10) {
-            plusOne[i] = 0;
-            plusOne[i - 1]++;
-        }
-    }
-
-    if (plusOne[0] == 10) {
-        (*returnSize)++;
-        plusOne = realloc(plusOne, *returnSize * sizeof(int));
-        if (plusOne == NULL) {
-            return (NULL);
-        }
-        memmove(plusOne + 1, plusOne, digitsSize * sizeof(int));
-        plusOne[0] = 1;
-        plusOne[1] = 0;
-    }
-    return (plusOne);
-}
-
-// Solution 2
-int* solution2(int* digits, int digitsSize, int* returnSize) {
+/**
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(N)
+ */
+int* solution(int* digits, int digitsSize, int* returnSize) {
     for (int i = digitsSize - 1; i >= 0; i--) {
         digits[i]++;
+
         if (digits[i] < 10) {
+            int* result = (int*)malloc(sizeof(int) * digitsSize);
+            memcpy(result, digits, sizeof(int) * digitsSize);
             *returnSize = digitsSize;
-            return digits;
+            return result;
         }
+
         digits[i] = 0;
     }
-    int* result = (int*)malloc((digitsSize + 1) * sizeof(int));
+
+    *returnSize = digitsSize + 1;
+    int* result = (int*)malloc(sizeof(int) * (*returnSize));
+
     result[0] = 1;
-    for (int i = 1; i <= digitsSize; i++) {
+    for (int i = 1; i < *returnSize; i++) {
         result[i] = 0;
     }
-    *returnSize = digitsSize + 1;
+
     return result;
-}
-
-// Solution 3
-int* solution3(int* digits, int digitsSize, int* returnSize) {
-    int* res = (int*)malloc(sizeof(int) * 100);
-    int cf = 1;
-
-    for (int i = digitsSize - 1; i >= 0; i--) {
-        if (digits[i] + cf > 9) {
-            digits[i] = (digits[i] + cf) % 10;
-        } else {
-            digits[i] = digits[i] + cf;
-            cf = 0;
-            break;
-        }
-    }
-
-    if (cf) {
-        res[0] = 1;
-    }
-
-    for (int i = cf, j = 0; j < digitsSize; i++, j++) {
-        res[i] = digits[j];
-    }
-
-    *returnSize = digitsSize + cf;
-    return res;
 }
