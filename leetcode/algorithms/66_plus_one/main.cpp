90cad6c8abe6b7c4e8970cd89e85ca5ea24c0290
miga-choi <miga.choi.1004@gmail.com>
2025-10-18 01:04:56 +0900

    leetcode: "66_plus_one" add c++ solution

--- leetcode/algorithms/66_plus_one/main.cpp
+++ leetcode/algorithms/66_plus_one/main.cpp
@@ -3,25 +3,27 @@ using namespace std;
 
 class PlusOne {
 public:
+    /**
+     * - Time Complexity: O(N)
+     * - Space Complexity: O(N)
+     */
     vector<int> plusOne(vector<int>& digits) {
         vector<int> result;
-        vector<int>::iterator it = result.begin();
+
         int up = 1;
-
         for (int i = digits.size() - 1; i >= 0; i--) {
-            int num = digits[i] + up;
-            if (num >= 10) {
-                num -= 10;
-                up = 1;
-            } else {
-                up = 0;
-            }
+            digits[i] += up;
+            up = 0;
 
-            it = result.insert(it, num);
+            if (digits[i] > 9) {
+                digits[i] -= 10;
+                up = 1;
+            }
+            result.insert(result.begin(), digits[i]);
         }
 
         if (up > 0) {
-            result.insert(it, 1);
+            result.insert(result.begin(), 1);
         }
 
         return result;
@@ -29,50 +31,25 @@ public:
 
 
     // Solution
-    // Solution 1
+    /**
+     * - Time Complexity: O(N)
+     * - Space Complexity: O(N)
+     */
     vector<int> solution1(vector<int>& digits) {
         int n = digits.size();
-        for (int i = n - 1; i >= 0; i--) {
-            if (i == n - 1) {
-                digits[i]++;
-            }
-            if (digits[i] == 10) {
-                digits[i] = 0;
-                if (i != 0) {
-                    digits[i - 1]++;
-                } else {
-                    digits.push_back(0);
-                    digits[i] = 1;
-                }
-            }
-        }
-        return digits;
-    }
 
-    // Solution 2
-    vector<int> solution2(vector<int>& digits) {
-        int n = digits.size() - 1;
-
-        for (int i = n; i >= 0; --i) {
-            // traverse digits from the last element (least significant)
-            // since we begin with the last digit, increasing that digit by one
-            // results in overflow.  Therefore, all elements PRIOR to digits[0]
-            // need to be considered since there may be additional nines between
-            // digits[0], ... , digits[n].
-            if (digits[i] == 9) {
-                digits[i] = 0;
-            } else {
-                // current digit is not 9 so we can safely increment by one
-                digits[i] += 1;
+        for (int i = n - 1; i >= 0; --i) {
+            digits[i]++;
+            
+            if (digits[i] < 10) {
                 return digits;
             }
+            
+            digits[i] = 0;
         }
-        // if the program runs to this point, each 9 is now a 0.
-        // to get a correct solution, we need to add one more element with
-        // a value of zero AND set digits[0] to 1 (in the most significant position)
-        // to account for the carry digit.
-        digits.push_back(0);
-        digits[0] = 1;
+
+        digits.insert(digits.begin(), 1);
+        
         return digits;
     }
 };
