c16508d0106dcad93fb4a2370868de7fa39909b9
miga-choi <miga.choi.1004@gmail.com>
2025-10-19 04:13:55 +0900

    leetcode: "66_plus_one" add javascript solution

--- leetcode/algorithms/66_plus_one/main.js
+++ leetcode/algorithms/66_plus_one/main.js
@@ -1,67 +1,47 @@
 /**
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(1)
+ *
  * @param {number[]} digits
  * @return {number[]}
  */
 var plusOne = function (digits) {
-  digits.reverse();
-  let up = 0;
-  for (let i = 0; i < digits.length; i++) {
-    let result = digits[i] + up;
-    up = 0;
-    if (i === 0) {
-      result += 1;
+  for (let i = digits.length - 1; i >= 0; i--) {
+    digits[i]++;
+
+    if (digits[i] < 10) {
+      return digits;
     }
-    if (result >= 10) {
-      up = 1;
-      digits[i] = result - 10;
-    } else {
-      digits[i] = result;
-    }
+
+    digits[i] -= 10;
   }
-  if (up > 0) {
-    digits.push(up);
-  }
-  return digits.reverse();
+
+  return new Array(1).fill(1).concat(new Array(digits.length).fill(0));
 };
 
 
 // Solution
-// Solution 1
-var solution1 = function (digits) {
-  for (var i = digits.length - 1; i >= 0; i--) {
+/**
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(1)
+ *
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var solution = function (digits) {
+  const n = digits.length;
+
+  for (let i = n - 1; i >= 0; i--) {
     digits[i]++;
-    if (digits[i] > 9) {
-      digits[i] = 0;
-    } else {
-      return digits;
-    }
-  }
-  digits.unshift(1);
-  return digits;
-};
 
-// Solution 2
-var solution2 = function (digits) {
-  for (var i = digits.length - 1; i >= 0; i--) {
-    if (++digits[i] > 9) {
-      digits[i] = 0;
-    } else {
+    if (digits[i] < 10) {
       return digits;
     }
-  }
-  digits.unshift(1);
-  return digits;
-};
 
-// Solution 3
-var solution3 = function (digits) {
-  for (let i = digits.length - 1; i >= 0; i--) {
-    if (digits[i] === 9) {
-      digits[i] = 0;
-    } else {
-      digits[i]++;
-      return digits;
-    }
+    digits[i] = 0;
   }
-  return [1, ...digits];
+
+  digits.unshift(1);
+
+  return digits;
 };
