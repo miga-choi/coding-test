f3c77986f92585562ec6ddf3b4900cc2db72d7c8
miga-choi <miga.choi.1004@gmail.com>
2025-10-22 02:04:43 +0900

    leetcode: "70_climbing_stairs" add javascript solution

--- leetcode/algorithms/70_climbing_stairs/main.js
+++ leetcode/algorithms/70_climbing_stairs/main.js
@@ -1,123 +1,122 @@
 /**
+ * DP (Dynamic Programming)
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(N)
+ *
  * @param {number} n
  * @return {number}
  */
 var climbStairs = function (n) {
-  if (n < 2) {
-    return 1;
+  if (n < 3) {
+    return n;
   }
 
-  // Fibonacci numbers
-  let dp = [];
-  dp[0] = 1;
-  dp[1] = 2;
+  const dp = new Array(n + 1);
+  dp[0] = 0;
+  dp[1] = 1;
+  dp[2] = 2;
 
-  for (let i = 2; i < n; i++) {
+  for (let i = 3; i <= n; i++) {
     dp[i] = dp[i - 1] + dp[i - 2];
   }
-
-  return dp[n - 1];
-};
-
-
-// Solution
-// Solution 1
-var solution1 = function (n) {
-  if (n < 2) {
-    return 1;
-  }
-
-  let firstStep = 1;
-
-  let secondStep = 1;
-
-  let thirdStep = 0;
-
-  for (let i = 2; i <= n; i++) {
-    thirdStep = firstStep + secondStep;
-
-    firstStep = secondStep;
-
-    secondStep = thirdStep;
-  }
-
-  return thirdStep;
-};
-
-// Solution 2: Math
-var solution2 = function (n) {
-  let ways = 1;
-
-  for (let i = 1; i <= n / 2; i++) {
-    let sum = 1;
-
-    for (let j = i; j < 2 * i; j++) {
-      sum *= (n - j) / (j - i + 1);
-    }
-
-    ways += sum;
-  }
-
-  return ways;
-};
-
-// Solution 3: Top-Down DP
-var solve3 = function (n, dp) {
-  if (n < 0) {
-    return 0;
-  }
-  if (n == 0) {
-    return 1;
-  }
-
-  if (dp[n] != -1) {
-    return dp[n];
-  }
-
-  dp[n] = solve3(n - 1, dp) + solve3(n - 2, dp);
 
   return dp[n];
 };
 
+
+// Solution
+/**
+ * Solution 1
+ *
+ * Recursion (Time Limit Exceeded)
+ * - Time Complexity: O(2ᴺ)
+ * - Space Complexity: O(1)
+ *
+ * @param {number} n
+ * @return {number}
+ */
+var solution1 = function (n) {
+  if (n <= 2) {
+    return n;
+  }
+  return solution1(n - 1) + solution1(n - 2);
+};
+
+/**
+ * Solution 2
+ *
+ * DP (Dynamic Programming): Top-down with Memoization
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(N)
+ *
+ * @param {number} n
+ * @param {Map} memo
+ * @return {number}
+ */
+var solution2 = function (n, memo = {}) {
+  if (n in memo) {
+    return memo[n];
+  }
+
+  if (n <= 2) {
+    return n;
+  }
+
+  memo[n] = solution2(n - 1, memo) + solution2(n - 2, memo);
+
+  return memo[n];
+};
+
+/**
+ * Solution 3
+ *
+ * DP (Dynamic Programming): Bottom-up with Tabulation
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(N)
+ *
+ * @param {number} n
+ * @return {number}
+ */
 var solution3 = function (n) {
-  const dp = new Array(n + 1).fill(-1);
-  return solve3(n, dp);
+  if (n <= 2) {
+    return n;
+  }
+
+  const dp = new Array(n + 1);
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[n];
 };
 
-// Solution 4: Bottom-Up DP
-var solve4 = function (i, n, dp) {
-  if (i === n) {
-    return 1;
-  }
-  if (i > n) {
-    return 0;
-  }
-
-  if (dp[i] !== -1) {
-    return dp[i];
-  }
-
-  dp[i] = solve4(i + 1, n, dp) + solve4(i + 2, n, dp);
-
-  return dp[i];
-};
-
+/**
+ * Solution 4
+ *
+ * DP (Dynamic Programming): Space Optimization
+ * - Time Complexity: O(N)
+ * - Space Complexity: O(1)
+ *
+ * @param {number} n
+ * @return {number}
+ */
 var solution4 = function (n) {
-  const dp = new Array(n + 1).fill(-1);
-  return solve4(0, n, dp);
-};
-
-// Solution 5: Bottom-Up DP
-var solution5 = function (n) {
-  let a = 0;
-  let b = 1;
-  let c = 0;
-
-  for (let i = 0; i < n; i++) {
-    c = a + b;
-    a = b;
-    b = c;
+  if (n <= 2) {
+    return n;
   }
 
-  return b;
+  let two_steps_before = 1;
+  let one_step_before = 2;
+
+  for (let i = 3; i <= n; i++) {
+    const current = one_step_before + two_steps_before;
+
+    two_steps_before = one_step_before;
+    one_step_before = current;
+  }
+
+  return one_step_before;
 };
