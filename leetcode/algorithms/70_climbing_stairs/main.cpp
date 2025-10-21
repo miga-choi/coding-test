95a22319fef0cd716a8e37fa4c792aa568b04e34
miga-choi <miga.choi.1004@gmail.com>
2025-10-21 22:11:46 +0900

    leetcode: "70_climbing_stairs" add c++ solution

--- leetcode/algorithms/70_climbing_stairs/main.cpp
+++ leetcode/algorithms/70_climbing_stairs/main.cpp
@@ -4,104 +4,93 @@ using namespace std;
 
 class ClimbingStairs {
 public:
+    /**
+     * DP (Dynamic Programming)
+     * 
+     * Recursion (Time Limit Exceeded)
+     * - Time Complexity: O(N)
+     * - Space Complexity: O(N)
+     */
     int climbStairs(int n) {
-        if (n < 2) {
-            return 1;
-        }
-
-        int dp[n];
-        dp[0] = 1;
-        dp[1] = 2;
-
-        for (int i = 2; i < n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-
-        return dp[n - 1];
-    }
-
-
-    // Solution
-    // Solution 1: Fibonacci sequence
-    int solution1(int n) {
-        // base cases
-        if (n <= 0) {
-            return 0;
-        }
-        if (n == 1) {
-            return 1;
-        }
-        if (n == 2) {
-            return 2;
-        }
-
-        int one_step_before = 2;
-        int two_steps_before = 1;
-        int all_ways = 0;
-
-        for (int i = 2; i < n; i++) {
-            all_ways = one_step_before + two_steps_before;
-            two_steps_before = one_step_before;
-            one_step_before = all_ways;
-        }
-
-        return all_ways;
-    }
-
-    // Solution 2: Recursion
-    int solution2(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        return solution2(n - 1) + solution2(n - 2);
-    }
-
-    // Solution 3: Memoization
-    int solution3(int n) {
-        unordered_map<int, int> memo;
-        return solution3(n, memo);
-    }
-
-    int solution3(int n, unordered_map<int, int> &memo) {
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        if (memo.find(n) == memo.end()) {
-            memo[n] = solution3(n - 1, memo) + solution3(n - 2, memo);
-        }
-        return memo[n];
-    }
-
-    // Solution 4: Tabulation
-    int solution4(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
+        if (n < 3) {
+            return n;
         }
 
         vector<int> dp(n + 1);
-        dp[0] = dp[1] = 1;
 
-        for (int i = 2; i <= n; i++) {
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
 
         return dp[n];
     }
 
-    // Solution 5: Space Optimization
-    int solution5(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
+
+    // Solution
+    /**
+     * Solution 1
+     *
+     * Recursion (Time Limit Exceeded)
+     * - Time Complexity: O(2ᴺ)
+     * - Space Complexity: O(1)
+     */
+    int solution1(int n) {
+        if (n < 3) {
+            return n;
+        }
+        return solution1(n - 1) + solution1(n - 2);
+    }
+
+    /**
+     * Solution 2
+     *
+     * DP (Dynamic Programming)
+     * - Time Complexity: O(N)
+     * - Space Complexity: O(N)
+     */
+    int solution2(int n) {
+        if (n <= 2) {
+            return n;
         }
 
-        int prev = 1, curr = 1;
+        vector<int> dp(n + 1);
 
-        for (int i = 2; i <= n; i++) {
-            int temp = curr;
-            curr = prev + curr;
-            prev = temp;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
 
-        return curr;
+        return dp[n];
+    }
+
+    /**
+     * Solution 3
+     *
+     * DP (Dynamic Programming)
+     * - Time Complexity: O(N)
+     * - Space Complexity: O(1)
+     */
+    int solution3(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        int one_step_before = 2;
+        int two_steps_before = 1;
+
+        for (int i = 3; i <= n; i++) {
+            int current_ways = one_step_before + two_steps_before;
+
+            two_steps_before = one_step_before;
+            one_step_before = current_ways;
+        }
+
+        return one_step_before;
     }
 };
