82d742ed866580b293ac45b36771a8a61c015468
miga-choi <miga.choi.1004@gmail.com>
2025-10-26 02:21:02 +0900

    leetcode: "15_3Sum" add python solution

---
+++ leetcode/algorithms/15_3Sum/main.py
@@ -0,0 +1,78 @@
+from typing import List
+
+
+class ThreeSum:
+    """
+    # Sort + Two-pointer
+    # - Time Complexity: O(N²)
+    # - Space Complexity: O(N)
+    """
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums = sorted(nums)
+
+        result: List[List[int]] = []
+        nums_len: int = len(nums)
+
+        for flag in range(nums_len - 2):
+            if flag > 0 and nums[flag] == nums[flag - 1]:
+                continue
+
+            left: int = flag + 1
+            right: int = nums_len - 1
+
+            while left < right:
+                sum: int = nums[flag] + nums[left] + nums[right]
+
+                if sum < 0:
+                    left += 1
+                elif sum > 0:
+                    right -= 1
+                else:
+                    result.append([nums[flag], nums[left], nums[right]])
+
+                    left += 1
+                    right -= 1
+
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+        return result
+
+
+    # Solution
+    """
+    # Sort + Two-pointer
+    # - Time Complexity: O(N²)
+    # - Space Complexity: O(N)
+    """
+    def solution(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            left, right = i + 1, len(nums) - 1
+
+            while left < right:
+                three_sum = nums[i] + nums[left] + nums[right]
+
+                if three_sum < 0:
+                    left += 1
+                elif three_sum > 0:
+                    right -= 1
+                else:
+                    results.append([nums[i], nums[left], nums[right]])
+
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+
+                    left += 1
+                    right -= 1
+
+        return results
