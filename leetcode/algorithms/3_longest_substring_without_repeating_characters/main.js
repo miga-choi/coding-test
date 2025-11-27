e6c87be8158a823a717cd745ad18e95d07d42b13
miga-choi <miga.choi.1004@gmail.com>
2025-11-28 01:23:24 +0900

    leetcode: "3_longest_substring_without_repeating_characters" add javascript solution

--- leetcode/algorithms/3_longest_substring_without_repeating_characters/main.js
+++ leetcode/algorithms/3_longest_substring_without_repeating_characters/main.js
@@ -1,9 +1,18 @@
 /**
+ * Sliding Window (Using Array)
+ *
+ * Complexities:
+ *   N - Length of `s`
+ *   M - Length of Available Characters in `s`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(Min(M, N))
+ */
+/**
  * @param {string} s
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-  const charMap = Array(128).fill(-1);
+  const charMap = new Array(128).fill(-1);
   let left = 0;
   let maxLength = 0;
 
@@ -24,59 +33,30 @@ var lengthOfLongestSubstring = function (s) {
 
 
 // Solution
-// Solution 1: Sliding Window & Set
-var solution1 = function (s) {
+/**
+ * Sliding Window (Using Map)
+ *
+ * Complexities:
+ *   N - Length of `s`
+ *   M - Length of Available Characters in `s`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(Min(M, N))
+ */
+var solution = function (s) {
+  const map = new Map();
   let left = 0;
   let maxLength = 0;
-  let charSet = new Set();
 
   for (let right = 0; right < s.length; right++) {
-    while (charSet.has(s[right])) {
-      charSet.delete(s[left]);
-      left++;
+    const char = s[right];
+
+    if (map.has(char) && map.get(char) >= left) {
+      left = map.get(char) + 1;
     }
 
-    charSet.add(s[right]);
-    maxLength = Math.max(maxLength, right - left + 1);
-  }
-
-  return maxLength;
-};
-
-// Solution 2: Sliding Window and Hashing
-var solution2 = function (s) {
-  let maxLength = 0;
-  let left = 0;
-  let count = {};
-
-  for (let right = 0; right < s.length; right++) {
-    let c = s[right];
-    count[c] = (count[c] || 0) + 1;
-
-    while (count[c] > 1) {
-      count[s[left]] -= 1;
-      left++;
-    }
+    map.set(char, right);
 
     maxLength = Math.max(maxLength, right - left + 1);
-  }
-
-  return maxLength;
-};
-
-// Solution 3: the last position where each character was seen
-var solution3 = function (s) {
-  let maxLength = 0;
-  let left = 0;
-  let lastSeen = {};
-
-  for (let right = 0; right < s.length; right++) {
-    let c = s.charAt(right);
-    if (c in lastSeen && lastSeen[c] >= left) {
-      left = lastSeen[c] + 1;
-    }
-    maxLength = Math.max(maxLength, right - left + 1);
-    lastSeen[c] = right;
   }
 
   return maxLength;
