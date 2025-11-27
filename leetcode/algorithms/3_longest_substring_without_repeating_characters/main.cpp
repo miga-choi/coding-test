6903d3a472ee1c738b62a07d6bc7d62e4d5c8e9e
miga-choi <miga.choi.1004@gmail.com>
2025-11-28 01:22:40 +0900

    leetcode: "3_longest_substring_without_repeating_characters" add c++ solution

--- leetcode/algorithms/3_longest_substring_without_repeating_characters/main.cpp
+++ leetcode/algorithms/3_longest_substring_without_repeating_characters/main.cpp
@@ -7,6 +7,14 @@ using namespace std;
 
 class LongestSubstringWithoutRepeatingCharacters {
 public:
+    /**
+     * Sliding Window (Using Vector)
+     *
+     * Complexities:
+     *   N - Length of `s`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O()
+     */
     int lengthOfLongestSubstring(string s) {
         vector<int> charMap(128, -1);
         int left = 0;
@@ -29,61 +37,29 @@ public:
 
 
     // Solution
-    // Solution 1: Set
-    int solution1(string s) {
-        int n = s.length();
+    /**
+     * Sliding Window (Using Vector)
+     *
+     * Complexities:
+     *   N - Length of `s`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
+    int solution(string s) {
+        vector<int> charIndexMap(128, -1);
+        
         int maxLength = 0;
-        unordered_set<char> charSet;
-        int left = 0;
-        
-        for (int right = 0; right < n; right++) {
-            if (charSet.count(s[right]) == 0) {
-                charSet.insert(s[right]);
-                maxLength = max(maxLength, right - left + 1);
-            } else {
-                while (charSet.count(s[right])) {
-                    charSet.erase(s[left]);
-                    left++;
-                }
-                charSet.insert(s[right]);
-            }
-        }
-        
-        return maxLength;
-    }
-
-    // Solution 2: Unordered Map
-    int solution2(string s) {
-        int n = s.length();
-        int maxLength = 0;
-        unordered_map<char, int> charMap;
-        int left = 0;
-        
-        for (int right = 0; right < n; right++) {
-            if (charMap.count(s[right]) == 0 || charMap[s[right]] < left) {
-                charMap[s[right]] = right;
-                maxLength = max(maxLength, right - left + 1);
-            } else {
-                left = charMap[s[right]] + 1;
-                charMap[s[right]] = right;
-            }
-        }
-        
-        return maxLength;
-    }
-
-    // Solution 3: Integer Array
-    int solution3(string s) {
-        int n = s.length();
-        int maxLength = 0;
-        vector<int> charIndex(128, -1);
         int left = 0;
 
-        for (int right = 0; right < n; right++) {
-            if (charIndex[s[right]] >= left) {
-                left = charIndex[s[right]] + 1;
+        for (int right = 0; right < s.length(); ++right) {
+            char currentChar = s[right];
+
+            if (charIndexMap[currentChar] >= left) {
+                left = charIndexMap[currentChar] + 1;
             }
-            charIndex[s[right]] = right;
+
+            charIndexMap[currentChar] = right;
+
             maxLength = max(maxLength, right - left + 1);
         }
 
