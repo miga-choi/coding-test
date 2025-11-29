acfc5a19a3c0981a0e9ecdd96b9c767f499e7f5f
miga-choi <miga.choi.1004@gmail.com>
2025-11-30 03:35:34 +0900

    leetcode: "3_longest_substring_without_repeating_characters" add dart solution

--- leetcode/algorithms/3_longest_substring_without_repeating_characters/main.dart
+++ leetcode/algorithms/3_longest_substring_without_repeating_characters/main.dart
@@ -1,6 +1,14 @@
 import 'dart:math';
 
 class LongestSubstringWithoutRepeatingCharacters {
+  /**
+   * Sliding Window
+   *
+   * Complexities:
+   *   N - Length of `s`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
   int lengthOfLongestSubstring(String s) {
     List<int> charMap = List<int>.filled(128, -1);
     int left = 0;
@@ -23,62 +31,64 @@ class LongestSubstringWithoutRepeatingCharacters {
 
 
   // Solution
-  // Solution 1: Set
-  int solution1(String s) {
-    int n = s.length;
+  /**
+   * Solution 1
+   * 
+   * Sliding Window (Using Map)
+   *
+   * Complexities:
+   *   N - Length of `s`
+   *   M - Length of Available Characters in `s`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(Min(M, N))
+   */
+  int solution(String s) {
     int maxLength = 0;
-    Set<String> charSet = Set<String>();
     int left = 0;
 
-    for (int right = 0; right < n; right++) {
-      if (!charSet.contains(s[right])) {
-        charSet.add(s[right]);
-        maxLength = max(maxLength, right - left + 1);
-      } else {
-        while (charSet.contains(s[right])) {
-          charSet.remove(s[left]);
-          left++;
-        }
-        charSet.add(s[right]);
+    Map<String, int> lastSeen = {};
+
+    for (int right = 0; right < s.length; right++) {
+      String currentChar = s[right];
+
+      if (lastSeen.containsKey(currentChar) && lastSeen[currentChar]! >= left) {
+        left = lastSeen[currentChar]! + 1;
       }
-    }
 
-    return maxLength;
-  }
-
-  // Solution 2: Map
-  int solution2(String s) {
-    int n = s.length;
-    int maxLength = 0;
-    Map<String, int> charMap = Map<String, int>();
-    int left = 0;
-
-    for (int right = 0; right < n; right++) {
-      if (!charMap.containsKey(s[right]) || charMap[s[right]]! < left) {
-        charMap[s[right]] = right;
-        maxLength = max(maxLength, right - left + 1);
-      } else {
-        left = charMap[s[right]]! + 1;
-        charMap[s[right]] = right;
-      }
-    }
-
-    return maxLength;
-  }
-
-  // Solution 3: Integer Array
-  int solution3(String s) {
-    int n = s.length;
-    int maxLength = 0;
-    List<int> charIndex = List<int>.filled(128, -1);
-    int left = 0;
-
-    for (int right = 0; right < n; right++) {
-      if (charIndex[s.codeUnitAt(right)] >= left) {
-        left = charIndex[s.codeUnitAt(right)] + 1;
-      }
-      charIndex[s.codeUnitAt(right)] = right;
       maxLength = max(maxLength, right - left + 1);
+
+      lastSeen[currentChar] = right;
+    }
+
+    return maxLength;
+  }
+
+  /**
+   * Solution 2
+   * 
+   * Sliding Window (Using List)
+   *
+   * Complexities:
+   *   N - Length of `s`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
+  int solution2(String s) {
+    int maxLength = 0;
+    int left = 0;
+
+    List<int> lastSeen = List.filled(128, -1);
+    List<int> codes = s.codeUnits;
+
+    for (int right = 0; right < codes.length; right++) {
+      int charCode = codes[right];
+
+      if (lastSeen[charCode] >= left) {
+        left = lastSeen[charCode] + 1;
+      }
+
+      maxLength = max(maxLength, right - left + 1);
+      lastSeen[charCode] = right;
     }
 
     return maxLength;
