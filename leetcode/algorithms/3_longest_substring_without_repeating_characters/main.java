0aafb0c3e539692f64a056dcab9a6c42200d0a88
miga-choi <miga.choi.1004@gmail.com>
2025-11-29 14:10:27 +0900

    leetcode: "3_longest_substring_without_repeating_characters" add java solution

--- leetcode/algorithms/3_longest_substring_without_repeating_characters/main.java
+++ leetcode/algorithms/3_longest_substring_without_repeating_characters/main.java
@@ -1,10 +1,16 @@
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 class LongestSubstringWithoutRepeatingCharacters {
+    /**
+     * Sliding Window
+     *
+     * Complexities:
+     *   N - Length of `s`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
     public int lengthOfLongestSubstring(String s) {
         int[] charmap = new int[128];
         Arrays.fill(charmap, -1);
@@ -29,62 +35,30 @@ class LongestSubstringWithoutRepeatingCharacters {
 
 
     // Solution
-    // Solution 1: Set
-    public int solution1(String s) {
-        int n = s.length();
+    /**
+     * Sliding Window (using Map)
+     *
+     * Complexities:
+     *   N - Length of `s`
+     *   M - Length of Available Characters in `s`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(Min(M, N))
+     */
+    public int solution(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
         int maxLength = 0;
-        Set<Character> charSet = new HashSet<>();
         int left = 0;
 
-        for (int right = 0; right < n; right++) {
-            if (!charSet.contains(s.charAt(right))) {
-                charSet.add(s.charAt(right));
-                maxLength = Math.max(maxLength, right - left + 1);
-            } else {
-                while (charSet.contains(s.charAt(right))) {
-                    charSet.remove(s.charAt(left));
-                    left++;
-                }
-                charSet.add(s.charAt(right));
+        for (int right = 0; right < s.length(); right++) {
+            char currentChar = s.charAt(right);
+
+            if (map.containsKey(currentChar)) {
+                left = Math.max(left, map.get(currentChar) + 1);
             }
-        }
 
-        return maxLength;
-    }
+            map.put(currentChar, right);
 
-    // Solution 2: Map
-    public int solution2(String s) {
-        int n = s.length();
-        int maxLength = 0;
-        Map<Character, Integer> charMap = new HashMap<>();
-        int left = 0;
-
-        for (int right = 0; right < n; right++) {
-            if (!charMap.containsKey(s.charAt(right)) || charMap.get(s.charAt(right)) < left) {
-                charMap.put(s.charAt(right), right);
-                maxLength = Math.max(maxLength, right - left + 1);
-            } else {
-                left = charMap.get(s.charAt(right)) + 1;
-                charMap.put(s.charAt(right), right);
-            }
-        }
-
-        return maxLength;
-    }
-
-    // Solution 3: Integer Array
-    public int solution3(String s) {
-        int n = s.length();
-        int maxLength = 0;
-        int[] charIndex = new int[128];
-        Arrays.fill(charIndex, -1);
-        int left = 0;
-
-        for (int right = 0; right < n; right++) {
-            if (charIndex[s.charAt(right)] >= left) {
-                left = charIndex[s.charAt(right)] + 1;
-            }
-            charIndex[s.charAt(right)] = right;
             maxLength = Math.max(maxLength, right - left + 1);
         }
 
