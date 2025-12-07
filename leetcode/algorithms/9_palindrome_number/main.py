0353e23601a94921661b94c49dd000369d0fc9aa
miga-choi <miga.choi.1004@gmail.com>
2025-12-08 01:17:52 +0900

    leetcode: "9_palindrome_number" add python solution

--- leetcode/algorithms/9_palindrome_number/main.py
+++ leetcode/algorithms/9_palindrome_number/main.py
@@ -1,42 +1,61 @@
 class PalindromeNumber:
+    """
+    # Complexities:
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(N)
+    """
     def isPalindrome(self, x: int) -> bool:
         result = True
+
         if x < 0:
             result = False
+
         xStr = str(x)
+
         for i in range(0, len(xStr) - 1):
             if i == len(xStr) - 1 - i:
                 break
             if xStr[i] != xStr[len(xStr) - 1 - i]:
                 result = False
+
         return result
 
 
     # Solution
-    # Solution 1: Reversing the Entire Number
+    """
+    # Solution 1
+    #
+    # String Conversion
+    #
+    # Complexities:
+    #   - Time Complexity: O(logᴺ)
+    #   - Space Complexity: O(logᴺ)
+    """
     def solution1(self, x: int) -> bool:
         if x < 0:
             return False
 
-        reversed_num = 0
-        temp = x
+        s = str(x)
 
-        while temp != 0:
-            digit = temp % 10
-            reversed_num = reversed_num * 10 + digit
-            temp //= 10
+        return s == s[::-1]
 
-        return reversed_num == x
-
-    # Solution 2: Reversing Half of the Number
+    """
+    # Solution 2
+    #
+    # Revert Half of the Number
+    #
+    # Complexities:
+    #   - Time Complexity: O(logᴺ)
+    #   - Space Complexity: O(logᴺ)
+    """
     def solution2(self, x: int) -> bool:
-        if x < 0 or (x != 0 and x % 10 == 0):
+        if x < 0 or (x % 10 == 0 and x != 0):
             return False
 
-        reversed_num = 0
+        reverted_number = 0
 
-        while x > reversed_num:
-            reversed_num = reversed_num * 10 + x % 10
+        while x > reverted_number:
+            reverted_number = reverted_number * 10 + x % 10
             x //= 10
 
-        return x == reversed_num or x == reversed_num // 10
+        return x == reverted_number or x == reverted_number // 10
