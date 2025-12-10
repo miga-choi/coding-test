f2be26e113737c5637ba89f651e39b20019496cf
miga-choi <miga.choi.1004@gmail.com>
2025-12-11 01:22:12 +0900

    leetcode: "11_container_with_most_water" add python solution

--- leetcode/algorithms/11_container_with_most_water/main.py
+++ leetcode/algorithms/11_container_with_most_water/main.py
@@ -2,13 +2,17 @@ from typing import List
 
 
 class ContainerWithMostWater:
+    """
     # Two Pointers
-    # - Time Complexity: O(N)
-    # - Space Complexity: O(1)
+    # 
+    # Complexities:
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
     def maxArea(self, height: List[int]) -> int:
         left: int = 0
         right: int = len(height) - 1
-        maxStored: int = 0
+        maxStore: int = 0
 
         while left < right:
             leftHeight: int = height[left]
@@ -24,3 +28,31 @@ class ContainerWithMostWater:
                 right -= 1
 
         return maxStore
+
+
+    # Solution
+    """
+    # Two Pointers
+    # 
+    # Complexities:
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
+    def solution(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+        
+        while left < right:
+            width = right - left
+            
+            current_area = min(height[left], height[right]) * width
+            
+            max_area = max(max_area, current_area)
+            
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+                
+        return max_area
