6211bd71d6e81f5af1d38e705a4741ba94193fd9
miga-choi <miga.choi.1004@gmail.com>
2025-12-11 01:21:28 +0900

    leetcode: "11_container_with_most_water" add java solution

--- leetcode/algorithms/11_container_with_most_water/main.java
+++ leetcode/algorithms/11_container_with_most_water/main.java
@@ -1,8 +1,10 @@
 class ContainerWithMostWater {
     /**
      * Two Pointers
-     * - Time Complexity: O(N)
-     * - Space Complexity: O(1)
+     *
+     * Complexities:
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
      */
     public int maxArea(int[] height) {
         int left = 0;
@@ -25,5 +27,36 @@ class ContainerWithMostWater {
         }
 
         return maxStored;
+    }
+
+
+    // Solution
+    /**
+     * Two Pointers
+     *
+     * Complexities:
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
+    public int solution(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int currentHeight = Math.min(height[left], height[right]);
+            int currentWidth = right - left;
+            int currentArea = currentHeight * currentWidth;
+
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
     }
 }
