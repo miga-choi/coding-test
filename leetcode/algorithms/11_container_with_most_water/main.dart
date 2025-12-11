17f420ce167770e5fae81bdb4a4ce388a19ea3e5
miga-choi <miga.choi.1004@gmail.com>
2025-12-12 07:44:14 +0900

    leetcode: "11_container_with_most_water" add dart solution

--- leetcode/algorithms/11_container_with_most_water/main.dart
+++ leetcode/algorithms/11_container_with_most_water/main.dart
@@ -3,8 +3,10 @@ import 'dart:math';
 class ContainerWithMostWater {
   /**
    * Two Pointers
-   * - Time Complexity: O(N)
-   * - Space Complexity: O(1)
+   *
+   * Complexities:
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
    */
   int maxArea(List<int> height) {
     int left = 0;
@@ -27,5 +29,36 @@ class ContainerWithMostWater {
     }
 
     return maxStored;
+  }
+
+
+  // Solution
+  /**
+   * Two Pointers
+   *
+   * Complexities:
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
+  int solution(List<int> height) {
+    int left = 0;
+    int right = height.length - 1;
+    int maxWater = 0;
+
+    while (left < right) {
+      int currentHeight = min(height[left], height[right]);
+      int currentWidth = right - left;
+      int currentArea = currentWidth * currentHeight;
+
+      maxWater = max(maxWater, currentArea);
+
+      if (height[left] < height[right]) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+
+    return maxWater;
   }
 }
