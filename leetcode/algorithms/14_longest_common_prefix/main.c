9ea36252f5d91faec74d8e98194933530ce5b232
miga-choi <miga.choi.1004@gmail.com>
2025-12-16 01:04:22 +0900

    leetcode: "14_longest_common_prefix" add c++ solution

--- leetcode/algorithms/14_longest_common_prefix/main.c
+++ leetcode/algorithms/14_longest_common_prefix/main.c
@@ -1,6 +1,8 @@
 #include <string.h>
 
 /**
+ * Vertical Scanning
+ * 
  * Complexities:
  *   S = N * L
  *   N - `strsSize`
@@ -13,6 +15,7 @@ char* longestCommonPrefix(char** strs, int strsSize) {
         if (strs[0][i] == '\0') {
             return strs[0];
         }
+
         for (int j = 1; j < strsSize; j++) {
             if (strs[j][i] != strs[0][i]) {
                 strs[0][i] = '\0';
--- leetcode/algorithms/14_longest_common_prefix/main.cpp
+++ leetcode/algorithms/14_longest_common_prefix/main.cpp
@@ -4,11 +4,22 @@ using namespace std;
 
 class LongestCommonPrefix {
 public:
+    /**
+     * Vertical Scanning
+     * 
+     * Complexities:
+     *   S = N * L
+     *   N - `strsSize`
+     *   L - Length of Shortest `str` in `strs
+     *   - Time Complexity: O(S)
+     *   - Space Complexity: O(1)
+     */
     string longestCommonPrefix(vector<string>& strs) {
         for (int i = 0;; i++) {
             if (strs[0][i] == '\0') {
                 return strs[0];
             }
+
             for (int j = 1; j < strs.size(); j++) {
                 if (strs[j][i] != strs[0][i]) {
                     return strs[0].substr(0, i);
@@ -19,41 +30,59 @@ public:
 
 
     // Solution
-    // Solution 1
+    /**
+     * Solution 1
+     * 
+     * Sorting
+     * 
+     * Complexities:
+     *   S = N * L
+     *   N - Size of `strs`
+     *   L - Length of Shortest `str` in `strs
+     *   - Time Complexity: O(N * Logᴺ + M)
+     *   - Space Complexity: O(1)
+     */
     string solution1(vector<string>& strs) {
-        string ans = "";
         sort(strs.begin(), strs.end());
-        int n = strs.size();
-        string first = strs[0], last = strs[n - 1];
-        for (int i = 0; i < min(first.size(), last.size()); i++) {
+
+        string first = strs.front();
+        string last = strs.back();
+
+        string commonPrefix = "";
+
+        for (int i = 0; i < min(first.length(), last.length()); ++i) {
             if (first[i] != last[i]) {
-                return ans;
+                return commonPrefix;
             }
-            ans += first[i];
+            commonPrefix += first[i];
         }
-        return ans;
+
+        return commonPrefix;
     }
 
-    // Solution 2
-    string solution2(vector<string>& strs) {
-        int n = strs.size();
-        if (n == 0) {
-            return "";
-        }
+    /**
+     * Solution 2
+     * 
+     * Vertical Scanning
+     * 
+     * Complexities:
+     *   S = N * L
+     *   N - Size of `strs`
+     *   L - Length of Shortest `str` in `strs
+     *   - Time Complexity: O(S)
+     *   - Space Complexity: O(1)
+     */
+    string solution(vector<string>& strs) {
+        for (int i = 0; i < strs[0].length(); ++i) {
+            char c = strs[0][i];
 
-        string ans = "";
-        sort(begin(strs), end(strs));
-        string a = strs[0];
-        string b = strs[n - 1];
-
-        for (int i = 0; i < a.size(); i++) {
-            if (a[i] == b[i]) {
-                ans = ans + a[i];
-            } else {
-                break;
+            for (int j = 1; j < strs.size(); ++j) {
+                if (i >= strs[j].length() || strs[j][i] != c) {
+                    return strs[0].substr(0, i);
+                }
             }
         }
 
-        return ans;
+        return strs[0];
     }
 };
