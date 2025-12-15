5220da266e7835f1e14210378c5287dcdbda7b3d
miga-choi <miga.choi.1004@gmail.com>
2025-12-16 01:04:56 +0900

    leetcode: "14_longest_common_prefix" add javascript solution

--- leetcode/algorithms/14_longest_common_prefix/main.js
+++ leetcode/algorithms/14_longest_common_prefix/main.js
@@ -1,4 +1,12 @@
 /**
+ * Complexities:
+ *   S = N * L
+ *   N - `strsSize`
+ *   L - Length of Shortest `str` in `strs
+ *   - Time Complexity: O(S)
+ *   - Space Complexity: O(1)
+ */
+/**
  * @param {string[]} strs
  * @return {string}
  */
@@ -20,35 +28,65 @@ var longestCommonPrefix = function (strs) {
 
 
 // Solution
-// Solution 1: reduce
+/**
+ * Solution 1
+ *
+ * Horizontal Scanning
+ *
+ * Complexities:
+ *   S - Quantity of characters in `strs`
+ *   - Time Complexity: O(S)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
 var solution1 = function (strs) {
-  if (strs === undefined || strs.length === 0) {
-    return "";
+  let prefix = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+
+      if (prefix === "") {
+        return "";
+      }
+    }
   }
 
-  return strs.reduce((prev, next) => {
-    let i = 0;
-    while (prev[i] && next[i] && prev[i] === next[i]) {
-      i++;
-    }
-    return prev.slice(0, i);
-  });
+  return prefix;
 };
 
-// Solution 2
+/**
+ * Solution 2
+ *
+ * Sorting
+ *
+ * Complexities:
+ *   N - Length of `strs`
+ *   L - Length of Longest `str` in `strs`
+ *   - Time Complexity: O(N * L * logᴺ)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
 var solution2 = function (strs) {
-  if (!strs.length) {
-    return "";
-  }
-  let prefix = "";
-  let maxPrefixLength = Math.min(...strs.map((str) => str.length));
-  for (let i = 0; i < maxPrefixLength; i++) {
-    let char = strs[0][i];
-    if (strs.every((str) => str[i] === char)) {
-      prefix += char;
+  strs.sort();
+
+  const s1 = strs[0];
+  const s2 = strs[strs.length - 1];
+
+  let idx = 0;
+  while (idx < s1.length && idx < s2.length) {
+    if (s1[idx] === s2[idx]) {
+      idx++;
     } else {
       break;
     }
   }
-  return prefix;
+
+  return s1.substring(0, idx);
 };
