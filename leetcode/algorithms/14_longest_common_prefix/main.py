9eff6b7c1fd229651557c9218f5e53c883bddc93
miga-choi <miga.choi.1004@gmail.com>
2025-12-17 01:05:52 +0900

    leetcode: "14_longest_common_prefix" add python solution

--- leetcode/algorithms/14_longest_common_prefix/main.py
+++ leetcode/algorithms/14_longest_common_prefix/main.py
@@ -2,76 +2,89 @@ from typing import List
 
 
 class LongestCommonPrefix:
+    """
+    # Complexities:
+    #   S = N * L
+    #   N - `strsSize`
+    #   L - Length of Shortest `str` in `strs
+    #   - Time Complexity: O(S)
+    #   - Space Complexity: O(1)
+    """
     def longestCommonPrefix(self, strs: List[str]) -> str:
         result = ""
         length = len(strs[0])
-        if len(strs) == 0:
-            return ""
+
         if len(strs) == 1:
             return strs[0]
+
         for strc in strs:
             if len(strc) == 0:
                 return ""
             if length > len(strc):
                 length = len(strc)
+
         for i in range(0, length):
             same = True
+
             for j in range(0, len(strs)):
                 if j >= len(strs) - 1:
                     continue
                 if strs[j][i] != strs[j + 1][i]:
                     same = False
+
             if same:
                 result += strs[j][i]
             else:
                 break
+
         return result
 
 
     # Solution
+    """
     # Solution 1
+    #
+    # Vertical Scanning with zip
+    #
+    # Complexities:
+    #   S - Quantity of characters in `strs`
+    #   - Time Complexity: O(S)
+    #   - Space Complexity: O(1)
+    """
     def solution1(self, strs: List[str]) -> str:
-        ans = ""
-        strs = sorted(strs)
-        first = strs[0]
-        last = strs[-1]
-        for i in range(min(len(first), len(last))):
-            if first[i] != last[i]:
-                return ans
-            ans += first[i]
-        return ans
-
-    # Solution 2
-    def solution2(self, strs: List[str]) -> str:
-        if not strs:
-            return ""
-        shortest = min(strs, key=len)
-        for i, ch in enumerate(shortest):
-            for other in strs:
-                if other[i] != ch:
-                    return shortest[:i]
-        return shortest
-
-    # Solution 3
-    def solution3(self, strs: List[str]) -> str:
-        if not strs:
-            return ""
-        # since list of string will be sorted and retrieved min max by alphebetic order
-        s1 = min(strs)
-        s2 = max(strs)
-
-        for i, c in enumerate(s1):
-            if c != s2[i]:
-                return s1[:i]  # stop until hit the split index
-        return s1
-
-    # Solution 4
-    def solution(self, strs: List[str]) -> str:
-        l = list(zip(*strs))
         prefix = ""
-        for i in l:
-            if len(set(i)) == 1:
-                prefix += i[0]
+        
+        for chars in zip(*strs):
+            if len(set(chars)) == 1:
+                prefix += chars[0]
             else:
                 break
+                
         return prefix
+
+    """
+    # Solution 2
+    # 
+    # Sorting
+    #
+    # Complexities:
+    #   N - Length of `strs`
+    #   L - Length of Longest `str` in `strs`
+    #   - Time Complexity: O(N * L * logᴺ)
+    #   - Space Complexity: O(logᴺ)
+    """
+    def solution2(self, strs: List[str]) -> str:
+        strs.sort()
+        
+        first = strs[0]
+        last = strs[-1]
+        
+        i = 0
+
+        while i < len(first) and i < len(last):
+            if first[i] != last[i]:
+                break
+
+            i += 1
+            
+        return first[:i]
