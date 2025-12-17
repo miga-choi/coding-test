734aa29a14fcfb3fbb44aea0d2ad295115fcce7b
miga-choi <miga.choi.1004@gmail.com>
2025-12-18 06:47:13 +0900

    leetcode: "14_longest_common_prefix" add dart solution

--- leetcode/algorithms/14_longest_common_prefix/main.dart
+++ leetcode/algorithms/14_longest_common_prefix/main.dart
@@ -19,15 +19,60 @@ class LongestCommonPrefix {
     return result;
   }
 
-
   // Solution
-  String solution(List<String> strs) {
-    String commonPrefix = strs.first;
-    for (final word in strs) {
-      while (!word.startsWith(commonPrefix)) {
-        commonPrefix = commonPrefix.substring(0, commonPrefix.length - 1);
+  /**
+   * Solution 1
+   *
+   * Horizontal Scanning
+   *
+   * Complexities:
+   *   S - Quantity of characters in `strs`
+   *   - Time Complexity: O(S)
+   *   - Space Complexity: O(1)
+   */
+  String solution1(List<String> strs) {
+    String prefix = strs[0];
+
+    for (int i = 1; i < strs.length; i++) {
+      while (!strs[i].startsWith(prefix)) {
+        prefix = prefix.substring(0, prefix.length - 1);
+
+        if (prefix.isEmpty) {
+          return "";
+        }
       }
     }
-    return commonPrefix;
+
+    return prefix;
+  }
+
+  /**
+   * Solution 2
+   *
+   * Sorting
+   *
+   * Complexities:
+   *   N - Length of `strs`
+   *   L - Length of Longest `str` in `strs`
+   *   - Time Complexity: O(N * L * logᴺ)
+   *   - Space Complexity: O(logᴺ)
+   */
+  String solution2(List<String> strs) {
+    strs.sort();
+
+    String s1 = strs.first;
+    String s2 = strs.last;
+
+    int idx = 0;
+
+    while (idx < s1.length && idx < s2.length) {
+      if (s1[idx] == s2[idx]) {
+        idx++;
+      } else {
+        break;
+      }
+    }
+
+    return s1.substring(0, idx);
   }
 }
