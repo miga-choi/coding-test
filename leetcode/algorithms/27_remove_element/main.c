1f723ddebc17d3d51d4efe1effeefbe2996f7cb4
miga-choi <miga.choi.1004@gmail.com>
2026-01-02 01:58:38 +0900

    leetcode: "27_remove_element" add c solution

--- leetcode/algorithms/27_remove_element/main.c
+++ leetcode/algorithms/27_remove_element/main.c
@@ -1,7 +1,16 @@
 #include <stdbool.h>
 
+/**
+ * Two Pointers
+ * 
+ * Complexities:
+ *   N - numsSize
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
 int removeElement(int* nums, int numsSize, int val) {
     int i = 0;
+
     for (int j = 0; j < numsSize; j++) {
         if (nums[j] != val) {
             int temp = nums[i];
@@ -10,40 +19,26 @@ int removeElement(int* nums, int numsSize, int val) {
             i++;
         }
     }
+
     return i;
 }
 
 
 // Solution
-// Solution 1
-int solution1(int* nums, int numsSize, int val) {
-    int count = 0;
-
-    for (int i = 0; i < numsSize; i++) {
-        if (nums[i] == val) {
-            count++;
-        } else {
-            nums[i - count] = nums[i];
-        }
-    }
-
-    return (numsSize - count);
-}
-
-// Solution 2
-int solution2(int* nums, int numsSize, int val) {
-    int appendIndex = 0;
-    bool find = false;
+/**
+ * Two Pointers
+ * 
+ * Complexities:
+ *   N - numsSize
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+int solution(int* nums, int numsSize, int val) {
     int k = 0;
 
     for (int i = 0; i < numsSize; i++) {
-        if (nums[i] == val && !find) {
-            appendIndex = i;
-            find = true;
-        }
         if (nums[i] != val) {
-            nums[appendIndex] = nums[i];
-            appendIndex++;
+            nums[k] = nums[i];
             k++;
         }
     }
