6d14b54a1be535da7f52b1dc558c7fce10a68952
miga-choi <miga.choi.1004@gmail.com>
2026-01-04 16:14:04 +0900

    leetcode: "27_remove_element" add java solution

--- leetcode/algorithms/27_remove_element/main.java
+++ leetcode/algorithms/27_remove_element/main.java
@@ -3,6 +3,14 @@ import java.util.Arrays;
 import java.util.List;
 
 class RemoveElement {
+  /**
+   * Two Pointers
+   *
+   * Complexities:
+   *   N - Length of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
   public int removeElement(int[] nums, int val) {
     List<Integer> newNums = new ArrayList<>();
     for (Integer num : nums) {
@@ -19,40 +27,24 @@ class RemoveElement {
 
 
   // Solution
-  // Solution 1
-  public int solution1(int[] nums, int val) {
-    int index = 0;
+  /**
+   * Two Pointers
+   *
+   * Complexities:
+   *   N - Length of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
+  public int solution(int[] nums, int val) {
+    int k = 0;
+
     for (int i = 0; i < nums.length; i++) {
       if (nums[i] != val) {
-        nums[index] = nums[i];
-        index++;
+        nums[k] = nums[i];
+        k++;
       }
     }
-    return index;
-  }
 
-  // Solution 2
-  public int solution2(int[] nums, int val) {
-    int i = 0;
-    for (int j = 0; j < nums.length; j++) {
-      if (nums[j] != val) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-        i++;
-      }
-    }
-    return i;
-  }
-
-  // Solution 3
-  public int solution3(int[] nums, int val) {
-    int l = nums.length;
-    for (int i = 0; i < l; i++) {
-      if (nums[i] == val) {
-        nums[i--] = nums[l-- - 1];
-      }
-    }
-    return l;
+    return k;
   }
 }
