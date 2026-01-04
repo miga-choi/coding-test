bb2b467fa3034b88088e4db134a50edcf89d9d27
miga-choi <miga.choi.1004@gmail.com>
2026-01-05 05:41:36 +0900

    leetcode: "27_remove_element" add dart solution

--- leetcode/algorithms/27_remove_element/main.dart
+++ leetcode/algorithms/27_remove_element/main.dart
@@ -1,19 +1,38 @@
 class RemoveElement {
+  /**
+   * Two Pointers
+   *
+   * Complexities:
+   *   N - Length of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
   int removeElement(List<int> nums, int val) {
     nums.removeWhere((e) => e == val);
+
     return 0;
   }
 
 
   // Solution
+  /**
+   * Two Pointers
+   *
+   * Complexities:
+   *   N - Length of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
   int solution(List<int> nums, int val) {
-    int cnt = 0;
+    int k = 0;
+
     for (int i = 0; i < nums.length; i++) {
       if (nums[i] != val) {
-        nums[cnt] = nums[i];
-        cnt++;
+        nums[k] = nums[i];
+        k++;
       }
     }
-    return cnt;
+
+    return k;
   }
 }
