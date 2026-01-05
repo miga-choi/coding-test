fa1eca4b2a956667ffec1323bb0949081f1fd791
miga-choi <miga.choi.1004@gmail.com>
2026-01-06 01:01:35 +0900

    leetcode: "28_find_the_index_of_the_first_occurrence_in_a_string" add c++ solution

--- leetcode/algorithms/28_find_the_index_of_the_first_occurrence_in_a_string/main.cpp
+++ leetcode/algorithms/28_find_the_index_of_the_first_occurrence_in_a_string/main.cpp
@@ -5,9 +5,13 @@ using namespace std;
 class FindTheIndexOfTheFirstOccurrenceInAString {
 public:
     /**
-     * Brute-Force
-     * - Time Complexity: O(N * M)
-     * - Space Complexity: O(1)
+     * Brute Force, Sliding Window
+     *
+     * Complexities:
+     *   N - Length of `haystack`
+     *   M - Length of `needle`
+     *   - Time Complexity: O(N * M)
+     *   - Space Complexity: O(1)
      */
     int strStr(string haystack, string needle) {
         int haystack_len = haystack.size();
@@ -40,46 +44,33 @@ public:
     /**
      * Solution 1
      * 
-     * std::string::find()
-     * - Time Complexity: O(N)
-     * - Space Complexity: O(1)
+     * Brute Force, Sliding Window 
+     *
+     * Complexities:
+     *   N - Length of `haystack`
+     *   M - Length of `needle`
+     *   - Time Complexity: O(N * M)
+     *   - Space Complexity: O(1)
      */
     int solution1(string haystack, string needle) {
-        size_t found_pos = haystack.find(needle);
+        int h_len = haystack.length();
+        int n_len = needle.length();
 
-        if (found_pos == string::npos) {
+        if (h_len < n_len) {
             return -1;
         }
 
-        return static_cast<int>(found_pos);
-    }
-
-    /**
-     * Solution 2
-     * 
-     * Brute-Force
-     * - Time Complexity: O(N * M)
-     * - Space Complexity: O(1)
-     */
-    int strStr(string haystack, string needle) {
-        int haystack_len = haystack.length();
-        int needle_len = needle.length();
-
-        if (haystack_len < needle_len) {
-            return -1;
-        }
-
-        for (int haystack_idx = 0; haystack_idx <= haystack_len - needle_len; ++haystack_idx) {
-            int needle_idx = 0;
-
-            for (; needle_idx < needle_len; ++needle_idx) {
-                if (haystack[haystack_idx + needle_idx] != needle[needle_idx]) {
+        for (int i = 0; i <= h_len - n_len; ++i) {
+            int j = 0;
+            
+            for (; j < n_len; ++j) {
+                if (haystack[i + j] != needle[j]) {
                     break;
                 }
             }
 
-            if (needle_idx == needle_len) {
-                return haystack_idx;
+            if (j == n_len) {
+                return i;
             }
         }
 
@@ -87,60 +78,23 @@ public:
     }
 
     /**
-     * Solution3
+     * Solution 2
      * 
-     * Knuth-Morris-Pratt (KMP)
-     * - Time Complexity: O(N + M)
-     * - Space Complexity: O(M)
+     * string::find
+     *
+     * Complexities:
+     *   N - Length of `haystack`
+     *   M - Length of `needle`
+     *   - Time Complexity: O(N * M)
+     *   - Space Complexity: O(1)
      */
-    vector<int> compute_lps_array(const string& pattern) {
-        int pattern_len = pattern.length();
-        vector<int> lps(pattern_len, 0);
-        int length = 0;
-        int idx = 1;
-
-        while (idx < pattern_len) {
-            if (pattern[idx] == pattern[length]) {
-                length++;
-                lps[idx] = length;
-                idx++;
-            } else {
-                if (length != 0) {
-                    length = lps[length - 1];
-                } else {
-                    lps[idx] = 0;
-                    idx++;
-                }
-            }
+    int solution2(string haystack, string needle) {
+        size_t found = haystack.find(needle);
+        
+        if (found != string::npos) {
+            return (int)found;
         }
-
-        return lps;
-    }
-
-    int solution3(string haystack, string needle) {
-        int haystack_len = haystack.size();
-        int needle_len = needle.size();
-
-        if (haystack_len < needle_len) {
-            return -1;
-        }
-
-        vector<int> lps = compute_lps_array(needle);
-
-        for (int haystack_idx = 0, needle_idx = 0; haystack_idx < haystack_len;) {
-            if (haystack[haystack_idx] == needle[needle_idx]) {
-                haystack_idx++, needle_idx++;
-            }
-
-            if (needle_idx == needle_len) {
-                return haystack_idx - needle_idx;
-            }
-
-            if (haystack_idx < haystack_len && haystack[haystack_idx] != needle[needle_idx]) {
-                needle_idx ? needle_idx = lps[needle_idx - 1] : haystack_idx++;
-            }
-        }
-
+        
         return -1;
     }
 };
