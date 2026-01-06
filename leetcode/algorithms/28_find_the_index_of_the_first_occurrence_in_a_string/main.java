b5417ca71309dd8be62ff1a43c24f148ba6c7f44
miga-choi <miga.choi.1004@gmail.com>
2026-01-07 01:05:37 +0900

    leetcode: "28_find_the_index_of_the_first_occurrence_in_a_string" add java solution

--- leetcode/algorithms/28_find_the_index_of_the_first_occurrence_in_a_string/main.java
+++ leetcode/algorithms/28_find_the_index_of_the_first_occurrence_in_a_string/main.java
@@ -1,8 +1,12 @@
 class FindTheIndexOfTheFirstOccurrenceInAString {
   /**
-   * Brute-Force
-   * - Time Complexity: O(N * M)
-   * - Space Complexity: O(1)
+   * Brute Force, Sliding Window
+   *
+   * Complexities:
+   *   N - Length of `haystack`
+   *   M - Length of `needle`
+   *   - Time Complexity: O(N * M)
+   *   - Space Complexity: O(1)
    */
   public int strStr(String haystack, String needle) {
     int haystackLen = haystack.length();
@@ -36,11 +40,37 @@ class FindTheIndexOfTheFirstOccurrenceInAString {
 
   // Solution
   /**
-   * java.lang.String indexOf()
-   * - Time Complexity: O(N * M)
-   * - Space Complexity: O(1)
+   * Brute Force, Sliding Window
+   *
+   * Complexities:
+   *   N - Length of `haystack`
+   *   M - Length of `needle`
+   *   - Time Complexity: O(N * M)
+   *   - Space Complexity: O(1)
    */
   public int solution(String haystack, String needle) {
-    return haystack.indexOf(needle);
+    int n = haystack.length();
+    int m = needle.length();
+
+    if (m > n) {
+      return -1;
+    }
+
+    for (int i = 0; i <= n - m; i++) {
+      int j = 0;
+
+      while (j < m) {
+        if (haystack.charAt(i + j) != needle.charAt(j)) {
+          break;
+        }
+        j++;
+      }
+
+      if (j == m) {
+        return i;
+      }
+    }
+
+    return -1;
   }
 }
