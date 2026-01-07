4519629fafb7561925badec82c41759fe1c0699f
miga-choi <miga.choi.1004@gmail.com>
2026-01-08 07:57:01 +0900

    leetcode: "35_search_insert_position" add c solution

--- leetcode/algorithms/35_search_insert_position/main.c
+++ leetcode/algorithms/35_search_insert_position/main.c
@@ -1,3 +1,11 @@
+/**
+ * Binary Search
+ * 
+ * Complexities:
+ *   N - numsSize
+ *   - Time Complexity: O(logᴺ)
+ *   - Space Complexity: O(1)
+ */
 int searchInsert(int* nums, int numsSize, int target) {
     int left = 0;
     int right = numsSize - 1;
@@ -18,28 +26,29 @@ int searchInsert(int* nums, int numsSize, int target) {
 
 
 // Solution
-// Solution 1: Linear Search O(n)
-int solution1(int* nums, int numsSize, int target) {
-    for (int i = 0; i < numsSize; i++) {
-        if (nums[i] >= target) {
-            return i;
-        }
-    }
-    return numsSize;
-}
+/**
+ * Binary Search
+ * 
+ * Complexities:
+ *   N - numsSize
+ *   - Time Complexity: O(logᴺ)
+ *   - Space Complexity: O(1)
+ */
+int solution(int* nums, int numsSize, int target) {
+    int left = 0;
+    int right = numsSize - 1;
 
-// Solution 2: Binary Search O(log(n))
-int solution2(int* nums, int numsSize, int target) {
-    int left = 0, right = numsSize - 1, mid;
     while (left <= right) {
-        mid = (right + left) / 2;
-        if (nums[mid] < target) {
-            left = mid + 1;
-        } else if (nums[mid] > target) {
-            right = mid - 1;
-        } else {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
             return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
+
     return left;
 }
