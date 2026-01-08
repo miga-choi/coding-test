201d5334471f339890345e3b8cd6718f32be5f3c
miga-choi <miga.choi.1004@gmail.com>
2026-01-09 01:10:52 +0900

    leetcode: "35_search_insert_position" add c++ solution

--- leetcode/algorithms/35_search_insert_position/main.cpp
+++ leetcode/algorithms/35_search_insert_position/main.cpp
@@ -1,8 +1,17 @@
+#include <algorithm>
 #include <vector>
 using namespace std;
 
 class SearchInsertPosition {
 public:
+    /**
+     * Binary Search
+     *
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(logᴺ)
+     *   - Space Complexity: O(1)
+     */
     int searchInsert(vector<int>& nums, int target) {
         int left = 0;
         int right = nums.size() - 1;
@@ -23,29 +32,45 @@ public:
 
 
     // Solution
-    // Solution 1: Binary Search
-    // Time complexity: O(log(n)), Space complexity: O(1)
+    /**
+     * Solution 1
+     * 
+     * Binary Search
+     *
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(logᴺ)
+     *   - Space Complexity: O(1)
+     */
     int solution1(vector<int>& nums, int target) {
-        int low = 0;
-        int high = nums.size();
-        int mid;
-        if (target > nums[high - 1]) {
-            return high;
-        }
-        while (low <= high) {
-            mid = (low + high) / 2;
-            if (target > nums[mid]) {
-                low = mid + 1;
-            } else if (target < nums[mid]) {
-                high = mid - 1;
-            } else {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
                 return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        return low;
+
+        return left;
     }
 
-    // Solution 2:
+    /**
+     * Solution 2
+     * 
+     * algorithm::lower_bound
+     *
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(logᴺ)
+     *   - Space Complexity: O(1)
+     */
     int solution2(vector<int>& nums, int target) {
         return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
     }
