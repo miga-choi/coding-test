45756f76b0fccf903ba3d6752c1e786692c7c328
miga-choi <miga.choi.1004@gmail.com>
2026-01-12 01:04:04 +0900

    leetcode: "48_rotate_image" add javascript solution

--- leetcode/algorithms/48_rotate_image/main.js
+++ leetcode/algorithms/48_rotate_image/main.js
@@ -1,7 +1,10 @@
 /**
- * Reverse & Transpose
- * - Time Complexity: O(N²)
- * - Space Complexity: O(1)
+ * Transpose & Reverse
+ *
+ * Complexities:
+ *   N - Length of `matrix`
+ *   - Time Complexity: O(N²)
+ *   - Space Complexity: O(1)
  */
 /**
  * @param {number[][]} matrix
@@ -24,57 +27,27 @@ var rotate = function (matrix) {
 
 // Solution
 /**
- * Solution 1
- *
  * Transpose & Reverse
- * - Time Complexity: O(N²)
- * - Space Complexity: O(1)
+ *
+ * Complexities:
+ *   N - Length of `matrix`
+ *   - Time Complexity: O(N²)
+ *   - Space Complexity: O(1)
  */
 /**
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var solution1 = function (matrix) {
+var solution = function (matrix) {
   const n = matrix.length;
 
   for (let i = 0; i < n; i++) {
     for (let j = i; j < n; j++) {
-      const temp = matrix[i][j];
-      matrix[i][j] = matrix[j][i];
-      matrix[j][i] = temp;
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
     }
   }
 
   for (let i = 0; i < n; i++) {
     matrix[i].reverse();
-  }
-};
-
-/**
- * Solution 2
- *
- * Rotate Groups of Four
- * - Time Complexity: O(N²)
- * - Space Complexity: O(1)
- */
-/**
- * @param {number[][]} matrix
- * @return {void} Do not return anything, modify matrix in-place instead.
- */
-var solution2 = function (matrix) {
-  const n = matrix.length;
-
-  for (let i = 0; i < Math.floor(n / 2); i++) {
-    for (let j = i; j < n - 1 - i; j++) {
-      const temp = matrix[i][j];
-
-      matrix[i][j] = matrix[n - 1 - j][i];
-
-      matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
-
-      matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
-
-      matrix[j][n - 1 - i] = temp;
-    }
   }
 };
