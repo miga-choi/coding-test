da79efd53314955842f9725f1d310dc5ec67e115
miga-choi <miga.choi.1004@gmail.com>
2026-01-14 06:42:49 +0900

    leetcode: "48_rotate_image" add dart solution

--- leetcode/algorithms/48_rotate_image/main.dart
+++ leetcode/algorithms/48_rotate_image/main.dart
@@ -1,8 +1,11 @@
 class RotateImage {
   /**
-   * Reverse & Transpose
-   * - Time Complexity: O(N²)
-   * - Space Complexity: O(1)
+   * Transpose & Reverse
+   *
+   * Complexities:
+   *   N - Length of `matrix`
+   *   - Time Complexity: O(N²)
+   *   - Space Complexity: O(1)
    */
   void rotate(List<List<int>> matrix) {
     final int length = matrix.length;
@@ -29,64 +32,36 @@ class RotateImage {
 
   // Solution
   /**
-   * Solution 1
-   * 
-   * Transpose & Reflect
-   * - Time Complexity: O(N²)
-   * - Space Complexity: O(1)
+   * Transpose & Reverse
+   *
+   * Complexities:
+   *   N - Length of `matrix`
+   *   - Time Complexity: O(N²)
+   *   - Space Complexity: O(1)
    */
   void solution1(List<List<int>> matrix) {
     int n = matrix.length;
 
-    // Transpose
     for (int i = 0; i < n; i++) {
-      for (int j = i; j < n; j++) {
+      for (int j = i + 1; j < n; j++) {
         int temp = matrix[i][j];
         matrix[i][j] = matrix[j][i];
         matrix[j][i] = temp;
       }
     }
 
-    // Reflect
     for (int i = 0; i < n; i++) {
-      matrix[i] = matrix[i].reversed.toList();
-    }
-  }
+      int left = 0;
+      int right = n - 1;
 
-  /**
-   * Solution 2
-   * 
-   * Rotate Layer by Layer
-   * - Time Complexity: O(N²)
-   * - Space Complexity: O(1)
-   */
-  void solution2(List<List<int>> matrix) {
-    int n = matrix.length;
-    int left = 0, right = n - 1;
+      while (left < right) {
+        int temp = matrix[i][left];
+        matrix[i][left] = matrix[i][right];
+        matrix[i][right] = temp;
 
-    while (left < right) {
-      for (int i = 0; i < right - left; i++) {
-        int top = left, bottom = right;
-
-        // top-left -> temp
-        int topLeft = matrix[top][left + i];
-
-        // bottom-left -> top-left
-        matrix[top][left + i] = matrix[bottom - i][left];
-
-        // bottom-right -> bottom-left
-        matrix[bottom - i][left] = matrix[bottom][right - i];
-
-        // top-right -> bottom-right
-        matrix[bottom][right - i] = matrix[top + i][right];
-
-        // top-left -> top-right
-        matrix[top + i][right] = topLeft;
+        left++;
+        right--;
       }
-
-      // next inner layer
-      left++;
-      right--;
     }
   }
 }
