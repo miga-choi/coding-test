b42bf0d9bc1e70c539419ca1689bf1b877be2f91
miga-choi <miga.choi.1004@gmail.com>
2026-01-21 01:13:30 +0900

    leetcode: "58_length_of_last_word" add c solution

--- leetcode/algorithms/58_length_of_last_word/main.c
+++ leetcode/algorithms/58_length_of_last_word/main.c
@@ -1,5 +1,13 @@
 #include <string.h>
 
+/**
+ * Reverse Traversal
+ * 
+ * Complexities:
+ *   N - Length of `s`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
 int lengthOfLastWord(char* s) {
     int i = 0;
     while (s[i] != '\0') {
@@ -20,62 +28,27 @@ int lengthOfLastWord(char* s) {
 
 
 // Solution
-// Solution 1
-int solution1(char* s) {
-    int c = 0;
-    for (int i = strlen(s) - 1; i >= 0; i--) {
-        if (s[i] == ' ') {
-            c++;
-        } else {
-            break;
-        }
-    }
-    int c1 = 0;
-    for (int i = strlen(s) - c - 1; i >= 0; i--) {
-        if (s[i] != ' ') {
-            c1++;
-        } else {
-            break;
-        }
-    }
-    return c1;
-}
+/**
+ * Reverse Traversal
+ * 
+ * Complexities:
+ *   N - Length of `s`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+int solution(char* s) {
+    int len = strlen(s);
+    int length = 0;
+    int i = len - 1;
 
-// Solution 2
-int solution2(char* s) {
-    int lastSize = 0;
-    int i = strlen(s) - 1;
-
-    if (s[i] == ' ') {
-        while (i >= 0 && s[i] == ' ') {
-            i--;
-        }
-    }
-
-    while (i >= 0) {
-        if (s[i] == ' ') {
-            break;
-        }
-        if ((s[i] >= 97 && s[i] <= 122) || (s[i] >= 65 && s[i] <= 90)) {
-            lastSize++;
-        }
+    while (i >= 0 && s[i] == ' ') {
         i--;
     }
 
-    return lastSize;
-}
-
-// Solution 3
-int solution3(char* s) {
-    int len = 0;
-    int i = strlen(s) - 1;
-
-    while (s[i] == ' ' && i >= 0) {
+    while (i >= 0 && s[i] != ' ') {
+        length++;
         i--;
     }
-    while (i >= 0 && s[i--] != ' ') {
-        len++;
-    }
 
-    return len;
+    return length;
 }
