133595278eb505f8d0462aff273bb10ec93f48a5
miga-choi <miga.choi.1004@gmail.com>
2026-01-23 01:05:18 +0900

    leetcode: "58_length_of_last_word" add dart solution

--- leetcode/algorithms/58_length_of_last_word/main.dart
+++ leetcode/algorithms/58_length_of_last_word/main.dart
@@ -1,38 +1,63 @@
 class LengthOfLastWord {
+  /**
+   * Reverse Traversal
+   * 
+   * Complexities:
+   *   N - Length of `s`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
   int lengthOfLastWord(String s) {
     List<String> sArray = s.trim().split(" ");
+
     for (var i = sArray.length - 1; i >= 0; i--) {
       if (sArray[i].trim().length > 0) {
         return sArray[i].length;
       }
     }
+
     return 0;
   }
 
 
   // Solution
-  int solution(String s) {
-    // Initialize the index variable to the last index of the string
-    int index = s.length - 1;
-    // Loop through any trailing spaces at the end of the string
-    while (s[index] == " ") {
-      index--;
+  /**
+   * Solution 1
+   * 
+   * Reverse Traversal
+   * 
+   * Complexities:
+   *   N - Length of `s`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
+  int solution1(String s) {
+    int length = 0;
+    int i = s.length - 1;
+
+    while (i >= 0 && s[i] == ' ') {
+      i--;
     }
-    // Initialize the count variable to 0
-    int count = 0;
-    // Loop through the last word in the string
-    while (s[index] != " ") {
-      // Increment the count variable for each character in the last word
-      count++;
-      // Check if the previous character was a space or if we have reached the beginning of the string
-      if (s[index] == " " || index == 0) {
-        // Break out of the loop if the previous character was a space or if we have reached the beginning of the string
-        break;
-      }
-      // Move the index to the previous character in the string
-      index--;
+
+    while (i >= 0 && s[i] != ' ') {
+      length++;
+      i--;
     }
-    // Return the count variable which contains the length of the last word in the string
-    return count;
+
+    return length;
+  }
+
+  /**
+   * Solution 2
+   * 
+   * Built-in function
+   * 
+   * Complexities:
+   *   N - Length of `s`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(N)
+   */
+  int solution2(String s) {
+    return s.trim().split(RegExp(r'\s+')).last.length;
   }
 }
