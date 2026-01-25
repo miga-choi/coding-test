76f45843b05cd008a3eda386e0c9893a24820827
miga-choi <miga.choi.1004@gmail.com>
2026-01-26 01:07:57 +0900

    leetcode: "66_plus_one" add python solution

--- leetcode/algorithms/66_plus_one/main.py
+++ leetcode/algorithms/66_plus_one/main.py
@@ -3,8 +3,10 @@ from typing import List
 
 class PlusOne:
     """
-    - Time Complexity: O(N)
-    - Space Complexity: O(1)
+    # Complexities:
+    #   N - Length of `digits`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(N)
     """
     def plusOne(self, digits: List[int]) -> List[int]:
         for i in range(len(digits) - 1, -1, -1):
@@ -20,9 +22,12 @@ class PlusOne:
 
     # Solution
     """
-    Solution 1
-    - Time Complexity: O(N)
-    - Space Complexity: O(1)
+    # Solution 1
+    #
+    # Complexities:
+    #   N - Length of `digits`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
     """
     def solution1(self, digits: List[int]) -> List[int]:
         n = len(digits)
@@ -37,13 +42,16 @@ class PlusOne:
         return [1] + digits
 
     """
-    Solution 1
-    - Time Complexity: O(N)
-    - Space Complexity: O(N)
+    # Solution 2
+    #
+    # Complexities:
+    #   N - Length of `digits`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(N)
     """
     def solution2(self, digits: List[int]) -> List[int]:
         s = "".join(map(str, digits))
-
+        
         num_plus_one_str = str(int(s) + 1)
-
+        
         return [int(digit) for digit in num_plus_one_str]
