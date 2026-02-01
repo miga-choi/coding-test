c0b38c04542a67057636db63fce4f712311b1038
miga-choi <miga.choi.1004@gmail.com>
2026-02-01 22:30:36 +0900

    leetcode: "69_sqrt_x" add dart solution

--- leetcode/algorithms/69_sqrt_x/main.dart
+++ leetcode/algorithms/69_sqrt_x/main.dart
@@ -1,20 +1,76 @@
 import 'dart:math';
 
 class SqrtX {
+  /**
+   * Built-in function
+   * 
+   * Complexities:
+   *   N - Value of `x`
+   *   - Time Complexity: O(logᴺ)
+   *   - Space Complexity: O(1)
+   */
   int mySqrt(int x) {
     return sqrt(x).floor();
   }
 
 
   // Solution
-  int solution(int x) {
-    int base = 2;
-    if (x <= 1) {
-      return x == 1 ? 1 : 0;
+  /**
+   * Solution 1
+   * 
+   * Binary Search
+   * 
+   * Complexities:
+   *   N - Value of `x`
+   *   - Time Complexity: O(logᴺ)
+   *   - Space Complexity: O(1)
+   */
+  int solution1(int x) {
+    if (x == 0) {
+      return 0;
     }
-    while (base * base <= x) {
-      base++;
+
+    int low = 1;
+    int high = x;
+
+    while (low <= high) {
+      int mid = low + (high - low) ~/ 2;
+
+      int square = mid * mid;
+
+      if (square == x) {
+        return mid;
+      } else if (square < x) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
     }
-    return base - 1;
+
+    return high;
+  }
+
+  /**
+   * Solution 2
+   * 
+   * Newton-Raphson Method
+   * 
+   * Complexities:
+   *   N - Value of `x`
+   *   - Time Complexity: O(logᴺ)
+   *   - Space Complexity: O(1)
+   */
+  int solution2(int x) {
+    if (x == 0) {
+      return 0;
+    }
+
+    double r = x.toDouble();
+
+    while ((r * r - x).abs() > 0.1) {
+      r = (r + x / r) / 2;
+    }
+
+    return r.toInt();
   }
 }
