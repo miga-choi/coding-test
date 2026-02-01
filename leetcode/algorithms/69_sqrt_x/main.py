4ed9ef0f9d5644d08d854f04f6d4660ce80a7579
miga-choi <miga.choi.1004@gmail.com>
2026-02-01 22:30:10 +0900

    leetcode: "69_sqrt_x" add python solution

--- leetcode/algorithms/69_sqrt_x/main.py
+++ leetcode/algorithms/69_sqrt_x/main.py
@@ -1,4 +1,10 @@
 class SqrtX:
+    """
+    # Complexities:
+    #   N - Value of `x`
+    #   - Time Complexity: O(logᴺ)
+    #   - Space Complexity: O(1)
+    """
     def mySqrt(self, x: int) -> int:
         for i in range(x + 1):
             if i * i <= x and (i + 1) * (i + 1) > x:
@@ -6,29 +12,52 @@ class SqrtX:
 
 
     # Solution
-    # Solution 1:
+    """
+    # Solution 1
+    #
+    # Binary Search
+    #
+    # Complexities:
+    #   N - Value of `x`
+    #   - Time Complexity: O(logᴺ)
+    #   - Space Complexity: O(1)
+    """
     def solution1(self, x: int) -> int:
-        if x == 0:
-            return 0
-        first, last = 1, x
-        while first <= last:
-            mid = first + (last - first) // 2
-            if mid == x // mid:
+        if x < 2:
+            return x
+        
+        left, right = 1, x
+        
+        while left <= right:
+            mid = (left + right) // 2
+            square = mid * mid
+            
+            if square == x:
                 return mid
-            elif mid > x // mid:
-                last = mid - 1
+            elif square < x:
+                left = mid + 1
             else:
-                first = mid + 1
-        return last
+                right = mid - 1
+        
+        return right
 
-    # Solution 2:
+    """
+    # Solution 2
+    #
+    # Newton-Raphson Method
+    #
+    # Complexities:
+    #   N - Value of `x`
+    #   - Time Complexity: O(logᴺ)
+    #   - Space Complexity: O(1)
+    """
     def solution2(self, x: int) -> int:
-        l, r = 0, x
-        while l <= r:
-            mid = l + (r - l) // 2
-            if mid * mid <= x < (mid + 1) * (mid + 1):
-                return mid
-            elif x < mid * mid:
-                r = mid - 1
-            else:
-                l = mid + 1
+        if x < 2:
+            return x
+        
+        r = x
+        
+        while r * r > x:
+            r = (r + x // r) // 2
+            
+        return r
