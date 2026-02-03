9c0fee2b1e9a4fc89a654b22001276963c4737ae
miga-choi <miga.choi.1004@gmail.com>
2026-02-04 01:41:16 +0900

    leetcode: "70_climbing_stairs" add python solution

--- leetcode/algorithms/70_climbing_stairs/main.py
+++ leetcode/algorithms/70_climbing_stairs/main.py
@@ -3,9 +3,12 @@ from typing import List
 
 class ClimbingStairs:
     """
-    # DP (Dynamic Programming)
-    # - Time Complexity: O(N)
-    # - Space Complexity: O(N)
+    # DP (Dynamic Programming) with Array
+    #
+    # Complexities:
+    #   N - Value of `n`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(N)
     """
     def climbStairs(self, n: int) -> int:
         if n < 3:
@@ -26,75 +29,61 @@ class ClimbingStairs:
     """
     # Solution 1
     #
-    # Recursion (Time Limit Exceeded)
-    # - Time Complexity: O(2ᴺ)
-    # - Space Complexity: O(1)
+    # Recursion
+    #
+    # Complexities:
+    #   N - Value of `n`
+    #   - Time Complexity: O(2ᴺ)
+    #   - Space Complexity: O(1)
     """
     def solution1(self, n: int) -> int:
         if n <= 2:
             return n
-        return self.solution1(n - 1) + self.solution1(n - 2)
+
+        return self.solution1(n-1) + self.solution1(n-2)
 
     """
     # Solution 2
     #
-    # DP (Dynamic Programming): Top-down with Memoization
-    # - Time Complexity: O(N)
-    # - Space Complexity: O(N)
+    # DP (Dynamic Programming)
+    #
+    # Complexities:
+    #   N - Value of `n`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
     """
     def solution2(self, n: int) -> int:
-        memo = {}
+        if n <= 2:
+            return n
 
-        def solve(k: int) -> int:
-            if k in memo:
-                return memo[k]
+        prev2, prev1 = 1, 2
 
-            if k <= 2:
-                return k
+        for _ in range(3, n + 1):
+            current = prev1 + prev2
 
-            memo[k] = solve(k - 1) + solve(k - 2)
+            prev2 = prev1
+            prev1 = current
 
-            return memo[k]
-
-        return solve(n)
+        return prev1
 
     """
     # Solution 3
     #
-    # DP (Dynamic Programming): Bottom-up with Tabulation
-    # - Time Complexity: O(N)
-    # - Space Complexity: O(N)
+    # DP (Dynamic Programming) with Array
+    #
+    # Complexities:
+    #   N - Value of `n`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(N)
     """
     def solution3(self, n: int) -> int:
         if n <= 2:
             return n
 
         dp = [0] * (n + 1)
-        dp[1] = 1
-        dp[2] = 2
+        dp[1], dp[2] = 1, 2
 
         for i in range(3, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
+            dp[i] = dp[i-1] + dp[i-2]
 
         return dp[n]
-
-    """
-    # Solution 4
-    #
-    # DP (Dynamic Programming): Space Optimization
-    # - Time Complexity: O(N)
-    # - Space Complexity: O(1)
-    """
-    def solution4(self, n: int) -> int:
-        if n <= 2:
-            return n
-            
-        two_steps_back, one_step_back = 1, 2
-        
-        for _ in range(3, n + 1):
-            current_step = one_step_back + two_steps_back
-
-            two_steps_back = one_step_back
-            one_step_back = current_step
-            
-        return one_step_back
