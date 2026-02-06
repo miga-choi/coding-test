da53887477f190d70e586847d6e5c9edbc8a912a
miga-choi <miga.choi.1004@gmail.com>
2026-02-07 01:10:47 +0900

    leetcode: "83_remove_duplicates_from_sorted_list" add python solution

--- leetcode/algorithms/83_remove_duplicates_from_sorted_list/main.py
+++ leetcode/algorithms/83_remove_duplicates_from_sorted_list/main.py
@@ -8,11 +8,14 @@ class ListNode:
 
 
 class RemoveDuplicatesFromSortedList:
-    ##
+    """
     # Iteration
-    # - Time Complexity: O(N)
-    # - Space Complexity: O(1)
     #
+    # Complexities:
+    #   N - Size of `head`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         curr_head: Optional[ListNode] = head
 
@@ -26,41 +29,44 @@ class RemoveDuplicatesFromSortedList:
 
 
     # Solution
-    ##
+    """
     # Solution 1
     #
     # Iteration
-    # - Time Complexity: O(N)
-    # - Space Complexity: O(1)
     #
+    # Complexities:
+    #   N - Size of `head`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
     def solution1(self, head: ListNode) -> ListNode:
         if not head:
             return None
-
+        
         current = head
-
+        
         while current and current.next:
             if current.val == current.next.val:
                 current.next = current.next.next
             else:
                 current = current.next
-
+                
         return head
 
-    ##
+    """
     # Solution 2
     #
     # Recursion
-    # - Time Complexity: O(N)
-    # - Space Complexity: O(N)
     #
+    # Complexities:
+    #   N - Size of `head`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(N)
+    """
     def solution2(self, head: ListNode) -> ListNode:
-        if not head or not head.next:
+        if head == None or head.next == None:
             return head
 
         head.next = self.solution2(head.next)
 
-        if head.val == head.next.val:
-            return head.next
-        else:
-            return head
+        return head.next if head.val == head.next.val else head
