dc5dfdf6f01ec14688daa24d28907134dbf5d5a0
miga-choi <miga.choi.1004@gmail.com>
2026-02-10 06:57:47 +0900

    leetcode: "88_merge_sorted_array" add python solution

--- leetcode/algorithms/88_merge_sorted_array/main.py
+++ leetcode/algorithms/88_merge_sorted_array/main.py
@@ -2,11 +2,15 @@ from typing import List
 
 
 class MergeSortedArray:
-    ##
-    # Three Pointers: from end to begin
-    # - Time Complexity: O(M+N)
-    # - Space Complexity: O(1)
-    #
+    """
+    # Three Pointers from the End
+    # 
+    # Complexities:
+    #   M - Size of `nums1`
+    #   N - Size of `nums2`
+    #   - Time Complexity: O(M + N)
+    #   - Time Complexity: O(1)
+    """
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
@@ -26,64 +30,30 @@ class MergeSortedArray:
 
 
     # Solution
-    # Solution 1: STL
-    def solution1(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        for j in range(n):
-            nums1[m + j] = nums2[j]
-        nums1.sort()
-
-    # Solution 2: Two Pointer
-    def solution2(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        i = m - 1
-        j = n - 1
-        k = m + n - 1
-
-        while j >= 0:
-            if i >= 0 and nums1[i] > nums2[j]:
-                nums1[k] = nums1[i]
-                i -= 1
+    """
+    # Three Pointers from the End
+    # 
+    # Complexities:
+    #   M - Size of `nums1`
+    #   N - Size of `nums2`
+    #   - Time Complexity: O(M + N)
+    #   - Time Complexity: O(1)
+    """
+    def solution(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p1 = m - 1
+        p2 = n - 1
+        p = m + n - 1
+        
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
             else:
-                nums1[k] = nums2[j]
-                j -= 1
-            k -= 1
-
-    # Solution 3:
-    def solution3(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        while m > 0 and n > 0:
-            if nums1[m - 1] >= nums2[n - 1]:
-                nums1[m + n - 1] = nums1[m - 1]
-                m -= 1
-            else:
-                nums1[m + n - 1] = nums2[n - 1]
-                n -= 1
-        if n > 0:
-            nums1[:n] = nums2[:n]
-
-    # Solution 3:
-    def solution3(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        # Initialize nums1's index
-        i = m - 1
-        # Initialize nums2's index
-        j = n - 1
-        # Initialize a variable k to store the last index of the 1st array...
-        k = m + n - 1
-        while j >= 0:
-            if i >= 0 and nums1[i] > nums2[j]:
-                nums1[k] = nums1[i]
-                k -= 1
-                i -= 1
-            else:
-                nums1[k] = nums2[j]
-                k -= 1
-                j -= 1
-
-    # Solution 4:
-    def solution4(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        idx = 0
-        for i in range(len(nums1)):
-            if idx >= n:
-                break
-            if nums1[i] == 0:
-                nums1[i] = nums2[idx]
-                idx += 1
-        nums1.sort()
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
+        
+        nums1[:p2 + 1] = nums2[:p2 + 1]
