534a355e51a468ec712d9016404ad12d7e04e3bb
miga-choi <miga.choi.1004@gmail.com>
2026-02-21 21:54:14 +0900

    leetcode: "101_symmetric_tree" add javascript solution

--- leetcode/algorithms/101_symmetric_tree/main.js
+++ leetcode/algorithms/101_symmetric_tree/main.js
@@ -5,6 +5,15 @@ function TreeNode(val, left, right) {
 }
 
 /**
+ * Recursion - DFS
+ *
+ * Complexities:
+ *   N - The number of nodes in `root`
+ *   H - The heights of `root`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(H)
+ */
+/**
  * @param {TreeNode} leftNode
  * @param {TreeNode} rightNode
  */
@@ -36,135 +45,78 @@ var isSymmetric = function (root) {
 
 
 // Solution
-// Solution 1
+/**
+ * Solution 1
+ *
+ * Recursion - DFS
+ *
+ * Complexities:
+ *   N - The number of nodes in `root`
+ *   H - The heights of `root`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(H)
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
 var solution1 = function (root) {
-  // Special case...
   if (!root) {
     return true;
   }
-  // Return the function recursively...
-  return solution1_isSame(root.left, root.right);
+
+  function isMirror(t1, t2) {
+    if (!t1 && !t2) {
+      return true;
+    }
+
+    if (!t1 || !t2 || t1.val !== t2.val) {
+      return false;
+    }
+
+    return isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+  }
+
+  return isMirror(root.left, root.right);
 };
 
-// A tree is called symmetric if the left subtree must be a mirror reflection of the right subtree...
-var solution1_isSame = function (leftroot, rightroot) {
-  // If both root nodes are null pointers, return true...
-  // If exactly one of them is a null node, return false...
-  // If root nodes haven't same value, return false...
-  if (
-    (!leftroot && rightroot) ||
-    (leftroot && !rightroot) ||
-    (leftroot && rightroot && leftroot.val !== rightroot.val)
-  )
-    return false;
-  // Return true if the values of root nodes are same and left as well as right subtrees are symmetric...
-  if (leftroot && rightroot)
-    return (
-      isSame(leftroot.left, rightroot.right) &&
-      isSame(leftroot.right, rightroot.left)
-    );
-  return true;
-};
-
-// Solution 2: Recursive - Preorder traversal
+/**
+ * Solution 2
+ *
+ * Iteration
+ *
+ * Complexities:
+ *   N - The number of nodes in `root`
+ *   H - The heights of `root`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(H)
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
 var solution2 = function (root) {
   if (!root) {
-    // Sanity check
     return true;
   }
 
-  // Check if tree s & t are mirroring each other
-  function isMirror(s, t) {
-    if (!s && !t) {
-      return true; // Both nodes are null, ok
-    }
-    if (!s || !t || s.val !== t.val) {
-      return false; // Found a mismatch
-    }
-    // Compare the left subtree of `s` with the right subtree of `t`
-    // and the right subtree of `s` with the left subtree of `t`
-    return isMirror(s.left, t.right) && isMirror(s.right, t.left);
-  }
+  const stack = [root.left, root.right];
 
-  return isMirror(root.left, root.right);
-};
+  while (stack.length > 0) {
+    const t1 = stack.pop();
+    const t2 = stack.pop();
 
-// Solution 3: Iterative - Traverse using stack
-var solution3 = function (root) {
-  if (!root) {
-    // Sanity check
-    return true;
-  }
-
-  // Check if tree s & t are mirroring each other
-  function isMirror(p, q) {
-    // Create two stacks
-    var s1 = [p], s2 = [q];
-
-    // Perform preorder traversal
-    while (s1.length > 0 || s2.length > 0) {
-      var n1 = s1.pop(), n2 = s2.pop();
-
-      // Two null nodes, let's continue
-      if (!n1 && !n2) {
-        continue;
-      }
-
-      // Return false as long as there is a mismatch
-      if (!n1 || !n2 || n1.val !== n2.val) {
-        return false;
-      }
-
-      // Scan tree s from left to right
-      // and scan tree t from right to left
-      s1.push(n1.left);
-      s1.push(n1.right);
-      s2.push(n2.right);
-      s2.push(n2.left);
+    if (!t1 && !t2) {
+      continue;
     }
 
-    return true;
-  }
-
-  return isMirror(root.left, root.right);
-};
-
-// Solution 4: BFS - Traverse both subtrees in level order
-var solution4 = function (root) {
-  if (!root) {
-    // Sanity check
-    return true;
-  }
-
-  // Check if tree s & t are mirroring each other
-  function isMirror(s, t) {
-    var q1 = [s], q2 = [t];
-
-    // Perform breadth-first search
-    while (q1.length > 0 || q2.length > 0) {
-      // Dequeue
-      var n1 = q1.shift(), n2 = q2.shift();
-
-      // Two null nodes, let's continue
-      if (!n1 && !n2) {
-        continue;
-      }
-
-      // Return false as long as there is a mismatch
-      if (!n1 || !n2 || n1.val !== n2.val) {
-        return false;
-      }
-
-      // Scan tree s from left to right
-      // and scan tree t from right to left
-      q1.push(n1.left);
-      q1.push(n1.right);
-      q2.push(n2.right);
-      q2.push(n2.left);
+    if (!t1 || !t2 || t1.val !== t2.val) {
+      return false;
     }
 
-    return true;
+    stack.push(t1.left, t2.right);
+    stack.push(t1.right, t2.left);
   }
 
-  return isMirror(root.left, root.right);
+  return true;
 };
