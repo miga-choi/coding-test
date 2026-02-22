ceb4eb365a06c1068b688164c56a648407d3712e
miga-choi <miga.choi.1004@gmail.com>
2026-02-22 15:38:30 +0900

    leetcode: "101_symmetric_tree" add python solution

--- leetcode/algorithms/101_symmetric_tree/main.py
+++ leetcode/algorithms/101_symmetric_tree/main.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import Optional
 
 
@@ -9,96 +10,93 @@ class TreeNode:
 
 
 class SymmetricTree:
+    """
+    # Recursion - DFS
+    #
+    # Complexities:
+    #   N - The number of nodes in `root`
+    #   H - The heights of `root`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(H)
+    """
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         if root is None:
             return True
         return self.checkNode(root.left, root.right)
 
-    def checkNode(
-        self, leftNode: Optional[TreeNode], rightNode: Optional[TreeNode]
-    ) -> bool:
+    def checkNode(self, leftNode: Optional[TreeNode], rightNode: Optional[TreeNode]) -> bool:
         if leftNode is None and rightNode is None:
             return True
-        if (leftNode is None and rightNode is not None) or (
-            leftNode is not None and rightNode is None
-        ):
+
+        if (leftNode is None and rightNode is not None) or (leftNode is not None and rightNode is None):
             return False
+
         if leftNode is not None and rightNode is not None:
             if leftNode.val != rightNode.val:
                 return False
             else:
-                return self.checkNode(
-                    leftNode.left, rightNode.right
-                ) and self.checkNode(leftNode.right, rightNode.left)
+                return self.checkNode(leftNode.left, rightNode.right) and self.checkNode(leftNode.right, rightNode.left)
+
         return False
 
 
     # Solution
+    """
     # Solution 1
+    #
+    # Recursion - DFS
+    #
+    # Complexities:
+    #   N - The number of nodes in `root`
+    #   H - The heights of `root`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(H)
+    """
     def solution1(self, root: Optional[TreeNode]) -> bool:
-        # Special case...
         if not root:
             return True
-        # Return the function recursively...
-        return self.isSame(root.left, root.right)
 
-    # A tree is called symmetric if the left subtree must be a mirror reflection of the right subtree...
-    def isSame(self, leftroot, rightroot):
-        # If both root nodes are null pointers, return true...
-        if leftroot == None and rightroot == None:
+        return self.isMirror(root.left, root.right)
+
+    def isMirror(self, t1: Optional[TreeNode], t2: Optional[TreeNode]) -> bool:
+        if not t1 and not t2:
             return True
-        # If exactly one of them is a null node, return false...
-        if leftroot == None or rightroot == None:
-            return False
-        # If root nodes haven't same value, return false...
-        if leftroot.val != rightroot.val:
-            return False
-        # Return true if the values of root nodes are same and left as well as right subtrees are symmetric...
-        return self.isSame(leftroot.left, rightroot.right) and self.isSame(
-            leftroot.right, rightroot.left
-        )
 
-    # Solution 2: Recursive
+        if not t1 or not t2:
+            return False
+
+        if t1.val != t2.val:
+            return False
+
+        return self.isMirror(t1.left, t2.right) and self.isMirror(t1.right, t2.left)
+
+    """
+    # Solution 2
+    #
+    # Iteration
+    #
+    # Complexities:
+    #   N - The number of nodes in `root`
+    #   H - The heights of `root`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(H)
+    """
     def solution2(self, root: Optional[TreeNode]) -> bool:
-        if root is None:
-            return True
-        else:
-            return self.isMirror(root.left, root.right)
-
-    def isMirror(self, left, right):
-        if left is None and right is None:
+        if not root:
             return True
 
-        if left is None or right is None:
-            return False
+        queue = deque([(root.left, root.right)])
 
-        if left.val == right.val:
-            outPair = self.isMirror(left.left, right.right)
-            inPiar = self.isMirror(left.right, right.left)
-            return outPair and inPiar
-        else:
-            return False
+        while queue:
+            t1, t2 = queue.popleft()
 
-    # Solution 3: Iterative
-    def solution3(self, root: Optional[TreeNode]) -> bool:
-        if root is None:
-            return True
-
-        stack = [[root.left, root.right]]
-
-        while len(stack) > 0:
-            pair = stack.pop(0)
-            left = pair[0]
-            right = pair[1]
-
-            if left is None and right is None:
+            if not t1 and not t2:
                 continue
-            if left is None or right is None:
+
+            if not t1 or not t2 or t1.val != t2.val:
                 return False
-            if left.val == right.val:
-                stack.insert(0, [left.left, right.right])
-                stack.insert(0, [left.right, right.left])
-            else:
-                return False
+
+            queue.append((t1.left, t2.right))
+            queue.append((t1.right, t2.left))
 
         return True
