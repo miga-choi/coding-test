faf0aac913bbbe9d5aa5e81af45519cca4ef2b88
miga-choi <miga.choi.1004@gmail.com>
2026-02-22 15:39:12 +0900

    leetcode: "101_symmetric_tree" add dart solution

--- leetcode/algorithms/101_symmetric_tree/main.dart
+++ leetcode/algorithms/101_symmetric_tree/main.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 class TreeNode {
   int val;
   TreeNode? left;
@@ -6,6 +8,15 @@ class TreeNode {
 }
 
 class SymmetricTree {
+  /**
+   * Recursion - DFS
+   *
+   * Complexities:
+   *   N - The number of nodes in `root`
+   *   H - The heights of `root`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(H)
+   */
   bool isSymmetric(TreeNode? root) {
     if (root == null) {
       return true;
@@ -30,17 +41,81 @@ class SymmetricTree {
 
 
   // Solution
-  bool solution(TreeNode? root) => isMirror(root, root);
+  /**
+   * Solution 1
+   *
+   * Recursion - DFS
+   *
+   * Complexities:
+   *   N - The number of nodes in `root`
+   *   H - The heights of `root`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(H)
+   */
+  bool solution1(TreeNode? root) {
+    if (root == null) {
+      return true;
+    }
 
-  bool isMirror(TreeNode? t1, TreeNode? t2) {
+    return _isMirror(root.left, root.right);
+  }
+
+  bool _isMirror(TreeNode? t1, TreeNode? t2) {
     if (t1 == null && t2 == null) {
       return true;
     }
+
     if (t1 == null || t2 == null) {
       return false;
     }
-    return t1.val == t2.val &&
-        isMirror(t1.left, t2.right) &&
-        isMirror(t1.right, t2.left);
+
+    if (t1.val != t2.val) {
+      return false;
+    }
+
+    return _isMirror(t1.left, t2.right) && _isMirror(t1.right, t2.left);
+  }
+
+  /**
+   * Solution 2
+   *
+   * Iteration
+   *
+   * Complexities:
+   *   N - The number of nodes in `root`
+   *   H - The heights of `root`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(H)
+   */
+  bool solution2(TreeNode? root) {
+    if (root == null) {
+      return true;
+    }
+
+    Queue<TreeNode?> queue = Queue();
+
+    queue.add(root.left);
+    queue.add(root.right);
+
+    while (queue.isNotEmpty) {
+      TreeNode? t1 = queue.removeFirst();
+      TreeNode? t2 = queue.removeFirst();
+
+      if (t1 == null && t2 == null) {
+        continue;
+      }
+
+      if (t1 == null || t2 == null || t1.val != t2.val) {
+        return false;
+      }
+
+      queue.add(t1.left);
+      queue.add(t2.right);
+
+      queue.add(t1.right);
+      queue.add(t2.left);
+    }
+
+    return true;
   }
 }
