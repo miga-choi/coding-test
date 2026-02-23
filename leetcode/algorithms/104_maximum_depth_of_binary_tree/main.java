e9a7612b816703007e82153c82cef0cf80d22cf1
miga-choi <miga.choi.1004@gmail.com>
2026-02-24 01:11:28 +0900

    leetcode: "104_maximum_depth_of_binary_tree" add java solution

--- leetcode/algorithms/104_maximum_depth_of_binary_tree/main.java
+++ leetcode/algorithms/104_maximum_depth_of_binary_tree/main.java
@@ -1,6 +1,5 @@
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
 
 class MaximumDepthOfBinaryTree {
   public class TreeNode {
@@ -22,13 +21,14 @@ class MaximumDepthOfBinaryTree {
     }
   }
 
-  private record NodeDepthRecord(TreeNode node, int depth) {
-  }
-
   /**
-   * Recursion: DFS
-   * - Time Complexity: O(N)
-   * - Space Complexity: O(H)
+   * Recursion - DFS
+   *
+   * Complexities:
+   *   N - The number of nodes in `root`
+   *   H - The heights of `root`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(H)
    */
   public int maxDepth(TreeNode root) {
     if (root == null) {
@@ -45,28 +45,36 @@ class MaximumDepthOfBinaryTree {
   // Solution
   /**
    * Solution 1
-   * 
-   * Recursion: DFS
-   * - Time Complexity: O(N)
-   * - Space Complexity: O(H)
+   *
+   * Recursion - DFS
+   *
+   * Complexities:
+   *   N - The number of nodes in `root`
+   *   H - The heights of `root`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(H)
    */
   public int solution1(TreeNode root) {
     if (root == null) {
       return 0;
     }
 
-    int leftDepth = maxDepth(root.left);
-    int rightDepth = maxDepth(root.right);
+    int leftDepth = solution1(root.left);
+    int rightDepth = solution1(root.right);
 
     return Math.max(leftDepth, rightDepth) + 1;
   }
 
   /**
    * Solution 2
-   * 
-   * Iteration: BFS (Queue)
-   * - Time Complexity: O(N)
-   * - Space Complexity: O(W)
+   *
+   * Iteration - BFS
+   *
+   * Complexities:
+   *   N - The number of nodes in `root`
+   *   H - The heights of `root`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(H)
    */
   public int solution2(TreeNode root) {
     if (root == null) {
@@ -79,55 +87,22 @@ class MaximumDepthOfBinaryTree {
 
     while (!queue.isEmpty()) {
       int levelSize = queue.size();
-      depth++;
 
       for (int i = 0; i < levelSize; i++) {
-        TreeNode currentNode = queue.poll();
+        TreeNode node = queue.poll();
 
-        if (currentNode.left != null) {
-          queue.offer(currentNode.left);
+        if (node.left != null) {
+          queue.offer(node.left);
         }
-        if (currentNode.right != null) {
-          queue.offer(currentNode.right);
+
+        if (node.right != null) {
+          queue.offer(node.right);
         }
       }
+
+      depth++;
     }
 
     return depth;
-  }
-
-  /**
-   * Solution 3
-   * 
-   * Iteration: DFS (Stack)
-   * - Time Complexity: O(N)
-   * - Space Complexity: O(H)
-   */
-  public int solution3(TreeNode root) {
-    if (root == null) {
-      return 0;
-    }
-
-    Stack<NodeDepthRecord> stack = new Stack<>();
-    stack.push(new NodeDepthRecord(root, 1));
-
-    int maxDepth = 0;
-
-    while (!stack.isEmpty()) {
-      NodeDepthRecord currentPair = stack.pop();
-      TreeNode currentNode = currentPair.node;
-      int currentDepth = currentPair.depth;
-
-      maxDepth = Math.max(maxDepth, currentDepth);
-
-      if (currentNode.right != null) {
-        stack.push(new NodeDepthRecord(currentNode.right, currentDepth + 1));
-      }
-      if (currentNode.left != null) {
-        stack.push(new NodeDepthRecord(currentNode.left, currentDepth + 1));
-      }
-    }
-
-    return maxDepth;
   }
 }
