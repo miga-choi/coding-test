e41504bd6f0adf5a6d0fc52d65fd5cb3532b8f3d
miga-choi <miga.choi.1004@gmail.com>
2026-03-13 06:52:32 +0900

    leetcode: "46_permutations" add c++ solution

---
+++ leetcode/algorithms/46_permutations/main.cpp
@@ -0,0 +1,91 @@
+#include <vector>
+using namespace std;
+
+class Permutations {
+public:
+    /**
+     * Backtracking
+     * 
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(N * N!)
+     *   - Space Complexity: O(N)
+     */
+    void backtrack(vector<int>& nums, int start, vector<vector<int>>& result) {
+        if (start == nums.size()) {
+            result.push_back(nums);
+            return;
+        }
+
+        for (size_t i = start; i < nums.size(); i++) {
+            swap(nums[start], nums[i]);
+
+            backtrack(nums, start + 1, result);
+
+            swap(nums[start], nums[i]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+
+        backtrack(nums, 0, result);
+
+        return result;
+    }
+
+
+    // Solution
+    /**
+     * Solution 1
+     * 
+     * Backtracking
+     * 
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(N * N!)
+     *   - Space Complexity: O(N)
+     */
+    vector<vector<int>> solution1(vector<int>& nums) {
+        vector<vector<int>> result;
+        backtrack(nums, 0, result);
+        return result;
+    }
+
+    void backtrack(vector<int>& nums, int start, vector<vector<int>>& result) {
+        if (start == nums.size()) {
+            result.push_back(nums);
+            return;
+        }
+
+        for (int i = start; i < nums.size(); ++i) {
+            swap(nums[start], nums[i]);
+
+            backtrack(nums, start + 1, result);
+
+            swap(nums[start], nums[i]);
+        }
+    }
+
+    /**
+     * Solution 2
+     * 
+     * C++ STL - `next_permutation`
+     * 
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(N * N!)
+     *   - Space Complexity: O(N)
+     */
+    vector<vector<int>> solution2(vector<int>& nums) {
+        vector<vector<int>> result;
+        
+        sort(nums.begin(), nums.end());
+
+        do {
+            result.push_back(nums);
+        } while (next_permutation(nums.begin(), nums.end()));
+
+        return result;
+    }
+};
