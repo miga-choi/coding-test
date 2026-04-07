9009e3f40d4693ba5976ce09f3b5361ca6b0b21f
miga-choi <miga.choi.1004@gmail.com>
2026-04-08 07:18:17 +0900

    leetcode: "108_convert_sorted_array_to_binary_search_tree" add dart solution

--- leetcode/algorithms/108_convert_sorted_array_to_binary_search_tree/main.dart
+++ leetcode/algorithms/108_convert_sorted_array_to_binary_search_tree/main.dart
@@ -6,6 +6,14 @@ class TreeNode {
 }
 
 class ConvertSortedArrayToBinarySearchTree {
+  /**
+   * Divide-and-conquer
+   *
+   * Complexities:
+   *   N - Size of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(logᴺ)
+   */
   TreeNode? sortedArrayToBST(List<int> nums) {
     if (nums.length == 0) return null;
 
@@ -21,16 +29,33 @@ class ConvertSortedArrayToBinarySearchTree {
 
 
   // Solution
+  /**
+   * Divide-and-conquer
+   *
+   * Complexities:
+   *   N - Size of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(logᴺ)
+   */
   TreeNode? solution(List<int> nums) {
     if (nums.isEmpty) {
       return null;
     }
 
-    int mid = nums.length ~/ 2;
-    TreeNode root = TreeNode();
-    root.val = nums[mid];
-    root.left = solution(nums.sublist(0, mid));
-    root.right = solution(nums.sublist(mid + 1));
+    return _buildBST(nums, 0, nums.length - 1);
+  }
+
+  TreeNode? _buildBST(List<int> nums, int left, int right) {
+    if (left > right) {
+      return null;
+    }
+
+    int mid = left + (right - left) ~/ 2;
+
+    TreeNode root = TreeNode(nums[mid]);
+
+    root.left = _buildBST(nums, left, mid - 1);
+    root.right = _buildBST(nums, mid + 1, right);
 
     return root;
   }
