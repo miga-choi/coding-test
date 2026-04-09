d74e2fd796cfd372e763bcee919a8120023a4638
miga-choi <miga.choi.1004@gmail.com>
2026-04-10 01:07:11 +0900

    leetcode: "110_balanced_binary_tree" add java solution

--- leetcode/algorithms/108_convert_sorted_array_to_binary_search_tree/main.java
+++ leetcode/algorithms/108_convert_sorted_array_to_binary_search_tree/main.java
@@ -1,6 +1,4 @@
 import java.util.Arrays;
-import java.util.Deque;
-import java.util.LinkedList;
 
 class ConvertSortedArrayToBinarySearchTree {
   public class TreeNode {
@@ -22,6 +20,14 @@ class ConvertSortedArrayToBinarySearchTree {
     }
   }
 
+  /**
+   * Divide-and-conquer
+   *
+   * Complexities:
+   *   N - Size of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(logᴺ)
+   */
   public TreeNode sortedArrayToBST(int[] nums) {
     if (nums.length == 0) {
       return null;
@@ -38,105 +44,34 @@ class ConvertSortedArrayToBinarySearchTree {
 
 
   // Solution
-  // Solution 1: Recursion
-  public TreeNode solution1(int[] nums) {
-    if (nums.length == 0) {
-      return null;
-    }
-    TreeNode head = solution1_helper(nums, 0, nums.length - 1);
-    return head;
-  }
+  /**
+   * Divide-and-conquer
+   *
+   * Complexities:
+   *   N - Size of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(logᴺ)
+   */
+    public TreeNode solution(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
 
-  public TreeNode solution1_helper(int[] nums, int low, int high) {
-    if (low > high) {
-      // Done
-      return null;
-    }
-    int mid = (low + high) / 2;
-    TreeNode node = new TreeNode(nums[mid]);
-    node.left = solution1_helper(nums, low, mid - 1);
-    node.right = solution1_helper(nums, mid + 1, high);
-    return node;
-  }
-
-  // Solution 2: Recursion
-  public TreeNode solution2(int[] nums) {
-    return solution2_createBST(nums, 0, nums.length - 1);
-  }
-
-  private TreeNode solution2_createBST(int nums[], int l, int r) {
-    if (l > r) {
-      // Base Condition or Recursion Stoping Condition
-      return null;
+        return constructBST(nums, 0, nums.length - 1);
     }
 
-    // so basically in this question we have to convert
-    // sorted array to height balanced tree
-    // so if we directly create tree in given sorted order
-    // it will become linked list
-    // so we have to take middle element as head value
-    // such it will become height balanced tree
+    private TreeNode constructBST(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
 
-    // this is the formula to find mid value
-    int mid = l + (r - l) / 2;
+        int mid = left + (right - left) / 2;
 
-    // mid value or median
-    TreeNode root = new TreeNode(nums[mid]);
+        TreeNode node = new TreeNode(nums[mid]);
 
-    // assign the value for left of subtree that is l to mid -1 for given array
-    root.left = solution2_createBST(nums, l, mid - 1);
+        node.left = constructBST(nums, left, mid - 1);
+        node.right = constructBST(nums, mid + 1, right);
 
-    // assign the value for right go subtree that is mid+1 to r for given array
-    root.right = solution2_createBST(nums, mid + 1, r);
-
-    return root;
-  }
-
-  // Solution 3: Iteration
-  public TreeNode solution3(int[] nums) {
-    int len = nums.length;
-    if (len == 0) {
-      return null;
+        return node;
     }
-
-    // 0 as a placeholder
-    TreeNode head = new TreeNode(0);
-
-    Deque<TreeNode> nodeStack = new LinkedList<TreeNode>() {
-      {
-        push(head);
-      }
-    };
-    Deque<Integer> leftIndexStack = new LinkedList<Integer>() {
-      {
-        push(0);
-      }
-    };
-    Deque<Integer> rightIndexStack = new LinkedList<Integer>() {
-      {
-        push(len - 1);
-      }
-    };
-
-    while (!nodeStack.isEmpty()) {
-      TreeNode currNode = nodeStack.pop();
-      int left = leftIndexStack.pop();
-      int right = rightIndexStack.pop();
-      int mid = left + (right - left) / 2; // avoid overflow
-      currNode.val = nums[mid];
-      if (left <= mid - 1) {
-        currNode.left = new TreeNode(0);
-        nodeStack.push(currNode.left);
-        leftIndexStack.push(left);
-        rightIndexStack.push(mid - 1);
-      }
-      if (mid + 1 <= right) {
-        currNode.right = new TreeNode(0);
-        nodeStack.push(currNode.right);
-        leftIndexStack.push(mid + 1);
-        rightIndexStack.push(right);
-      }
-    }
-    return head;
-  }
 }
