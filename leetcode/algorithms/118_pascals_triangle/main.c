d3f8c688e1b9af4e17427984e4fc8adc62a6404b
miga-choi <miga.choi.1004@gmail.com>
2026-04-21 03:48:49 +0900

    leetcode: "118_pascals_triangle" add c solution

--- leetcode/algorithms/118_pascals_triangle/main.c
+++ leetcode/algorithms/118_pascals_triangle/main.c
@@ -1,18 +1,29 @@
+#include <stdlib.h>
+
 /**
  * Return an array of arrays of size *returnSize.
  * The sizes of the arrays are returned as *returnColumnSizes array.
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
-#include <stdlib.h>
 
+/**
+ * DP
+ * 
+ * Complexities:
+ *   N - `numRows`
+ *   - Time Complexity: O(N²)
+ *   - Space Complexity: O(N²)
+ */
 int** generate(int numRows, int* returnSize, int** returnColumnSizes) {
     *returnSize = numRows;
+
     int** result = (int**)(malloc(numRows * sizeof(int*)));
     *returnColumnSizes = (int*)malloc(numRows * sizeof(int));
 
     for (int i = 0; i < numRows; i++) {
         (*returnColumnSizes)[i] = i + 1;
         result[i] = (int*)malloc(numRows * sizeof(int));
+
         for (int j = 0; j <= i; j++) {
             if (j == 0 || j == i) {
                 result[i][j] = 1;
@@ -21,31 +32,40 @@ int** generate(int numRows, int* returnSize, int** returnColumnSizes) {
             }
         }
     }
+
     return result;
 }
 
 
 // Solution
+/**
+ * DP
+ * 
+ * Complexities:
+ *   N - `numRows`
+ *   - Time Complexity: O(N²)
+ *   - Space Complexity: O(N²)
+ */
 int** solution(int numRows, int* returnSize, int** returnColumnSizes) {
     *returnSize = numRows;
-    int** output = (int**)malloc(numRows * sizeof(int*));
-    *returnColumnSizes = (int*)malloc(numRows * sizeof(int));
-
-    // Allocate memory for the whole triangle
+    
+    int** result = (int**)malloc(sizeof(int*) * numRows);
+    
+    *returnColumnSizes = (int*)malloc(sizeof(int) * numRows);
+    
     for (int i = 0; i < numRows; i++) {
-        (*returnColumnSizes)[i] = i + 1;
-        output[i] = (int*)malloc((i + 1) * sizeof(int));
-    }
-
-    // The first row will always be 1
-    output[0][0] = 1;
-
-    for (int i = 1; i < numRows; i++) {
-        output[i][0] = 1;
+        int colSize = i + 1;
+        (*returnColumnSizes)[i] = colSize;
+        
+        result[i] = (int*)malloc(sizeof(int) * colSize);
+        
+        result[i][0] = 1;
+        result[i][colSize - 1] = 1;
+        
         for (int j = 1; j < i; j++) {
-            output[i][j] = output[i - 1][j - 1] + output[i - 1][j];
+            result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
         }
-        output[i][i] = 1;
     }
-    return output;
+    
+    return result;
 }
