1a9abd5be82cdf56c0f8d700fab1e98319d97544
miga-choi <miga.choi.1004@gmail.com>
2026-04-21 03:48:03 +0900

    leetcode: "112_path_sum" add dart solution

--- leetcode/algorithms/112_path_sum/main.dart
+++ leetcode/algorithms/112_path_sum/main.dart
@@ -6,6 +6,15 @@ class TreeNode {
 }
 
 class PathSum {
+  /**
+   * Recursion - DFS
+   *
+   * Complexities:
+   *   N - Number of nodes in `root`
+   *   H - Height of `root`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(N)
+   */
   bool hasPathSum(TreeNode? root, int targetSum) {
     if (root == null) {
       return false;
@@ -15,22 +24,68 @@ class PathSum {
       return (targetSum - root.val) == 0;
     }
 
-    return hasPathSum(root.left, targetSum - root.val) ||
-        hasPathSum(root.right, targetSum - root.val);
+    return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
   }
 
 
   // Solution
-  bool solution(TreeNode? root, int targetSum) {
+  /**
+   * Solution 1
+   *
+   * Recursion - DFS
+   *
+   * Complexities:
+   *   N - Number of nodes in `root`
+   *   H - Height of `root`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(N)
+   */
+  bool solution1(TreeNode? root, int targetSum) {
     if (root == null) {
       return false;
     }
 
-    if (root.left == null && root.right == null && root.val == targetSum) {
-      return true;
+    if (root.left == null && root.right == null) {
+      return targetSum == root.val;
     }
 
-    return solution(root.left, targetSum - root.val) ||
-        solution(root.right, targetSum - root.val);
+    return solution1(root.left, targetSum - root.val) || solution1(root.right, targetSum - root.val);
+  }
+
+  /**
+   * Solution 2
+   *
+   * Iteration - DFS with Stack
+   *
+   * Complexities:
+   *   N - Number of nodes in `root`
+   *   H - Height of `root`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(N)
+   */
+  bool solution2(TreeNode? root, int targetSum) {
+    if (root == null) {
+      return false;
+    }
+
+    List<(TreeNode, int)> stack = [(root, targetSum - root.val)];
+
+    while (stack.isNotEmpty) {
+      final (node, currentTarget) = stack.removeLast();
+
+      if (node.left == null && node.right == null && currentTarget == 0) {
+        return true;
+      }
+
+      if (node.right != null) {
+        stack.add((node.right!, currentTarget - node.right!.val));
+      }
+
+      if (node.left != null) {
+        stack.add((node.left!, currentTarget - node.left!.val));
+      }
+    }
+
+    return false;
   }
 }
