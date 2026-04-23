f0101a4075aec7a119903a2e4678b6de8130a7fc
miga-choi <miga.choi.1004@gmail.com>
2026-04-24 01:03:51 +0900

    leetcode: "119_pascals_triangle_2" add c solution

--- leetcode/algorithms/119_pascals_triangle_2/main.c
+++ leetcode/algorithms/119_pascals_triangle_2/main.c
@@ -1,8 +1,17 @@
+#include <stdlib.h>
+
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-#include <stdlib.h>
 
+/**
+ * DP
+ *
+ * Complexities:
+ *   N - `rowIndex`
+ *   - Time Complexity: O(N²)
+ *   - Space Complexity: O(N)
+ */
 int* getRow(int rowIndex, int* returnSize) {
     *returnSize = rowIndex + 1;
     int** result = (int**)malloc(sizeof(int*) * (rowIndex + 1));
@@ -22,14 +31,56 @@ int* getRow(int rowIndex, int* returnSize) {
 
 
 // Solution
-int* solution(int rowIndex, int* returnSize) {
-    int* ans = calloc(rowIndex + 1, sizeof(int));
-    long temp = 1;
-    ans[0] = 1;
-    for (int i = 1, up = rowIndex; i <= rowIndex; i++, up--) {
-        temp = temp * up / i;
-        ans[i] = temp;
-    }
+/**
+ * Solution 1
+ *
+ * DP
+ *
+ * Complexities:
+ *   N - `rowIndex`
+ *   - Time Complexity: O(N²)
+ *   - Space Complexity: O(N)
+ */
+int* solution1(int rowIndex, int* returnSize) {
     *returnSize = rowIndex + 1;
-    return ans;
+    
+    int* row = (int*)calloc(*returnSize, sizeof(int));
+
+    row[0] = 1;
+
+    for (int i = 1; i <= rowIndex; i++) {
+        for (int j = i; j >= 1; j--) {
+            row[j] = row[j] + row[j - 1];
+        }
+    }
+
+    return row;
+}
+
+/**
+ * Solution 2
+ *
+ * Binomial Coefficient
+ *
+ * Complexities:
+ *   N - `rowIndex`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+int* solution2(int rowIndex, int* returnSize) {
+    *returnSize = rowIndex + 1;
+    int* row = (int*)malloc(sizeof(int) * (*returnSize));
+
+    row[0] = 1;
+    
+    long long prev = 1; 
+
+    for (int k = 1; k <= rowIndex; k++) {
+        long long current = prev * (rowIndex - k + 1) / k;
+        
+        row[k] = (int)current;
+        prev = current;
+    }
+
+    return row;
 }
