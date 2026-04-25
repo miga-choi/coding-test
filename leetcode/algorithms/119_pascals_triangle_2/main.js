9ea81fc6504bb2677121ddde361c7a1945eb9b78
miga-choi <miga.choi.1004@gmail.com>
2026-04-25 12:19:53 +0900

    leetcode: "119_pascals_triangle_2" add javascript solution

--- leetcode/algorithms/119_pascals_triangle_2/main.js
+++ leetcode/algorithms/119_pascals_triangle_2/main.js
@@ -1,9 +1,18 @@
 /**
+ * DP
+ *
+ * Complexities:
+ *   N - `rowIndex`
+ *   - Time Complexity: O(N²)
+ *   - Space Complexity: O(N)
+ */
+/**
  * @param {number} rowIndex
  * @return {number[]}
  */
 var getRow = function (rowIndex) {
   const rows = [];
+
   for (let i = 0; i <= rowIndex; i++) {
     const row = [];
     for (let j = 0; j <= i; j++) {
@@ -15,16 +24,63 @@ var getRow = function (rowIndex) {
     }
     rows.push(row);
   }
+
   return rows[rowIndex];
 };
 
 
 // Solution
-var solution = function (r) {
-  var ans = new Array(r + 1);
-  ans[0] = ans[r] = 1;
-  for (i = 1, up = r; i < r; i++, up--) {
-    ans[i] = (ans[i - 1] * up) / i;
+/**
+ * Solution 1
+ *
+ * DP
+ *
+ * Complexities:
+ *   N - `rowIndex`
+ *   - Time Complexity: O(N²)
+ *   - Space Complexity: O(N)
+ */
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var solution1 = function (rowIndex) {
+  const row = new Array(rowIndex + 1).fill(1);
+
+  for (let i = 2; i <= rowIndex; i++) {
+    for (let j = i - 1; j > 0; j--) {
+      row[j] = row[j] + row[j - 1];
+    }
   }
-  return ans;
+
+  return row;
+};
+
+/**
+ * Solution 2
+ *
+ * Binomial Coefficient
+ *
+ * Complexities:
+ *   N - `rowIndex`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var solution2 = function (rowIndex) {
+  const row = new Array(rowIndex + 1).fill(1);
+
+  let prev = 1;
+
+  for (let j = 1; j <= rowIndex; j++) {
+    let current = (prev * (rowIndex - j + 1)) / j;
+
+    row[j] = current;
+    prev = current;
+  }
+
+  return row;
 };
