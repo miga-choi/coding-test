3d8fdd789e57e0e3872abead377d61711f3bf8ff
miga-choi <miga.choi.1004@gmail.com>
2026-04-27 04:03:11 +0900

    leetcode: "119_pascals_triangle_2" add dart solution

--- leetcode/algorithms/119_pascals_triangle_2/main.dart
+++ leetcode/algorithms/119_pascals_triangle_2/main.dart
@@ -1,8 +1,18 @@
 class PascalsTriangleII {
+  /**
+   * DP
+   *
+   * Complexities:
+   *   N - `rowIndex`
+   *   - Time Complexity: O(N²)
+   *   - Space Complexity: O(N)
+   */
   List<int> getRow(int rowIndex) {
     final List<List<int>> result = List<List<int>>.empty(growable: true);
+
     for (int i = 0; i <= rowIndex; i++) {
       final List<int> row = List<int>.empty(growable: true);
+
       for (int j = 0; j <= i; j++) {
         if (j == 0 || j == i) {
           row.add(1);
@@ -10,20 +20,56 @@ class PascalsTriangleII {
           row.add(result[i - 1][j - 1] + result[i - 1][j]);
         }
       }
+
       result.add(row);
     }
+
     return result[rowIndex];
   }
 
 
   // Solution
+  /**
+   * Solution 1
+   *
+   * DP
+   *
+   * Complexities:
+   *   N - `rowIndex`
+   *   - Time Complexity: O(N²)
+   *   - Space Complexity: O(N)
+   */
   List<int> solution(int rowIndex) {
-    List<int> result = [];
-    int value = 1;
+    List<int> row = List.filled(rowIndex + 1, 1);
 
-    for (int i = 0; i <= rowIndex; i++) {
-      result.add(value);
-      value = (value * (rowIndex - i)) ~/ (i + 1);
+    for (int i = 2; i <= rowIndex; i++) {
+      for (int j = i - 1; j > 0; j--) {
+        row[j] = row[j] + row[j - 1];
+      }
+    }
+
+    return row;
+  }
+
+  /**
+   * Solution 2
+   *
+   * Binomial Coefficient
+   *
+   * Complexities:
+   *   N - `rowIndex`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(N)
+   */
+  List<int> solution2(int rowIndex) {
+    List<int> result = List.filled(rowIndex + 1, 1);
+
+    int prev = 1;
+
+    for (int i = 1; i < rowIndex; i++) {
+      prev = prev * (rowIndex - i + 1) ~/ i;
+
+      result[i] = prev;
     }
 
     return result;
