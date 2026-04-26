61b040403b683e586768540ef0874cd67915a847
miga-choi <miga.choi.1004@gmail.com>
2026-04-26 18:57:09 +0900

    leetcode: "119_pascals_triangle_2" add python solution

--- leetcode/algorithms/119_pascals_triangle_2/main.py
+++ leetcode/algorithms/119_pascals_triangle_2/main.py
@@ -2,25 +2,65 @@ from typing import List
 
 
 class PascalsTriangleII:
+    """
+    # DP
+    #
+    # Complexities:
+    #   N - `rowIndex`
+    #   - Time Complexity: O(N²)
+    #   - Space Complexity: O(N)
+    """
     def getRow(self, rowIndex: int) -> List[int]:
         rows = []
+
         for i in range(rowIndex + 1):
             row = []
+
             for j in range(i + 1):
                 if j == 0 or j == i:
                     row.append(1)
                 else:
                     row.append(rows[i - 1][j - 1] + rows[i - 1][j])
+
             rows.append(row)
+
         return rows[rowIndex]
 
 
     # Solution
-    def solution(self, rowIndex: int) -> List[int]:
+    """
+    # Solution 1
+    #
+    # DP
+    #
+    # Complexities:
+    #   N - `rowIndex`
+    #   - Time Complexity: O(N²)
+    #   - Space Complexity: O(N)
+    """
+    def solution1(self, rowIndex: int) -> List[int]:
         row = [1] * (rowIndex + 1)
-        if rowIndex == 0:
-            return row
-        prev_row = self.getRow(rowIndex - 1)
-        for i in range(1, len(row) - 1):
-            row[i] = prev_row[i - 1] + prev_row[i]
+
+        for i in range(2, rowIndex + 1):
+            for j in range(i - 1, 0, -1):
+                row[j] = row[j] + row[j - 1]
+
+        return row
+
+    """
+    # Solution 2
+    #
+    # Binomial Coefficient
+    #
+    # Complexities:
+    #   N - `rowIndex`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(N)
+    """
+    def solution1(self, rowIndex: int) -> List[int]:
+        row = [1] * (rowIndex + 1)
+
+        for i in range(1, rowIndex):
+            row[i] = row[i - 1] * (rowIndex - i + 1) // i
+
         return row
