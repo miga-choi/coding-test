b25876e116b416873df56bf02c10fe6cb1a82a0d
miga-choi <miga.choi.1004@gmail.com>
2026-04-26 18:56:31 +0900

    leetcode: "119_pascals_triangle_2" add java solution

--- leetcode/algorithms/119_pascals_triangle_2/main.java
+++ leetcode/algorithms/119_pascals_triangle_2/main.java
@@ -1,12 +1,21 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 class PascalsRiangleII {
+  /**
+   * DP
+   *
+   * Complexities:
+   *   N - `rowIndex`
+   *   - Time Complexity: O(N²)
+   *   - Space Complexity: O(N)
+   */
   public List<Integer> getRow(int rowIndex) {
-    List<List<Integer>> rows = new ArrayList<List<Integer>>();
+    List<List<Integer>> rows = new ArrayList<>();
+
     for (int i = 0; i <= rowIndex; i++) {
-      List<Integer> row = new ArrayList<Integer>();
+      List<Integer> row = new ArrayList<>();
+
       for (int j = 0; j <= i; j++) {
         if (j == 0 || j == i) {
           row.add(1);
@@ -14,24 +23,62 @@ class PascalsRiangleII {
           row.add(rows.get(i - 1).get(j - 1) + rows.get(i - 1).get(j));
         }
       }
+
       rows.add(row);
     }
+
     return rows.get(rowIndex);
   }
 
 
   // Solution
-  public List<Integer> solution(int k) {
-    Integer[] arr = new Integer[k + 1];
-    Arrays.fill(arr, 0);
-    arr[0] = 1;
+  /**
+   * Solution 1
+   * 
+   * DP
+   *
+   * Complexities:
+   *   N - `rowIndex`
+   *   - Time Complexity: O(N²)
+   *   - Space Complexity: O(N)
+   */
+  public List<Integer> solution1(int rowIndex) {
+    List<Integer> row = new ArrayList<>();
 
-    for (int i = 1; i <= k; i++) {
-      for (int j = i; j > 0; j--) {
-        arr[j] = arr[j] + arr[j - 1];
+    row.add(1);
+
+    for (int i = 1; i <= rowIndex; i++) {
+      for (int j = i - 1; j > 0; j--) {
+        row.set(j, row.get(j) + row.get(j - 1));
       }
+
+      row.add(1);
     }
 
-    return Arrays.asList(arr);
+    return row;
+  }
+
+  /**
+   * Solution 2
+   *
+   * Binomial Coefficient
+   *
+   * Complexities:
+   *   N - `rowIndex`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(N)
+   */
+  public List<Integer> solution2(int rowIndex) {
+    List<Integer> row = new ArrayList<>();
+
+    long val = 1;
+
+    for (int j = 0; j <= rowIndex; j++) {
+      row.add((int) val);
+
+      val = val * (rowIndex - j) / (j + 1);
+    }
+
+    return row;
   }
 }
