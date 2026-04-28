10c0e36ae03e5c6e17d74954ffc17fd589236c24
miga-choi <miga.choi.1004@gmail.com>
2026-04-29 01:05:10 +0900

    leetcode: "121_best_time_to_buy_and_sell_stock" add python solution

--- leetcode/algorithms/121_best_time_to_buy_and_sell_stock/main.py
+++ leetcode/algorithms/121_best_time_to_buy_and_sell_stock/main.py
@@ -2,10 +2,14 @@ from typing import List
 
 
 class BestTimeToBuyAndSellStock:
-    ##
-    # - Time Complexity: O(N)
-    # - Space Complexity: O(1)
+    """
+    # Greedy
     #
+    # Complexities:
+    #   N - Size of `prices`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
     def maxProfit(self, prices: List[int]) -> int:
         minPrice: int = 100000
         maxProfit: int = 0
@@ -22,16 +26,22 @@ class BestTimeToBuyAndSellStock:
 
 
     # Solution
-    ##
-    # - Time Complexity: O(N)
-    # - Space Complexity: O(1)
+    """
+    # Greedy
     #
+    # Complexities:
+    #   N - Size of `prices`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
     def solution(self, prices: List[int]) -> int:
         min_price = float('inf')
         max_profit = 0
 
         for price in prices:
-            min_price = min(min_price, price)
-            max_profit = max(max_profit, price - min_price)
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
 
         return max_profit
