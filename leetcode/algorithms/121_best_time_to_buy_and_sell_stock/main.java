a6d55cfb1e14135de194d0b14cbf8dc1659bf353
miga-choi <miga.choi.1004@gmail.com>
2026-04-29 01:04:27 +0900

    leetcode: "121_best_time_to_buy_and_sell_stock" add java solution

--- leetcode/algorithms/121_best_time_to_buy_and_sell_stock/main.java
+++ leetcode/algorithms/121_best_time_to_buy_and_sell_stock/main.java
@@ -1,7 +1,11 @@
 class BestTimeToBuyAndSellStock {
   /**
-   * - Time Complexity: O(N)
-   * - Space Complexity: O(1)
+   * Greedy
+   *
+   * Complexities:
+   *   N - Size of `prices`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
    */
   public int maxProfit(int[] prices) {
     int minPrice = 100000;
@@ -24,23 +28,26 @@ class BestTimeToBuyAndSellStock {
 
   // Solution
   /**
-   * - Time Complexity: O(N)
-   * - Space Complexity: O(1)
+   * Greedy
+   *
+   * Complexities:
+   *   N - Size of `prices`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
    */
   public int solution(int[] prices) {
-    if (prices == null || prices.length < 2) {
+    if (prices == null || prices.length <= 1) {
       return 0;
     }
 
     int minPrice = Integer.MAX_VALUE;
     int maxProfit = 0;
 
-    for (int currentPrice : prices) {
-      if (currentPrice < minPrice) {
-        minPrice = currentPrice;
-      } else {
-        int potentialProfit = currentPrice - minPrice;
-        maxProfit = Math.max(maxProfit, potentialProfit);
+    for (int price : prices) {
+      if (price < minPrice) {
+        minPrice = price;
+      } else if (price - minPrice > maxProfit) {
+        maxProfit = price - minPrice;
       }
     }
 
