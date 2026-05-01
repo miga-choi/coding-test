302211b61a9f278cd87afaf480cf8c6782176aae
miga-choi <miga.choi.1004@gmail.com>
2026-05-02 01:23:16 +0900

    leetcode: "125_valid_palindrome" add python solution

--- leetcode/algorithms/125_valid_palindrome/main.py
+++ leetcode/algorithms/125_valid_palindrome/main.py
@@ -2,10 +2,14 @@ import re
 
 
 class ValidPalindrome:
-    ##
-    # Two-Pointer
-    # - Time Complexity: O(N)
-    # - Space Complexity: O(1)
+    """
+    # Two-Pointers
+    #
+    # Complexities:
+    #   N - Size of `s`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
     def isPalindrome(self, s: str) -> bool:
         left: int = 0
         right: int = len(s) - 1
@@ -29,31 +33,31 @@ class ValidPalindrome:
 
 
     # Solution
-    ##
+    """
     # Solution 1
     #
-    # Compare strings
-    # - Time Complexity: O(N)
-    # - Space Complexity: O(N)
+    # Pythonic Way
     #
+    # Complexities:
+    #   N - Size of `s`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
     def solution1(self, s: str) -> bool:
-        filtered_chars = []
+        filtered_chars = [char.lower() for char in s if char.isalnum()]
 
-        for char in s:
-            if char.isalnum():
-                filtered_chars.append(char.lower())
+        return filtered_chars == filtered_chars[::-1]
 
-        processed_s = "".join(filtered_chars)
-
-        return processed_s == processed_s[::-1]
-
-    ##
+    """
     # Solution 2
     #
-    # Two-Pointer
-    # - Time Complexity: O(N)
-    # - Space Complexity: O(1)
+    # Two-Pointers
     #
+    # Complexities:
+    #   N - Size of `s`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
     def solution2(self, s: str) -> bool:
         left, right = 0, len(s) - 1
 
@@ -71,14 +75,3 @@ class ValidPalindrome:
             right -= 1
 
         return True
-
-    ##
-    # Solution 3
-    #
-    # Regular Expressions
-    # - Time Complexity: O(N)
-    # - Space Complexity: O(N)
-    #
-    def solution3(self, s: str) -> bool:
-        s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
-        return s == s[::-1]
