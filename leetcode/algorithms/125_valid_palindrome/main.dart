1aaef8d5299ae9efd40a97366b36398ddbc11b0c
miga-choi <miga.choi.1004@gmail.com>
2026-05-03 19:27:17 +0900

    leetcode: "125_valid_palindrome" add dart solution

--- leetcode/algorithms/125_valid_palindrome/main.dart
+++ leetcode/algorithms/125_valid_palindrome/main.dart
@@ -1,8 +1,11 @@
 class ValidPalindrome {
   /*
-   * Two-Pointer
-   * - Time Complexity: O(N)
-   * - Space Complexity: O(1)
+   * Two-Pointers
+   * 
+   * Complexities:
+   *   N - Size of `s`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
    */
   bool isPalindrome(String s) {
     int left = 0;
@@ -39,55 +42,60 @@ class ValidPalindrome {
 
   // Solution
   /*
-   * Solution 1
+   * Two-Pointers
    * 
-   * Compare strings
-   * - Time Complexity: O(N)
-   * - Space Complexity: O(N)
+   * Complexities:
+   *   N - Size of `s`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
    */
-  bool solution1(String s) {
-    String sanitized = s.replaceAll(RegExp(r'[^A-Za-z0-9]'), '').toLowerCase();
+  bool solution(String s) {
+    if (s.isEmpty) {
+      return true;
+    }
 
-    String reversed = sanitized.split('').reversed.join('');
-
-    return sanitized == reversed;
-  }
-
-  /*
-   * Solution 2
-   * 
-   * Two-Pointer
-   * - Time Complexity: O(N)
-   * - Space Complexity: O(1)
-   */
-  bool _isAlphanumeric(String char) {
-    final int code = char.codeUnitAt(0);
-    return (code >= 97 && code <= 122) ||
-        (code >= 65 && code <= 90) ||
-        (code >= 48 && code <= 57);
-  }
-
-  bool solution2(String s) {
     int left = 0;
     int right = s.length - 1;
 
     while (left < right) {
-      while (left < right && !_isAlphanumeric(s[left])) {
+      int leftCode = s.codeUnitAt(left);
+      while (left < right && !_isAlphanumeric(leftCode)) {
         left++;
+        leftCode = s.codeUnitAt(left);
       }
 
-      while (left < right && !_isAlphanumeric(s[right])) {
+      int rightCode = s.codeUnitAt(right);
+      while (left < right && !_isAlphanumeric(rightCode)) {
+        right--;
+        rightCode = s.codeUnitAt(right);
+      }
+
+      if (left < right) {
+        int leftLower = _toLowerCase(leftCode);
+        int rightLower = _toLowerCase(rightCode);
+
+        if (leftLower != rightLower) {
+          return false;
+        }
+
+        left++;
         right--;
       }
-
-      if (s[left].toLowerCase() != s[right].toLowerCase()) {
-        return false;
-      }
-
-      left++;
-      right--;
     }
 
     return true;
+  }
+
+  bool _isAlphanumeric(int code) {
+    return (code >= 48 && code <= 57) ||
+        (code >= 65 && code <= 90) ||
+        (code >= 97 && code <= 122);
+  }
+
+  int _toLowerCase(int code) {
+    if (code >= 65 && code <= 90) {
+      return code + 32;
+    }
+    return code;
   }
 }
