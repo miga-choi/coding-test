3c5c5845776e7dfc6e3fcc6b1065333e854e90f3
miga-choi <miga.choi.1004@gmail.com>
2026-05-06 06:38:42 +0900

    leetcode: "136_single_number" add c solution

--- leetcode/algorithms/136_single_number/main.c
+++ leetcode/algorithms/136_single_number/main.c
@@ -1,13 +1,36 @@
 /**
  * Bit Manipulation
- * - Time Complexity: O(N)
- * - Space Complexity: O(1)
+ * 
+ * Complexities:
+ *   N - `numsSize`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
  */
 int singleNumber(int* nums, int numsSize) {
     int result = 0;
 
     for (int i = 0; i < numsSize; i++) {
         result = result ^ nums[i];
+    }
+
+    return result;
+}
+
+
+// Solution
+/**
+ * XOR
+ * 
+ * Complexities:
+ *   N - `numsSize`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+int solution(int* nums, int numsSize) {
+    int result = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        result ^= nums[i]; 
     }
 
     return result;
