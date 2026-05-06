f68ff9fde70d917572c1f36edccf5b4e139f6e00
miga-choi <miga.choi.1004@gmail.com>
2026-05-07 01:15:25 +0900

    leetcode: "136_single_number" add javascript solution

--- leetcode/algorithms/136_single_number/main.js
+++ leetcode/algorithms/136_single_number/main.js
@@ -1,8 +1,12 @@
 /**
  * Bit Manipulation
- * - Time Complexity: O(N)
- * - Space Complexity: O(1)
  *
+ * Complexities:
+ *   N - Size of `nums`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+/**
  * @param {number[]} nums
  * @return {number}
  */
@@ -14,4 +18,48 @@ var singleNumber = function (nums) {
   }
 
   return result;
+};
+
+
+// Solution
+/**
+ * Solution 1
+ *
+ * XOR
+ *
+ * Complexities:
+ *   N - Size of `nums`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var solution1 = function (nums) {
+  let result = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    result ^= nums[i];
+  }
+
+  return result;
+};
+
+/**
+ * Solution 2
+ *
+ * Array.prototype.reduce
+ *
+ * Complexities:
+ *   N - Size of `nums`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var solution2 = function (nums) {
+  return nums.reduce((acc, current) => acc ^ current, 0);
 };
