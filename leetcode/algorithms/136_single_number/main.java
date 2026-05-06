cf2cfdef8e264cd883fa63d01436d6bc95221f7d
miga-choi <miga.choi.1004@gmail.com>
2026-05-07 01:15:53 +0900

    leetcode: "136_single_number" add java solution

--- leetcode/algorithms/136_single_number/main.java
+++ leetcode/algorithms/136_single_number/main.java
@@ -1,10 +1,33 @@
 class SingleNumber {
   /**
    * Bit Manipulation
-   * - Time Complexity: O(N)
-   * - Space Complexity: O(1)
+   *
+   * Complexities:
+   *   N - Size of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
    */
   public int singleNumber(int[] nums) {
+    int result = 0;
+
+    for (int num : nums) {
+      result = result ^ num;
+    }
+
+    return result;
+  }
+
+
+  // Solution
+  /**
+   * XOR
+   *
+   * Complexities:
+   *   N - Size of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
+  public int solution1(int[] nums) {
     int result = 0;
 
     for (int num : nums) {
