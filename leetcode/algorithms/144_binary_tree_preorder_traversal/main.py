da8f6832ba58818845ab9c3c11b8e0d5fb7b795c
miga-choi <miga.choi.1004@gmail.com>
2026-05-13 07:21:35 +0900

    leetcode: "144_binary_tree_preorder_traversal" add python solution

--- leetcode/algorithms/144_binary_tree_preorder_traversal/main.py
+++ leetcode/algorithms/144_binary_tree_preorder_traversal/main.py
@@ -9,9 +9,19 @@ class TreeNode:
 
 
 class BinaryTreePreorderTraversal:
+    """
+    # Iteration
+    #
+    # Complexities:
+    #   N - Number of nodes in `root`
+    #   H - Height of `root`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(H)
+    """
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         result = []
         totalNodes = []
+
         if root is not None:
             totalNodes.append(root)
 
@@ -29,44 +39,61 @@ class BinaryTreePreorderTraversal:
 
 
     # Solution
-    # Solution 1: The iterative version
+    """
+    # Solution 1
+    #
+    # Recursion
+    #
+    # Complexities:
+    #   N - Number of nodes in `root`
+    #   H - Height of `root`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(H)
+    """
     def solution1(self, root: Optional[TreeNode]) -> List[int]:
-                                          #  Ex: root = [1, 2,None, 3,4]
-        if not root:                      #
-            return []                     #         1
-        stack, ans = [root], []           #        /
-                                          #       2
-        while stack:                      #      / \
-            node = stack.pop()            #     3   4
-            ans.append(node.val)          #
-                                          #     node     node.left   node.right  stack    ans
-            if node.right:                #   –––––––––  –––––––––   –––––––––   ––––––  ––––––
-                stack.append(node.right)  #                                       [1]     []
-            if node.left:                 #       1          2         None       [2]     [1]
-                stack.append(node.left)   #       2          3          4         [4,3]   [1,2]
-                                          #       3        None        None       [4]     [1,2,3]
-                                          #       4        None        None       [4]     [1,2,3,4]
-        return ans
+        result = []
 
-    # Solution 2: The recursive version
-    def solution2(self, root: Optional[TreeNode]) -> List[int]:
-        def dfs(node):
+        def traverse(node):
             if not node:
                 return
 
-            ans.append(node.val)
+            result.append(node.val)
 
-            dfs(node.left)
-            dfs(node.right)
+            traverse(node.left)
 
-            return
+            traverse(node.right)
 
-        ans = []
+        traverse(root)
 
-        dfs(root)
+        return result
 
-        return ans
+    """
+    # Solution 2
+    #
+    # Iteration
+    #
+    # Complexities:
+    #   N - Number of nodes in `root`
+    #   H - Height of `root`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(H)
+    """
+    def solution2(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
 
-    # Solution 3: And here's a one-liner:
-    def solution3(self, root: TreeNode) -> list[int]:
-        return [] if not root else ([root.val]+self.preorderTraversal(root.left)+self.preorderTraversal(root.right))
+        result = []
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+
+            result.append(node.val)
+
+            if node.right:
+                stack.append(node.right)
+
+            if node.left:
+                stack.append(node.left)
+
+        return result
