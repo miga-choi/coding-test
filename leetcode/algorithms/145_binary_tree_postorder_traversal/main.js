75291298b375f5a4ce15cb3a2df119ee077e047a
miga-choi <miga.choi.1004@gmail.com>
2026-05-15 01:20:44 +0900

    leetcode: "145_binary_tree_postorder_traversal" add javascript solution

--- leetcode/algorithms/145_binary_tree_postorder_traversal/main.js
+++ leetcode/algorithms/145_binary_tree_postorder_traversal/main.js
@@ -5,6 +5,15 @@ function TreeNode(val, left, right) {
 }
 
 /**
+ * Iteration
+ *
+ * Complexities:
+ *   N - Number of nodes in `root`
+ *   H - Height of `root`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(H)
+ */
+/**
  * @param {TreeNode} root
  * @return {number[]}
  */
@@ -14,12 +23,16 @@ var postorderTraversal = function (root) {
 
   if (root) {
     nodes.push(root);
+
     while (nodes.length > 0) {
       const node = nodes.pop();
+
       result.push(node.val);
+
       if (node.left) {
         nodes.push(node.left);
       }
+
       if (node.right) {
         nodes.push(node.right);
       }
@@ -31,44 +44,73 @@ var postorderTraversal = function (root) {
 
 
 // Solution
-// Solution 1: Iterative
+/**
+ * Solution 1
+ *
+ * Recursion
+ *
+ * Complexities:
+ *   N - Number of nodes in `root`
+ *   H - Height of `root`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(H)
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
 var solution1 = function (root) {
-  if (!root) {
-    return [];
-  }
-  let stack = [], res = [];
-  stack.push(root);
-  while (stack.length) {
-    let node = stack[stack.length - 1];
-    if (node.left) {
-      stack.push(node.left);
-      node.left = null;
-    } else if (node.right) {
-      stack.push(node.right);
-      node.right = null;
-    } else {
-      res.push(stack.pop().val);
-    }
-  }
-  return res;
-  // Time Complexity: O(n)
-  // Space Complexity: O(n)
-};
-
-// Solution 2: Recursive
-var solution2 = function (root) {
-  let res = [];
-  traverse(root);
-  return res;
+  const result = [];
 
   function traverse(node) {
     if (!node) {
       return;
     }
+
     traverse(node.left);
     traverse(node.right);
-    res.push(node.val);
+    result.push(node.val);
   }
-  // Time Complexity: O(n)
-  // Space Complexity: O(n)
+
+  traverse(root);
+
+  return result;
+};
+
+/**
+ * Solution 2
+ *
+ * Iteration
+ *
+ * Complexities:
+ *   N - Number of nodes in `root`
+ *   H - Height of `root`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(H)
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var solution2 = function (root) {
+  if (!root) {
+    return [];
+  }
+
+  const result = [];
+  const stack = [root];
+
+  while (stack.length > 0) {
+    const node = stack.pop();
+    result.push(node.val);
+
+    if (node.left !== null) {
+      stack.push(node.left);
+    }
+    if (node.right !== null) {
+      stack.push(node.right);
+    }
+  }
+
+  return result.reverse();
 };
