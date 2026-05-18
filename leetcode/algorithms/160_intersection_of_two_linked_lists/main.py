283f9b4ff8bf9ea557cb262a9cf83085698fea3b
miga-choi <miga.choi.1004@gmail.com>
2026-05-19 01:03:55 +0900

    leetcode: "160_intersection_of_two_linked_lists" add python solution

--- leetcode/algorithms/160_intersection_of_two_linked_lists/main.py
+++ leetcode/algorithms/160_intersection_of_two_linked_lists/main.py
@@ -8,29 +8,44 @@ class ListNode:
 
 
 class IntersectionOfTwoLinkedLists:
-    ##
+    """
     # Two-Pointer
-    # - Time Complexity: O(M + N)
-    # - Space Complexity: O(1)
     #
+    # Complexities:
+    #   N - Number of Nodes in `headA`
+    #   M - Number of Nodes in `headB`
+    #   - Time Complexity: O(M + N)
+    #   - Space Complexity: O(1)
+    """
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        dummyA: Optional[ListNode] = headA
-        dummyB: Optional[ListNode] = headB
+        if not headA or not headB:
+            return None
 
-        while dummyA != dummyB:
-            dummyA = headB if dummyA == None else dummyA.next
-            dummyB = headA if dummyB == None else dummyB.next
+        ptrA, ptrB = headA, headB
 
-        return dummyA
+        while ptrA != ptrB:
+            ptrA = ptrA.next if ptrA else headB
+            ptrB = ptrB.next if ptrB else headA
+
+        return ptrA
 
 
     # Solution
-    ##
-    # Two-Pointer
-    # - Time Complexity: O(M + N)
-    # - Space Complexity: O(1)
+    """
+    # Solution 1
     #
-    def solution(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+    # Two-Pointer
+    #
+    # Complexities:
+    #   N - Number of Nodes in `headA`
+    #   M - Number of Nodes in `headB`
+    #   - Time Complexity: O(M + N)
+    #   - Space Complexity: O(1)
+    """
+    def solution1(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        if not headA or not headB:
+            return None
+
         pA, pB = headA, headB
 
         while pA != pB:
@@ -38,3 +53,42 @@ class IntersectionOfTwoLinkedLists:
             pB = pB.next if pB else headA
 
         return pA
+
+    """
+    # Solution 2
+    #
+    # Length Difference
+    #
+    # Complexities:
+    #   N - Number of Nodes in `headA`
+    #   M - Number of Nodes in `headB`
+    #   - Time Complexity: O(M + N)
+    #   - Space Complexity: O(1)
+    """
+    def solution2(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        def get_length(node: ListNode) -> int:
+            length = 0
+            while node:
+                length += 1
+                node = node.next
+            return length
+
+        lenA = get_length(headA)
+        lenB = get_length(headB)
+
+        pA, pB = headA, headB
+
+        if lenA > lenB:
+            for _ in range(lenA - lenB):
+                pA = pA.next
+        else:
+            for _ in range(lenB - lenA):
+                pB = pB.next
+
+        while pA and pB:
+            if pA == pB:
+                return pA
+            pA = pA.next
+            pB = pB.next
+
+        return None
