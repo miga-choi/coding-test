b71fa1991e591d9fb500699e4a4f25625a5c6bce
miga-choi <miga.choi.1004@gmail.com>
2026-05-20 01:06:13 +0900

    leetcode: "168_excel_sheet_column_title" add c++ solution

--- leetcode/algorithms/168_excel_sheet_column_title/main.cpp
+++ leetcode/algorithms/168_excel_sheet_column_title/main.cpp
@@ -3,37 +3,48 @@ using namespace std;
 
 class ExcelSheetColumnTitle {
 public:
+    /**
+     * Complexities:
+     *   N - `columnNumber`
+     *   - Time Complexity: O(logᴺ)
+     *   - Space Complexity: O(1)
+     */
     string convertToTitle(int columnNumber) {
         string result = "";
+        
         while (columnNumber > 0) {
             columnNumber -= 1;
+
             result = (char)(columnNumber % 26 + 65) + result;
+
             columnNumber /= 26;
         }
+
         return result;
     }
 
 
     // Solution
-    // Solution 1: Iterative
-    string solution1(int columnNumber) {
+    /**
+     * Complexities:
+     *   N - `columnNumber`
+     *   - Time Complexity: O(logᴺ)
+     *   - Space Complexity: O(1)
+     */
+    string solution(int columnNumber) {
         string result = "";
-        while (columnNumber) {
-            columnNumber--;
-            char c = 'A' + columnNumber % 26;
-            result = c + result;
+
+        while (columnNumber > 0) {
+            columnNumber--; 
+
+            char currentChar = 'A' + (columnNumber % 26);
+            result += currentChar;
+
             columnNumber /= 26;
         }
-        return result;
-    }
 
-    // Solution 2: Recusrive
-    string solution2(int columnNumber) {
-        if (columnNumber == 0) {
-            return "";
-        }
-        columnNumber--;
-        char c = 'A' + columnNumber % 26;
-        return convertToTitle(columnNumber / 26) + c;
+        reverse(result.begin(), result.end());
+
+        return result;
     }
 };
