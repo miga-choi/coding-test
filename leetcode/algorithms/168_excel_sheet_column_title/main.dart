3a3c4d437df8310980b57f748cad97596a966d67
miga-choi <miga.choi.1004@gmail.com>
2026-05-22 06:04:19 +0900

    leetcode: "168_excel_sheet_column_title" add dart solution

--- leetcode/algorithms/168_excel_sheet_column_title/main.dart
+++ leetcode/algorithms/168_excel_sheet_column_title/main.dart
@@ -1,23 +1,45 @@
 class ExcelSheetColumnTitle {
+  /**
+   * Complexities:
+   *   N - `columnNumber`
+   *   - Time Complexity: O(logᴺ)
+   *   - Space Complexity: O(logᴺ)
+   */
   String convertToTitle(int columnNumber) {
     String result = "";
+
     while (columnNumber > 0) {
       columnNumber -= 1;
+
       result = String.fromCharCode(columnNumber % 26 + 65) + result;
+
       columnNumber = (columnNumber / 26).floor();
     }
+
     return result;
   }
 
 
   // Solution
+  /**
+   * Complexities:
+   *   N - `columnNumber`
+   *   - Time Complexity: O(logᴺ)
+   *   - Space Complexity: O(logᴺ)
+   */
   String solution(int columnNumber) {
-    StringBuffer stringBuffer = StringBuffer();
+    StringBuffer result = StringBuffer();
+
     while (columnNumber > 0) {
-      int reminder = (columnNumber - 1) % 26;
-      stringBuffer.write(String.fromCharCode(reminder + 'A'.codeUnitAt(0)));
-      columnNumber = (columnNumber - 1) ~/ 26;
+      columnNumber--;
+
+      int remainder = columnNumber % 26;
+
+      result.write(String.fromCharCode(65 + remainder));
+
+      columnNumber ~/= 26;
     }
-    return stringBuffer.toString().split('').reversed.join();
+
+    return result.toString().split('').reversed.join('');
   }
 }
