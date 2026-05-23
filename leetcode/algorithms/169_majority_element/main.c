9e99d051447273ebaf1fbc28a6939c743162122a
miga-choi <miga.choi.1004@gmail.com>
2026-05-23 19:31:14 +0900

    leetcode: "169_majority_element" add c solution

--- leetcode/algorithms/169_majority_element/main.c
+++ leetcode/algorithms/169_majority_element/main.c
@@ -1,33 +1,55 @@
+/**
+ * Boyer-Moore Voting Algorithm
+ * 
+ * Complexities:
+ *   N - `numsSize`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
 int majorityElement(int* nums, int numsSize) {
     int result;
     int count = 0;
+
     for (int i = 0; i < numsSize; i++) {
         if (count == 0) {
             result = nums[i];
         }
+
         if (result == nums[i]) {
             count++;
         } else {
             count--;
         }
     }
+
     return result;
 }
 
 
 // Solution
+/**
+ * Boyer-Moore Voting Algorithm
+ * 
+ * Complexities:
+ *   N - `numsSize`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
 int solution(int* nums, int numsSize) {
-    int sol;
-    int cnt = 0;
+    int candidate = 0;
+    int count = 0;
+
     for (int i = 0; i < numsSize; i++) {
-        if (cnt == 0) {
-            sol = nums[i];
+        if (count == 0) {
+            candidate = nums[i];
         }
-        if (nums[i] == sol) {
-            cnt++;
+
+        if (nums[i] == candidate) {
+            count++;
         } else {
-            cnt--;
+            count--;
         }
     }
-    return sol;
+
+    return candidate;
 }
