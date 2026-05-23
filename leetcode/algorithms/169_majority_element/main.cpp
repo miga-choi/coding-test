924e063dc3898ea31381a9189ea639427e2904ae
miga-choi <miga.choi.1004@gmail.com>
2026-05-23 19:31:38 +0900

    leetcode: "169_majority_element" add c++ solution

--- leetcode/algorithms/169_majority_element/main.cpp
+++ leetcode/algorithms/169_majority_element/main.cpp
@@ -6,65 +6,73 @@ using namespace std;
 
 class MajorityElement {
 public:
+    /**
+     * Boyer-Moore Voting Algorithm
+     *
+     * Complexities:
+     *   N - `numsSize`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
     int majorityElement(vector<int>& nums) {
         int result;
         int count = 0;
+
         for (int i = 0; i < nums.size(); i++) {
             if (count == 0) {
                 result = nums[i];
             }
+
             if (result == nums[i]) {
                 count++;
             } else {
                 count--;
             }
         }
+
         return result;
     }
 
 
     // Solution
-    // Solution 1: Sorting
+    /**
+     * Solution 1
+     * 
+     * Boyer-Moore Voting Algorithm
+     *
+     * Complexities:
+     *   N - `numsSize`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
     int solution1(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        return nums[n / 2];
-    }
-
-    // Solution 2: Hash Map
-    int solution2(vector<int>& nums) {
-        int n = nums.size();
-        unordered_map<int, int> m;
-
-        for (int i = 0; i < n; i++) {
-            m[nums[i]]++;
-        }
-        n = n / 2;
-        for (auto x : m) {
-            if (x.second > n) {
-                return x.first;
-            }
-        }
-        return 0;
-    }
-
-    // Solution 3: Moore Voting Algorithm
-    int solution3(vector<int>& nums) {
-        int count = 0;
         int candidate = 0;
+        int count = 0;
 
         for (int num : nums) {
             if (count == 0) {
                 candidate = num;
             }
 
-            if (num == candidate) {
-                count++;
-            } else {
-                count--;
-            }
+            count += (num == candidate) ? 1 : -1;
         }
 
         return candidate;
+    }
+
+    /**
+     * Solution 2
+     * 
+     * Sorting
+     *
+     * Complexities:
+     *   N - `numsSize`
+     *   - Time Complexity: O(N * logᴺ)
+     *   - Space Complexity: O(logᴺ)
+     */
+    int solution2(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        return nums[nums.size() / 2];
     }
 };
