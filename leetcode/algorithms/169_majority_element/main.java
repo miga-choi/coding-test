f6757ec89701b6843f4ac61a8a16071cc3bc603c
miga-choi <miga.choi.1004@gmail.com>
2026-05-24 12:24:13 +0900

    leetcode: "169_majority_element" add java solution

--- leetcode/algorithms/169_majority_element/main.java
+++ leetcode/algorithms/169_majority_element/main.java
@@ -2,8 +2,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 class MajorityElement {
+  /**
+   * HashMap
+   *
+   * Complexities:
+   *   N - `numsSize`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(N)
+   */
   public int majorityElement(int[] nums) {
-    Map<Integer, Integer> numsMap = new HashMap<Integer, Integer>();
+    Map<Integer, Integer> numsMap = new HashMap<>();
 
     for (int i = 0; i < nums.length; i++) {
       int num = nums[i];
@@ -21,5 +29,34 @@ class MajorityElement {
     }
 
     return 0;
+  }
+
+
+  // Solution
+  /**
+   * Boyer-Moore Voting Algorithm
+   *
+   * Complexities:
+   *   N - `numsSize`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
+  public int solution(int[] nums) {
+    int candidate = 0;
+    int count = 0;
+
+    for (int num : nums) {
+      if (count == 0) {
+        candidate = num;
+      }
+
+      if (num == candidate) {
+        count++;
+      } else {
+        count--;
+      }
+    }
+
+    return candidate;
   }
 }
