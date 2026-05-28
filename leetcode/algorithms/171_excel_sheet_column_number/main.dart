00981ac09929d722a38de27838b3a1f2bf581247
miga-choi <miga.choi.1004@gmail.com>
2026-05-29 01:21:38 +0900

    leetcode: "171_excel_sheet_column_number" add dart solution

--- leetcode/algorithms/171_excel_sheet_column_number/main.dart
+++ leetcode/algorithms/171_excel_sheet_column_number/main.dart
@@ -1,24 +1,45 @@
 import 'dart:math';
 
 class ExcelSheetColumnNumber {
+  /**
+   * Complexities:
+   *   N - Size of `columnTitle`
+   *   - Time Complexity: O(N²)
+   *   - Space Complexity: O(1)
+   */
   int titleToNumber(String columnTitle) {
     int length = columnTitle.length;
     int result = 0;
+
     for (int i = 0; i < length; i++) {
       result += (columnTitle[length - i - 1].codeUnitAt(0) - 64) * pow(26, i).toInt();
     }
+
     return result;
   }
 
 
   // Solution
+  /**
+   * Complexities:
+   *   N - Size of `columnTitle`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
   int solution(String columnTitle) {
-    int res = 0;
-    for (int i = 0; i < columnTitle.length; i++) {
-      // 64 = codeUnits['A'] -1
-      int s = columnTitle.codeUnits[i] - 64;
-      res = res * 26 + s;
+    int result = 0;
+    int n = columnTitle.length;
+
+    final int asciiA = 'A'.codeUnitAt(0);
+
+    for (int i = 0; i < n; i++) {
+      int charCode = columnTitle.codeUnitAt(i);
+
+      int charValue = charCode - asciiA + 1;
+
+      result = result * 26 + charValue;
     }
-    return res;
+
+    return result;
   }
 }
