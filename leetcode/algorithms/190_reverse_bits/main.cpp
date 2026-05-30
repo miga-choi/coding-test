7759ea68f5bec67aaf4d7a455f850ab86e3c8d80
miga-choi <miga.choi.1004@gmail.com>
2026-05-30 14:57:30 +0900

    leetcode: "190_reverse_bits" add c++ solution

--- leetcode/algorithms/190_reverse_bits/main.cpp
+++ leetcode/algorithms/190_reverse_bits/main.cpp
@@ -2,6 +2,11 @@
 
 class ReverseBits {
 public:
+    /**
+     * Complexities:
+     *   - Time Complexity: O(1)
+     *   - Space Complexity: O(1)
+     */
     uint32_t reverseBits(uint32_t n) {
         uint32_t result = 0;
 
@@ -16,12 +21,44 @@ public:
 
 
     // Solution
-    uint32_t solution(uint32_t n) {
-        n = ((n & 0xffff0000) >> 16) | ((n & 0x0000ffff) << 16);
-        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
-        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
-        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
-        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
+    /**
+     * Solution 1
+     *
+     * Complexities:
+     *   - Time Complexity: O(1)
+     *   - Space Complexity: O(1)
+     */
+    uint32_t solution1(uint32_t n) {
+        uint32_t result = 0;
+
+        for (int i = 0; i < 32; ++i) {
+            result = (result << 1) | (n & 1);
+            n >>= 1;
+        }
+
+        return result;
+    }
+
+    /**
+     * Solution 2
+     *
+     * Bit Hack
+     *
+     * Complexities:
+     *   - Time Complexity: O(1)
+     *   - Space Complexity: O(1)
+     */
+    uint32_t solution2(uint32_t n) {
+        n = ((n >> 16) & 0x0000FFFF) | ((n << 16) & 0xFFFF0000);
+        
+        n = ((n >> 8) & 0x00FF00FF) | ((n << 8) & 0xFF00FF00);
+        
+        n = ((n >> 4) & 0x0F0F0F0F) | ((n << 4) & 0xF0F0F0F0);
+        
+        n = ((n >> 2) & 0x33333333) | ((n << 2) & 0xCCCCCCCC);
+        
+        n = ((n >> 1) & 0x55555555) | ((n << 1) & 0xAAAAAAAA);
+        
         return n;
     }
 };
