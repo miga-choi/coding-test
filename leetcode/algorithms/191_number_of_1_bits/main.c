faf29f9098e5afd0919573e65e69861940cddc7c
miga-choi <miga.choi.1004@gmail.com>
2026-06-01 01:13:04 +0900

    leetcode: "191_number_of_1_bits" add c solution

--- leetcode/algorithms/191_number_of_1_bits/main.c
+++ leetcode/algorithms/191_number_of_1_bits/main.c
@@ -1,3 +1,10 @@
+/**
+ * Shift & Mask
+ * 
+ * Complexities:
+ *   - Time Complexity: O(1)
+ *   - Space Complexity: O(1)
+ */
 int hammingWeight(int n) {
     int count = 0;
 
@@ -13,13 +20,42 @@ int hammingWeight(int n) {
 
 
 // Solution
-int solution(int n) {
+/**
+ * Solution 1
+ * 
+ * Brian Kernighan's Algorithm
+ * 
+ * Complexities:
+ *   - Time Complexity: O(1)
+ *   - Space Complexity: O(1)
+ */
+int solution1(int n) {
     int count = 0;
-
-    while (n) {
-        n &= (n - 1);
+    
+    while (n > 0) {
+        n = n & (n - 1);
         count++;
     }
+    
+    return count;
+}
 
+/**
+ * Solution 2
+ * 
+ * Shift & Mask
+ * 
+ * Complexities:
+ *   - Time Complexity: O(1)
+ *   - Space Complexity: O(1)
+ */
+int solution2(int n) {
+    int count = 0;
+    
+    while (n > 0) {
+        count += (n & 1);
+        n >>= 1;
+    }
+    
     return count;
 }
