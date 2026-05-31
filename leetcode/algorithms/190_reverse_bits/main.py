4504428300df07589ea4bada667d59add4267083
miga-choi <miga.choi.1004@gmail.com>
2026-05-31 22:17:15 +0900

    leetcode: "190_reverse_bits" add python solution

--- leetcode/algorithms/190_reverse_bits/main.py
+++ leetcode/algorithms/190_reverse_bits/main.py
@@ -1,4 +1,9 @@
 class ReverseBits:
+    """
+    # Complexities:
+    #   - Time Complexity: O(1)
+    #   - Space Complexity: O(1)
+    """
     def reverseBits(self, n: int) -> int:
         n = bin(n).replace("0b", "")[::-1]
 
@@ -9,9 +14,31 @@ class ReverseBits:
 
 
     # Solution
-    def solution(self, n: int) -> int:
+    """
+    # Solution 1
+    #
+    # Bitwise Operations
+    #
+    # Complexities:
+    #   - Time Complexity: O(1)
+    #   - Space Complexity: O(1)
+    """
+    def solution1(self, n: int) -> int:
         res = 0
+
         for _ in range(32):
-            res = (res << 1) + (n & 1)
+            res = (res << 1) | (n & 1)
+
             n >>= 1
+
         return res
+
+    """
+    # Solution 2
+    #
+    # Complexities:
+    #   - Time Complexity: O(1)
+    #   - Space Complexity: O(1)
+    """
+    def solution2(self, n: int) -> int:
+        return int('{:032b}'.format(n)[::-1], 2)
