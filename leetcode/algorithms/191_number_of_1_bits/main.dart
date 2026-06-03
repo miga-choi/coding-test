bc2dfb0ee4fa438b86f1d5b8deb6bb799de7e219
miga-choi <miga.choi.1004@gmail.com>
2026-06-04 01:02:45 +0900

    leetcode: "191_number_of_1_bits" add dart solution

--- leetcode/algorithms/191_number_of_1_bits/main.dart
+++ leetcode/algorithms/191_number_of_1_bits/main.dart
@@ -1,4 +1,11 @@
 class NumberOf1Bits {
+  /**
+   * Bit Shift
+   *
+   * Complexities:
+   *   - Time Complexity: O(1)
+   *   - Space Complexity: O(1)
+   */
   int hammingWeight(int n) {
     int count = 0;
 
@@ -14,12 +21,60 @@ class NumberOf1Bits {
 
 
   // Solution
-  int solution(int n) {
-    int res = 0;
+  /**
+   * Solution 1
+   * 
+   * Brian Kernighan's Algorithm
+   *
+   * Complexities:
+   *   - Time Complexity: O(1)
+   *   - Space Complexity: O(1)
+   */
+  int solution1(int n) {
+    n = n & 0xFFFFFFFF;
+    int count = 0;
+
     while (n > 0) {
-      n &= n - 1;
-      res++;
+      n &= (n - 1);
+      count++;
     }
-    return res;
+
+    return count;
+  }
+
+  /**
+   * Solution 2
+   * 
+   * Bit Shift
+   *
+   * Complexities:
+   *   - Time Complexity: O(1)
+   *   - Space Complexity: O(1)
+   */
+  int solution2(int n) {
+    n = n & 0xFFFFFFFF;
+    int count = 0;
+
+    for (int i = 0; i < 32; i++) {
+      if ((n & 1) == 1) {
+        count++;
+      }
+      n >>>= 1;
+    }
+
+    return count;
+  }
+
+  /**
+   * Solution 3
+   * 
+   * Built-in Function
+   *
+   * Complexities:
+   *   - Time Complexity: O(1)
+   *   - Space Complexity: O(1)
+   */
+  int solution3(int n) {
+    return n.toRadixString(2).replaceAll('0', '').length;
   }
 }
