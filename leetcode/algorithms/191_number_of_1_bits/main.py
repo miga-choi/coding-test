4daa8946cc40549e32c8c612a721640dd47aba33
miga-choi <miga.choi.1004@gmail.com>
2026-06-03 14:26:57 +0900

    leetcode: "191_number_of_1_bits" add python solution

--- leetcode/algorithms/191_number_of_1_bits/main.py
+++ leetcode/algorithms/191_number_of_1_bits/main.py
@@ -1,27 +1,68 @@
-from collections import Counter
-
-
 class NumberOf1Bits:
+    """
+    # Built-in Function
+    #
+    # Complexities:
+    #   - Time Complexity: O(1)
+    #   - Space Complexity: O(1)
+    """
     def hammingWeight(self, n: int) -> int:
         n = bin(n).replace("0b", "")
         result = 0
+
         for c in n:
             if c == "1":
                 result += 1
+
         return result
 
 
     # Solution
-    # Solution 1: Using Inbuilt Counter
-    def solution1(self, n):
-        counter = Counter(bin(n)[2:])
-        return counter.get("1", 0)
-
-    # Solution 2: Using Bit Manipulation
-    def solution2(self, n):
+    """
+    # Solution 1
+    #
+    # Brian Kernighan's Algorithm
+    #
+    # Complexities:
+    #   - Time Complexity: O(1)
+    #   - Space Complexity: O(1)
+    """
+    def solution1(self, n: int) -> int:
         count = 0
-        while n:
-            if n & 1:
-                count += 1
-            n = n >> 1
+
+        while n > 0:
+            n &= (n - 1)
+            count += 1
+
         return count
+
+    """
+    # Solution 2
+    #
+    # Shift & Mask
+    #
+    # Complexities:
+    #   - Time Complexity: O(1)
+    #   - Space Complexity: O(1)
+    """
+    def solution2(self, n: int) -> int:
+        count = 0
+
+        while n > 0:
+            count += n & 1
+            n >>= 1
+
+        return count
+
+    """
+    # Solution 3
+    #
+    # Built-in Function
+    #
+    # Complexities:
+    #   - Time Complexity: O(1)
+    #   - Space Complexity: O(1)
+    """
+    def solution3(self, n: int) -> int:
+        # return bin(n).count('1')
+        return n.bit_count()
