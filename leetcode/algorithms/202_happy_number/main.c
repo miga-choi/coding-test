aa95bf4e203f45dfd8eb01b97681d2f83ed7eb84
miga-choi <miga.choi.1004@gmail.com>
2026-06-04 01:03:27 +0900

    leetcode: "202_happy_number" add c solution

--- leetcode/algorithms/202_happy_number/main.c
+++ leetcode/algorithms/202_happy_number/main.c
@@ -1,11 +1,21 @@
 #include <stdbool.h>
 
+/**
+ * Floyd's Cycle-Finding Algorithm
+ * 
+ * Complexities:
+ *   N - `n`
+ *   - Time Complexity: O(logᴺ)
+ *   - Space Complexity: O(1)
+ */
 int sum(int n) {
     int sum = 0;
+
     while (n) {
         sum += (n % 10) * (n % 10);
         n /= 10;
     }
+
     return sum;
 }
 
@@ -30,49 +40,34 @@ bool isHappy(int n) {
 
 
 // Solution
-// Solution 1
-int digitSquareSum(int n) {
-    int sum = 0, tmp;
-    while (n) {
-        tmp = n % 10;
-        sum += tmp * tmp;
+/**
+ * Floyd's Cycle-Finding Algorithm
+ * 
+ * Complexities:
+ *   N - `n`
+ *   - Time Complexity: O(logᴺ)
+ *   - Space Complexity: O(1)
+ */
+int getNext(int n) {
+    int sum = 0;
+
+    while (n > 0) {
+        int digit = n % 10;
+        sum += digit * digit;
         n /= 10;
     }
+
     return sum;
 }
 
-bool solution1(int n) {
-    int slow, fast;
-    slow = fast = n;
+bool isHappy(int n) {
+    int slow = n;
+    int fast = getNext(n);
 
-    do {
-        slow = digitSquareSum(slow);
-        fast = digitSquareSum(fast);
-        fast = digitSquareSum(fast);
-    } while (slow != fast);
-
-    if (slow == 1) {
-        return 1;
-    } else {
-        return 0;
+    while (fast != 1 && slow != fast) {
+        slow = getNext(slow);
+        fast = getNext(getNext(fast));
     }
-}
 
-// Solution 2
-bool solution2(int n) {
-    int total = 0;
-
-    while (true) {
-        while (n > 0) {
-            total += (n % 10) * (n % 10);
-            n /= 10;
-        }
-        if (total == 1) {
-            return true;
-        } else if (total == 4) {
-            return false;
-        }
-        n = total;
-        total = 0;
-    }
+    return fast == 1;
 }
