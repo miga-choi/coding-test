983d2e2b65d6e713234383f0154e0f2304e1ed05
miga-choi <miga.choi.1004@gmail.com>
2026-06-03 14:26:30 +0900

    leetcode: "191_number_of_1_bits" add java solution

--- leetcode/algorithms/191_number_of_1_bits/main.java
+++ leetcode/algorithms/191_number_of_1_bits/main.java
@@ -1,17 +1,56 @@
 class NumberOf1Bits {
-  // you need to treat n as an unsigned value
+  /**
+   * Built-in Function
+   *
+   * Complexities:
+   *   - Time Complexity: O(1)
+   *   - Space Complexity: O(1)
+   */
   public int hammingWeight(int n) {
     return Integer.toBinaryString(n).replaceAll("0", "").length();
   }
 
 
   // Solution
-  public static int solution(int n) {
-    int ones = 0;
-    while (n != 0) {
-      ones = ones + (n & 1);
-      n = n >>> 1;
+  /**
+   * Solution 1
+   *
+   * Bit Shift
+   *
+   * Complexities:
+   *   - Time Complexity: O(1)
+   *   - Space Complexity: O(1)
+   */
+  public static int solution1(int n) {
+    int count = 0;
+
+    for (int i = 0; i < 32; i++) {
+      if ((n & 1) == 1) {
+        count++;
+      }
+      n >>>= 1;
     }
-    return ones;
+
+    return count;
+  }
+
+  /**
+   * Solution 2
+   *
+   * Brian Kernighan's Algorithm
+   *
+   * Complexities:
+   *   - Time Complexity: O(1)
+   *   - Space Complexity: O(1)
+   */
+  public static int solution2(int n) {
+    int count = 0;
+
+    while (n != 0) {
+      n = n & (n - 1);
+      count++;
+    }
+
+    return count;
   }
 }
