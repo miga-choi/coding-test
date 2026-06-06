ae60026a9b17ab01c007c2639239f769c1efdc29
miga-choi <miga.choi.1004@gmail.com>
2026-06-06 14:25:52 +0900

    leetcode: "202_happy_number" add python solution

--- leetcode/algorithms/202_happy_number/main.py
+++ leetcode/algorithms/202_happy_number/main.py
@@ -1,50 +1,91 @@
 class HappyNumber:
+    """
+    # Hash Map
+    #
+    # Complexities:
+    #   N - `n`
+    #   - Time Complexity: O(logᴺ)
+    #   - Space Complexity: O(logᴺ)
+    """
     def isHappy(self, n: int) -> bool:
         if n <= 1:
             return True
+
         numDict: dict = {}
+
         while n > 1:
             nString = str(n)
+
             if numDict.get(nString) is True:
                 return False
+
             numDict[nString] = True
             n = 0
+
             for c in nString:
                 n += int(c) ** 2
+
             if n is 1:
                 return True
+
         return False
 
 
-# Solution
-# solution 1
-def squared(self, n):
-    result = 0
-    while n > 0:
-        last = n % 10
-        result += last * last
-        n = n // 10
-    return result
+    # Solution
+    """
+    # Solution 1
+    #
+    # Hash Set
+    #
+    # Complexities:
+    #   N - `n`
+    #   - Time Complexity: O(logᴺ)
+    #   - Space Complexity: O(logᴺ)
+    """
+    def solution1(self, n: int) -> bool:
+        def get_next(number: int) -> int:
+            total_sum = 0
 
-def solution1(self, n: int) -> bool:
-    # 20 -> 4 -> 16 -> 37 -> 58 -> 89 -> 145 > 42 -> 20
-    slow = self.squared(n)
-    fast = self.squared(self.squared(n))
+            while number > 0:
+                number, digit = divmod(number, 10)
+                total_sum += digit ** 2
 
-    while slow != fast and fast != 1:
-        slow = self.squared(slow)
-        fast = self.squared(self.squared(fast))
+            return total_sum
+        
+        seen = set()
+        
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = get_next(n)
+            
+        return n == 1
 
-    return fast == 1
 
-# solution 2
-def solution2(self, n):
-    mem = set()
-    while n != 1:
-        n = sum([int(i) ** 2 for i in str(n)])
-        if n in mem:
-            return False
-        else:
-            mem.add(n)
-    else:
-        return True
+    """
+    # Solution 2
+    #
+    # Floyd's Cycle-Finding Algorithm
+    #
+    # Complexities:
+    #   N - `n`
+    #   - Time Complexity: O(logᴺ)
+    #   - Space Complexity: O(1)
+    """
+    def solution2(self, n: int) -> bool:
+        def get_next(number: int) -> int:
+            total_sum = 0
+
+            while number > 0:
+                number, digit = divmod(number, 10)
+                total_sum += digit ** 2
+
+            return total_sum
+        
+        slow = n
+        fast = get_next(n)
+        
+        while fast != 1 and slow != fast:
+            slow = get_next(slow)
+            fast = get_next(get_next(fast))
+            
+        return fast == 1
