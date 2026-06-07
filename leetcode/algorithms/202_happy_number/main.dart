e5ed00223c50c337809a3d743b5e8c18e3fdf9d7
miga-choi <miga.choi.1004@gmail.com>
2026-06-07 21:05:07 +0900

    leetcode: "202_happy_number" add dart solution

--- leetcode/algorithms/202_happy_number/main.dart
+++ leetcode/algorithms/202_happy_number/main.dart
@@ -1,4 +1,12 @@
 class HappyNumber {
+  /**
+   * Floyd's Cycle-Finding Algorithm
+   *
+   * Complexities:
+   *   N - `n`
+   *   - Time Complexity: O(logᴺ)
+   *   - Space Complexity: O(1)
+   */
   int sum(int n) {
     int sum = 0;
     while (n > 0) {
@@ -28,18 +36,70 @@ class HappyNumber {
 
 
   // Solution
-  int getNext(int n) {
+  /**
+   * Solution 1
+   * 
+   * Floyd's Cycle-Finding Algorithm
+   *
+   * Complexities:
+   *   N - `n`
+   *   - Time Complexity: O(logᴺ)
+   *   - Space Complexity: O(1)
+   */
+  int _getNext1(int n) {
     int totalSum = 0;
+
     while (n > 0) {
-      int d = n % 10;
+      int digit = n % 10;
       n = n ~/ 10;
-      totalSum += d * d;
+      totalSum += digit * digit;
     }
+
     return totalSum;
   }
 
-  bool solution(int n) {
-    while (n != 1 && n != 4) n = getNext(n);
+  bool solution1(int n) {
+    int slow = n;
+    int fast = _getNext1(n);
+
+    while (fast != 1 && slow != fast) {
+      slow = _getNext1(slow);
+      fast = _getNext1(_getNext1(fast));
+    }
+
+    return fast == 1;
+  }
+
+  /**
+   * Solution 2
+   * 
+   * Hash Set
+   *
+   * Complexities:
+   *   N - `n`
+   *   - Time Complexity: O(logᴺ)
+   *   - Space Complexity: O(logᴺ)
+   */
+  int _getNext2(int n) {
+    int totalSum = 0;
+
+    while (n > 0) {
+      int digit = n % 10;
+      n = n ~/ 10;
+      totalSum += digit * digit;
+    }
+
+    return totalSum;
+  }
+
+  bool solution2(int n) {
+    final Set<int> seen = {};
+
+    while (n != 1 && !seen.contains(n)) {
+      seen.add(n);
+      n = _getNext2(n);
+    }
+
     return n == 1;
   }
 }
