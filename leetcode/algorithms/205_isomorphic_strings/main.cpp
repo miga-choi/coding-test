29eba96ea0824833774530d90daa79347832efcb
miga-choi <miga.choi.1004@gmail.com>
2026-06-11 01:02:30 +0900

    leetcode: "205_isomorphic_strings" add c++ solution

--- leetcode/algorithms/205_isomorphic_strings/main.cpp
+++ leetcode/algorithms/205_isomorphic_strings/main.cpp
@@ -1,8 +1,15 @@
-#include<string>
+#include <string>
+#include <vector>
 using namespace std;
 
 class RemoveLinkedListElements {
 public:
+    /**
+     * Complexities:
+     *   N - Size of `s`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
     bool isIsomorphic(string s, string t) {
         char sCharArray[256] = {0};
         char tCharArray[256] = {0};
@@ -21,14 +28,57 @@ public:
 
 
     // Solution
-    bool solution(string s, string t) {
-        int m1[256] = {0}, m2[256] = {0}, n = s.size();
-        for (int i = 0; i < n; ++i) {
-            if (m1[s[i]] != m2[t[i]])
+    /**
+     * Solution 1
+     * 
+     * Complexities:
+     *   N - Size of `s`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
+    bool solution1(string s, string t) {
+        vector<int> posS(256, 0);
+        vector<int> posT(256, 0);
+
+        for (int i = 0; i < s.length(); ++i) {
+            unsigned char charS = s[i];
+            unsigned char charT = t[i];
+
+            if (posS[charS] != posT[charT]) {
                 return false;
-            m1[s[i]] = i + 1;
-            m2[t[i]] = i + 1;
+            }
+
+            posS[charS] = i + 1;
+            posT[charT] = i + 1;
         }
+
+        return true;
+    }
+
+    /**
+     * Solution 2
+     * 
+     * Complexities:
+     *   N - Size of `s`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
+    bool solution2(string s, string t) {
+        char mapS[256] = {0};
+        char mapT[256] = {0};
+
+        for (int i = 0; i < s.length(); ++i) {
+            unsigned char charS = s[i];
+            unsigned char charT = t[i];
+
+            if (mapS[charS] == 0 && mapT[charT] == 0) {
+                mapS[charS] = charT;
+                mapT[charT] = charS;
+            } else if (mapS[charS] != charT || mapT[charT] != charS) {
+                return false;
+            }
+        }
+
         return true;
     }
 };
