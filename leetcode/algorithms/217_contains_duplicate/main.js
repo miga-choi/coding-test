7e6674b19f4cc793fb5e3cb70eecc7e1c6a30df2
miga-choi <miga.choi.1004@gmail.com>
2026-06-17 01:10:56 +0900

    leetcode: "217_contains_duplicate" add javascript solution

--- leetcode/algorithms/217_contains_duplicate/main.js
+++ leetcode/algorithms/217_contains_duplicate/main.js
@@ -1,8 +1,11 @@
 /**
- * Set
- * - Time Complexity: O(N)
- * - Space Complexity: O(N)
+ * Hash Set
  *
+ * Complexities:
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+/**
  * @param {number[]} nums
  * @return {boolean}
  */
@@ -27,14 +30,53 @@ var containsDuplicate = function (nums) {
 
 // Solution
 /**
- * Set
- * - Time Complexity: O(N)
- * - Space Complexity: O(N)
+ * Solution 1
  *
+ * Hash Set
+ *
+ * Complexities:
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+/**
  * @param {number[]} nums
  * @return {boolean}
  */
-var solution = function (nums) {
-  const s = new Set(nums);
-  return s.size !== nums.length;
+var solution1 = function (nums) {
+  const seen = new Set();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (seen.has(nums[i])) {
+      return true;
+    }
+    seen.add(nums[i]);
+  }
+
+  return false;
+  // return new Set(nums).size !== nums.length;
+};
+
+/**
+ * Solution 2
+ *
+ * Sort
+ *
+ * Complexities:
+ *   - Time Complexity: O(N * logᴺ)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var solution2 = function (nums) {
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] === nums[i + 1]) {
+      return true;
+    }
+  }
+
+  return false;
 };
