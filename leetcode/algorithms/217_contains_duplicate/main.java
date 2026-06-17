17b9e2e359174a29e1cdab5c053cc2ad09362e2e
miga-choi <miga.choi.1004@gmail.com>
2026-06-18 01:44:19 +0900

    leetcode: "217_contains_duplicate" add java solution

--- leetcode/algorithms/217_contains_duplicate/main.java
+++ leetcode/algorithms/217_contains_duplicate/main.java
@@ -1,11 +1,14 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 class ContainsDuplicate {
   /**
-   * Set
-   * - Time Complexity: O(N)
-   * - Space Complexity: O(N)
+   * Hash Set
+   *
+   * Complexities:
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(N)
    */
   public boolean containsDuplicate(int[] nums) {
     if (nums.length < 2) {
@@ -19,6 +22,50 @@ class ContainsDuplicate {
         return true;
       } else {
         set.add(num);
+      }
+    }
+
+    return false;
+  }
+
+
+  // Solution
+  /**
+   * Solution 1
+   *
+   * Hash Set
+   *
+   * Complexities:
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(N)
+   */
+  public boolean solution1(int[] nums) {
+    Set<Integer> set = new HashSet<>();
+
+    for (int num : nums) {
+      if (!set.add(num)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * Solution 2
+   *
+   * Sort
+   *
+   * Complexities:
+   *   - Time Complexity: O(N * logᴺ)
+   *   - Space Complexity: O(1)
+   */
+  public boolean solution2(int[] nums) {
+    Arrays.sort(nums);
+
+    for (int i = 0; i < nums.length - 1; i++) {
+      if (nums[i] == nums[i + 1]) {
+        return true;
       }
     }
 
