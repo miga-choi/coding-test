eca745138ede71fcd6b8bf15df6395eb26b25dec
miga-choi <miga.choi.1004@gmail.com>
2026-06-18 01:44:58 +0900

    leetcode: "217_contains_duplicate" add python solution

--- leetcode/algorithms/217_contains_duplicate/main.py
+++ leetcode/algorithms/217_contains_duplicate/main.py
@@ -2,11 +2,13 @@ from typing import List, Set
 
 
 class ContainsDuplicate:
-    ##
-    # Set
-    # - Time Complexity: O(N)
-    # - Space Complexity: O(N)
+    """
+    # Hash Set
     #
+    # Complexities:
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(N)
+    """
     def containsDuplicate(self, nums: List[int]) -> bool:
         if len(nums) < 2:
             return False
@@ -23,11 +25,47 @@ class ContainsDuplicate:
 
 
     # Solution
-    ##
-    # Set
-    # - Time Complexity: O(N)
-    # - Space Complexity: O(N)
+    """
+    # Solution 1
     #
-    def solution(self, nums: List[int]) -> bool:
-        hset: Set = set(nums)
-        return len(nums) != len(hset)
+    # Hash Set
+    #
+    # Complexities:
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(N)
+    """
+    def solution1(self, nums: List[int]) -> bool:
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+
+        return False
+
+    """
+    # Solution 2
+    #
+    # Complexities:
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(N)
+    """
+    def solution2(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
+
+    """
+    # Solution 3
+    #
+    # Complexities:
+    #   - Time Complexity: O(N * logᴺ)
+    #   - Space Complexity: O(1)
+    """
+    def solution3(self, nums: List[int]) -> bool:
+        nums.sort()
+
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return True
+
+        return False
