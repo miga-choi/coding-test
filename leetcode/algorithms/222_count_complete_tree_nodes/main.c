142b5531c608973a8b9cfce70c8d61b2506b8ebd
miga-choi <miga.choi.1004@gmail.com>
2026-06-22 07:04:23 +0900

    leetcode: "222_count_complete_tree_nodes" add c solution

--- leetcode/algorithms/222_count_complete_tree_nodes/main.c
+++ leetcode/algorithms/222_count_complete_tree_nodes/main.c
@@ -6,8 +6,15 @@ struct TreeNode {
     struct TreeNode* right;
 };
 
+/**
+ * Complexities:
+ *   N - Number of nodes in `root`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(logᴺ)
+ */
 int countNodes(struct TreeNode* root) {
     int count = 0;
+
     if (root) {
         count++;
         if (root->left) {
@@ -17,16 +24,47 @@ int countNodes(struct TreeNode* root) {
             count += countNodes(root->right);
         }
     }
+
     return count;
 }
 
 
 // Solution
+/**
+ * Complexities:
+ *   N - Number of nodes in `root`
+ *   - Time Complexity: O(log₂ᴺ)
+ *   - Space Complexity: O(logᴺ)
+ */
+int getLeftHeight(struct TreeNode* node) {
+    int height = 0;
+    while (node != NULL) {
+        height++;
+        node = node->left;
+    }
+    return height;
+}
+
+int getRightHeight(struct TreeNode* node) {
+    int height = 0;
+    while (node != NULL) {
+        height++;
+        node = node->right;
+    }
+    return height;
+}
+
 int solution(struct TreeNode* root) {
     if (root == NULL) {
         return 0;
     }
-    int a = solution(root->left);
-    int b = solution(root->right);
-    return a + b + 1;
+
+    int lh = getLeftHeight(root);
+    int rh = getRightHeight(root);
+
+    if (lh == rh) {
+        return (1 << lh) - 1;
+    }
+
+    return 1 + solution(root->left) + solution(root->right);
 }
