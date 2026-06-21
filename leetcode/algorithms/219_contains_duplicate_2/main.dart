1bbfa6e0db4e8dbe120e84cb8a701e742baf15f2
miga-choi <miga.choi.1004@gmail.com>
2026-06-22 07:03:30 +0900

    leetcode: "219_contains_duplicate_2" add dart solution

--- leetcode/algorithms/219_contains_duplicate_2/main.dart
+++ leetcode/algorithms/219_contains_duplicate_2/main.dart
@@ -1,6 +1,15 @@
 class ContainsDuplicateII {
+  /**
+   * Hash Map
+   *
+   * Complexities:
+   *   N - Size of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(N)
+   */
   bool containsNearbyDuplicate(List<int> nums, int k) {
     Map<int, int> numsMap = Map();
+
     for (int i = 0; i < nums.length; i++) {
       if (numsMap[nums[i]] == null) {
         numsMap[nums[i]] = i;
@@ -11,19 +20,62 @@ class ContainsDuplicateII {
         numsMap[nums[i]] = i;
       }
     }
+
     return false;
   }
 
 
   // Solution
-  bool solution(List<int> nums, int k) {
+  /**
+   * Solution 1
+   *
+   * Sliding Window + Hash Set
+   *
+   * Complexities:
+   *   N - Size of `nums`
+   *   K - `k`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(min(N, K))
+   */
+  bool solution1(List<int> nums, int k) {
+    final Set<int> window = {};
+
     for (int i = 0; i < nums.length; i++) {
-      for (int j = i + 1; j < nums.length && j <= i + k; j++) {
-        if ((nums[i] == nums[j])) {
-          return true;
-        }
+      if (i > k) {
+        window.remove(nums[i - k - 1]);
+      }
+
+      if (!window.add(nums[i])) {
+        return true;
       }
     }
+
+    return false;
+  }
+
+  /**
+   * Solution 2
+   *
+   * Hash Map
+   *
+   * Complexities:
+   *   N - Size of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(N)
+   */
+  bool solution2(List<int> nums, int k) {
+    final Map<int, int> lastSeen = {};
+
+    for (int i = 0; i < nums.length; i++) {
+      int num = nums[i];
+
+      if (lastSeen.containsKey(num) && (i - lastSeen[num]!) <= k) {
+        return true;
+      }
+
+      lastSeen[num] = i;
+    }
+
     return false;
   }
 }
