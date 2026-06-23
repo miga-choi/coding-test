20d6c8f72847e67de3c0789339192635eada1161
miga-choi <miga.choi.1004@gmail.com>
2026-06-24 07:22:20 +0900

    leetcode: "222_count_complete_tree_nodes" add python solution

--- leetcode/algorithms/222_count_complete_tree_nodes/main.py
+++ leetcode/algorithms/222_count_complete_tree_nodes/main.py
@@ -1,13 +1,21 @@
 from typing import Optional
 
 
-class CountCompleteTreeNodes:
-    class TreeNode:
-        def __init__(self, val=0, left=None, right=None):
-            self.val = val
-            self.left = left
-            self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
+class CountCompleteTreeNodes:
+    """
+    # DFS
+    #
+    # Complexities:
+    #   N - Number of nodes in `root`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(logᴺ)
+    """
     def countNodes(self, root: Optional[TreeNode]) -> int:
         if root == None:
             return 0
@@ -15,40 +23,36 @@ class CountCompleteTreeNodes:
 
 
     # Solution
-    def solution(self, root: TreeNode) -> int:
-        ## RC ##
-        ## APPROACH : RECURSION ##
-        ## TIME COMPLEXICITY : LOG N * LOG N ##
-
-        ## LOGIC ##
-        # If left sub tree height equals right sub tree height then,
-        #       a. left sub tree is perfect binary tree
-        #       b. right sub tree is complete binary tree
-        # If left sub tree height greater than right sub tree height then,
-        #       a. left sub tree is complete binary tree
-        #       b. right sub tree is perfect binary tree
-
+    """
+    # Perfect Binary Tree
+    #
+    # Complexities:
+    #   N - Number of nodes in `root`
+    #   - Time Complexity: O(log₂ᴺ)
+    #   - Space Complexity: O(logᴺ)
+    """
+    def solution(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-
-        def depthLeft(node):
-            d = 0
+        
+        def get_left_height(node: Optional[TreeNode]) -> int:
+            height = 0
             while node:
-                d += 1
+                height += 1
                 node = node.left
-            return d
-
-        def depthRight(node):
-            d = 0
+            return height
+            
+        def get_right_height(node: Optional[TreeNode]) -> int:
+            height = 0
             while node:
-                d += 1
+                height += 1
                 node = node.right
-            return d
-
-        ld = depthLeft(root.left)
-        rd = depthRight(root.right)
-
-        if ld == rd:
-            return 2 ** (ld + 1) - 1
-        else:
-            return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+            return height
+            
+        left_h = get_left_height(root)
+        right_h = get_right_height(root)
+        
+        if left_h == right_h:
+            return (1 << left_h) - 1
+            
+        return 1 + self.solution(root.left) + self.solution(root.right)
