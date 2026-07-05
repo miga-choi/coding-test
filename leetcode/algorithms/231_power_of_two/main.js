f71a35f4f4b01b7aa17cda98230aa7dbbc107be7
miga-choi <miga.choi.1004@gmail.com>
2026-07-05 13:57:40 +0900

    leetcode: "231_power_of_two" add javascript solution

--- leetcode/algorithms/231_power_of_two/main.js
+++ leetcode/algorithms/231_power_of_two/main.js
@@ -17,4 +17,43 @@ var isPowerOfTwo = function (n) {
 
 
 // Solution
-let solution = (n) => (n > 0 ? !(n & (n - 1)) : false);
+/**
+ * solution 1
+ *
+ * bit manipulation
+ *
+ * complexities:
+ *   - time complexity: o(1)
+ *   - space complexity: o(1)
+ */
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var solution1 = function (n) {
+  return n > 0 && (n & (n - 1)) === 0;
+};
+
+/**
+ * Solution 2
+ *
+ * Complexities:
+ *   N - `n`
+ *   - Time Complexity: O(logᴺ)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var solution2 = function (n) {
+  if (n <= 0) {
+    return false;
+  }
+
+  while (n % 2 === 0) {
+    n = n / 2;
+  }
+
+  return n === 1;
+};
