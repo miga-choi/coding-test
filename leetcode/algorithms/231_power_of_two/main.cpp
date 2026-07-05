da23ab444634bc0641c4f5742641de940883f359
miga-choi <miga.choi.1004@gmail.com>
2026-07-05 13:57:04 +0900

    leetcode: "231_power_of_two" add c++ solution

--- leetcode/algorithms/231_power_of_two/main.cpp
+++ leetcode/algorithms/231_power_of_two/main.cpp
@@ -1,5 +1,11 @@
 class PowerOfTwo {
 public:
+    /**
+     * Complexities:
+     *   N - `n`
+     *   - Time Complexity: O(logᴺ)
+     *   - Space Complexity: O(1)
+     */
     bool isPowerOfTwo(int n) {
         if (n <= 0) {
             return false;
@@ -17,7 +23,14 @@ public:
 
 
     // Solution
+    /**
+     * Bit Manipulation
+     *
+     * Complexities:
+     *   - Time Complexity: O(1)
+     *   - Space Complexity: O(1)
+     */
     bool solution(int n) {
-        return n > 0 && not(n & n - 1);
+        return n > 0 && (n & (n - 1)) == 0;
     }
 };
