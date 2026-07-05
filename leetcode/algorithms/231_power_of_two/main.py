08d0ac87dbace448b40bf09407fca226ff3c198d
miga-choi <miga.choi.1004@gmail.com>
2026-07-06 01:07:23 +0900

    leetcode: "231_power_of_two" add python solution

--- leetcode/algorithms/231_power_of_two/main.py
+++ leetcode/algorithms/231_power_of_two/main.py
@@ -1,4 +1,10 @@
 class PowerOfTwo:
+    """
+    # Complexities:
+    #   N - `n`
+    #   - Time Complexity: O(logᴺ)
+    #   - Space Complexity: O(1)
+    """
     def isPowerOfTwo(self, n: int) -> bool:
         m = 0
 
@@ -11,10 +17,38 @@ class PowerOfTwo:
 
 
     # Solution
-    # Solution 1: O(1)
+    """
+    # Solution 1
+    #
+    # Bit Manipulation
+    #
+    # Complexities:
+    #   - Time Complexity: O(1)
+    #   - Space Complexity: O(1)
+    """
     def solution1(self, n: int) -> bool:
-        return n > 0 and n & (n - 1) == 0
+        return n > 0 and (n & (n - 1)) == 0
 
-    # Solution 2: O(n)
+    """
+    # Solution 2
+    #
+    # 최하위 비트(LSB) 추출
+    #
+    # Complexities:
+    #   - Time Complexity: O(1)
+    #   - Space Complexity: O(1)
+    """
     def solution2(self, n: int) -> bool:
-        return n > 0 and sum(list(map(int, bin(n)[2:]))) == 1
+        return n > 0 and (n & -n) == n
+
+    """
+    # Solution 3
+    #
+    # Built-in Function
+    #
+    # Complexities:
+    #   - Time Complexity: O(1)
+    #   - Space Complexity: O(1)
+    """
+    def solution3(self, n: int) -> bool:
+        return n > 0 and n.bit_count() == 1
