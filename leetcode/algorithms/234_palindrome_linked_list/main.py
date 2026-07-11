1eb1eeca6c37143dfcc50a299b35e2aab794a89e
miga-choi <miga.choi.1004@gmail.com>
2026-07-12 03:13:31 +0900

    leetcode: "234_palindrome_linked_list" add python solution

--- leetcode/algorithms/234_palindrome_linked_list/main.py
+++ leetcode/algorithms/234_palindrome_linked_list/main.py
@@ -8,6 +8,12 @@ class ListNode:
 
 
 class PalindromeLinkedList:
+    """
+    # Complexities:
+    #   N - The number of nodes in `head`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         obverse = []
         reverse = []
@@ -39,22 +45,66 @@ class PalindromeLinkedList:
 
 
     # Solution
-    def solution(self, head: ListNode) -> bool:
-        slow, fast, prev = head, head, None
+    """
+    # Solution 1
+    #
+    # Complexities:
+    #   N - The number of nodes in `head`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(N)
+    """
+    def solution1(self, head: ListNode) -> bool:
+        vals = []
+        curr = head
 
+        while curr:
+            vals.append(curr.val)
+            curr = curr.next
+
+        return vals == vals[::-1]
+
+    """
+    # Solution 2
+    #
+    # Complexities:
+    #   N - The number of nodes in `head`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
+    def solution2(self, head: ListNode) -> bool:
+        if not head or not head.next:
+            return True
+            
+        slow, fast = head, head
         while fast and fast.next:
-            slow, fast = slow.next, fast.next.next
-
-        prev, slow, prev.next = slow, slow.next, None
-
-        while slow:
-            slow.next, prev, slow = prev, slow, slow.next
-
-        fast, slow = head, prev
-
-        while slow:
-            if fast.val != slow.val:
-                return False
-            fast, slow = fast.next, slow.next
-
-        return True
+            slow = slow.next
+            fast = fast.next.next
+            
+        prev = None
+        curr = slow
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+            
+        p1 = head
+        p2 = prev
+        is_palindrome = True
+        
+        while p2:
+            if p1.val != p2.val:
+                is_palindrome = False
+                break
+            p1 = p1.next
+            p2 = p2.next
+            
+        curr = prev
+        prev_restore = None
+        while curr:
+            next_node = curr.next
+            curr.next = prev_restore
+            prev_restore = curr
+            curr = next_node
+            
+        return is_palindrome
