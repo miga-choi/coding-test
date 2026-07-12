e45fb5d2f892cc335bbf27c371f604ee93f214f9
miga-choi <miga.choi.1004@gmail.com>
2026-07-13 01:19:26 +0900

    leetcode: "242_valid_anagram" add c solution

--- leetcode/algorithms/242_valid_anagram/main.c
+++ leetcode/algorithms/242_valid_anagram/main.c
@@ -1,11 +1,13 @@
 #include <stdbool.h>
-#include <stdlib.h>
 #include <string.h>
 
 /**
- * Frequency Counter
- * - Time Complexity: O(N)
- * - Space Complexity: O(1)
+ * Frequency Counting
+ * 
+ * Complexities:
+ *   N - The size of `s`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
  */
 bool isAnagram(char* s, char* t) {
     int len = strlen(s);
@@ -33,13 +35,14 @@ bool isAnagram(char* s, char* t) {
 
 // Solution
 /**
- * Solution 1
+ * Frequency Counting
  * 
- * Frequency Counter
- * - Time Complexity: O(N)
- * - Space Complexity: O(1)
+ * Complexities:
+ *   N - The size of `s`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
  */
-bool solution1(char* s, char* t) {
+bool solution(char* s, char* t) {
     int len_s = strlen(s);
     int len_t = strlen(t);
 
@@ -47,50 +50,18 @@ bool solution1(char* s, char* t) {
         return false;
     }
 
-    int char_counts[26] = {0};
+    int counts[26] = {0};
 
     for (int i = 0; i < len_s; i++) {
-        char_counts[s[i] - 'a']++;
-    }
-
-    for (int i = 0; i < len_t; i++) {
-        char_counts[t[i] - 'a']--;
+        counts[s[i] - 'a']++;
+        counts[t[i] - 'a']--;
     }
 
     for (int i = 0; i < 26; i++) {
-        if (char_counts[i] != 0) {
+        if (counts[i] != 0) {
             return false;
         }
     }
 
     return true;
-}
-
-/**
- * Solution 2
- * 
- * Sort
- * - Time Complexity: O(N * logᴺ)
- * - Space Complexity: O(1) or O(logᴺ)
- */
-int compare_chars(const void* a, const void* b) {
-    return (*(char*)a - *(char*)b);
-}
-
-bool solution2(char* s, char* t) {
-    int len_s = strlen(s);
-    int len_t = strlen(t);
-
-    if (len_s != len_t) {
-        return false;
-    }
-
-    qsort(s, len_s, sizeof(char), compare_chars);
-    qsort(t, len_t, sizeof(char), compare_chars);
-
-    if (strcmp(s, t) == 0) {
-        return true;
-    } else {
-        return false;
-    }
 }
