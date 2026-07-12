4646c49704e22d09a16a80b02e63d1fb662c6863
miga-choi <miga.choi.1004@gmail.com>
2026-07-13 01:18:48 +0900

    leetcode: "234_palindrome_linked_list" add dart solution

--- leetcode/algorithms/234_palindrome_linked_list/main.dart
+++ leetcode/algorithms/234_palindrome_linked_list/main.dart
@@ -5,6 +5,12 @@ class ListNode {
 }
 
 class PalindromeLinkedList {
+  /**
+   * Complexities:
+   *   N - The number of nodes in `head`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
   bool isPalindrome(ListNode? head) {
     final List<int> intList = List<int>.empty(growable: true);
 
@@ -25,12 +31,53 @@ class PalindromeLinkedList {
 
 
   // Solution
+  /**
+   * Complexities:
+   *   N - The number of nodes in `head`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
   bool solution(ListNode? head) {
-    final values = <int>[];
-    while (head != null) {
-      values.add(head.val);
-      head = head.next;
+    if (head == null || head.next == null) {
+      return true;
     }
-    return values.join('') == values.reversed.join('');
+
+    ListNode? slow = head;
+    ListNode? fast = head;
+    while (fast != null && fast.next != null) {
+      slow = slow?.next;
+      fast = fast.next?.next;
+    }
+
+    ListNode? secondHalfHead = _reverse(slow);
+
+    ListNode? p1 = head;
+    ListNode? p2 = secondHalfHead;
+    bool isPalin = true;
+
+    while (p2 != null) {
+      if (p1!.val != p2.val) {
+        isPalin = false;
+        break;
+      }
+      p1 = p1.next;
+      p2 = p2.next;
+    }
+
+    _reverse(secondHalfHead);
+
+    return isPalin;
+  }
+
+  ListNode? _reverse(ListNode? head) {
+    ListNode? prev = null;
+    ListNode? curr = head;
+    while (curr != null) {
+      ListNode? nextNode = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = nextNode;
+    }
+    return prev;
   }
 }
