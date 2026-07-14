4392528112357d62db6de8dba668f34036d6c176
miga-choi <miga.choi.1004@gmail.com>
2026-07-14 18:02:44 +0900

    leetcode: "242_valid_anagram" add c++ solution

--- leetcode/algorithms/242_valid_anagram/main.cpp
+++ leetcode/algorithms/242_valid_anagram/main.cpp
@@ -1,14 +1,18 @@
 #include <algorithm>
 #include <string>
+#include <unordered_map>
 #include <vector>
 using namespace std;
 
 class ValidAnagram {
 public:
     /**
-     * Frequency Counter
-     * - Time Complexity: O(N)
-     * - Space Complexity: O(1)
+     * Frequency Counting
+     *
+     * Complexities:
+     *   N - The size of `s`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
      */
     bool isAnagram(string s, string t) {
         int len = s.length();
@@ -38,24 +42,27 @@ public:
     /**
      * Solution 1
      * 
-     * Frequency Counter
-     * - Time Complexity: O(N)
-     * - Space Complexity: O(1)
+     * Frequency Counting
+     *
+     * Complexities:
+     *   N - The size of `s`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
      */
     bool solution1(string s, string t) {
         if (s.length() != t.length()) {
             return false;
         }
 
-        vector<int> counts(26, 0);
+        vector<int> count(26, 0);
 
         for (int i = 0; i < s.length(); ++i) {
-            counts[s[i] - 'a']++;
-            counts[t[i] - 'a']--;
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
         }
 
-        for (int count : counts) {
-            if (count != 0) {
+        for (int val : count) {
+            if (val != 0) {
                 return false;
             }
         }
@@ -66,15 +73,49 @@ public:
     /**
      * Solution 2
      *
-     * Sort
-     * - Time Complexity: O(N * logᴺ)
-     * - Space Complexity: O(1) or O(logᴺ)
+     * Unicode
+     * 
+     * Complexities:
+     *   N - The size of `s`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(N)
      */
     bool solution2(string s, string t) {
         if (s.length() != t.length()) {
             return false;
         }
 
+        unordered_map<char, int> count;
+
+        for (int i = 0; i < s.length(); ++i) {
+            count[s[i]]++;
+            count[t[i]]--;
+        }
+
+        for (auto const& [key, val] : count) {
+            if (val != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Solution 3
+     *
+     * Sorting
+     *
+     * Complexities:
+     *   N - The size of `s`
+     *   - Time Complexity: O(N * logᴺ)
+     *   - Space Complexity: O(N)
+     */
+    bool solution3(string s, string t) {
+        if (s.size() != t.size()) {
+            return false;
+        }
+        
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
 
