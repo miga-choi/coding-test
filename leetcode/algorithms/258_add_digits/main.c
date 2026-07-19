25bb0f8f3356c09f619eaf6db5dedf47994e2b25
miga-choi <miga.choi.1004@gmail.com>
2026-07-19 14:27:52 +0900

    leetcode: "258_add_digits" add c solution

--- leetcode/algorithms/258_add_digits/main.c
+++ leetcode/algorithms/258_add_digits/main.c
@@ -1,5 +1,13 @@
+/**
+ * Iterative
+ * 
+ * Complexities:
+ *   - Time Complexity: O(log₁₀ᴺ)
+ *   - Space Complexity: O(1)
+ */
 int addDigits(int num) {
     int sum = num;
+
     while (sum >= 10) {
         sum = 0;
         while (num > 0) {
@@ -8,21 +16,48 @@ int addDigits(int num) {
         }
         num = sum;
     }
+
     return sum;
 }
 
 
 // Solution
-int solution(int num) {
-    int sum = 0;
-    while (num) {
-        sum += (num % 10);
-        num /= 10;
+/**
+ * Solution 1
+ * 
+ * Digital Root
+ * 
+ * Complexities:
+ *   - Time Complexity: O(1)
+ *   - Space Complexity: O(1)
+ */
+int solution1(int num) {
+    if (num == 0) {
+        return 0;
     }
-    if (sum < 10) {
-        return sum;
+    
+    return (num - 1) % 9 + 1;
+}
+
+/**
+ * Solution 2
+ * 
+ * Iterative
+ * 
+ * Complexities:
+ *   - Time Complexity: O(log₁₀ᴺ)
+ *   - Space Complexity: O(1)
+ */
+int solution2(int num) {
+    while (num >= 10) {
+        int sum = 0;
+
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        num = sum;
     }
-    else {
-        return addDigits(sum);
-    }
+
+    return num;
 }
