1c95db641e8d2aef3c490b81fe42dc9bd9a650f7
miga-choi <miga.choi.1004@gmail.com>
2026-07-20 01:17:32 +0900

    leetcode: "258_add_digits" add javascript solution

--- leetcode/algorithms/258_add_digits/main.js
+++ leetcode/algorithms/258_add_digits/main.js
@@ -1,21 +1,77 @@
 /**
+ * Iterative
+ *
+ * Complexities:
+ *   - Time Complexity: O(log₁₀ᴺ)
+ *   - Space Complexity: O(1)
+ */
+/**
  * @param {number} num
  * @return {number}
  */
 var addDigits = function (num) {
   let numString = num.toString();
+
   while (numString.length > 1) {
     let num = 0;
+
     for (const num_ of numString) {
       num += parseInt(num_);
     }
+
     numString = num.toString();
   }
+
   return numString;
 };
 
 
 // Solution
-var solution = function (num) {
-  return 1 + ((num - 1) % 9);
+/**
+ * Solution 1
+ *
+ * Digital Root
+ *
+ * Complexities:
+ *   - Time Complexity: O(1)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var solution1 = function (num) {
+  if (num === 0) {
+    return 0;
+  }
+
+  return ((num - 1) % 9) + 1;
+};
+
+/**
+ * Solution 2
+ *
+ * Iterative
+ *
+ * Complexities:
+ *   - Time Complexity: O(log₁₀ᴺ)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var solution2 = function (num) {
+  while (num >= 10) {
+    let sum = 0;
+
+    while (num > 0) {
+      sum += num % 10;
+      num = Math.floor(num / 10);
+    }
+
+    num = sum;
+  }
+
+  return num;
 };
