5e751f3ba7af907524368f2630a76d93966a44fb
miga-choi <miga.choi.1004@gmail.com>
2026-07-21 07:17:48 +0900

    leetcode: "258_add_digits" add python solution

--- leetcode/algorithms/258_add_digits/main.py
+++ leetcode/algorithms/258_add_digits/main.py
@@ -1,16 +1,58 @@
 class AddDigits:
+    """
+    # Iterative
+    #
+    # Complexities:
+    #   - Time Complexity: O(log₁₀ᴺ)
+    #   - Space Complexity: O(1)
+    """
     def addDigits(self, num: int) -> int:
         numString = str(num)
+
         while len(numString) > 1:
             num = 0
+
             for i in range(len(numString)):
                 num += int(numString[i])
+
             numString = str(num)
+
         return num
 
 
     # Solution
-    def solution(self, num: int) -> int:
-        while num > 9:
-            num = num % 10 + num // 10
+    """
+    # Solution 1
+    #
+    # Digital Root
+    #
+    # Complexities:
+    #   - Time Complexity: O(1)
+    #   - Space Complexity: O(1)
+    """
+    def solution1(self, num: int) -> int:
+        if num == 0:
+            return 0
+
+        return 1 + (num - 1) % 9
+
+    """
+    # Solution 2
+    #
+    # Iterative
+    #
+    # Complexities:
+    #   - Time Complexity: O(log₁₀ᴺ)
+    #   - Space Complexity: O(1)
+    """
+    def solution2(self, num: int) -> int:
+        while num >= 10:
+            total_sum = 0
+
+            while num > 0:
+                num, digit = divmod(num, 10)
+                total_sum += digit
+
+            num = total_sum
+
         return num
