3a76b565654df9f7b17e2be95e28c6e5b7dda2c5
miga-choi <miga.choi.1004@gmail.com>
2026-07-22 01:07:58 +0900

    leetcode: "258_add_digits" add dart solution

--- leetcode/algorithms/258_add_digits/main.dart
+++ leetcode/algorithms/258_add_digits/main.dart
@@ -1,36 +1,68 @@
 class AddDigits {
+  /**
+   * Iterative
+   *
+   * Complexities:
+   *   - Time Complexity: O(log₁₀ᴺ)
+   *   - Space Complexity: O(1)
+   */
   int addDigits(int num) {
     int sum = num;
+
     while (sum >= 10) {
       sum = 0;
+
       while (num > 0) {
         sum += num % 10;
         num = (num / 10).floor();
       }
+
       num = sum;
     }
+
     return sum;
   }
 
 
   // Solution
-  // Solution 1
+  /**
+   * Solution 1
+   *
+   * Digital Root
+   *
+   * Complexities:
+   *   - Time Complexity: O(1)
+   *   - Space Complexity: O(1)
+   */
   int solution1(int num) {
-    int temp = 0;
-    while (num > 0) {
-      temp = temp + num % 10;
-      num = num ~/ 10;
-
-      if (num == 0 && temp > 9) {
-        num = temp;
-        temp = 0;
-      }
+    if (num == 0) {
+      return 0;
     }
-    return temp;
+
+    return 1 + (num - 1) % 9;
   }
 
-  // Solution 2
+  /**
+   * Solution 2
+   *
+   * Iterative
+   *
+   * Complexities:
+   *   - Time Complexity: O(log₁₀ᴺ)
+   *   - Space Complexity: O(1)
+   */
   int solution2(int num) {
-    return num == 0 ? 0 : 1 + (num - 1) % 9;
+    while (num >= 10) {
+      int sum = 0;
+
+      while (num > 0) {
+        sum += num % 10;
+        num = num ~/ 10;
+      }
+
+      num = sum;
+    }
+
+    return num;
   }
 }
