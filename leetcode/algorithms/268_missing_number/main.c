b963dbb9c9e9c4e041f6ed121ad3638ac7bf1d9a
miga-choi <miga.choi.1004@gmail.com>
2026-07-25 13:47:04 +0900

    leetcode: "268_missing_number" add c solution

--- leetcode/algorithms/268_missing_number/main.c
+++ leetcode/algorithms/268_missing_number/main.c
@@ -1,62 +1,59 @@
+/**
+ * Complexities:
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
 int missingNumber(int *nums, int numsSize) {
     int sum = 0;
     int originSum = 0;
+
     for (int i = 0; i <= numsSize; i++) {
         originSum += i;
         if (i < numsSize) {
             sum += nums[i];
         }
     }
+
     return originSum - sum;
 }
 
 
 // Solution
-// Solution 1: Naive Approach - Sorting
-int compare(const void *a, const void *b) {
-    return (*(int *)a - *(int *)b);
-}
-
+/**
+ * Solution 1
+ * 
+ * Complexities:
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
 int solution1(int *nums, int numsSize) {
-    qsort(nums, numsSize, sizeof(int), compare);
-    for (int i = 0; i < numsSize; i++) {
-        if (nums[i] != i) {
-            return i;
-        }
+    int n = numsSize;
+    
+    int expectedSum = n * (n + 1) / 2;
+    
+    int actualSum = 0;
+    for (int i = 0; i < n; i++) {
+        actualSum += nums[i];
     }
-    return numsSize;
+    
+    return expectedSum - actualSum;
 }
 
-// Solution 2: Mathematical Formula
+/**
+ * Solution 2
+ * 
+ * XOR
+ * 
+ * Complexities:
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
 int solution2(int *nums, int numsSize) {
-    int sum = numsSize * (numsSize + 1) / 2;
-    for (int i = 0; i < numsSize; i++) {
-        sum -= nums[i];
-    }
-    return sum;
-}
+    int missing = numsSize;
 
-// Solution 3: Bit Manipulation (XOR)
-int solution3(int *nums, int numsSize) {
-    int missing = 0;
     for (int i = 0; i < numsSize; i++) {
         missing ^= i ^ nums[i];
     }
-    missing ^= numsSize;
-    return missing;
-}
 
-// Solution 4: Hash Table
-int solution4(int *nums, int numsSize) {
-    int hashTable[numsSize + 1];
-    memset(hashTable, 0, sizeof(hashTable));
-    for (int i = 0; i < numsSize; i++) {
-        hashTable[nums[i]] = 1;
-    }
-    for (int i = 0; i < numsSize + 1; i++) {
-        if (hashTable[i] == 0) {
-            return i;
-        }
-    }
-    return -1;
+    return missing;
 }
