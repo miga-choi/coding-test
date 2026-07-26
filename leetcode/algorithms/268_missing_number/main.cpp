3f963933d174a2b4a4196a7772e3adff26ebc571
miga-choi <miga.choi.1004@gmail.com>
2026-07-26 16:30:49 +0900

    leetcode: "268_missing_number" add c++ solution

--- leetcode/algorithms/268_missing_number/main.cpp
+++ leetcode/algorithms/268_missing_number/main.cpp
@@ -4,68 +4,61 @@ using namespace std;
 
 class MissingNumber {
 public:
+    /**
+     * Complexities:
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
     int missingNumber(vector<int> &nums) {
         int originSum = 0;
         int sum = 0;
+
         for (int i = 0; i < nums.size(); i++) {
             originSum += (i + 1);
             sum = nums[i];
         }
+
         return originSum - sum;
     }
 
 
     // Solution
-    // Solution 1: Using Vectors
+    /**
+     * Solution 1
+     * 
+     * std::accumulate
+     *
+     * Complexities:
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
     int solution1(vector<int> &nums) {
         int n = nums.size();
-        vector<int> v(n + 1, -1);
-        for (int i = 0; i < nums.size(); i++) {
-            v[nums[i]] = nums[i];
-        }
-        for (int i = 0; i < v.size(); i++) {
-            if (v[i] == -1)
-                return i;
-        }
-        return 0;
+        
+        int expectedSum = n * (n + 1) / 2;
+        
+        int actualSum = accumulate(nums.begin(), nums.end(), 0);
+        
+        return expectedSum - actualSum;
     }
 
-    // Solution 2: XOR Operation
+    /**
+     * Solution 2
+     * 
+     * XOR
+     *
+     * Complexities:
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
     int solution2(vector<int> &nums) {
         int n = nums.size();
-        int ans = 0;
-        for (int i = 1; i <= n; i++) {
-            ans = ans ^ i;
-        }
-        for (int i = 0; i < nums.size(); i++) {
-            ans = ans ^ nums[i];
-        }
-        return ans;
-    }
+        int missing = n;
 
-    // Solution 3: Sum of all elememnts
-    int solution3(vector<int> &nums) {
-        int n = nums.size();
-        int Tsum = (n * (n + 1)) / 2;
-        return Tsum - accumulate(nums.begin(), nums.end(), 0);
-    }
-
-    // Solution 4: Sorting
-    int solution4(vector<int> &nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        // case 1
-        if (nums[0] != 0)
-            return 0;
-        // case 2
-        if (nums[n - 1] != n)
-            return n;
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] != i) {
-                // case 3
-                return i;
-            }
+        for (int i = 0; i < n; ++i) {
+            missing ^= i ^ nums[i];
         }
-        return 0;
+
+        return missing;
     }
 };
