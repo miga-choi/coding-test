7d456a1d32b0dc0378b5d0fbc6dec7cec8155041
miga-choi <miga.choi.1004@gmail.com>
2026-07-26 16:31:28 +0900

    leetcode: "268_missing_number" add javascript solution

--- leetcode/algorithms/268_missing_number/main.js
+++ leetcode/algorithms/268_missing_number/main.js
@@ -1,36 +1,67 @@
 /**
+ * Complexities:
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+/**
  * @param {number[]} nums
  * @return {number}
  */
 var missingNumber = function (nums) {
   nums.sort((p, n) => p - n);
+
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== i) {
       return i;
     }
   }
+
   return nums.length;
 };
 
 
 // Solution
-// Solution 1
+/**
+ * Solution 1
+ *
+ * Complexities:
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var solution1 = function (nums) {
   const n = nums.length;
-  const expectedSum = (n * (n + 1)) / 2;
-  let actualSum = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    actualSum = actualSum + nums[i];
-  }
+  const expectedSum = (n * (n + 1)) / 2;
+
+  const actualSum = nums.reduce((acc, curr) => acc + curr, 0);
 
   return expectedSum - actualSum;
 };
 
-// Solution 2
+/**
+ * Solution 2
+ *
+ * XOR
+ *
+ * Complexities:
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var solution2 = function (nums) {
   const n = nums.length;
-  const expectedSum = (n * (n + 1)) / 2;
-  const actualSum = nums.reduce((sum, num) => sum + num, 0);
-  return expectedSum - actualSum;
+  let xor = n;
+
+  for (let i = 0; i < n; i++) {
+    xor ^= i ^ nums[i];
+  }
+
+  return xor;
 };
