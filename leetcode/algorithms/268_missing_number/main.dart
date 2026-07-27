0ed4023c3d77d7d85202f03b2de1a1a7e2c99130
miga-choi <miga.choi.1004@gmail.com>
2026-07-28 01:08:56 +0900

    leetcode: "268_missing_number" add dart solution

--- leetcode/algorithms/268_missing_number/main.dart
+++ leetcode/algorithms/268_missing_number/main.dart
@@ -1,37 +1,57 @@
 class MissingNumber {
+  /**
+   * Complexities:
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
   int missingNumber(List<int> nums) {
     int result = 0;
+
     for (int i = 0; i < nums.length; i++) {
       result = result ^ (i + 1) ^ nums[i];
     }
+
     return result;
   }
 
 
   // Solution
-  // Solution 1
+  /**
+   * Solution 1
+   *
+   * Complexities:
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
   int solution1(List<int> nums) {
-    nums.sort();
-    int count = 0;
-    int result = 0;
-    if (nums[0] != 0) return 0;
-    for (var i = 0; nums.length > i; i++) {
-      if (nums[i] != i) {
-        result = nums[i - 1] + 1;
-        count++;
-        break;
-      }
+    int n = nums.length;
+
+    int expectedSum = n * (n + 1) ~/ 2;
+
+    int actualSum = 0;
+    for (int num in nums) {
+      actualSum += num;
     }
-    if (count == 0) {
-      result = nums.last + 1;
-    }
-    return result;
+
+    return expectedSum - actualSum;
   }
 
-  // Solution 2
+  /**
+   * Solution 2
+   * 
+   * XOR
+   *
+   * Complexities:
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
   int solution2(List<int> nums) {
-    int n = nums.length;
-    int sum = nums.reduce((a, b) => a + b);
-    return n * (n + 1) ~/ 2 - sum;
+    int missing = nums.length;
+
+    for (int i = 0; i < nums.length; i++) {
+      missing ^= i ^ nums[i];
+    }
+
+    return missing;
   }
 }
