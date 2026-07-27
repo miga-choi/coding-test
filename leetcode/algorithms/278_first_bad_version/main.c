3e94eb711bfce23ef8199206f62279f9f33cdbf9
miga-choi <miga.choi.1004@gmail.com>
2026-07-28 01:09:39 +0900

    leetcode: "278_first_bad_version" add c solution

--- leetcode/algorithms/278_first_bad_version/main.c
+++ leetcode/algorithms/278_first_bad_version/main.c
@@ -3,32 +3,51 @@
 // The API isBadVersion is defined for you.
 bool isBadVersion(int version);
 
+/**
+ * Complexities:
+ *   N - `n`
+ *   - Time Complexity: O(logᴺ)
+ *   - Space Complexity: O(1)
+ */
 int firstBadVersion(int n) {
     int low = 1;
     int high = n;
     int mid;
+
     while (low < high) {
         mid = low + ((high - low) / 2);
+
         if (isBadVersion(mid)) {
             high = mid;
         } else {
             low = mid + 1;
         }
     }
+
     return low;
 }
 
 
 // Solution
+/**
+ * Complexities:
+ *   N - `n`
+ *   - Time Complexity: O(logᴺ)
+ *   - Space Complexity: O(1)
+ */
 int solution(int n) {
-    int start = 0, end = n;
-    while (end - start > 1) {
-        int mid = start + (end - start) / 2;
+    int left = 1;
+    int right = n;
+
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+
         if (isBadVersion(mid)) {
-            end = mid;
+            right = mid;
         } else {
-            start = mid;
+            left = mid + 1;
         }
     }
-    return end;
+
+    return left;
 }
