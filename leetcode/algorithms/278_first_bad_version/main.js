74e399dfb370ba75df260b6ae7bf4f9ae2547465
miga-choi <miga.choi.1004@gmail.com>
2026-07-29 01:10:13 +0900

    leetcode: "278_first_bad_version" add javascript solution

--- leetcode/algorithms/278_first_bad_version/main.js
+++ leetcode/algorithms/278_first_bad_version/main.js
@@ -41,23 +41,31 @@ var firstBadVersion = function (isBadVersion) {
 
 
 // Solution
+/**
+ * Complexities:
+ *   N - `n`
+ *   - Time Complexity: O(logᴺ)
+ *   - Space Complexity: O(1)
+ */
 var solution = function (isBadVersion) {
   /**
-   * @param {integer} n Total versions
-   * @return {integer} The first bad version
+   * @param {number} n Total versions
+   * @return {number} The first bad version
    */
   return function (n) {
-    // binary search
-    var start = 1,
-      end = n;
-    while (start < end) {
-      var mid = Math.floor(start + (end - start) / 2);
+    let left = 1;
+    let right = n;
+
+    while (left < right) {
+      const mid = left + Math.floor((right - left) / 2);
+
       if (isBadVersion(mid)) {
-        end = mid; // look on left side of mid
+        right = mid;
       } else {
-        start = mid + 1; // look on the right side of mid
+        left = mid + 1;
       }
     }
-    return start;
+
+    return left;
   };
 };
