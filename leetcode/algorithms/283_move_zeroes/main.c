4fb2690d86437a62255277434840a51a2bc5c001
miga-choi <miga.choi.1004@gmail.com>
2026-07-31 02:01:53 +0900

    leetcode: "283_move_zeroes" add c solution

--- leetcode/algorithms/283_move_zeroes/main.c
+++ leetcode/algorithms/283_move_zeroes/main.c
@@ -1,3 +1,9 @@
+/**
+ * Complexities:
+ *   N - `numsSize`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
 void moveZeroes(int* nums, int numsSize) {
     int newNums[numsSize];
     int count = 0;
@@ -19,34 +25,26 @@ void moveZeroes(int* nums, int numsSize) {
 
 
 // Solution
-// Solution 1
-void solution1(int* nums, int numsSize) {
-    int j = 0;
+/**
+ * Two Pointers with Swap
+ * 
+ * Complexities:
+ *   N - `numsSize`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+void solution(int* nums, int numsSize) {
+    int lastNonZeroFoundAt = 0;
 
-    // move all the non-zero elements advance
     for (int i = 0; i < numsSize; i++) {
         if (nums[i] != 0) {
-            nums[j++] = nums[i];
-        }
-    }
+            if (i != lastNonZeroFoundAt) {
+                int temp = nums[lastNonZeroFoundAt];
+                nums[lastNonZeroFoundAt] = nums[i];
+                nums[i] = temp;
+            }
 
-    for (; j < numsSize; j++) {
-        nums[j] = 0;
-    }
-}
-
-// Solution 2
-void solution2(int* nums, int numsSize) {
-    int slow = 0;
-    int fast = 0;
-    while (fast < numsSize) {
-        if (nums[fast] != 0) {
-            nums[slow] = nums[fast];
-            slow++;
+            lastNonZeroFoundAt++;
         }
-        fast++;
-    }
-    for (int i = slow; i < numsSize; i++) {
-        nums[i] = 0;
     }
 }
