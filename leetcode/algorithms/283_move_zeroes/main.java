b6b662f5f8d5d50273d629c584896652e5f190fa
miga-choi <miga.choi.1004@gmail.com>
2026-08-01 02:05:00 +0900

    leetcode: "283_move_zeroes" add java solution

--- leetcode/algorithms/283_move_zeroes/main.java
+++ leetcode/algorithms/283_move_zeroes/main.java
@@ -1,10 +1,15 @@
 class MoveZeroes {
+    /**
+     * Complexities:
+     *   N - The size of `nums`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(N)
+     */
     public void moveZeroes(int[] nums) {
-        int temp = 0;
         for (int i = 0; i < nums.length; i++) {
             for (int j = 1; j < nums.length - i; j++) {
                 if (nums[j - 1] == 0) {
-                    temp = nums[j - 1];
+                    int temp = nums[j - 1];
                     nums[j - 1] = nums[j];
                     nums[j] = temp;
                 }
@@ -14,12 +19,49 @@ class MoveZeroes {
 
 
     // Solution
-    public void solution(int[] nums) {
-        if (nums == null || nums.length == 0) {
+    /**
+     * Solution 1
+     *
+     * Two Pointers
+     *
+     * Complexities:
+     *   N - The size of `nums`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
+    public void solution1(int[] nums) {
+        if (nums == null || nums.length <= 1) {
             return;
         }
 
+        int lastNonZeroFoundAt = 0;
+
+        for (int cur = 0; cur < nums.length; cur++) {
+            if (nums[cur] != 0) {
+                if (cur != lastNonZeroFoundAt) {
+                    int temp = nums[lastNonZeroFoundAt];
+                    nums[lastNonZeroFoundAt] = nums[cur];
+                    nums[cur] = temp;
+                }
+
+                lastNonZeroFoundAt++;
+            }
+        }
+    }
+
+    /**
+     * Solution 2
+     *
+     * Overwrite & Fill
+     *
+     * Complexities:
+     *   N - The size of `nums`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
+    public void solution2(int[] nums) {
         int insertPos = 0;
+
         for (int num : nums) {
             if (num != 0) {
                 nums[insertPos++] = num;
