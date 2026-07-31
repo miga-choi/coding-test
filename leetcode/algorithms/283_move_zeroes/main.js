7d490dcf1429d76449932a353b5714d9e91aa9d5
miga-choi <miga.choi.1004@gmail.com>
2026-08-01 02:04:26 +0900

    leetcode: "283_move_zeroes" add javascript solution

--- leetcode/algorithms/283_move_zeroes/main.js
+++ leetcode/algorithms/283_move_zeroes/main.js
@@ -1,4 +1,10 @@
 /**
+ * Complexities:
+ *   N - The size of `nums`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+/**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
@@ -17,17 +23,57 @@ var moveZeroes = function (nums) {
 
 
 // Solution
-var solution = function (nums) {
-  //two pointers
-  var pos = 0;
+/**
+ * Solution 1
+ *
+ * Two Pointers
+ *
+ * Complexities:
+ *   N - The size of `nums`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var solution1 = function (nums) {
+  let lastNonZeroIndex = 0;
 
-  for (var i = 0; i < nums.length; i++) {
+  for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== 0) {
-      nums[pos++] = nums[i];
+      nums[lastNonZeroIndex] = nums[i];
+      lastNonZeroIndex++;
     }
   }
 
-  for (i = pos; i < nums.length; i++) {
+  for (let i = lastNonZeroIndex; i < nums.length; i++) {
     nums[i] = 0;
+  }
+};
+
+/**
+ * Solution 2
+ *
+ * Swap
+ *
+ * Complexities:
+ *   N - The size of `nums`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var solution2 = function (nums) {
+  let lastNonZeroIndex = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      [nums[lastNonZeroIndex], nums[i]] = [nums[i], nums[lastNonZeroIndex]];
+
+      lastNonZeroIndex++;
+    }
   }
 };
