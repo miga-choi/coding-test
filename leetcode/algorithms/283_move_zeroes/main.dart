0e7a9aa892c75fcbd5ac82b53ce0fce019c1a2e8
miga-choi <miga.choi.1004@gmail.com>
2026-08-02 03:24:58 +0900

    leetcode: "283_move_zeroes" add dart solution

--- leetcode/algorithms/283_move_zeroes/main.dart
+++ leetcode/algorithms/283_move_zeroes/main.dart
@@ -1,22 +1,46 @@
 class MoveZeroes {
+  /**
+   * Complexities:
+   *   N - The size of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(N)
+   */
   void moveZeroes(List<int> nums) {
     int zeroCount = 0;
+
     for (int num in nums) {
       if (num == 0) {
         zeroCount++;
       }
     }
+
     nums.removeWhere((int num) => num == 0);
     nums.addAll(List<int>.filled(zeroCount, 0));
   }
 
 
   // Solution
+  /**
+   * Two Pointers Swap
+   *
+   * Complexities:
+   *   N - The size of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
   void solution(List<int> nums) {
-    List<int> testValues = [];
-    nums.forEach((value) => testValues.add(value));
-    testValues.retainWhere((value) => value == 0);
-    nums.removeWhere((value) => value == 0);
-    nums.addAll(testValues);
+    int slow = 0;
+
+    for (int fast = 0; fast < nums.length; fast++) {
+      if (nums[fast] != 0) {
+        if (slow != fast) {
+          int temp = nums[slow];
+          nums[slow] = nums[fast];
+          nums[fast] = temp;
+        }
+
+        slow++;
+      }
+    }
   }
 }
