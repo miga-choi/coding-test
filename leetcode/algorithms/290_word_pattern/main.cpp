cb4aa71a42787d5e350df87ecf9042589b4d4b55
miga-choi <miga.choi.1004@gmail.com>
2026-08-03 01:10:00 +0900

    leetcode: "290_word_pattern" add c++ solution

--- leetcode/algorithms/290_word_pattern/main.cpp
+++ leetcode/algorithms/290_word_pattern/main.cpp
@@ -1,30 +1,97 @@
-#include <map>
+#include <iostream>
 #include <sstream>
 #include <string>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
 class WordPattern {
 public:
+    /**
+     * Complexities:
+     *   N - The size of `pattern`
+     *   M - The size of `s`
+     *   L - The max size of word
+     *   - Time Complexity: O(M + N * L)
+     *   - Space Complexity: O(M)
+     */
     bool wordPattern(string pattern, string s) {
+        istringstream iss(s);
+        
+        vector<string> charToWord(26, "");
+        unordered_map<string, char> wordToChar;
+
+        string word;
+        int i = 0;
+        int patternLen = pattern.length();
+
+        while (iss >> word) {
+            if (i >= patternLen) {
+                return false;
+            }
+
+            char c = pattern[i];
+            int charIdx = c - 'a';
+
+            if (!charToWord[charIdx].empty() && charToWord[charIdx] != word) {
+                return false;
+            }
+
+            if (wordToChar.count(word) && wordToChar[word] != c) {
+                return false;
+            }
+
+            charToWord[charIdx] = word;
+            wordToChar[word] = c;
+
+            i++;
+        }
+
+        return i == patternLen;
     }
 
 
     // Solution
+    /**
+     * Complexities:
+     *   N - The size of `pattern`
+     *   M - The size of `s`
+     *   L - The max size of word
+     *   - Time Complexity: O(M + N * L)
+     *   - Space Complexity: O(M)
+     */
     bool solution(string pattern, string s) {
-        map<char, int> p2i;
-        map<string, int> w2i;
+        istringstream iss(s);
+        
+        vector<string> charToWord(26, "");
+        unordered_map<string, char> wordToChar;
 
-        istringstream in(s);
+        string word;
+        int i = 0;
+        int patternLen = pattern.length();
 
-        int i = 0, n = pattern.size();
-
-        for (string word; in >> word; ++i) {
-            if (i == n || p2i[pattern[i]] != w2i[word]) {
+        while (iss >> word) {
+            if (i >= patternLen) {
                 return false;
             }
-            p2i[pattern[i]] = w2i[word] = i + 1;
+
+            char c = pattern[i];
+            int charIdx = c - 'a';
+
+            if (!charToWord[charIdx].empty() && charToWord[charIdx] != word) {
+                return false;
+            }
+
+            if (wordToChar.count(word) && wordToChar[word] != c) {
+                return false;
+            }
+
+            charToWord[charIdx] = word;
+            wordToChar[word] = c;
+
+            i++;
         }
 
-        return i == n;
+        return i == patternLen;
     }
 };
