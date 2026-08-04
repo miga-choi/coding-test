74b27d73188408e7aa04a8a0cb639652d8079150
miga-choi <miga.choi.1004@gmail.com>
2026-08-05 01:33:44 +0900

    leetcode: "290_word_pattern" add dart solution

--- leetcode/algorithms/290_word_pattern/main.dart
+++ leetcode/algorithms/290_word_pattern/main.dart
@@ -1,4 +1,11 @@
 class WordPattern {
+  /**
+   * Complexities:
+   *   N - The size of `pattern`
+   *   M - The size of `s`
+   *   - Time Complexity: O(N * M)
+   *   - Space Complexity: O(N + M)
+   */
   bool wordPattern(String pattern, String s) {
     Map<String, String> patternMap = Map<String, String>();
     List<String> patternArray = pattern.split("");
@@ -26,68 +33,46 @@ class WordPattern {
 
 
   // Solution
-  // Solution 1
-  bool solution1(String pattern, String s) {
-    var list = s.split(' ');
-    if (pattern.length != list.length) {
-      return false;
-    }
+  /**
+   * Bijective Hash Maps
+   * 
+   * Complexities:
+   *   N - The size of `pattern`
+   *   M - The number of characters' length in `s`
+   *   - Time Complexity: O(N + M)
+   *   - Space Complexity: O(N + M)
+   */
+  bool solution(String pattern, String s) {
+    List<String> words = s.split(' ');
 
-    var mapK = {};
-    var mapV = {};
-
-    for (int i = 0; i < list.length; i++) {
-      var key = pattern[i];
-      var value = list[i];
-
-      if (mapK.containsKey(key) || mapV.containsKey(value)) {
-        if (mapK[key] != value) return false;
-        if (mapV[value] != key) return false;
-      } else {
-        mapK[key] = value;
-        mapV[value] = key;
-      }
-    }
-    return true;
-  }
-
-  // Solution 2
-  bool solution2(String pattern, String s) {
-    // Split the string `s` into individual words and store them in a list
-    List<String> words = s.split(" ");
-
-    // Create a dictionary to store the pattern-to-word mappings
-    Map<String, String> patternType = {};
-
-    // Boolean variable to store the result
-    bool res = false;
-
-    // If the length of the pattern is not equal to the number of words, return false
     if (pattern.length != words.length) {
       return false;
     }
 
-    // Iterate through each character in the pattern and its corresponding word
+    Map<String, String> charToWord = {};
+    Map<String, String> wordToChar = {};
+
     for (int i = 0; i < pattern.length; i++) {
-      // Check if the character is not already in the dictionary and the word is not already mapped
-      if (!patternType.containsKey(pattern[i]) &&
-          !patternType.containsValue(words[i])) {
-        // Add the character-word pair to the dictionary
-        patternType.addEntries({pattern[i]: words[i]}.entries);
+      String char = pattern[i];
+      String word = words[i];
+
+      if (charToWord.containsKey(char)) {
+        if (charToWord[char] != word) {
+          return false;
+        }
+      } else {
+        charToWord[char] = word;
       }
 
-      // Check if the current pattern character maps to the current word
-      if (patternType[pattern[i]] == words[i]) {
-        // If the mapping is correct, set the result to true
-        res = true;
+      if (wordToChar.containsKey(word)) {
+        if (wordToChar[word] != char) {
+          return false;
+        }
       } else {
-        // If the mapping is incorrect, set the result to false and break the loop
-        res = false;
-        break;
+        wordToChar[word] = char;
       }
     }
 
-    // Return the final result
-    return res;
+    return true;
   }
 }
