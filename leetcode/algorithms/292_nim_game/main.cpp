843d74c6e7e5cb97c1723cede31721deaf245e4a
miga-choi <miga.choi.1004@gmail.com>
2026-08-06 01:46:08 +0900

    leetcode: "292_nim_game" add c++ solution

--- leetcode/algorithms/292_nim_game/main.cpp
+++ leetcode/algorithms/292_nim_game/main.cpp
@@ -1,6 +1,46 @@
 class NimGame {
 public:
+    /**
+     * Complexities:
+     *   - Time Complexity: O(1)
+     *   - Space Complexity: O(1)
+     */
     bool canWinNim(int n) {
         return n % 4;
+    }
+
+
+    // Solution
+    /**
+     * Solution 1
+     *
+     * Complexities:
+     *   - Time Complexity: O(1)
+     *   - Space Complexity: O(1)
+     */
+    bool solution1(int n) {
+        return n % 4;
+    }
+
+    /**
+     * Solution 2
+     *
+     * Complexities:
+     *   - Time Complexity: O(1)
+     *   - Space Complexity: O(1)
+     */
+    bool solution2(int n) {
+        return (n & 3) != 0;
+    }
+
+    /**
+     * Solution 3
+     *
+     * Complexities:
+     *   - Time Complexity: O(1)
+     *   - Space Complexity: O(1)
+     */
+    static constexpr bool solution3(int n) noexcept {
+        return (n & 3) != 0;
     }
 };
