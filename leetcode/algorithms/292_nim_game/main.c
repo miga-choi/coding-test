6dee7528ae0c3028c3c6242cf59c0aeff28e5c64
miga-choi <miga.choi.1004@gmail.com>
2026-08-06 01:45:46 +0900

    leetcode: "292_nim_game" add c solution

--- leetcode/algorithms/292_nim_game/main.c
+++ leetcode/algorithms/292_nim_game/main.c
@@ -1,5 +1,34 @@
 #include <stdbool.h>
 
+/**
+ * Complexities:
+ *   - Time Complexity: O(1)
+ *   - Space Complexity: O(1)
+ */
 bool canWinNim(int n) {
     return n % 4;
+}
+
+
+// Solution
+/**
+ * Solution 1
+ *
+ * Complexities:
+ *   - Time Complexity: O(1)
+ *   - Space Complexity: O(1)
+ */
+bool solution1(int n) {
+    return n % 4;
+}
+
+/**
+ * Solution 2
+ *
+ * Complexities:
+ *   - Time Complexity: O(1)
+ *   - Space Complexity: O(1)
+ */
+bool solution2(int n) {
+    return (n & 3) != 0;
 }
