71ee139dea6144c8406eb05a596dc9ab6ef1e74c
miga-choi <miga.choi.1004@gmail.com>
2026-08-08 02:17:57 +0900

    leetcode: "292_nim_game" add python solution

--- leetcode/algorithms/292_nim_game/main.java
+++ leetcode/algorithms/292_nim_game/main.java
@@ -1,8 +1,8 @@
 class NimGame {
     /**
      * Complexities:
-     * - Time Complexity: O(1)
-     * - Space Complexity: O(1)
+     *   - Time Complexity: O(1)
+     *   - Space Complexity: O(1)
      */
     public boolean canWinNim(int n) {
         if (n % 4 == 0) {
@@ -15,8 +15,8 @@ class NimGame {
     // Solution
     /**
      * Complexities:
-     * - Time Complexity: O(1)
-     * - Space Complexity: O(1)
+     *   - Time Complexity: O(1)
+     *   - Space Complexity: O(1)
      */
     public boolean solution(int n) {
         return n % 4 != 0;
--- leetcode/algorithms/292_nim_game/main.py
+++ leetcode/algorithms/292_nim_game/main.py
@@ -1,4 +1,9 @@
 class NimGame:
+    """
+    # Complexities:
+    #   - Time Complexity: O(1)
+    #   - Space Complexity: O(1)
+    """
     def canWinNim(self, n: int) -> bool:
         if n % 4 == 0:
             return False
@@ -6,22 +11,10 @@ class NimGame:
 
 
     # Solution
+    """
+    # Complexities:
+    #   - Time Complexity: O(1)
+    #   - Space Complexity: O(1)
+    """
     def solution(self, n: int) -> bool:
-        if n <= 3:
-            return True
-        new_size = n + 1
-        memo = [False] * (new_size)
-
-        for i in range(4):
-            memo[i] = True
-
-        for i in range(4, new_size):
-            for j in range(1, 4):
-                if memo[i] == True:
-                    break
-                if memo[i - j] == True:
-                    memo[i] = False
-                else:
-                    memo[i] = True
-
-        return memo[n]
+        return n % 4 != 0
