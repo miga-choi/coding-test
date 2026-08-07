f7968f6113bfc58b822fe79a6b260a173d6171e8
miga-choi <miga.choi.1004@gmail.com>
2026-08-08 02:18:34 +0900

    leetcode: "292_nim_game" add dart solution

--- leetcode/algorithms/292_nim_game/main.dart
+++ leetcode/algorithms/292_nim_game/main.dart
@@ -1,5 +1,30 @@
 class NimGame {
+  /**
+   * Complexities:
+   *   - Time Complexity: O(1)
+   *   - Space Complexity: O(1)
+   */
   bool canWinNim(int n) {
     return n % 4 > 0;
   }
+
+
+  // Solution
+  /**
+   * Solution 1
+   * 
+   * Complexities:
+   *   - Time Complexity: O(1)
+   *   - Space Complexity: O(1)
+   */
+  bool solution1(int n) => n % 4 != 0;
+
+  /**
+   * Solution 2
+   * 
+   * Complexities:
+   *   - Time Complexity: O(1)
+   *   - Space Complexity: O(1)
+   */
+  bool solution2(int n) => (n & 3) != 0;
 }
