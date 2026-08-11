600cf0020e4538e49e81d64de92f16af0a134344
miga-choi <miga.choi.1004@gmail.com>
2026-08-12 01:06:49 +0900

    leetcode: "322_coin_change" add c solution

--- leetcode/algorithms/322_coin_change/main.c
+++ leetcode/algorithms/322_coin_change/main.c
@@ -4,8 +4,12 @@ int min(int a, int b) {
 
 /**
  * DP (Dynamic Programming)
- * - Time Complexity: O(M * N)
- * - Space Complexity: O(N)
+ * 
+ * Complexities:
+ *   N - `amounta
+ *   M - The types of coins
+ *   - Time Complexity: O(M * N)
+ *   - Space Complexity: O(N)
  */
 int coinChange(int* coins, int coinsSize, int amount) {
     if (amount == 0) {
@@ -22,6 +26,7 @@ int coinChange(int* coins, int coinsSize, int amount) {
     for (int i = 1; i <= amount; i++) {
         for (int j = 0; j < coinsSize; j++) {
             int coin = coins[j];
+
             if (i >= coin) {
                 dp[i] = min(dp[i], dp[i - coin] + 1);
             }
@@ -36,38 +41,36 @@ int coinChange(int* coins, int coinsSize, int amount) {
 }
 
 
-
 // Solution
 /**
  * DP (Dynamic Programming)
- * - Time Complexity: O(M * N)
- * - Space Complexity: O(N)
+ * 
+ * Complexities:
+ *   N - `amounta
+ *   M - The types of coins
+ *   - Time Complexity: O(M * N)
+ *   - Space Complexity: O(N)
  */
 int solution(int* coins, int coinsSize, int amount) {
-    if (amount == 0) {
-        return 0;
-    }
-
-    long long dp[amount + 1];
-
-    for (int i = 0; i <= amount; i++) {
-        dp[i] = amount + 1;
-    }
+    int INF = amount + 1;
+    int* dp = malloc((amount + 1) * sizeof(int));
 
     dp[0] = 0;
+    for (int a = 1; a <= amount; a++) dp[a] = INF;
 
-    for (int i = 1; i <= amount; i++) {
-        for (int j = 0; j < coinsSize; j++) {
-            int coin = coins[j];
-            if (i >= coin) {
-                dp[i] = min(dp[i], dp[i - coin] + 1);
+    for (int a = 1; a <= amount; a++) {
+        for (int i = 0; i < coinsSize; i++) {
+            int c = coins[i];
+
+            if (c <= a && dp[a - c] + 1 < dp[a]) {
+                dp[a] = dp[a - c] + 1;
             }
         }
     }
 
-    if (dp[amount] > amount) {
-        return -1;
-    } else {
-        return (int)dp[amount];
-    }
+    int ans = (dp[amount] > amount) ? -1 : dp[amount];
+
+    free(dp);
+
+    return ans;
 }
