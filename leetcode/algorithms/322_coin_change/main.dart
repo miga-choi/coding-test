4c377b1ccc7ebc494017f8784c202afe87f3ebc9
miga-choi <miga.choi.1004@gmail.com>
2026-08-14 01:13:40 +0900

    leetcode: "322_coin_change" add dart solution

--- leetcode/algorithms/322_coin_change/main.dart
+++ leetcode/algorithms/322_coin_change/main.dart
@@ -3,8 +3,12 @@ import 'dart:math';
 class CoinChange {
   /**
    * DP (Dynamic Programming)
-   * - Time Complexity: O(M * N)
-   * - Space Complexity: O(N)
+   *
+   * Complexities:
+   *   N - The size of `coins`
+   *   M - `amount`
+   *   - Time Complexity: O(N * M)
+   *   - Space Complexity: O(N)
    */
   int coinChange(List<int> coins, int amount) {
     if (amount == 0) {
@@ -28,23 +32,76 @@ class CoinChange {
 
   // Solution
   /**
+   * Solution 1
+   * 
    * DP (Dynamic Programming)
-   * - Time Complexity: O(M * N)
-   * - Space Complexity: O(N)
+   *
+   * Complexities:
+   *   N - The size of `coins`
+   *   M - `amount`
+   *   - Time Complexity: O(N * M)
+   *   - Space Complexity: O(N)
    */
-  int solution(List<int> coins, int amount) {
-    List<int> dp = List.filled(amount + 1, amount + 1);
-
+  int solution1(List<int> coins, int amount) {
+    final unreachable = amount + 1;
+    final dp = List<int>.filled(amount + 1, unreachable);
     dp[0] = 0;
 
-    for (int i = 1; i <= amount; i++) {
-      for (final coin in coins) {
-        if (i - coin >= 0) {
-          dp[i] = min(dp[i], 1 + dp[i - coin]);
+    for (var i = 1; i <= amount; i++) {
+      for (final c in coins) {
+        if (c <= i && dp[i - c] + 1 < dp[i]) {
+          dp[i] = dp[i - c] + 1;
         }
       }
     }
 
     return dp[amount] > amount ? -1 : dp[amount];
+  }
+
+  /**
+   * Solution 2
+   * 
+   * BFS
+   *
+   * Complexities:
+   *   N - The size of `coins`
+   *   M - `amount`
+   *   - Time Complexity: O(N * M)
+   *   - Space Complexity: O(M)
+   */
+  int solution2(List<int> coins, int amount) {
+    if (amount == 0) {
+      return 0;
+    }
+
+    final visited = List<bool>.filled(amount + 1, false);
+    var queue = <int>[amount];
+    visited[amount] = true;
+    var steps = 0;
+
+    while (queue.isNotEmpty) {
+      steps++;
+
+      final next = <int>[];
+
+      for (final cur in queue) {
+        for (final c in coins) {
+          final rest = cur - c;
+
+          if (rest == 0) {
+            return steps;
+          }
+
+          if (rest > 0 && !visited[rest]) {
+            visited[rest] = true;
+            next.add(rest);
+          }
+        }
+      }
+
+      queue = next;
+    }
+
+    return -1;
   }
 }
