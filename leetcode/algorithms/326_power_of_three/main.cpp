85fef4a3aded2ccf297277a9d0d625b47d90f754
miga-choi <miga.choi.1004@gmail.com>
2026-08-15 17:30:48 +0900

    leetcode: "326_power_of_three" add c++ solution

--- leetcode/algorithms/326_power_of_three/main.cpp
+++ leetcode/algorithms/326_power_of_three/main.cpp
@@ -1,38 +1,56 @@
 class PowerOfThree {
 public:
+    /**
+     * Complexities:
+     *   N - `n`
+     *   - Time Complexity: O(log₃ᴺ)
+     *   - Space Complexity: O(1)
+     */
     bool isPowerOfThree(int n) {
         if (n < 1) {
             return false;
         }
+        
         while (n > 1) {
             if (n % 3) {
                 return false;
             }
             n /= 3;
         }
+
         return true;
     }
 
 
     // Solution
-    // Solution 1
+    /**
+     * Solution 1
+     *
+     * Complexities:
+     *   N - `n`
+     *   - Time Complexity: O(log₃ᴺ)
+     *   - Space Complexity: O(1)
+     */
     bool solution1(int n) {
-        // 1162261467 is 3^19,  3^20 is bigger than int
-        return n > 0 && (1162261467 % n == 0);
-    }
-
-    // Solution 2: Recursive Solution
-    bool solution2(int n) {
-        return n > 0 && (n == 1 || (n % 3 == 0 && isPowerOfThree(n / 3)));
-    }
-
-    // Solution 3: Recursive Solution
-    bool solution3(int n) {
-        if (n > 1) {
-            while (n % 3 == 0) {
-                n /= 3;
-            }
+        if (n <= 0) {
+            return false;
         }
+
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+
         return n == 1;
+    }
+
+    /**
+     * Solution 2
+     *
+     * Complexities:
+     *   - Time Complexity: O(1)
+     *   - Space Complexity: O(1)
+     */
+    bool solution2(int n) {
+        return n > 0 && 1162261467 % n == 0;
     }
 };
