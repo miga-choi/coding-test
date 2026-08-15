5f60b25bacefd2a87bc8bb64178c23a4f8bbbf25
miga-choi <miga.choi.1004@gmail.com>
2026-08-15 17:30:26 +0900

    leetcode: "326_power_of_three" add c solution

--- leetcode/algorithms/326_power_of_three/main.c
+++ leetcode/algorithms/326_power_of_three/main.c
@@ -1,37 +1,57 @@
 #include <stdbool.h>
 
+/**
+ * Complexities:
+ *   N - `n`
+ *   - Time Complexity: O(log₃ᴺ)
+ *   - Space Complexity: O(1)
+ */
 bool isPowerOfThree(int n) {
     if (n <= 0) {
         return false;
     }
+
     while (n > 1) {
         if (n % 3) {
             return false;
         }
         n /= 3;
     }
+
     return true;
 }
 
 
-//  Solution
-//  Solution 1: Loops
+// Solution
+/**
+ * Solution 1
+ * 
+ * Complexities:
+ *   N - `n`
+ *   - Time Complexity: O(log₃ᴺ)
+ *   - Space Complexity: O(1)
+ */
 bool solution1(int n) {
-    if (!n) {
+    if (n <= 0) {
         return false;
     }
+
     while (n % 3 == 0) {
         n /= 3;
     }
+
     return n == 1;
 }
 
-// Solution 2: Recurison
+/**
+ * Solution 2
+ * 
+ * Complexities:
+ *   - Time Complexity: O(1)
+ *   - Space Complexity: O(1)
+ */
 bool solution2(int n) {
-    if (n <= 1) {
-        return n == 1;
-    }
-    return n % 3 == 0 && isPowerOfThree(n / 3);
+    return n > 0 && 1162261467 % n == 0;
 }
 
 // Solution 3: Base Conversion
