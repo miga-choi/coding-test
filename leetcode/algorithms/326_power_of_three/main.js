803c32c76916b9880171a3cad3a5a2f882f96ba8
miga-choi <miga.choi.1004@gmail.com>
2026-08-16 15:35:57 +0900

    leetcode: "326_power_of_three" add javascript solution

--- leetcode/algorithms/326_power_of_three/main.js
+++ leetcode/algorithms/326_power_of_three/main.js
@@ -1,23 +1,62 @@
 /**
+ * Complexities:
+ *   N - `n`
+ *   - Time Complexity: O(log₃ᴺ)
+ *   - Space Complexity: O(1)
+ */
+/**
  * @param {number} n
  * @return {boolean}
  */
 var isPowerOfThree = function (n) {
-  if ((Math.log10(n) / Math.log10(3)) % 1 === 0) {
-    return true;
+  if (n < 1) {
+    return false;
   }
-  return false;
+
+  while (n % 3 === 0) {
+    n /= 3;
+  }
+
+  return n === 1;
 };
 
 
 // Solution
-// Solution 1: Logarithms
+/**
+ * Solution 1
+ *
+ * Complexities:
+ *   N - `n`
+ *   - Time Complexity: O(log₃ᴺ)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
 var solution1 = function (n) {
-  let a = Math.log(n) / Math.log(3);
-  return Math.abs(a - Math.round(a)) < 1e-10;
+  if (n < 1) {
+    return false;
+  }
+
+  while (n % 3 === 0) {
+    n /= 3;
+  }
+
+  return n === 1;
 };
 
-// Solution 2: Modulo
+/**
+ * Solution 2
+ *
+ * Complexities:
+ *   - Time Complexity: O(1)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
 var solution2 = function (n) {
   return n > 0 && 1162261467 % n === 0;
 };
