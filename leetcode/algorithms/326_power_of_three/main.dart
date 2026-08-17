d0cdc0d84e5208fe613d22327ac684e347e940b7
miga-choi <miga.choi.1004@gmail.com>
2026-08-17 12:21:07 +0900

    leetcode: "326_power_of_three" add dart solution

--- leetcode/algorithms/326_power_of_three/main.dart
+++ leetcode/algorithms/326_power_of_three/main.dart
@@ -1,16 +1,32 @@
 class PowerOfThree {
+  /**
+   * Complexities:
+   *   N - `n`
+   *   - Time Complexity: O(log₃ᴺ)
+   *   - Space Complexity: O(1)
+   */
   bool isPowerOfThree(int n) {
-    if (n > 1) {
-      while (n % 3 == 0) {
-        n = (n / 3).floor();
-      }
+    if (n <= 0) {
+      return false;
     }
+
+    while (n % 3 == 0) {
+      n ~/= 3;
+    }
+
     return n == 1;
   }
 
 
   // Solution
-  // Solution 1
+  /**
+   * Solution 1
+   *
+   * Complexities:
+   *   N - `n`
+   *   - Time Complexity: O(log₃ᴺ)
+   *   - Space Complexity: O(1)
+   */
   bool solution1(int n) {
     if (n <= 0) {
       return false;
@@ -23,13 +39,12 @@ class PowerOfThree {
     return n == 1;
   }
 
-  // Solution 2
-  bool solution2(int n) {
-    for (var i = 1; i <= n; i *= 3) {
-      if (i == n) {
-        return true;
-      }
-    }
-    return false;
-  }
+  /**
+   * Solution 2
+   *
+   * Complexities:
+   *   - Time Complexity: O(1)
+   *   - Space Complexity: O(1)
+   */
+  bool solution2(int n) => n > 0 && 1162261467 % n == 0;
 }
