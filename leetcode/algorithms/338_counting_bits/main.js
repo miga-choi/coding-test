a60427c0c25ed6ca346bd5dc613fd185fec25d35
miga-choi <miga.choi.1004@gmail.com>
2026-08-19 01:37:07 +0900

    leetcode: "338_counting_bits" add javascript solution

--- leetcode/algorithms/338_counting_bits/main.js
+++ leetcode/algorithms/338_counting_bits/main.js
@@ -1,4 +1,11 @@
 /**
+ * Time Complexity should <= O(N)
+ *
+ * Complexities:
+ *   - Time Complexity: O(N * logᴺ)
+ *   - Space Complexity: O(N * logᴺ)
+ */
+/**
  * @param {number} n
  * @return {number[]}
  */
@@ -19,16 +26,107 @@ var countBits = function (n) {
 
 
 // Solution
-const solution = (n) => {
-  let result = Array(n + 1).fill(0);
-  let offset = 1;
+/**
+ * Solution 1
+ *
+ * Time Complexity should <= O(N)
+ *
+ * Complexities:
+ *   - Time Complexity: O(N * logᴺ)
+ *   - Space Complexity: O(N)
+ */
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var solution1 = function (n) {
+  const result = [];
 
-  for (let i = 1; i < n + 1; i++) {
-    if (offset * 2 === i) {
-      offset = i;
+  for (let i = 0; i <= n; i++) {
+    let count = 0,
+      x = i;
+
+    while (x > 0) {
+      count += x & 1;
+      x >>= 1;
     }
-    result[i] = 1 + result[i - offset];
+
+    result.push(count);
   }
 
   return result;
+};
+
+/**
+ * Solution 2
+ *
+ * DP (Dynamic programming)
+ *
+ * Complexities:
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var solution2 = function (n) {
+  const dp = new Array(n + 1).fill(0);
+
+  for (let i = 1; i <= n; i++) {
+    dp[i] = dp[i >> 1] + (i & 1);
+  }
+
+  return dp;
+};
+
+/**
+ * Solution 3
+ *
+ * DP (Dynamic programming)
+ *
+ * Complexities:
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var solution3 = function (n) {
+  const dp = new Array(n + 1).fill(0);
+
+  for (let i = 1; i <= n; i++) {
+    dp[i] = dp[i & (i - 1)] + 1;
+  }
+
+  return dp;
+};
+
+/**
+ * Solution 4
+ *
+ * DP (Dynamic programming)
+ *
+ * Complexities:
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var solution4 = function (n) {
+  const dp = new Array(n + 1).fill(0);
+  let offset = 1;
+
+  for (let i = 1; i <= n; i++) {
+    if (offset * 2 === i) {
+      offset *= 2;
+    }
+
+    dp[i] = dp[i - offset] + 1;
+  }
+
+  return dp;
 };
