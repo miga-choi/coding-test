f95980100400f88f601eca66130fcb34886e7495
miga-choi <miga.choi.1004@gmail.com>
2026-08-20 01:44:01 +0900

    leetcode: "338_counting_bits" add python solution

--- leetcode/algorithms/338_counting_bits/main.py
+++ leetcode/algorithms/338_counting_bits/main.py
@@ -2,6 +2,13 @@ from typing import List
 
 
 class CountBits:
+    """
+    # Time Complexity should <= O(N)
+    #
+    # Complexities:
+    #   - Time Complexity: O(N * logᴺ)
+    #   - Space Complexity: O(N * logᴺ)
+    """
     def countBits(self, n: int) -> List[int]:
         result: List[int] = []
 
@@ -16,10 +23,60 @@ class CountBits:
 
 
     # Solution
-    def solution(self, num: int) -> List[int]:
-        counter = [0]
+    """
+    # Solution 1
+    #
+    # Time Complexity should <= O(N)
+    #
+    # Complexities:
+    #   - Time Complexity: O(N * logᴺ)
+    #   - Space Complexity: O(N * logᴺ)
+    """
+    def solution1(self, n: int) -> List[int]:
+        return [bin(i).count('1') for i in range(n + 1)]
 
-        for i in range(1, num + 1):
-            counter.append(counter[i >> 1] + i % 2)
+    """
+    # Solution 2
+    #
+    # DP (Dynamic programming)
+    #
+    # Complexities:
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(N)
+    """
+    def solution2(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
 
-        return counter
+        for i in range(1, n + 1):
+            dp[i] = dp[i >> 1] + (i & 1)
+
+        return dp
+
+    """
+    # Solution 3
+    #
+    # DP (Dynamic programming)
+    #
+    # Complexities:
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(N)
+    """
+    def solution3(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+
+        for i in range(1, n + 1):
+            dp[i] = dp[i & (i - 1)] + 1
+
+        return dp
+
+    """
+    # Solution 4
+    #
+    # Built-in function
+    #
+    # Complexities:
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(N)
+    """
+    def solution4(self, n: int) -> List[int]:
+        return [i.bit_count() for i in range(n + 1)]
