bc849bed1922d64e1f78ec7ac2a2d8ab00622161
miga-choi <miga.choi.1004@gmail.com>
2026-08-20 01:44:27 +0900

    leetcode: "338_counting_bits" add dart solution

--- leetcode/algorithms/338_counting_bits/main.dart
+++ leetcode/algorithms/338_counting_bits/main.dart
@@ -1,16 +1,25 @@
 class CountingBits {
+  /**
+   * Time Complexity should <= O(N)
+   *
+   * Complexities:
+   *   - Time Complexity: O(N * logᴺ)
+   *   - Space Complexity: O(N * logᴺ)
+   */
   List<int> countBits(int n) {
     List<int> result = List<int>.filled(n + 1, 0);
 
     for (int i = 0; i <= n; i++) {
       int sum = 0;
       int num = i;
+
       while (num > 0) {
         if (num % 2 == 1) {
           sum++;
         }
         num = (num / 2).floor();
       }
+
       result[i] = sum;
     }
 
@@ -19,13 +28,63 @@ class CountingBits {
 
 
   // Solution
-  List<int> solution(int n) {
-    List<int> result = List.filled(n + 1, 0);
+  /**
+   * Solution 1
+   *
+   * Complexities:
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(N)
+   */
+  List<int> solution1(int n) {
+    final dp = List<int>.filled(n + 1, 0);
 
-    for (int i = 1; i <= n; i++) {
-      result[i] = result[i >> 1] + (i & 1);
+    for (var i = 1; i <= n; i++) {
+      dp[i] = dp[i >> 1] + (i & 1);
     }
 
-    return result;
+    return dp;
+  }
+
+  /**
+   * Solution 2
+   *
+   * Complexities:
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(N)
+   */
+  List<int> solution2(int n) {
+    final dp = List<int>.filled(n + 1, 0);
+
+    for (var i = 1; i <= n; i++) {
+      dp[i] = dp[i & (i - 1)] + 1;
+    }
+
+    return dp;
+  }
+
+  /**
+   * Solution 3
+   * 
+   * Time Complexity should <= O(N)
+   *
+   * Complexities:
+   *   - Time Complexity: O(N * logᴺ)
+   *   - Space Complexity: O(N * logᴺ)
+   */
+  List<int> solution3(int n) {
+    final res = List<int>.filled(n + 1, 0);
+
+    for (var i = 0; i <= n; i++) {
+      var x = i, count = 0;
+
+      while (x != 0) {
+        x &= x - 1;
+        count++;
+      }
+
+      res[i] = count;
+    }
+
+    return res;
   }
 }
