3398944963b4b06c9f99ca9f8f917d8e025da6cb
miga-choi <miga.choi.1004@gmail.com>
2026-08-22 01:35:12 +0900

    leetcode: "342_power_of_four" add java solution

--- leetcode/algorithms/342_power_of_four/main.java
+++ leetcode/algorithms/342_power_of_four/main.java
@@ -1,16 +1,62 @@
 class PowerOfFour {
+    /**
+     * Built-in function
+     *
+     * Complexities:
+     *   - Time Complexity: O(1)
+     *   - Space Complexity: O(1)
+     */
     public boolean isPowerOfFour(int n) {
-        if (Math.log10(n) / Math.log(4) % 1 == 0) {
-            return true;
-        }
-        return false;
+        return Math.log10(n) / Math.log(4) % 1 == 0;
     }
 
 
     // Solution
-    public boolean solution(int num) {
-        // 0x55555555 is to get rid of those power of 2 but not power of 4
-        // so that the single 1 bit always appears at the odd position
-        return num > 0 && (num & (num - 1)) == 0 && (num & 0x55555555) != 0;
+    /**
+     * Solution 1
+     *
+     * Repeated Division
+     *
+     * Complexities:
+     *   N - `n`
+     *   - Time Complexity: O(log₄ᴺ)
+     *   - Space Complexity: O(1)
+     */
+    public boolean solution1(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n % 4 == 0) {
+            n /= 4;
+        }
+
+        return n == 1;
+    }
+
+    /**
+     * Solution 2
+     *
+     * Bit Trick
+     *
+     * Complexities:
+     *   - Time Complexity: O(1)
+     *   - Space Complexity: O(1)
+     */
+    public boolean solution2(int n) {
+        return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0;
+    }
+
+    /**
+     * Solution 3
+     *
+     * Remaining 3
+     *
+     * Complexities:
+     *   - Time Complexity: O(1)
+     *   - Space Complexity: O(1)
+     */
+    public boolean solution3(int n) {
+        return n > 0 && (n & (n - 1)) == 0 && (n - 1) % 3 == 0;
     }
 }
