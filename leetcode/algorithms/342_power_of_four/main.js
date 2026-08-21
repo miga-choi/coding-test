aeb9deb90eb3d64d27be723c688beef68977bed7
miga-choi <miga.choi.1004@gmail.com>
2026-08-22 01:34:32 +0900

    leetcode: "342_power_of_four" add javascript solution

--- leetcode/algorithms/342_power_of_four/main.js
+++ leetcode/algorithms/342_power_of_four/main.js
@@ -1,4 +1,11 @@
 /**
+ * Built-in function
+ *
+ * Complexities:
+ *   - Time Complexity: O(1)
+ *   - Space Complexity: O(1)
+ */
+/**
  * @param {number} n
  * @return {boolean}
  */
@@ -6,9 +13,68 @@ var isPowerOfFour = function (n) {
   if ((Math.log10(n) / Math.log10(4)) % 1 === 0) {
     return true;
   }
+
   return false;
 };
 
 
 // Solution
-var solution = (n) => n > 0 && Math.log2(n) % 2 === 0;
+/**
+ * Solution 1
+ *
+ * Repeated Division
+ *
+ * Complexities:
+ *   N - `n`
+ *   - Time Complexity: O(log₄ᴺ)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var solution1 = function (n) {
+  if (n < 1) {
+    return false;
+  }
+
+  while (n % 4 === 0) {
+    n /= 4;
+  }
+
+  return n === 1;
+};
+
+/**
+ * Solution 2
+ *
+ * Bit Trick
+ *
+ * Complexities:
+ *   - Time Complexity: O(1)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var solution2 = function (n) {
+  return n > 0 && (n & (n - 1)) === 0 && (n & 0x55555555) !== 0;
+};
+
+/**
+ * Solution 3
+ *
+ * Remaining 3
+ *
+ * Complexities:
+ *   - Time Complexity: O(1)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var solution3 = function (n) {
+  return n > 0 && (n & (n - 1)) === 0 && (n - 1) % 3 === 0;
+};
