070de26781cc78909f7afab399307697ec2b7b73
miga-choi <miga.choi.1004@gmail.com>
2026-08-23 05:39:20 +0900

    leetcode: "342_power_of_four" add dart solution

--- leetcode/algorithms/342_power_of_four/main.dart
+++ leetcode/algorithms/342_power_of_four/main.dart
@@ -1,26 +1,67 @@
 class PowerOfFour {
+  /**
+   * Repeated Division
+   *
+   * Complexities:
+   *   N - `n`
+   *   - Time Complexity: O(log₄ᴺ)
+   *   - Space Complexity: O(1)
+   */
   bool isPowerOfFour(int n) {
     while (n > 1) {
       if (n % 4 > 0) {
         return false;
       }
+
       n = (n / 4).floor();
     }
+
     return n == 1;
   }
 
 
   // Solution
-  bool solution(int n) {
+  /**
+   * Solution 1
+   *
+   * Repeated Division
+   *
+   * Complexities:
+   *   N - `n`
+   *   - Time Complexity: O(log₄ᴺ)
+   *   - Space Complexity: O(1)
+   */
+  bool solution1(int n) {
     if (n <= 0) {
       return false;
     }
-    while (1 < n) {
-      if (n % 4 != 0) {
-        return false;
-      }
+
+    while (n % 4 == 0) {
       n ~/= 4;
     }
-    return true;
+
+    return n == 1;
   }
+
+  /**
+   * Solution 2
+   *
+   * Bit Trick
+   *
+   * Complexities:
+   *   - Time Complexity: O(1)
+   *   - Space Complexity: O(1)
+   */
+  bool solution2(int n) => n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0;
+
+  /**
+   * Solution 3
+   *
+   * Remaining 3
+   *
+   * Complexities:
+   *   - Time Complexity: O(1)
+   *   - Space Complexity: O(1)
+   */
+  bool solution3(int n) => n > 0 && (n & (n - 1)) == 0 && (n - 1) % 3 == 0;
 }
