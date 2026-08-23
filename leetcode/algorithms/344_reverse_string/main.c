5fc2d5c5ec95e74d3e365bb0439c4436aee0780f
miga-choi <miga.choi.1004@gmail.com>
2026-08-24 01:06:22 +0900

    leetcode: "344_reverse_string" add c solution

--- leetcode/algorithms/344_reverse_string/main.c
+++ leetcode/algorithms/344_reverse_string/main.c
@@ -1,3 +1,11 @@
+/**
+ * Two pointers
+ * 
+ * Complexities:
+ *   N - `sSize`
+ *   - Time Complexities: O(N)
+ *   - Space Complexities: O(1)
+ */
 void reverseString(char *s, int sSize) {
     for (int i = 0; i < sSize / 2; i++) {
         char temp = s[i];
@@ -8,15 +16,45 @@ void reverseString(char *s, int sSize) {
 
 
 // Solution
+/**
+ * Solution 1
+ * 
+ * Two pointers
+ * 
+ * Complexities:
+ *   - Time Complexities: O(N)
+ *   - Space Complexities: O(1)
+ */
 void solution(char *s, int sSize) {
-    int i = 0;
-    int j = sSize - 1;
-
-    while (i <= j) {
-        char temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
-        i++;
-        j--;
+    for (int left = 0, right = sSize - 1; left < right; left++, right--) {
+        char tmp = s[left];
+        s[left] = s[right];
+        s[right] = tmp;
     }
+}
+
+/**
+ * Solution 2
+ * 
+ * Recursion
+ * 
+ * Complexities:
+ *   N - `sSize`
+ *   - Time Complexities: O(N)
+ *   - Space Complexities: O(N)
+ */
+static void helper(char* s, int left, int right) {
+    if (left >= right) {
+        return;
+    }
+
+    char tmp = s[left];
+    s[left] = s[right];
+    s[right] = tmp;
+
+    helper(s, left + 1, right - 1);
+}
+
+void solution2(char* s, int sSize) {
+    helper(s, 0, sSize - 1);
 }
