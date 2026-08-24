15edfb77e44e210ae59f35259ead5aeb5b91c039
miga-choi <miga.choi.1004@gmail.com>
2026-08-25 01:10:35 +0900

    leetcode: "344_reverse_string" add javascript solution

--- leetcode/algorithms/344_reverse_string/main.js
+++ leetcode/algorithms/344_reverse_string/main.js
@@ -1,4 +1,12 @@
 /**
+ * Two pointers
+ *
+ * Complexities:
+ *   N - The size of `s`
+ *   - Time Complexities: O(N)
+ *   - Space Complexities: O(1)
+ */
+/**
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
@@ -12,49 +20,91 @@ var reverseString = function (s) {
 
 
 // Solution
-// Solution 1: Implementation by mirror image:
+/**
+ * Solution 1
+ *
+ * Two pointers
+ *
+ * Complexities:
+ *   N - The size of `s`
+ *   - Time Complexities: O(N)
+ *   - Space Complexities: O(1)
+ */
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
 var solution1 = function (s) {
-  let size = s.length;
+  let left = 0;
+  let right = s.length - 1;
 
-  // reverse string by mirror image
-  for (let i = 0; i < Math.floor(size / 2); i++) {
-    [s[i], s[size - 1 - i]] = [s[size - 1 - i], s[i]];
-  }
-
-  return;
-};
-
-// Solution 2: Implementation by two pointers:
-var solution2 = function (s) {
-  // one points to head position, the other points to tail position
-  let [left, right] = [0, s.length - 1];
-
-  // reverse string by two pointers
   while (left < right) {
     [s[left], s[right]] = [s[right], s[left]];
-
-    left += 1;
-    right -= 1;
+    left++;
+    right--;
   }
-
-  return;
 };
 
-// Solution 3: Implementation by recursion:
+/**
+ * Solution 2
+ *
+ * Two pointers
+ *
+ * Complexities:
+ *   N - The size of `s`
+ *   - Time Complexities: O(N)
+ *   - Space Complexities: O(1)
+ */
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var solution1 = function (s) {
+  for (let left = 0, right = s.length - 1; left < right; left++, right--) {
+    const temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+  }
+};
+
+/**
+ * Solution 3
+ *
+ * Built-in function
+ *
+ * Complexities:
+ *   N - The size of `s`
+ *   - Time Complexities: O(N)
+ *   - Space Complexities: O(1)
+ */
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
 var solution3 = function (s) {
-  function helper(left, right) {
-    // base case aka stop condition
-    if (left >= right) {
-      return;
-    }
+  s.reverse();
+};
 
-    // general cases
-    [s[left], s[right]] = [s[right], s[left]];
-
-    helper(left + 1, right - 1);
+/**
+ * Solution 4
+ *
+ * Recursion
+ *
+ * Complexities:
+ *   N - The size of `s`
+ *   - Time Complexities: O(N)
+ *   - Space Complexities: O(N)
+ */
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var solution4 = function (s) {
+  if (left >= right) {
     return;
   }
 
-  helper((left = 0), (right = s.length - 1));
-  return;
+  [s[left], s[right]] = [s[right], s[left]];
+
+  solution4(s, left + 1, right - 1);
 };
