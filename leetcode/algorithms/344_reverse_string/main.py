8ac186404620fa332664a33b039488a26d5cbeed
miga-choi <miga.choi.1004@gmail.com>
2026-08-26 01:33:52 +0900

    leetcode: "344_reverse_string" add python solution

--- leetcode/algorithms/344_reverse_string/main.py
+++ leetcode/algorithms/344_reverse_string/main.py
@@ -1,18 +1,55 @@
-import math
+from math import floor
 from typing import List
 
 
 class ReverseString:
+    """
+    # Two pointers
+    #
+    # Complexities:
+    #   N - The size of `s`
+    #   - Time Complexities: O(N)
+    #   - Space Complexities: O(1)
+    """
     def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
         """
-        for i in range(math.floor(len(s) / 2)):
+        for i in range(floor(len(s) / 2)):
             temp = s[i]
             s[i] = s[len(s) - 1 - i]
             s[len(s) - 1 - i] = temp
 
 
     # Solution
-    def solution(self, s: List[str]) -> None:
-        s[:] = s[::-1]
+    """
+    # Solution 1
+    #
+    # Two pointers
+    #
+    # Complexities:
+    #   N - The size of `s`
+    #   - Time Complexities: O(N)
+    #   - Space Complexities: O(1)
+    """
+    def solution1(self, s: List[str]) -> None:
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+    """
+    # Solution 2
+    #
+    # Built-in function
+    #
+    # Complexities:
+    #   N - The size of `s`
+    #   - Time Complexities: O(N)
+    #   - Space Complexities: O(1)
+    """
+    def solution2(self, s: List[str]) -> None:
+        s.reverse()
+    
