feb1f8f7f53b16f465f8bdd54e2c9b30d127d24c
miga-choi <miga.choi.1004@gmail.com>
2026-08-27 01:15:51 +0900

    leetcode: "345_reverse_vowels_of_a_string" add c++ solution

--- leetcode/algorithms/345_reverse_vowels_of_a_string/main.cpp
+++ leetcode/algorithms/345_reverse_vowels_of_a_string/main.cpp
@@ -1,8 +1,17 @@
+#include <array>
 #include <string>
 using namespace std;
 
 class ReverseVowelsOfAString {
 public:
+    /**
+     * Two Pointers
+     *
+     * Complexities:
+     *   N - The size of `s`
+     *   - Time Complexities: O(N)
+     *   - Space Complexities: O(1)
+     */
     string reverseVowels(string s) {
         int i = 0;
         int j = s.size();
@@ -33,34 +42,117 @@ public:
 
 
     // Solution
-    string solution(string s) {
-        // Convert the input string to a character array.
-        string word = s;
-        int start = 0;
-        int end = s.length() - 1;
-        string vowels = "aeiouAEIOU";
+    /**
+     * Solution 1
+     *
+     * Two Pointers
+     *
+     * Complexities:
+     *   N - The size of `s`
+     *   - Time Complexities: O(N)
+     *   - Space Complexities: O(1)
+     */
+    static bool isVowel(char c) {
+        switch (c) {
+            case 'a': case 'e': case 'i': case 'o': case 'u':
+            case 'A': case 'E': case 'I': case 'O': case 'U':
+                return true;
+            default:
+                return false;
+        }
+    }
 
-        // Loop until the start pointer is no longer less than the end pointer.
-        while (start < end) {
-            // Move the start pointer towards the end until it points to a vowel.
-            while (start < end && vowels.find(word[start]) == string::npos) {
-                start++;
+    string solution1(string s) {
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+            while (left < right && !isVowel(s[left])) {
+                ++left;
+            }
+            while (left < right && !isVowel(s[right])) {
+                --right;
             }
 
-            // Move the end pointer towards the start until it points to a vowel.
-            while (start < end && vowels.find(word[end]) == string::npos) {
-                end--;
+            if (left < right) {
+                swap(s[left], s[right]);
+                ++left;
+                --right;
             }
-
-            // Swap the vowels found at the start and end positions.
-            swap(word[start], word[end]);
-
-            // Move the pointers towards each other for the next iteration.
-            start++;
-            end--;
         }
 
-        // Return the modified string.
-        return word;
+        return s;
+    }
+
+    /**
+     * Solution 2
+     *
+     * Two Pointers
+     *
+     * Complexities:
+     *   N - The size of `s`
+     *   - Time Complexities: O(N)
+     *   - Space Complexities: O(1)
+     */
+    static constexpr auto VOWEL = [] {
+        array<bool, 256> t{};
+        for (char c : {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'})
+            t[static_cast<unsigned char>(c)] = true;
+        return t;
+    }();
+
+    string solution2(string s) {
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+            while (left < right && !VOWEL[static_cast<unsigned char>(s[left])]) {
+                ++left;
+            }
+            while (left < right && !VOWEL[static_cast<unsigned char>(s[right])]) {
+                --right;
+            }
+
+            if (left < right) {
+                swap(s[left], s[right]);
+                ++left;
+                --right;
+            }
+        }
+
+        return s;
+    }
+
+    /**
+     * Solution 3
+     *
+     * Two Pointers
+     *
+     * Complexities:
+     *   N - The size of `s`
+     *   - Time Complexities: O(N)
+     *   - Space Complexities: O(1)
+     */
+    static bool isVowel2(char c) {
+        return string_view("aeiouAEIOU").find(c) != string_view::npos;
+    }
+
+    string solution3(string s) {
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+            while (left < right && !isVowel2(s[left])) {
+                ++left;
+            }
+            while (left < right && !isVowel2(s[right])) {
+                --right;
+            }
+
+            if (left < right) {
+                swap(s[left], s[right]);
+                ++left;
+                --right;
+            }
+        }
+
+        return s;
     }
 };
