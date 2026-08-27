d4eb9dbc064ca0a46c9ec882ff3f3e79db13d305
miga-choi <miga.choi.1004@gmail.com>
2026-08-28 01:38:24 +0900

    leetcode: "345_reverse_vowels_of_a_string" add javascript solution

--- leetcode/algorithms/345_reverse_vowels_of_a_string/main.js
+++ leetcode/algorithms/345_reverse_vowels_of_a_string/main.js
@@ -1,4 +1,12 @@
 /**
+ * Two Pointers
+ *
+ * Complexities:
+ *   N - The size of `s`
+ *   - Time Complexities: O(N)
+ *   - Space Complexities: O(N)
+ */
+/**
  * @param {string} s
  * @return {string}
  */
@@ -26,32 +34,108 @@ var reverseVowels = function (s) {
 
 
 // Solution
-// Solution 1
+/**
+ * Solution 1
+ *
+ * Two Pointers
+ *
+ * Complexities:
+ *   N - The size of `s`
+ *   - Time Complexities: O(N)
+ *   - Space Complexities: O(N)
+ */
+/**
+ * @param {string} s
+ * @return {string}
+ */
 var solution1 = function (s) {
-  const VOWELS = { a: 1, e: 1, i: 1, o: 1, u: 1, A: 1, E: 1, I: 1, O: 1, U: 1 };
+  const vowels = new Set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]);
   const arr = s.split("");
-  let i = 0, j = arr.length - 1;
+  let left = 0;
+  let right = arr.length - 1;
 
-  while (i < j) {
-    if (VOWELS[arr[i]] && VOWELS[arr[j]]) {
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-      i++;
-      j--;
-    } else if (VOWELS[arr[i]]) {
-      j--;
-    } else {
-      i++;
+  while (left < right) {
+    while (left < right && !vowels.has(arr[left])) {
+      left++;
+    }
+    while (left < right && !vowels.has(arr[right])) {
+      right--;
+    }
+
+    if (left < right) {
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+      left++;
+      right--;
     }
   }
 
   return arr.join("");
 };
 
+/**
+ * Solution 2
+ *
+ * Two Pointers
+ *
+ * Complexities:
+ *   N - The size of `s`
+ *   - Time Complexities: O(N)
+ *   - Space Complexities: O(N)
+ */
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const isVowel = (c) => "aeiou".includes(c.toLowerCase());
 
-// Solution 2
 var solution2 = function (s) {
-  const vowels = s.split("").filter((a) => /[aeiou]/i.test(a));
-  return s
-    .split(/[aeiou]/i)
-    .reduce((res, a) => res + a + (vowels.pop() || ""), "");
+  const vowels = new Set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]);
+  const arr = s.split("");
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    while (left < right && !vowels.has(arr[left])) {
+      left++;
+    }
+    while (left < right && !vowels.has(arr[right])) {
+      right--;
+    }
+
+    if (left < right) {
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+      left++;
+      right--;
+    }
+  }
+
+  return arr.join("");
+};
+
+/**
+ * Solution 3
+ *
+ * Two Pointers
+ *
+ * Complexities:
+ *   N - The size of `s`
+ *   - Time Complexities: O(N)
+ *   - Space Complexities: O(N)
+ */
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var solution3 = function (s) {
+  const vowels = new Set("aeiouAEIOU");
+  const found = [];
+
+  for (const c of s) {
+    if (vowels.has(c)) {
+      found.push(c);
+    }
+  }
+
+  let idx = found.length - 1;
+  return s.replace(/[aeiouAEIOU]/g, () => found[idx--]);
 };
