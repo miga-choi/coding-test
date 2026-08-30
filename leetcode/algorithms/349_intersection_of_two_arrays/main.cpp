1b7b7c07a5219ed982df3e7da503d9ae2bcce04d
miga-choi <miga.choi.1004@gmail.com>
2026-08-30 20:06:28 +0900

    leetcode: "349_intersection_of_two_arrays" add c++ solution

--- leetcode/algorithms/349_intersection_of_two_arrays/main.cpp
+++ leetcode/algorithms/349_intersection_of_two_arrays/main.cpp
@@ -1,11 +1,24 @@
-#include <map>
-#include <unordered_set>
-#include <vector>
+#include <algorithm>     // std::set_intersection, std::sort, std::unique
+#include <array>         // std::array
+#include <iterator>      // std::back_inserter
+#include <map>           // std::map
+#include <unordered_set> // std::unordered_set
+#include <vector>        // std::vector
 using namespace std;
 
 class IntersectionOfTwoArrays {
 public:
-    vector<int> intersection(vector<int> &nums1, vector<int> &nums2) {
+    /**
+     * std::map
+     *
+     * Complexities:
+     *   K - The size of `map`
+     *   N - The size of `nums1`
+     *   M - The size of `nums2`
+     *   - Time Complexity: O(N + M)
+     *   - Space Complexity: O(K)
+     */
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         vector<int> result;
         map<int, bool> numsMap;
 
@@ -21,17 +34,79 @@ public:
         return result;
     }
 
-    // Solution
-    vector<int> solution(vector<int> &nums1, vector<int> &nums2) {
-        unordered_set<int> m(nums1.begin(), nums1.end());
-        vector<int> res;
 
-        for (auto a : nums2) {
-            if (m.count(a)) {
-                res.push_back(a);
-                m.erase(a);
+    // Solution
+    /**
+     * Solution 1
+     *
+     * Lookup table
+     *
+     * Complexities:
+     *   N - The size of `nums1`
+     *   M - The size of `nums2`
+     *   - Time Complexity: O(N + M)
+     *   - Space Complexity: O(1)
+     */
+    vector<int> solution1(vector<int>& nums1, vector<int>& nums2) {
+        array<bool, 1001> seen{};
+
+        for (int v : nums1) seen[v] = true;
+
+        vector<int> res;
+        for (int v : nums2) {
+            if (seen[v]) {
+                res.push_back(v);
+                seen[v] = false;
             }
         }
+
+        return res;
+    }
+
+    /**
+     * Solution 2
+     *
+     * std::unordered_set
+     *
+     * Complexities:
+     *   N - The size of `nums1`
+     *   M - The size of `nums2`
+     *   K - The size of `unordered_set`
+     *   - Time Complexity: O(N + M)
+     *   - Space Complexity: O(K)
+     */
+    vector<int> solution2(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> s(nums1.begin(), nums1.end());
+
+        vector<int> res;
+        for (int v : nums2) {
+            if (s.erase(v)){
+                res.push_back(v);
+            }
+        }
+
+        return res;
+    }
+
+    /**
+     * Solution 3
+     *
+     * std::unordered_set
+     *
+     * Complexities:
+     *   N - The size of `unordered_set`
+     *   M - The size of `nums2`
+     *   - Time Complexity: O(N * logᴺ + M * logᴹ)
+     *   - Space Complexity: O(1)
+     */
+    vector<int> solution3(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        vector<int> res;
+        set_intersection(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), back_inserter(res));
+
+        res.erase(unique(res.begin(), res.end()), res.end());
 
         return res;
     }
