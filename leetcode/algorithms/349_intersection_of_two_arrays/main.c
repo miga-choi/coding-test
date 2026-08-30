62ef836e996c0a50708ee3c878f021c86825772f
miga-choi <miga.choi.1004@gmail.com>
2026-08-30 20:05:45 +0900

    leetcode: "349_intersection_of_two_arrays" add c solution

--- leetcode/algorithms/349_intersection_of_two_arrays/main.c
+++ leetcode/algorithms/349_intersection_of_two_arrays/main.c
@@ -1,7 +1,18 @@
-#include <stdlib.h>
+#include <stdbool.h> // bool
+#include <stdlib.h>  // qsort
+#include <string.h>  // memset
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+/**
+ * Complexities:
+ *   N - `nums1Size`
+ *   M - `nums2Size`
+ *   K - 1001 * 4 bytes
+ *   - Time Complexity: O(N + M)
+ *   - Space Complexity: O(K)
  */
 int* intersection(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
     int count = 0;
@@ -22,20 +33,80 @@ int* intersection(int* nums1, int nums1Size, int* nums2, int nums2Size, int* ret
     return result;
 }
 
+
 // Solution
-int* solution(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
-    int* map = malloc(sizeof(int) * 1000);
-    int* res = malloc(sizeof(int) * 1000);
-    int cnt = 0;
+/**
+ * Solution 1
+ * 
+ * Lookup table
+ * 
+ * Complexities:
+ *   N - `nums1Size`
+ *   M - `nums2Size`
+ *   - Time Complexity: O(N + M)
+ *   - Space Complexity: O(1)
+ */
+#define MAXV 1001
+
+int* solution1(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+    bool seen[MAXV];
+    memset(seen, 0, sizeof(seen));
 
     for (int i = 0; i < nums1Size; i++) {
-        map[nums1[i]] = 1;
+        seen[nums1[i]] = true;
     }
 
+    int cap = nums1Size < nums2Size ? nums1Size : nums2Size;
+    int* res = malloc(cap * sizeof(int));
+    int cnt = 0;
+
     for (int i = 0; i < nums2Size; i++) {
-        if (map[nums2[i]] == 1) {
-            res[cnt++] = nums2[i];
-            map[nums2[i]] = 0;
+        int v = nums2[i];
+        if (seen[v]) {
+            res[cnt++] = v;
+            seen[v] = false;
+        }
+    }
+
+    *returnSize = cnt;
+    return res;
+}
+
+/**
+ * Solution 2
+ * 
+ * Sort + Two pointers
+ * 
+ * Complexities:
+ *   N - `nums1Size`
+ *   M - `nums2Size`
+ *   - Time Complexity: O(N * logᴺ + M * logᴹ)
+ *   - Space Complexity: O(1)
+ */
+static int cmp(const void* a, const void* b) {
+    int x = *(const int*)a, y = *(const int*)b;
+    return (x > y) - (x < y);
+}
+
+int* solution2(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+    qsort(nums1, nums1Size, sizeof(int), cmp);
+    qsort(nums2, nums2Size, sizeof(int), cmp);
+
+    int cap = nums1Size < nums2Size ? nums1Size : nums2Size;
+    int* res = malloc(cap * sizeof(int));
+    int i = 0, j = 0, cnt = 0;
+
+    while (i < nums1Size && j < nums2Size) {
+        if (nums1[i] < nums2[j]) {
+            i++;
+        } else if (nums1[i] > nums2[j]) {
+            j++;
+        } else {
+            if (cnt == 0 || res[cnt - 1] != nums1[i]) {
+                res[cnt++] = nums1[i];
+            }
+            i++;
+            j++;
         }
     }
 
