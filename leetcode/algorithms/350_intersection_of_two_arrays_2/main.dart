9cb053e1f8c631298be72b1d2e670c775226d8a8
miga-choi <miga.choi.1004@gmail.com>
2026-09-04 01:24:59 +0900

    leetcode: "350_intersection_of_two_arrays_2" add dart solution

--- leetcode/algorithms/350_intersection_of_two_arrays_2/main.dart
+++ leetcode/algorithms/350_intersection_of_two_arrays_2/main.dart
@@ -1,6 +1,14 @@
 class IntersectionOfTwoArraysII {
+  /**
+   * Complexities:
+   *   N - The size of `nums1`
+   *   M - The size of `nums2`
+   *   - Time Complexity: O(N * M)
+   *   - Space Complexity: O(N + M)
+   */
   List<int> intersect(List<int> nums1, List<int> nums2) {
     List<int> result = List<int>.empty(growable: true);
+
     for (int i = 0; i < nums1.length; i++) {
       for (int j = 0; j < nums2.length; j++) {
         if (nums1[i] != -1 && nums1[i] == nums2[j]) {
@@ -10,27 +18,37 @@ class IntersectionOfTwoArraysII {
         }
       }
     }
+
     return result;
   }
 
 
   // Solution
+  /**
+   * Map
+   *
+   * Complexities:
+   *   N - The size of `nums1`
+   *   M - The size of `nums2`
+   *   - Time Complexity: O(N + M)
+   *   - Space Complexity: O(min(N, M))
+   */
   List<int> solution(List<int> nums1, List<int> nums2) {
-    nums1.sort();
-    nums2.sort();
+    if (nums1.length > nums2.length) {
+      return intersect(nums2, nums1);
+    }
 
-    List<int> res = [];
-    int i = 0, j = 0;
+    final counts = <int, int>{};
+    for (final x in nums1) {
+      counts.update(x, (v) => v + 1, ifAbsent: () => 1);
+    }
 
-    while (i < nums1.length && j < nums2.length) {
-      if (nums1[i] == nums2[j]) {
-        res.add(nums1[i]);
-        i++;
-        j++;
-      } else if (nums1[i] > nums2[j])
-        j++;
-      else {
-        i++;
+    final res = <int>[];
+    for (final x in nums2) {
+      final c = counts[x];
+      if (c != null && c > 0) {
+        res.add(x);
+        counts[x] = c - 1;
       }
     }
 
