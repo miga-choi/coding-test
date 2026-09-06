1c4f47bf6d38d013c4554c1e61cb23a4ab57d71d
miga-choi <miga.choi.1004@gmail.com>
2026-09-07 01:07:49 +0900

    leetcode: "367_valid_perfect_square" add dart solution

--- leetcode/algorithms/367_valid_perfect_square/main.dart
+++ leetcode/algorithms/367_valid_perfect_square/main.dart
@@ -1,4 +1,10 @@
 class ValidPerfectSquare {
+  /**
+   * Complexities:
+   *   N - `num`
+   *   - Time Complexity: O(√N)
+   *   - Space Complexity: O(1)
+   */
   bool isPerfectSquare(int num) {
     if (num > 1) {
       for (int i = 0; i < num; i++) {
@@ -9,17 +15,80 @@ class ValidPerfectSquare {
         }
       }
     }
+
     return true;
   }
 
 
   // Solution
-  bool solution(int num) {
-    for (int i = 1; i <= num; i++) {
-      if (i * i == num.abs()) {
+  /**
+   * Solution 1
+   *
+   * Binary Search
+   *
+   * Complexities:
+   *   N - `num`
+   *   - Time Complexity: O(logᴺ)
+   *   - Space Complexity: O(1)
+   */
+  bool solution1(int num) {
+    var left = 1;
+    var right = num;
+
+    while (left <= right) {
+      final mid = left + (right - left) ~/ 2;
+      final square = mid * mid;
+
+      if (square == num) {
         return true;
+      } else if (square < num) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
       }
     }
+
     return false;
+  }
+
+  /**
+   * Solution 2
+   *
+   * Sum of Odd Numbers
+   *
+   * Complexities:
+   *   N - `num`
+   *   - Time Complexity: O(√N)
+   *   - Space Complexity: O(1)
+   */
+  bool solution2(int num) {
+    var odd = 1;
+
+    while (num > 0) {
+      num -= odd;
+      odd += 2;
+    }
+
+    return num == 0;
+  }
+
+  /**
+   * Solution 3
+   *
+   * Newton-Raphson Method
+   *
+   * Complexities:
+   *   N - `num`
+   *   - Time Complexity: O(logᴺ)
+   *   - Space Complexity: O(1)
+   */
+  bool solution3(int num) {
+    var x = num;
+
+    while (x * x > num) {
+      x = (x + num ~/ x) ~/ 2;
+    }
+
+    return x * x == num;
   }
 }
