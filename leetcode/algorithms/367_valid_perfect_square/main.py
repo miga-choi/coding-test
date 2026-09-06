57c9179fd3f1c4cdd2675edeb3fdaa05cd35e094
miga-choi <miga.choi.1004@gmail.com>
2026-09-07 01:07:23 +0900

    leetcode: "367_valid_perfect_square" add python solution

--- leetcode/algorithms/367_valid_perfect_square/main.py
+++ leetcode/algorithms/367_valid_perfect_square/main.py
@@ -1,6 +1,16 @@
+import math
+
+
 class ValidPerfectSquare:
+    """
+    # Complexities:
+    #   N - `num`
+    #   - Time Complexity: O(√N)
+    #   - Space Complexity: O(1)
+    """
     def isPerfectSquare(self, num: int) -> bool:
         i = 0
+
         while True:
             if i**2 == num:
                 return True
@@ -11,5 +21,83 @@ class ValidPerfectSquare:
 
 
     # Solution
-    def solution(self, num: int) -> bool:
-        return int(num**0.5) == num**0.5
+    """
+    # Solution 1
+    #
+    # Binary Search
+    #
+    # Complexities:
+    #   N - `num`
+    #   - Time Complexity: O(logᴺ)
+    #   - Space Complexity: O(1)
+    """
+    def solution1(self, num: int) -> bool:
+        if num < 2:
+            return True
+
+        left, right = 2, num // 2
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            square = mid * mid
+
+            if square == num:
+                return True
+            elif square < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return False
+
+    """
+    # Solution 2
+    #
+    # Sum of Odd Numbers
+    #
+    # Complexities:
+    #   N - `num`
+    #   - Time Complexity: O(√N)
+    #   - Space Complexity: O(1)
+    """
+    def solution2(self, num: int) -> bool:
+        odd = 1
+
+        while num > 0:
+            num -= odd
+            odd += 2
+
+        return num == 0
+
+    """
+    # Solution 3
+    #
+    # Newton-Raphson Method
+    #
+    # Complexities:
+    #   N - `num`
+    #   - Time Complexity: O(logᴺ)
+    #   - Space Complexity: O(1)
+    """
+    def solution3(self, num: int) -> bool:
+        x = num
+
+        while x * x > num:
+            x = (x + num // x) // 2
+
+        return x * x == num
+
+    """
+    # Solution 4
+    #
+    # math.isqrt
+    #
+    # Complexities:
+    #   N - `num`
+    #   - Time Complexity: O(logᴺ)
+    #   - Space Complexity: O(1)
+    """
+    def solution4(self, num: int) -> bool:
+        r = math.isqrt(num)
+
+        return r * r == num
