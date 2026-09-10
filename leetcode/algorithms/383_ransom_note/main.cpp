80ee02b5949d5157fcc3270cf644c84abef4dc12
miga-choi <miga.choi.1004@gmail.com>
2026-09-11 01:03:14 +0900

    leetcode: "383_ransom_note" add c++ solution

--- leetcode/algorithms/383_ransom_note/main.cpp
+++ leetcode/algorithms/383_ransom_note/main.cpp
@@ -1,5 +1,7 @@
-#include <string>
-#include <unordered_map>
+#include <algorithm>     // std::none_of
+#include <array>         // std::array
+#include <string>        // std::string
+#include <unordered_map> // std::unordered_map
 using namespace std;
 
 class RansomNote {
@@ -24,50 +26,93 @@ public:
 
 
     // Solution
-    // Solution 1: Map
+    /**
+     * Solution 1
+     *
+     * Counting Array
+     *
+     * Complexities:
+     *   N - The size of `ransomNote`
+     *   M - The size of `magazine`
+     *   - Time Complexity: O(N + M)
+     *   - Space Complexity: O(1)
+     */
     bool solution1(string ransomNote, string magazine) {
-        unordered_map<char, int> dictionary;
-
-        // Iterate through the magazine and count characters
-        for (char c : magazine) {
-            if (dictionary.find(c) == dictionary.end()) {
-                dictionary[c] = 1;
-            } else {
-                dictionary[c]++;
-            }
+        if (ransomNote.size() > magazine.size()) {
+            return false;
         }
 
-        // Iterate through the ransom note and check character counts
+        array<int, 26> cnt{};
+
+        for (char c : magazine) {
+            cnt[c - 'a']++;
+        }
+
         for (char c : ransomNote) {
-            if (dictionary.find(c) != dictionary.end() && dictionary[c] > 0) {
-                dictionary[c]--;
-            } else {
-                return false;
-            }
+            if (--cnt[c - 'a'] < 0) return false;
         }
 
         return true;
     }
 
-    // Solution 2
+    /**
+     * Solution 2
+     *
+     * unordered_map
+     *
+     * Complexities:
+     *   N - The size of `ransomNote`
+     *   M - The size of `magazine`
+     *   - Time Complexity: O(N + M)
+     *   - Space Complexity: O(1)
+     */
     bool solution2(string ransomNote, string magazine) {
-        // Initialize an array of count with the size 26...
-        int counter[26] = {0};
-
-        // Traverse a loop through the entire String of magazine where char ch stores the char at the index of magazine...
-        for (char ch : magazine) {
-            counter[ch - 'a']++;
+        if (ransomNote.size() > magazine.size()) {
+            return false;
         }
 
-        // Run another for loop for ransomNote...
-        for (char ch : ransomNote) {
-            // If the charachter doesn't exists in magazine for ransomNote, we return false...
-            if (counter[ch - 'a']-- <= 0) {
+        unordered_map<char, int> cnt;
+
+        for (char c : magazine) {
+            cnt[c]++;
+        }
+
+        for (char c : ransomNote) {
+            auto it = cnt.find(c);
+
+            if (it == cnt.end() || it->second == 0) {
                 return false;
             }
+
+            --it->second;
         }
 
-        // If nothing goes wrong, return true...
         return true;
+    }
+
+    /**
+     * Solution 3
+     *
+     * Complexities:
+     *   N - The size of `ransomNote`
+     *   M - The size of `magazine`
+     *   - Time Complexity: O(N + M)
+     *   - Space Complexity: O(1)
+     */
+    bool solution3(string ransomNote, string magazine) {
+        if (ransomNote.size() > magazine.size()) {
+            return false;
+        }
+
+        array<int, 26> cnt{};
+
+        for (char c : magazine) {
+            cnt[c - 'a']++;
+        }
+        for (char c : ransomNote) {
+            cnt[c - 'a']--;
+        }
+
+        return none_of(cnt.begin(), cnt.end(), [](int x) { return x < 0; });
     }
 };
