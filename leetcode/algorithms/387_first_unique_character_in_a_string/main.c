7c1fd035663de54c77958fe2210f6e9575111a7f
miga-choi <miga.choi.1004@gmail.com>
2026-09-14 06:47:48 +0900

    leetcode: "387_first_unique_character_in_a_string" add c solution

--- leetcode/algorithms/387_first_unique_character_in_a_string/main.c
+++ leetcode/algorithms/387_first_unique_character_in_a_string/main.c
@@ -1,9 +1,12 @@
 #include <string.h>
 
 /**
- * Frequency Counter
- * - Time Complexity: O(N)
- * - Space Complexity: O(1)
+ * Two-Pass
+ * 
+ * Complexities:
+ *   N - The Size of `s`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
  */
 int firstUniqChar(char* s) {
     int alphabetNumArray[26] = {0};
@@ -26,23 +29,63 @@ int firstUniqChar(char* s) {
 /**
  * Solution 1
  * 
- * Frequency Counter
- * - Time Complexity: O(N)
- * - Space Complexity: O(1)
+ * Two-Pass
+ * 
+ * Complexities:
+ *   N - The Size of `s`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
  */
-int solution(char* s) {
-    int counts[26] = {0};
-    int len = strlen(s);
+int solution1(char* s) {
+    int cnt[26] = {0};
 
-    for (int i = 0; i < len; i++) {
-        counts[s[i] - 'a']++;
+    for (int i = 0; s[i]; i++) {
+        cnt[s[i] - 'a']++;
     }
 
-    for (int i = 0; i < len; i++) {
-        if (counts[s[i] - 'a'] == 1) {
+    for (int i = 0; s[i]; i++) {
+        if (cnt[s[i] - 'a'] == 1) {
             return i;
         }
     }
 
     return -1;
+}
+
+/**
+ * Solution 2
+ * 
+ * Indexing + 1.5-Pass
+ * 
+ * Complexities:
+ *   N - The Size of `s`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+int solution2(char* s) {
+    int first[26];
+    int cnt[26] = {0};
+
+    for (int i = 0; i < 26; i++) {
+        first[i] = -1;
+    }
+
+    for (int i = 0; s[i]; i++) {
+        int c = s[i] - 'a';
+
+        if (cnt[c] == 0) {
+            first[c] = i;
+        }
+
+        cnt[c]++;
+    }
+
+    int ans = -1;
+    for (int c = 0; c < 26; c++) {
+        if (cnt[c] == 1 && (ans == -1 || first[c] < ans)) {
+            ans = first[c];
+        }
+    }
+
+    return ans;
 }
