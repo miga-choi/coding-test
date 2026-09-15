43044e93fcd14b6aa6944b76cb1589aae6cdcc60
miga-choi <miga.choi.1004@gmail.com>
2026-09-16 01:07:35 +0900

    leetcode: "387_first_unique_character_in_a_string" add dart solution

--- leetcode/algorithms/387_first_unique_character_in_a_string/main.dart
+++ leetcode/algorithms/387_first_unique_character_in_a_string/main.dart
@@ -1,8 +1,11 @@
 class FirstUniqueCharacterInAString {
   /**
-   * Frequency Counter
-   * - Time Complexity: O(N)
-   * - Space Complexity: O(1)
+   * Counting Array + Two-Pass
+   *
+   * Complexities:
+   *   N - The Size of `s`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
    */
   int firstUniqChar(String s) {
     final List<int> alphabetNumArray = List<int>.filled(26, 0);
@@ -26,22 +29,24 @@ class FirstUniqueCharacterInAString {
   /**
    * Solution 1
    * 
-   * Frequency Counter
-   * - Time Complexity: O(N)
-   * - Space Complexity: O(1)
+   * Counting Array + Two-Pass
+   *
+   * Complexities:
+   *   N - The Size of `s`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
    */
   int solution1(String s) {
-    List<int> counts = List.filled(26, 0);
-    final int aCode = 'a'.codeUnitAt(0);
+    final a = 'a'.codeUnitAt(0);
+    final counts = List<int>.filled(26, 0);
+    final codes = s.codeUnits;
 
-    for (int i = 0; i < s.length; i++) {
-      int index = s.codeUnitAt(i) - aCode;
-      counts[index]++;
+    for (final c in codes) {
+      counts[c - a]++;
     }
 
-    for (int i = 0; i < s.length; i++) {
-      int index = s.codeUnitAt(i) - aCode;
-      if (counts[index] == 1) {
+    for (var i = 0; i < codes.length; i++) {
+      if (counts[codes[i] - a] == 1) {
         return i;
       }
     }
@@ -51,48 +56,59 @@ class FirstUniqueCharacterInAString {
 
   /**
    * Solution 2
-   * 
-   * Hash table
-   * - Time Complexity: O(N)
-   * - Space Complexity: O(1)
+   *
+   * Counting Array + Indexing + 1.5-Pass
+   *
+   * Complexities:
+   *   N - The Size of `s`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
    */
   int solution2(String s) {
-    Map<String, int> counts = {};
+    final a = 'a'.codeUnitAt(0);
+    final first = List<int>.filled(26, -1);
+    final codes = s.codeUnits;
 
-    for (int i = 0; i < s.length; i++) {
-      String char = s[i];
-      counts[char] = (counts[char] ?? 0) + 1;
-    }
+    for (var i = 0; i < codes.length; i++) {
+      final k = codes[i] - a;
 
-    for (int i = 0; i < s.length; i++) {
-      String char = s[i];
-      if (counts[char] == 1) {
-        return i;
+      if (first[k] == -1) {
+        first[k] = i;
+      } else {
+        first[k] = -2;
       }
     }
 
-    return -1;
+    var ans = -1;
+
+    for (final idx in first) {
+      if (idx >= 0 && (ans == -1 || idx < ans)) {
+        ans = idx;
+      }
+    }
+
+    return ans;
   }
 
   /**
    * Solution 3
-   * 
-   * Brute-Force
-   * - Time Complexity: O(N²)
-   * - Space Complexity: O(1)
+   *
+   * Map
+   *
+   * Complexities:
+   *   N - The Size of `s`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
    */
   int solution3(String s) {
-    for (int i = 0; i < s.length; i++) {
-      bool isUnique = true;
+    final counts = <int, int>{};
 
-      for (int j = 0; j < s.length; j++) {
-        if (i != j && s[i] == s[j]) {
-          isUnique = false;
-          break;
-        }
-      }
+    for (final c in s.codeUnits) {
+      counts.update(c, (v) => v + 1, ifAbsent: () => 1);
+    }
 
-      if (isUnique) {
+    for (var i = 0; i < s.length; i++) {
+      if (counts[s.codeUnitAt(i)] == 1) {
         return i;
       }
     }
