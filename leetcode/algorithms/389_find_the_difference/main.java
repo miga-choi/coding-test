9c62b1173fc5c31577dfbe578ffed9165c1c09db
miga-choi <miga.choi.1004@gmail.com>
2026-09-18 01:06:17 +0900

    leetcode: "389_find_the_difference" add java solution

--- leetcode/algorithms/389_find_the_difference/main.java
+++ leetcode/algorithms/389_find_the_difference/main.java
@@ -1,24 +1,86 @@
 class FindTheDifference {
+    /**
+     * replaceFirst()
+     *
+     * Complexities:
+     *   N - The Size of `s`
+     *   - Time Complexity: O(N²)
+     *   - Space Complexity: O(N)
+     */
     public char findTheDifference(String s, String t) {
         for (int i = 0; i < s.length(); i++) {
             t = t.replaceFirst(String.valueOf(s.charAt(i)), "");
         }
+
         return t.charAt(0);
     }
 
 
     // Solution
-    public char solution(String s, String t) {
-        char c = 0;
+    /**
+     * Solution 1
+     *
+     * Counting Array
+     *
+     * Complexities:
+     *   N - The Size of `s`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
+    public char solution1(String s, String t) {
+        int[] count = new int[26];
 
-        for (char cs : s.toCharArray()) {
-            c ^= cs;
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
         }
 
-        for (char ct : t.toCharArray()) {
-            c ^= ct;
+        for (int i = 0; i < t.length(); i++) {
+            if (--count[t.charAt(i) - 'a'] < 0) {
+                return t.charAt(i);
+            }
         }
 
-        return c;
+        return ' ';
+    }
+
+    /**
+     * Solution 2
+     *
+     * XOR
+     *
+     * Complexities:
+     *   N - The Size of `s`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
+    public char solution2(String s, String t) {
+        char result = t.charAt(t.length() - 1);
+
+        for (int i = 0; i < s.length(); i++) {
+            result ^= s.charAt(i) ^ t.charAt(i);
+        }
+
+        return result;
+    }
+
+    /**
+     * Solution 3
+     *
+     * Complexities:
+     *   N - The Size of `s`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
+    public char solution3(String s, String t) {
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            sum -= s.charAt(i);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            sum += t.charAt(i);
+        }
+
+        return (char) sum;
     }
 }
