49b3aaa5bbbf642980222f8250e86ce03ce56f99
miga-choi <miga.choi.1004@gmail.com>
2026-09-20 03:33:55 +0900

    leetcode: "392_is_subsequence" add c solution

--- leetcode/algorithms/392_is_subsequence/main.c
+++ leetcode/algorithms/392_is_subsequence/main.c
@@ -1,6 +1,12 @@
 #include <stdbool.h>
-#include <string.h>
 
+/**
+ * Complexities:
+ *   N - The Size of `s`
+ *   M - The Size of `t`
+ *   - Time Complexity: O(M)
+ *   - Space Complexity: O(1)
+ */
 bool isSubsequence(char* s, char* t) {
     int i = 0;
 
@@ -15,59 +21,46 @@ bool isSubsequence(char* s, char* t) {
 
 
 // Solution
-// Solution 1
+/**
+ * Solution 1
+ * 
+ * Complexities:
+ *   N - The Size of `s`
+ *   M - The Size of `t`
+ *   - Time Complexity: O(M)
+ *   - Space Complexity: O(1)
+ */
 bool solution1(char* s, char* t) {
-    while (*t) {
-        s += *s == *t++;
-    }
-    return !*s;
-}
+    int i = 0;
 
-// Solution 2
-bool solution2(char* s, char* t) {
-    int n = strlen(s);
-    int m = strlen(t);
-
-    // For index of s (or subsequence)
-    int j = 0;
-
-    // Traverse s and t,
-    // and compare current character of s with first unmatched char of t,
-    // if matched then move ahead in s
-    for (int i = 0; i < m && j < n; i++) {
-        if (s[j] == t[i]) {
-            j++;
+    for (int j = 0; t[j]; j++) {
+        if (s[i] == t[j]) {
+            i++;
         }
     }
 
-    // If all characters of s were found in t
-    return (j == n);
+    return s[i] == '\0';
 }
 
-// Solution 3
-int isSubSequence(char* s1, char* s2, int i, int j) {
-    if (i == 0 || j == 0) {
-        return 0;
+/**
+ * Solution 2
+ * 
+ * Complexities:
+ *   N - The Size of `s`
+ *   M - The Size of `t`
+ *   - Time Complexity: O(M)
+ *   - Space Complexity: O(1)
+ */
+bool solution2(char* s, char* t) {
+    int i = 0, j = 0;
+
+    while (s[i] && t[j]) {
+        if (s[i] == t[j]) {
+            i++;
+        }
+
+        j++;
     }
 
-    if (s1[i - 1] == s2[j - 1]) {
-        return 1 + isSubSequence(s1, s2, i - 1, j - 1);
-    } else {
-        return isSubSequence(s1, s2, i, j - 1);
-    }
-}
-
-bool solution3(char* s, char* t) {
-    int m = strlen(s);
-    int n = strlen(t);
-
-    if (m > n) {
-        return false;
-    }
-
-    if (isSubSequence(s, t, m, n) == m) {
-        return true;
-    } else {
-        return false;
-    }
+    return s[i] == '\0';
 }
