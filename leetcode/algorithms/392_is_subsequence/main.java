ae0bf3ac8e35c0283ae671a662fc63c7bbb534cf
miga-choi <miga.choi.1004@gmail.com>
2026-09-21 01:02:57 +0900

    leetcode: "392_is_subsequence" add java solution

--- leetcode/algorithms/392_is_subsequence/main.java
+++ leetcode/algorithms/392_is_subsequence/main.java
@@ -1,40 +1,50 @@
 class IsSubsequence {
+    /**
+     * Two Pointers
+     *
+     * Complexities:
+     *   N - The Size of `s`
+     *   M - The Size of `t`
+     *   - Time Complexity: O(M)
+     *   - Space Complexity: O(1)
+     */
     public boolean isSubsequence(String s, String t) {
         while (s.length() > 0) {
             int index = t.indexOf(s.charAt(0));
+
             if (index == -1) {
                 return false;
             }
+
             t = t.substring(index + 1);
             s = s.substring(1, s.length());
         }
+
         return true;
     }
 
 
     // Solution
+    /**
+     * Two Pointers
+     *
+     * Complexities:
+     *   N - The Size of `s`
+     *   M - The Size of `t`
+     *   - Time Complexity: O(M)
+     *   - Space Complexity: O(1)
+     */
     public boolean solution(String s, String t) {
         int i = 0, j = 0;
-        int n = t.length();
-        int m = s.length();
-        char ss[] = s.toCharArray();
-        char tt[] = t.toCharArray();
 
-        if (m < 1) {
-            return true;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+
+            j++;
         }
 
-        while (i < n) {
-            if (tt[i] == ss[j]) {
-                j++;
-            }
-            i++;
-
-            if (j == m) {
-                return true;
-            }
-        }
-
-        return false;
+        return i == s.length();
     }
 }
