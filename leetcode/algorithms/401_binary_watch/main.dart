2e8cf5bcba07f1df94e11bc905984a5557daa50d
miga-choi <miga.choi.1004@gmail.com>
2026-09-24 22:28:23 +0900

    leetcode: "401_binary_watch" add dart solution

--- leetcode/algorithms/401_binary_watch/main.dart
+++ leetcode/algorithms/401_binary_watch/main.dart
@@ -1,4 +1,9 @@
 class BinaryWatch {
+  /**
+   * Complexities:
+   *   - Time Complexity: O(1)
+   *   - Space Complexity: O(1)
+   */
   List<String> readBinaryWatch(int turnedOn) {
     List<String> result = List<String>.empty(growable: true);
 
@@ -20,22 +25,96 @@ class BinaryWatch {
     return result;
   }
 
-  // Solution
-  List<String> solution(int turnedOn) {
-    List<String> res = [];
 
-    for (int hour = 0; hour < 12; hour++) {
-      for (int min = 0; min < 60; min++) {
-        String temp = hour.toRadixString(2) + min.toRadixString(2);
-        int count = 0;
-        for (int i = 0; i < temp.length; i++) {
-          if (temp[i] == '1') count++;
-        }
-        if (count == turnedOn) {
-          res.add('$hour:${min.toString().padLeft(2, '0')}');
+  // Solution
+  /**
+   * Solution 1
+   * 
+   * Complexities:
+   *   - Time Complexity: O(1)
+   *   - Space Complexity: O(1)
+   */
+  int _popCount(int x) {
+    var count = 0;
+
+    while (x != 0) {
+      x &= x - 1;
+      count++;
+    }
+
+    return count;
+  }
+
+  List<String> solution1(int turnedOn) {
+    final res = <String>[];
+
+    for (var h = 0; h < 12; h++) {
+      for (var m = 0; m < 60; m++) {
+        if (_popCount(h) + _popCount(m) == turnedOn) {
+          res.add('$h:${m.toString().padLeft(2, '0')}');
         }
       }
     }
+
+    return res;
+  }
+
+  /**
+   * Solution 2
+   * 
+   * 10-Bitmask
+   * 
+   * Complexities:
+   *   - Time Complexity: O(1)
+   *   - Space Complexity: O(1)
+   */
+  List<String> solution2(int turnedOn) {
+    final res = <String>[];
+
+    for (var mask = 0; mask < 1024; mask++) {
+      final h = mask >> 6;
+      final m = mask & 0x3F;
+
+      if (h < 12 && m < 60 && _popCount(mask) == turnedOn) {
+        res.add('$h:${m.toString().padLeft(2, '0')}');
+      }
+    }
+
+    return res;
+  }
+
+  /**
+   * Solution 3
+   * 
+   * Backtracking
+   * 
+   * Complexities:
+   *   - Time Complexity: O(1)
+   *   - Space Complexity: O(1)
+   */
+  List<String> solution3(int turnedOn) {
+    final res = <String>[];
+
+    void dfs(int start, int remaining, int h, int m) {
+      if (h > 11 || m > 59) {
+        return;
+      }
+
+      if (remaining == 0) {
+        res.add('$h:${m.toString().padLeft(2, '0')}');
+        return;
+      }
+
+      for (var i = start; i < 10; i++) {
+        if (i < 4) {
+          dfs(i + 1, remaining - 1, h | (1 << i), m);
+        } else {
+          dfs(i + 1, remaining - 1, h, m | (1 << (i - 4)));
+        }
+      }
+    }
+
+    dfs(0, turnedOn, 0, 0);
 
     return res;
   }
