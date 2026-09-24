77ee5280a5f6f052c7dbfe5696b1a718a03faa7f
miga-choi <miga.choi.1004@gmail.com>
2026-09-25 02:52:17 +0900

    leetcode: "404_sum_of_left_leaves" add c solution

--- leetcode/algorithms/404_sum_of_left_leaves/main.c
+++ leetcode/algorithms/404_sum_of_left_leaves/main.c
@@ -1,10 +1,20 @@
+#include <stddef.h> // NULL
+
 struct TreeNode {
     int val;
     struct TreeNode* left;
     struct TreeNode* right;
 };
 
-// My Solution 1
+/**
+ * Recursion
+ * 
+ * Complexities:
+ *   N - The Numbder of Nodes in `node`
+ *   H - The Height of `node`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(H)
+ */
 int sumOfLeftLeaves1(struct TreeNode* root) {
     int sum = 0;
 
@@ -14,13 +24,22 @@ int sumOfLeftLeaves1(struct TreeNode* root) {
                 sum += root->left->val;
             }
         }
+
         sum += sumOfLeftLeaves1(root->left) + sumOfLeftLeaves1(root->right);
     }
 
     return sum;
 }
 
-// My Solution 2: with flag
+/**
+ * Recursion with Flag
+ * 
+ * Complexities:
+ *   N - The Numbder of Nodes in `node`
+ *   H - The Height of `node`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(H)
+ */
 int addLeftVal(struct TreeNode* root, int isLeft) {
     int sum = 0;
 
@@ -37,4 +56,100 @@ int addLeftVal(struct TreeNode* root, int isLeft) {
 
 int sumOfLeftLeaves2(struct TreeNode* root) {
     return addLeftVal(root->left, 1) + addLeftVal(root->right, 0);
+}
+
+
+// Solution
+/**
+ * Solution 1
+ * 
+ * Recursion
+ * 
+ * Complexities:
+ *   N - The Numbder of Nodes in `node`
+ *   H - The Height of `node`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(H)
+ */
+int solution1(struct TreeNode* root) {
+    if (root == NULL) {
+        return 0;
+    }
+
+    int sum = 0;
+    struct TreeNode* L = root->left;
+
+    if (L != NULL && L->left == NULL && L->right == NULL) {
+        sum += L->val;
+    } else {
+        sum += solution1(L);
+    }
+
+    return sum + solution1(root->right);
+}
+
+/**
+ * Solution 2
+ * 
+ * Recursion with Flag
+ * 
+ * Complexities:
+ *   N - The Numbder of Nodes in `node`
+ *   H - The Height of `node`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(H)
+ */
+static int dfs(struct TreeNode* node, bool isLeft) {
+    if (node == NULL) {
+        return 0;
+    }
+
+    if (node->left == NULL && node->right == NULL) {
+        return isLeft ? node->val : 0;
+    }
+
+    return dfs(node->left, true) + dfs(node->right, false);
+}
+
+int solution2(struct TreeNode* root) {
+    return dfs(root, false);
+}
+
+/**
+ * Solution 3
+ * 
+ * Iteration with Stack
+ * 
+ * Complexities:
+ *   N - The Numbder of Nodes in `node`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+int solution3(struct TreeNode* root) {
+    if (root == NULL) {
+        return 0;
+    }
+
+    struct TreeNode* stack[1000];
+    int top = 0, sum = 0;
+    stack[top++] = root;
+
+    while (top > 0) {
+        struct TreeNode* node = stack[--top];
+        struct TreeNode* L = node->left;
+
+        if (L != NULL) {
+            if (L->left == NULL && L->right == NULL) {
+                sum += L->val;
+            } else {
+                stack[top++] = L;
+            }
+        }
+
+        if (node->right != NULL) {
+            stack[top++] = node->right;
+        }
+    }
+
+    return sum;
 }
